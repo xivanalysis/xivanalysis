5c2d46c06cc6a378c2b2897703ce24b7fea31d3f
Saxon Landers <saxon@ackwell.com.au>
2018-04-12 23:53:31 +1000

    Initial routing impl

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import {
+	HashRouter as Router,
+	Route
+} from 'react-router-dom'
 
-import DbLink from './ui/DbLink'
+import Home from './routes/Home'
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.<br/>
+			<Router>
+				<div>
+					{/* TODO: nav bar */}
 
-					<DbLink obj={{type: 'item', id: 16054, name: 'Name override'}}/>
-				</p>
-			</div>
+					<Route exact path="/" component={Home}/>
+				</div>
+			</Router>
 		)
 	}
 }
---
+++ src/routes/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+	render() {
+		return (
+			<span>Home page</span>
+		)
+	}
+}
+
+export default Home
