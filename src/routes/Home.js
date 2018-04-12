0908482c8eb8e4030d084323fccf9a75d7f605ef
Saxon Landers <saxon@ackwell.com.au>
2018-04-13 00:06:47 +1000

    Added terpander to the home because that's important

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 
+import DbLink from '../ui/DbLink'
+
 class Home extends Component {
 	render() {
 		return (
-			<span>Home page</span>
+			<div>
+				Home page
+				<DbLink obj={{type: 'item', id: 16054, name: 'Terpander Lux'}}/>
+			</div>
 		)
 	}
 }
