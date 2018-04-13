c5f653dd618543b60a4730a3484f4cd77b0f2c22
Saxon Landers <saxon@ackwell.com.au>
2018-04-13 22:24:23 +1000

    Filled out url generation a bit for the search

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
 		'indent': ['error', 'tab'],
 		'linebreak-style': ['error', 'unix'],
 		'quotes': ['error', 'single'],
-		'semi': ['error', 'never']
+		'semi': ['error', 'never'],
+		'no-console': 'off'
 	},
 	overrides: [{
 		files: ['**/*.test.js'],
--- src/ui/ReportSearch.js
+++ src/ui/ReportSearch.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react'
 
 class ReportSearch extends Component {
+	static getCode(input) {
+		const match = input.match(/^(?:.*reports\/)?([a-zA-Z0-9]{16})\/?(?:#.*)?$/)
+		return match && match[1]
+	}
+
+	static getFight(input) {
+		const match = input.match(/fight=([^&]*)/)
+		return match && match[1]
+	}
+
+	static getPlayer(input) {
+		const match = input.match(/source=([^&]*)/)
+		return match && match[1]
+	}
+
+	constructor(props) {
+		super(props)
+		this.inputField = React.createRef()
+	}
+
+	// Using arrow function to maintain bind
+	parseReportUrl = () => {
+		const input = this.inputField.current.value.trim()
+
+		const code = this.constructor.getCode(input)
+		const fight = this.constructor.getFight(input)
+		const player = this.constructor.getPlayer(input)
+
+		let url = '/'
+		if (code) {
+			url += `${code}/`
+
+			if (fight) {
+				url += `${fight}/`
+
+				if (player) {
+					url += `${player}/`
+				}
+			}
+
+			url = ((fight && player)? 'analyse' : 'find') + url
+		}
+
+		console.log(url)
+	}
+
 	render() {
 		return (
 			<div className="input-group">
-				<input type="text" className="form-control" placeholder="https://www.fflogs.com/reports/..."/>
+				<input
+					type="text"
+					className="form-control"
+					placeholder="https://www.fflogs.com/reports/..."
+					ref={this.inputField}
+					onChange={this.parseReportUrl}
+				/>
 				<div className="input-group-append">
-					<button type="button" className="btn btn-outline-primary">Analyse</button>
+					<button type="button" className="btn btn-outline-primary" onClick={this.parseReportUrl}>
+						Analyse
+					</button>
 				</div>
 			</div>
 		)
