8f919f4a1a127d126fb27c2fed45395dfdad8b81
Saxon Landers <saxon@ackwell.com.au>
2018-04-22 00:08:58 +1000

    Really, really basic resource handling

--- src/parser/core/modules/Combatant.js
+++ src/parser/core/modules/Combatant.js
@@ -1,7 +1,20 @@
 import Module from '@/parser/core/Module'
 
 export default class Combatant extends Module {
-	// on_damage(event) {
-	// 	console.log('DAMAGE AAAA', event)
-	// }
+	// TODO: Should probably abstract generic handling out of this so can use for enemies too
+
+	// TODO: tempted to store a history and use it for comparisons
+	resources = {}
+
+	on_damage_byPlayer(event) {
+		// Some byPlayer damage events don't have resources 'cus they're ticks or w/e
+		if (!event.sourceResources) { return }
+		this.resources = event.sourceResources
+	}
+	on_damage_toPlayer(event) {
+		this.resources = event.targetResources
+	}
+	on_heal_toPlayer(event) {
+		this.resources = event.targetResources
+	}
 }
--- src/parser/smn/modules/Ruin2.js
+++ src/parser/smn/modules/Ruin2.js
@@ -3,6 +3,10 @@ import Module from '@/parser/core/Module'
 import ACTIONS from '@/data/ACTIONS'
 
 export default class Ruin2 extends Module {
+	static dependencies = [
+		'combatant'
+	]
+
 	// TODO: If (when) I set up the timeline, should probably mark bad R2s on it
 
 	all = []
@@ -11,6 +15,7 @@ export default class Ruin2 extends Module {
 
 	lastGcd = null
 	ogcdUsed = false
+	pos = {}
 
 	// Limiting to player, not worried about pets for this check
 	on_cast_byPlayer(event) {
@@ -19,20 +24,26 @@ export default class Ruin2 extends Module {
 		const lastGcdAction = (this.lastGcd && ACTIONS[this.lastGcd.ability.guid]) || {}
 
 		// TODO: GCD metadata should be in a module
+		// TODO: I'm not checking for brohamut at the moment - no-movement, no oGCD r2
+		//       during akh morning is a-ok 'cus WWs
 		// If there was no oGCD cast between the R2 and now, mark an issue
 		if (
 			action.onGcd &&
 			lastGcdAction.id === ACTIONS.RUIN_II.id &&
 			!this.ogcdUsed
 		) {
-			// TODO: Distinguish between movement and no movement
-			//       pos will be in combatant module i guess
-			this.warnings.push(event)
+			// If they at least moved, only raise a warning
+			if (this.movedSinceLastGcd()) {
+				this.warnings.push(event)
+			} else {
+				this.issues.push(event)
+			}
 		}
 
 		if (action.onGcd) {
 			// If this cast is on the gcd, store it for comparison
 			this.lastGcd = event
+			this.pos = this.combatant.resources
 		} else {
 			// Otherwise take note that they've used an oGCD
 			this.ogcdUsed = true
@@ -49,11 +60,11 @@ export default class Ruin2 extends Module {
 		console.log(this)
 	}
 
-	// // TODO: Should this be in some other module?
-	// movedSinceLastGcd(event) {
-	// 	return (
-	// 		Math.abs(event.sourceResources.x - this.lastGcdEvent.sourceResources.x) > 1 &&
-	// 		Math.abs(event.sourceResources.y - this.lastGcdEvent.sourceResources.y) > 1
-	// 	)
-	// }
+	// TODO: Should this be in some other module?
+	movedSinceLastGcd() {
+		return (
+			Math.abs(this.combatant.resources.x - this.pos.x) > 1 &&
+			Math.abs(this.combatant.resources.y - this.pos.y) > 1
+		)
+	}
 }
