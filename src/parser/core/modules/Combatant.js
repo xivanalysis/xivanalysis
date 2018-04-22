10a076c0e1a5b49bab3522e6fb61aa93e5ed6763
Saxon Landers <saxon@ackwell.com.au>
2018-04-22 20:35:54 +1000

    Shoddy buff tracking

--- src/parser/core/modules/Combatant.js
+++ src/parser/core/modules/Combatant.js
@@ -3,8 +3,17 @@ import Module from '@/parser/core/Module'
 export default class Combatant extends Module {
 	// TODO: Should probably abstract generic handling out of this so can use for enemies too
 
+	// -----
+	// Properties
+	// -----
+
 	// TODO: tempted to store a history and use it for comparisons
 	resources = {}
+	statuses = {}
+
+	// -----
+	// Events
+	// -----
 
 	on_damage_byPlayer(event) {
 		// Some byPlayer damage events don't have resources 'cus they're ticks or w/e
@@ -16,5 +25,21 @@ export default class Combatant extends Module {
 	}
 	on_heal_toPlayer(event) {
 		this.resources = event.targetResources
+	}
+
+	on_applybuff_toPlayer(event) {
+		// TODO: Should I store more than this?
+		//       ...maybe stack info from applybuffstack/removebuffstack?
+		this.statuses[event.ability.guid] = true
+	}
+	on_removebuff_toPlayer(event) {
+		delete this.statuses[event.ability.guid]
+	}
+	on_applydebuff_toPlayer(event) {
+		// TODO: Combine these a bit, it's just duping shit up
+		this.statuses[event.ability.guid] = true
+	}
+	on_removedebuff_toPlayer(event) {
+		delete this.statuses[event.ability.guid]
 	}
 }
