595fead77b9c3011f3d7cc89009471f1305eb516
Saxon Landers <saxon@ackwell.com.au>
2018-04-25 01:39:14 +1000

    Fugly bahamut output

--- src/parser/smn/modules/Bahamut.js
+++ src/parser/smn/modules/Bahamut.js
@@ -1,3 +1,5 @@
+import React, { Fragment } from 'react'
+
 import Module from '@/parser/core/Module'
 
 import ACTIONS from '@/data/ACTIONS'
@@ -8,8 +10,12 @@ export default class Bahamut extends Module {
 	]
 
 	counting = false
-	wyrmwaveCount = 0
-	akhMornCount = 0
+	wwCount = 0
+	amCount = 0
+
+	results = []
+
+	// TODO: Handle final push bahamut - push to just toss it in for those akh morns and shit
 
 	// TODO: Limit to pet only?
 	on_damage(event) {
@@ -18,11 +24,11 @@ export default class Bahamut extends Module {
 		// Track casts - these only happen during the summon window so w/e
 		// TODO: Probably should have _some_ handling for shit outside the window 'cus that's jank
 		if (abilityId === ACTIONS.WYRMWAVE.id) {
-			this.wyrmwaveCount ++
+			this.wwCount ++
 		}
 
 		if (abilityId === ACTIONS.AKH_MORN.id) {
-			this.akhMornCount ++
+			this.amCount ++
 		}
 
 		// Set this up so we know that something's started
@@ -33,10 +39,21 @@ export default class Bahamut extends Module {
 		// Aaaand pull it apart again once we're done
 		if (this.counting && !this.gauge.bahamutSummoned()) {
 			this.counting = false
-			console.log('ww', this.wyrmwaveCount)
-			console.log('am', this.akhMornCount)
-			this.wyrmwaveCount = 0
-			this.akhMornCount = 0
+			this.results.push({ww: this.wwCount, am: this.amCount})
+			this.wwCount = 0
+			this.amCount = 0
 		}
+	}
+
+	output() {
+		// This is fugly
+		return <Fragment>
+			<ul>
+				{this.results.map((result, index) => <li key={index}>
+					WW: {result.ww}<br/>
+					AM: {result.am}
+				</li>)}
+			</ul>
+		</Fragment>
 	}
 }
