4563dc681cb44b0cc4ca637a5e72fdc5fce736e6
Saxon Landers <saxon@ackwell.com.au>
2018-05-01 23:49:22 +1000

    Bit more work on checklist handling

--- src/parser/core/modules/Checklist.js
+++ src/parser/core/modules/Checklist.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { Accordion, Icon, Progress } from 'semantic-ui-react'
+import { Accordion, Icon, Message, Progress } from 'semantic-ui-react'
 
 import Module, { DISPLAY_ORDER } from 'parser/core/Module'
 import styles from './Checklist.module.css'
@@ -7,30 +7,38 @@ import styles from './Checklist.module.css'
 export default class Checklist extends Module {
 	static displayOrder = DISPLAY_ORDER.FIRST
 
+	rules = []
+
 	output() {
-		const panels = [{
+		// If there's no rules, output a warning.
+		if (!this.rules.length) {
+			return <Message error>
+				The <code>Checklist</code> module has been included in this parser, but no rules have been defined for it. Make sure you&apos;re using your own subclass, and defining the <code>rules</code> class property.
+			</Message>
+		}
+
+		const panels = this.rules.map((rule, index) => ({
 			title: {
-				key: 'title-1',
-				// Children so I override their dropdown icon
+				key: `title-${index}`,
 				className: styles.title,
-				children: <Fragment>
-					<Icon name="checkmark"/>
-					<Icon name="remove"/>
-					Title
-					<Progress
-						percent="54"
-						// Using className instead of the prop for indicating to avoid the active animation
-						className={`indicating ${styles.progress}`}
-						size="small"
-					/>
+				content: <Fragment>
+					{/* Not sure 75 is a good aiming point. Maybe higher? */}
+					<Icon name={rule.percent > 75? 'checkmark' : 'remove'}/>
+					{rule.name}
+					{/* Using className for indicting to avoid the active animation */}
+					<Progress percent={rule.percent} className={`indicating ${styles.progress}`} size="small"/>
 				</Fragment>
 			},
 			content: {
-				key: 'content-1',
-				content: 'content'
+				key: `content-${index}`,
+				active: true,
+				content: <Fragment>
+					<Icon name="info"/>
+					{rule.description}
+				</Fragment>
 			}
-		}]
+		}))
 
-		return <Accordion exclusive={false} panels={panels} styled/>
+		return <Accordion exclusive={false} panels={panels} styled fluid/>
 	}
 }
--- src/parser/smn/modules/Checklist.js
+++ src/parser/smn/modules/Checklist.js
@@ -1,5 +1,16 @@
+import React, { Fragment } from 'react'
+
+import { ActionLink } from 'components/ui/DbLink'
+import ACTIONS from 'data/ACTIONS'
 import CoreChecklist from 'parser/core/modules/Checklist'
 
 export default class Checklist extends CoreChecklist {
-
+	// This probably should use proper Rule components and stuff.
+	rules = [{
+		name: 'Keep your DoTs up',
+		description: <Fragment>
+			As a Summoner, DoTs are significant portion of your sustained damage, and are required for optimal damage from <ActionLink {...ACTIONS.FESTER}/>, your primary stack spender. Aim to keep them up at all times.
+		</Fragment>,
+		percent: 90
+	}]
 }
