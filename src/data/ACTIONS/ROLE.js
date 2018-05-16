2ddf49bb584fcb78806d75137f06955fa1df1d08
Saxon Landers <saxon@ackwell.com.au>
2018-05-16 23:07:36 +1000

    Added CD info for role actions

--- src/data/ACTIONS/ROLE.js
+++ src/data/ACTIONS/ROLE.js
@@ -3,7 +3,8 @@ export default {
 	DIVERSION: {
 		id: 7545,
 		name: 'Diversion',
-		icon: 'https://secure.xivdb.com/img/game/000000/000827.png'
+		icon: 'https://secure.xivdb.com/img/game/000000/000827.png',
+		cooldown: 120
 	},
 
 	BREAK: {
@@ -15,31 +16,36 @@ export default {
 	SURECAST: {
 		id: 7559,
 		name: 'Surecast',
-		icon: 'https://secure.xivdb.com/img/game/000000/000869.png'
+		icon: 'https://secure.xivdb.com/img/game/000000/000869.png',
+		cooldown: 30
 	},
 
 	ADDLE: {
 		id: 7560,
 		name: 'Addle',
-		icon: 'https://secure.xivdb.com/img/game/000000/000861.png'
+		icon: 'https://secure.xivdb.com/img/game/000000/000861.png',
+		cooldown: 90
 	},
 
 	SWIFTCAST: {
 		id: 7561,
 		name: 'Swiftcast',
-		icon: 'https://secure.xivdb.com/img/game/000000/000866.png'
+		icon: 'https://secure.xivdb.com/img/game/000000/000866.png',
+		cooldown: 60
 	},
 
 	LUCID_DREAMING: {
 		id: 7562,
 		name: 'Lucid Dreaming',
-		icon: 'https://secure.xivdb.com/img/game/000000/000865.png'
+		icon: 'https://secure.xivdb.com/img/game/000000/000865.png',
+		cooldown: 120
 	},
 
 	APOCATASTASIS: {
 		id: 7563,
 		name: 'Apocatastasis',
-		icon: 'https://secure.xivdb.com/img/game/000000/000868.png'
+		icon: 'https://secure.xivdb.com/img/game/000000/000868.png',
+		cooldown: 90
 	},
 
 	DRAIN: {
@@ -51,12 +57,14 @@ export default {
 	MANA_SHIFT: {
 		id: 7565,
 		name: 'Mana Shift',
-		icon: 'https://secure.xivdb.com/img/game/000000/000867.png'
+		icon: 'https://secure.xivdb.com/img/game/000000/000867.png',
+		cooldown: 120
 	},
 
 	ERASE: {
 		id: 7566,
 		name: 'Erase',
-		icon: 'https://secure.xivdb.com/img/game/000000/000870.png'
+		icon: 'https://secure.xivdb.com/img/game/000000/000870.png',
+		cooldown: 90
 	}
 }
