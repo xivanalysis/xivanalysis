7057363627cfe5c773b84e65a9722fde6789731c
Saxon Landers <saxon@ackwell.com.au>
2018-05-26 22:04:33 +1000

    Added GCD cast check to DWT

--- src/parser/smn/modules/DWT.js
+++ src/parser/smn/modules/DWT.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-import { getAction } from 'data/ACTIONS'
+import ACTIONS, { getAction } from 'data/ACTIONS'
 import STATUSES from 'data/STATUSES'
 import Module from 'parser/core/Module'
+import { Suggestion, SEVERITY } from 'parser/core/modules/Suggestions'
+import { ActionLink } from 'components/ui/DbLink'
+
+const CORRECT_GCDS = [
+	ACTIONS.RUIN_III.id,
+	ACTIONS.RUIN_IV.id,
+	ACTIONS.TRI_BIND.id
+]
 
 export default class DWT extends Module {
-	static displayOrder = -100 // temp
+	static dependencies = [
+		'suggestions'
+	]
 	name = 'Dreadwyrm Trance'
 
 	active = false
@@ -49,6 +59,26 @@ export default class DWT extends Module {
 		// Clean up any existing casts
 		if (this.active) {
 			this.stopAndSave()
+		}
+
+		// Run some analytics for suggestions
+		let badGcds = 0
+		this.history.forEach(dwt => {
+			badGcds += Object.keys(dwt.casts)
+				.filter(actionId => getAction(actionId).onGcd && !CORRECT_GCDS.includes(parseInt(actionId, 10)))
+				.reduce((prev, actionId) => prev + dwt.casts[actionId], 0)
+		})
+
+		// Suggestions
+		if (badGcds > 1) {
+			this.suggestions.add(new Suggestion({
+				icon: ACTIONS.DREADWYRM_TRANCE.icon,
+				why: `${badGcds} incorrect GCDs used during DWT.`,
+				severity: badGcds > 5 ? SEVERITY.MAJOR : SEVERITY.MEDIUM,
+				content: <Fragment>
+					GCDs used during Dreadwyrm Trance should be limited to <ActionLink {...ACTIONS.RUIN_III}/> and <ActionLink {...ACTIONS.RUIN_IV}/>, or <ActionLink {...ACTIONS.TRI_BIND}/> in AoE situations.
+				</Fragment>
+			}))
 		}
 	}
 
