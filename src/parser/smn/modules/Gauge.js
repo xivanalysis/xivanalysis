cbdf635c7be30f2f122809234eb16ef04cd681d3
Saxon Landers <saxon@ackwell.com.au>
2018-05-26 23:08:49 +1000

    Tiny clean up for flow checklist rule

--- src/parser/smn/modules/Gauge.js
+++ src/parser/smn/modules/Gauge.js
@@ -45,12 +45,12 @@ export default class Gauge extends Module {
 	on_complete() {
 		// Checklist rule for aetherflow cooldown
 		this.checklist.add(new Rule({
-			name: <Fragment>Keep <ActionLink {...ACTIONS.AETHERFLOW} />&apos;s cooldown rolling</Fragment>,
+			name: <Fragment>Use <ActionLink {...ACTIONS.AETHERFLOW} /> effectively</Fragment>,
 			description: 'SMN\'s entire kit revolves around the Aetherflow cooldown. Make sure you squeeze every possible use out of it that you can.',
 			requirements: [
 				new Requirement({
 					name: <Fragment><ActionLink {...ACTIONS.AETHERFLOW} /> cooldown uptime</Fragment>,
-					percent: () => (this.cooldowns.getTimeOnCooldown(ACTIONS.AETHERFLOW.id) / this.parser.fightDuration) * 100
+					percent: (this.cooldowns.getTimeOnCooldown(ACTIONS.AETHERFLOW.id) / this.parser.fightDuration) * 100
 				})
 			]
 		}))
