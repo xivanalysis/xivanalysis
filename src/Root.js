0b994033650e4b3f1364f7dfacc7b1b41e7d60f2
Saxon Landers <saxon@ackwell.com.au>
2018-05-27 21:26:03 +1000

    Client changes for BrowserRouter

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from 'components/App'
 import configureStore from 'store'
