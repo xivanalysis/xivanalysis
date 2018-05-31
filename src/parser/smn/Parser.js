c3eb1b066f8879a2d576f339502f72fcea230a70
Saxon Landers <saxon@ackwell.com.au>
2018-05-31 23:08:10 +1000

    Moved FR enkindle CDR to R4 module

--- src/parser/smn/Parser.js
+++ src/parser/smn/Parser.js
@@ -3,7 +3,6 @@ import CoreParser from 'parser/core/Parser'
 import Bahamut from './modules/Bahamut'
 import DoTs from './modules/DoTs'
 import DWT from './modules/DWT'
-import Enkindle from './modules/Enkindle'
 import Gauge from './modules/Gauge'
 import Pets from './modules/Pets'
 import Ruin2 from './modules/Ruin2'
@@ -14,7 +13,6 @@ class Parser extends CoreParser {
 		bahamut: Bahamut,
 		dots: DoTs,
 		dwt: DWT,
-		enkindle: Enkindle,
 		gauge: Gauge,
 		pets: Pets,
 		ruin2: Ruin2,
--- src/parser/smn/modules/Ruin4.js
+++ src/parser/smn/modules/Ruin4.js
@@ -1,3 +1,5 @@
+import ACTIONS from 'data/ACTIONS'
+import STATUSES from 'data/STATUSES'
 import Module from 'parser/core/Module'
 
 // there's also the case where if you have further ruin and an egi is about to do gcd + ogcd and they held, that can be considered a no no
@@ -10,7 +12,19 @@ import Module from 'parser/core/Module'
 // Even if the player used r4 early hoping to get another for the r2 weave, it was impossible because the pet's cycle couldn't allow it, the next proc window was after the r2 cast(edited)
 
 export default class Ruin4 extends Module {
+	static dependencies = [
+		'cooldowns'
+	]
+
 	on_cast_byPlayerPet(event) {
 		console.log(event.ability.name)
+	}
+
+	on_applybuff(event) {
+		// Only care about further ruin
+		if (event.ability.guid !== STATUSES.FURTHER_RUIN.id) { return }
+
+		// Further Ruin (R4 proc) also reduces the CD on Enkindle by 10 seconds
+		this.cooldowns.reduceCooldown(ACTIONS.ENKINDLE.id, 10)
 	}
 }
