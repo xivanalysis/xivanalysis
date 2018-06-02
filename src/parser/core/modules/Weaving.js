55e3586f62823275d47f6e58a06b8e500b32e8fd
Saxon Landers <saxon@ackwell.com.au>
2018-06-02 22:18:56 +1000

    Bit of an overhaul for the weaving module

--- src/parser/core/modules/Weaving.js
+++ src/parser/core/modules/Weaving.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Accordion } from 'semantic-ui-react'
 
 import { getAction } from 'data/ACTIONS'
 import Module from 'parser/core/Module'
+import { Suggestion, SEVERITY } from 'parser/core/modules/Suggestions'
 
 // TODO: This doesn't account for Ninjutsu, or BRD weaves. Work it out later.
 const MAX_WEAVES = 2
+const MAJOR_SUGGESTION_WEAVES = 4
+const MAJOR_SUGGESTION_ISSUES = 5
 
 export default class Weaving extends Module {
 	static dependencies = [
-		'invuln'
+		'invuln',
+		'suggestions'
 	]
-	static displayOrder = -100
+	name = 'Weaving Issues'
 
 	weaves = []
 	gcdEvent = null
@@ -33,7 +38,9 @@ export default class Weaving extends Module {
 
 		// Throw the current state onto the history
 		this.history.push({
-			gcdEvent: this.gcdEvent || {},
+			gcdEvent: this.gcdEvent || {
+				timestamp: this.parser.fight.start_time
+			},
 			weaves: this.weaves
 		})
 
@@ -42,22 +49,62 @@ export default class Weaving extends Module {
 		this.weaves = []
 	}
 
+	on_complete() {
+		// Few triples is medium, quad+ or lots of triples is major
+		const badWeaves = this.badWeaves
+		if (badWeaves.length) {
+			const major = badWeaves.some(
+				item => item.weaves.length >= MAJOR_SUGGESTION_WEAVES
+			) || badWeaves.length > MAJOR_SUGGESTION_ISSUES
+			this.suggestions.add(new Suggestion({
+				icon: 'https://secure.xivdb.com/img/game/001000/001785.png', // WVR Focused synth lmao
+				content: <Fragment>
+					Weaving more than {MAX_WEAVES} actions in a single GCD window will cause you to clip into your next GCD, losing uptime. Check the <em>{this.name}</em> module below for more detailed analysis.
+				</Fragment>,
+				severity: major? SEVERITY.MAJOR : SEVERITY.MEDIUM,
+				why: `${badWeaves.length} instances of >${MAX_WEAVES} oGCDs weaved.`
+			}))
+		}
+	}
+
 	isOgcd(action) {
 		return !action.onGcd
 			&& !action.autoAttack
 	}
 
+	get badWeaves() {
+		return this.history.filter(item => item.weaves.length > MAX_WEAVES)
+	}
+
 	output() {
-		const badWeaves = this.history.filter(item => item.weaves.length > MAX_WEAVES)
-		return <ul>
-			{badWeaves.map(item => <li key={item.gcdEvent.timestamp}>
-				TS: {this.parser.formatTimestamp(item.gcdEvent.timestamp)}<br/>
-				Weaves: ({item.weaves.length}) <ul>
+		const badWeaves = this.badWeaves
+		if (badWeaves.length === 0) {
+			return false
+		}
+
+		const panels = this.badWeaves.map(item => ({
+			title: {
+				key: 'title-' + item.gcdEvent.timestamp,
+				content: <Fragment>
+					<strong>{this.parser.formatTimestamp(item.gcdEvent.timestamp)}</strong>
+					&nbsp;({item.weaves.length} weaves)
+				</Fragment>
+			},
+			content: {
+				key: 'content-' + item.gcdEvent.timestamp,
+				content: <ul>
 					{item.weaves.map(weave => <li key={weave.timestamp}>
 						{weave.ability.name}
 					</li>)}
 				</ul>
-			</li>)}
-		</ul>
+			}
+		}))
+
+		return <Accordion
+			exclusive={false}
+			panels={panels}
+			styled
+			fluid
+		/>
 	}
 }
