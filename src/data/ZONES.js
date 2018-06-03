ba23b09b375e87b4f8d5b8b5870febb0f8cfd17c
Saxon Landers <saxon@ackwell.com.au>
2018-06-03 14:32:27 +1000

    Added in bunch of ZONE data

--- src/data/ZONES.js
+++ src/data/ZONES.js
@@ -3,6 +3,70 @@ import { addExtraIndex } from 'utilities'
 // TODO: Move banner lookup into an api call w/ server cache?
 
 const ZONES = {
+	// 24man
+	RABANASTRE: {
+		logId: 734,
+		dbId: 30058,
+		banner: 'https://secure.xivdb.com/img/game/112000/112256.png'
+	},
+
+	RIDORANA_LIGHTHOUSE: {
+		logId: 776,
+		dbId: 30068
+	},
+
+	// Primal EX
+	SUSANO_EX: {
+		logId: 677,
+		dbId: 20047,
+		banner: 'https://secure.xivdb.com/img/game/112000/112245.png'
+	},
+
+	LAKSHMI_EX: {
+		logId: 720,
+		dbId: 20049,
+		banner: 'https://secure.xivdb.com/img/game/112000/112246.png'
+	},
+
+	SHINRYU_EX: {
+		logId: 730,
+		dbId: 20050,
+		banner: 'https://secure.xivdb.com/img/game/112000/112258.png'
+	},
+
+	BYAKKO_EX: {
+		logId: 758,
+		dbId: 20052,
+		banner: 'https://secure.xivdb.com/img/game/112000/112274.png'
+	},
+
+	TSUKUYOMI_EX: {
+		logId: 779,
+		dbId: 20055
+	},
+
+	// Omega Savage
+	// O1-4S seem to all be zoneId 17 for w/e reason
+	O1S: {
+		dbId: 30053,
+		banner: 'https://secure.xivdb.com/img/game/112000/112238.png'
+	},
+
+	O2S: {
+		dbId: 30054,
+		banner: 'https://secure.xivdb.com/img/game/112000/112239.png'
+	},
+
+	O3S: {
+		dbId: 30055,
+		banner: 'https://secure.xivdb.com/img/game/112000/112240.png'
+	},
+
+	O4S: {
+		dbId: 30056,
+		banner: 'https://secure.xivdb.com/img/game/112000/112241.png'
+	},
+
 	O5S: {
 		logId: 752,
 		dbId: 30063,
@@ -25,6 +89,13 @@ const ZONES = {
 		logId: 755,
 		dbId: 30066,
 		banner: 'https://secure.xivdb.com/img/game/112000/112272.png'
+	},
+
+	// Ultimate
+	UCOB: {
+		logId: 733,
+		dbId: 30057,
+		banner: 'https://secure.xivdb.com/img/game/112000/112261.png'
 	}
 }
 
