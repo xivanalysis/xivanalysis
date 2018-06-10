4b0def790ed36375cff9197ac45836adade7d7ff
Saxon Landers <saxon@ackwell.com.au>
2018-06-10 15:05:22 +1000

    Marked Rouse private vars

--- src/parser/smn/modules/Rouse.js
+++ src/parser/smn/modules/Rouse.js
@@ -14,33 +14,33 @@ export default class Rouse extends Module {
 		'suggestions'
 	]
 
-	lastRouse = null
-	wasted = 0
+	_lastRouse = null
+	_wasted = 0
 
 	on_cast_byPlayer(event) {
 		if (event.ability.guid !== ACTIONS.ROUSE.id) {
 			return
 		}
-		this.lastRouse = event.timestamp
+		this._lastRouse = event.timestamp
 	}
 
 	on_summonpet(event) {
-		const diff = event.timestamp - this.lastRouse
-		if (this.lastRouse === null || diff > ROUSE_DURATION) {
+		const diff = event.timestamp - this._lastRouse
+		if (this._lastRouse === null || diff > ROUSE_DURATION) {
 			return
 		}
-		this.wasted += ROUSE_DURATION - diff
+		this._wasted += ROUSE_DURATION - diff
 	}
 
 	on_complete() {
-		if (this.wasted > 0) {
+		if (this._wasted > 0) {
 			this.suggestions.add(new Suggestion({
 				icon: ACTIONS.ROUSE.icon,
 				content: <Fragment>
 					Avoid casting <ActionLink {...ACTIONS.ROUSE}/> less than {this.parser.formatDuration(ROUSE_DURATION)} before you swap pets or summon bahamut. Rouse is lost the moment your current pet despawns.
 				</Fragment>,
-				severity: this.wasted > ROUSE_DURATION? SEVERITY.MAJOR : this.wasted > 5000? SEVERITY.MEDIUM : SEVERITY.MINOR,
-				why: `${this.parser.formatDuration(this.wasted)} of Rouse wasted.`
+				severity: this._wasted > ROUSE_DURATION? SEVERITY.MAJOR : this._wasted > 5000? SEVERITY.MEDIUM : SEVERITY.MINOR,
+				why: `${this.parser.formatDuration(this._wasted)} of Rouse wasted.`
 			}))
 		}
 	}
