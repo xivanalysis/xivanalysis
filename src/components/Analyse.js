04ac2f56a9921c813712896a564996a474eb919f
Saxon Landers <saxon@punchbuggy.com.au>
2018-06-28 16:28:19 +1000

    Fixed job modules being bundled in vendor chunk

--- src/components/Analyse.js
+++ src/components/Analyse.js
@@ -156,7 +156,7 @@ class Analyse extends Component {
 			if (!modules[group]) { continue }
 			modulePromises.push(modules[group]())
 		}
-		(await Promise.all(modulePromises)).forEach((loadedModules, index) => {
+		(await Promise.all(modulePromises)).forEach(({default: loadedModules}, index) => {
 			modules[loadOrder[index]] = loadedModules
 			parser.addModules(loadedModules)
 		})
--- src/parser/AVAILABLE_MODULES.js
+++ src/parser/AVAILABLE_MODULES.js
@@ -1,16 +1,8 @@
 import JOBS from 'data/JOBS'
 
-// Pulling this handling out to reduce repetition below
-function importModules(folder) {
-	return () => import(
-		/* webpackChunkName: "[request]" */
-		`./${folder}`
-	).then(exports => exports.default)
-}
-
 export default {
 	JOBS: {
-		[JOBS.SUMMONER.logType]: importModules('jobs/smn'),
+		[JOBS.SUMMONER.logType]: () => import('./jobs/smn' /* webpackChunkName: "jobs-smn" */),
 	},
 
 	BOSSES: {
