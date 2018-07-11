b6cebfb5c5e5af2e50d86bb23f2b21bc6f5345db
Saxon Landers <saxon@ackwell.com.au>
2018-07-12 02:58:15 +1000

    Added super basic shadowflare output

---
+++ src/parser/jobs/smn/ShadowFlare.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import ACTIONS from 'data/ACTIONS'
+import STATUSES from 'data/STATUSES'
+import Module from 'parser/core/Module'
+
+export default class ShadowFlare extends Module {
+	static handle = 'shadowFlare'
+	static title = 'Shadow Flare'
+
+	_casts = []
+
+	constructor(...args) {
+		super(...args)
+
+		this.addHook('cast', {abilityId: ACTIONS.SHADOW_FLARE.id}, this._onCast)
+		this.addHook('aoedamage', {abilityId: STATUSES.SHADOW_FLARE.id}, this._onDamage)
+	}
+
+	_onCast(event) {
+		console.log('cast')
+		this._casts.push({
+			cast: event,
+			hits: [],
+		})
+	}
+
+	_onDamage(event) {
+		console.log('da')
+		this._casts[this._casts.length - 1].hits.push(event)
+	}
+
+	output() {
+		return <ul>
+			{this._casts.map(cast => <li key={cast.cast.timestamp}>
+				<strong>{this.parser.formatTimestamp(cast.cast.timestamp)}</strong>:&nbsp;
+				{cast.hits.length} ticks,&nbsp;
+				{cast.hits.reduce((carry, value) => carry + value.hits.length, 0)} hits
+			</li>)}
+		</ul>
+	}
+}
