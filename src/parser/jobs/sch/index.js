af8187ad0d67656ab27bc03b3b192f49882e393a
KJ <xiankai@gmail.com>
2018-07-22 22:17:53 +0800

    DoTs for SCH

--- src/parser/jobs/sch/index.js
+++ src/parser/jobs/sch/index.js
@@ -1,9 +1,11 @@
 import About from './About'
 import Aetherflow from './Aetherflow'
+import DoTs from './DoTs'
 import ShadowFlare from './ShadowFlare'
 
 export default [
 	About,
 	Aetherflow,
+	DoTs,
 	ShadowFlare,
 ]
