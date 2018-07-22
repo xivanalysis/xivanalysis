47446d1747a421733a6651340800695f94889dac
Charles Shao <charles@oneflare.com>
2018-07-22 23:59:40 +1000

    Move cleric stance status out of SCH to ROLE

--- src/data/STATUSES/SCH.js
+++ src/data/STATUSES/SCH.js
@@ -1,10 +1,4 @@
 export default {
-	CLERIC_STANCE: {
-		id: 145,
-		name: 'Cleric Stance',
-		icon: 'https://secure.xivdb.com/img/game/010000/010402.png'
-	},
-
 	FEY_WIND: {
 		id: 799,
 		name: 'Fey Wind',
