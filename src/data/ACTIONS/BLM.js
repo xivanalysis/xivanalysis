6b1b8a17f8665f39331e4fafd66c72f3479fc0f6
Fürst <timo.dornhoefer@web.de>
2018-07-22 17:31:22 +0200

    Added THM and BLM

---
+++ src/data/ACTIONS/BLM.js
@@ -0,0 +1,118 @@
+export default {
+	// -----
+	// Player GCDs
+	// -----
+
+	FREEZE: {
+		id: 159,
+		name: 'Freeze',
+		icon: 'https://secure.xivdb.com/img/game/002000/002653.png',
+		onGcd: true,
+		castTime: 3.0,
+		cooldown: 2.5
+	},
+
+	BLIZZARD_III: {
+		id: 154,
+		name: 'Blizzard III',
+		icon: 'https://secure.xivdb.com/img/game/000000/000456.png',
+		onGcd: true,
+		castTime: 3.5,
+		cooldown: 2.5
+	},
+
+	THUNDER_III: {
+		id: 153,
+		name: 'Thunder III',
+		icon: 'https://secure.xivdb.com/img/game/000000/000459.png',
+		onGcd: true,
+	},
+
+	FLARE: {
+		id: 162,
+		name: 'Flare',
+		icon: 'https://secure.xivdb.com/img/game/002000/002652.png',
+		onGcd: true,
+		castTime: 4.0,
+		cooldown: 2.5
+	},
+
+	BLIZZARD_IV: {
+		id: 3576,
+		name: 'Blizzard IV',
+		icon: 'https://secure.xivdb.com/img/game/002000/002659.png',
+		onGcd: true,
+		castTime: 2.8,
+		cooldown: 2.5
+	},
+
+	FIRE_IV: {
+		id: 3577,
+		name: 'Fire IV',
+		icon: 'https://secure.xivdb.com/img/game/002000/002660.png',
+		onGcd: true,
+		castTime: 2.8,
+		cooldown: 2.5
+	},
+
+	THUNDER_IV: {
+		id: 7420,
+		name: 'Thunder IV',
+		icon: 'https://secure.xivdb.com/img/game/002000/002662.png',
+		onGcd: true,
+		castTime: 3.0,
+		cooldown: 2.5
+	},
+
+	FOUL: {
+		id: 7422,
+		name: 'Foul',
+		icon: 'https://secure.xivdb.com/img/game/002000/002664.png',
+		onGcd: true,
+	},
+	// -----
+	// Player OGCDs
+	// -----
+
+	CONVERT: {
+		id: 158,
+		name: 'Convert',
+		icon: 'https://secure.xivdb.com/img/game/002000/002651.png',
+		cooldown: 180,
+	},
+
+	LEY_LINES: {
+		id: 3573,
+		name: 'Ley Lines',
+		icon: 'https://secure.xivdb.com/img/game/002000/002656.png',
+		cooldown: 90,
+	},
+
+	SHARPCAST: {
+		id: 3574,
+		name: 'Sharpcast',
+		icon: 'https://secure.xivdb.com/img/game/002000/002657.png',
+		cooldown: 60,
+	},
+
+	ENOCHIAN: {
+		id: 3575,
+		name: 'Eno-chan',
+		icon: 'https://secure.xivdb.com/img/game/002000/002658.png',
+		cooldown: 30,
+	},
+
+	BETWEEN_THE_LINES: {
+		id: 7419,
+		name: 'Between the Lines',
+		icon: 'https://secure.xivdb.com/img/game/002000/002661.png',
+		cooldown: 3,
+	},
+
+	TRIPLECAST: {
+		id: 7421,
+		name: 'Triplecast',
+		icon: 'https://secure.xivdb.com/img/game/002000/002663.pngg',
+		cooldown: 60,
+	},
+}
---
+++ src/data/ACTIONS/THM.js
@@ -0,0 +1,101 @@
+// Split between THM and BLM
+export default {
+	// -----
+	// Player GCDs
+	// -----
+	BLIZZARD: {
+		id: 142,
+		name: 'Blizzard',
+		icon: 'https://secure.xivdb.com/img/game/000000/000454.png',
+		onGcd: true,
+	},
+
+	FIRE: {
+		id: 141,
+		name: 'Fire',
+		icon: 'https://secure.xivdb.com/img/game/000000/000451.png',
+		onGcd: true,
+	},
+
+	THUNDER: {
+		id: 144,
+		name: 'Thunder',
+		icon: 'https://secure.xivdb.com/img/game/000000/000457.png',
+		onGcd: true,
+	},
+
+	SLEEP: {
+		id: 145,
+		name: 'Sleep',
+		icon: 'https://secure.xivdb.com/img/game/000000/000465.png',
+		onGcd: true,
+	},
+
+	BLIZZARD_II: {
+		id: 146,
+		name: 'Blizzard II',
+		icon: 'https://secure.xivdb.com/img/game/000000/000455.png',
+		onGcd: true,
+		castTime: 2.0,
+		cooldown: 2.5
+	},
+
+	SCATHE: {
+		id: 156,
+		name: 'Scathe',
+		icon: 'https://secure.xivdb.com/img/game/000000/000462.png',
+		onGcd: true,
+	},
+
+	FIRE_II: {
+		id: 147,
+		name: 'Fire II',
+		icon: 'https://secure.xivdb.com/img/game/000000/000452.png',
+		onGcd: true,
+		castTime: 3.0,
+		cooldown: 2.5
+	},
+
+	THUNDER_II: {
+		id: 7447,
+		name: 'Thunder II',
+		icon: 'https://secure.xivdb.com/img/game/000000/000468.png',
+		onGcd: true,
+		castTime: 3.0,
+		cooldown: 2.5
+	},
+
+FIRE_III: {
+		id: 152,
+		name: 'Fire III',
+		icon: 'https://secure.xivdb.com/img/game/000000/000453.png',
+		onGcd: true,
+		castTime: 3.5,
+		cooldown: 2.5
+	},
+
+	// -----
+	// Player OGCDs
+	// -----
+
+	TRANSPOSE: {
+		id: 149,
+		name: 'Transpose',
+		icon: 'https://secure.xivdb.com/img/game/000000/000466.png',
+		cooldown: 8,
+	},
+
+	MANAWARD: {
+		id: 157,
+		name: 'Manaward',
+		icon: 'https://secure.xivdb.com/img/game/000000/000463.png',
+		cooldown: 120,
+	},
+
+	AETHERIAL_MANIPULATION: {
+		id: 155,
+		name: 'Aetherial Manipulation',
+		icon: 'https://secure.xivdb.com/img/game/000000/000467.png',
+		cooldown: 10,
+	},
+}
