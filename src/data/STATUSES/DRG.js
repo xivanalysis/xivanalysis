135d93a510bacc1c6cb35ac3eeddc7c787f00f97
Astra <sweetie.puffhannah@gmail.com>
2018-07-23 18:34:30 +1000

    Completed Dragoon Status Buffs

--- src/data/STATUSES/DRG.js
+++ src/data/STATUSES/DRG.js
@@ -4,4 +4,34 @@ export default {
 		name: 'Piercing Resistance Down',
 		icon: 'https://secure.xivdb.com/img/game/015000/015065.png',
 	},
+
+	HEAVY_THRUST: {
+		id: 115,
+		name: 'Heavy Thrust',
+		icon: 'https://secure.xivdb.com/img/game/010000/010306.png',
+	},
+
+	SHARPER_FANG_AND_CLAW: {
+		id: 802,
+		name: 'Sharper Fang and Claw',
+		icon: 'https://secure.xivdb.com/img/game/012000/012579.png',
+	},
+
+	ENHANCED_WHEELING_THRUST: {
+		id: 803,
+		name: 'Enhanced Wheeling Thrust',
+		icon: 'https://secure.xivdb.com/img/game/012000/012580.png',
+	},
+
+	RIGHT_EYE: {
+		id: 1453,
+		name: 'Right Eye',
+		icon: 'https://secure.xivdb.com/img/game/012000/012581.png',
+	},
+
+	BLOOD_FOR_BLOOD: {
+		id: 117,
+		name: 'Blood For Blood',
+		icon: 'https://secure.xivdb.com/img/game/010000/010304.png',
+	},
 }
