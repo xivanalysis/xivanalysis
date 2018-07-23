b8f0ab6f726a98ce759686753e752445d451c56c
Ryan Beck <rjb17f@my.fsu.edu>
2018-07-23 15:34:29 -0400

    Added Samurai Actions and Statuses.

---
+++ src/data/ACTIONS/SAM.js
@@ -0,0 +1,237 @@
+// Samurai Actions
+export default {
+	//-----
+	//Player GCDs
+	//-----
+	HAKAZE: 
+	{
+        	id:7477,
+        	name: 'Hakazae',
+        	icon: 'https://secure.xivdb.com/img/game/003000/003151.png',
+        	onGCD: true,
+        },
+
+	JINPU: 
+	{
+        	id:7478,
+		name: 'Jinpu',
+	        icon: 'https://secure.xivdb.com/img/game/003000/003152.png',
+		onGCD: true,
+	},
+
+	ENPI:
+	{
+		id:7486,
+		name:'Enpi',
+		icon:'https://secure.xivdb.com/img/game/003000/003155.png'
+		onGCD: true,
+	},
+
+	SHIFU:
+	{
+		id:7479,
+		name:'Shifu',
+		icon:'https://secure.xivdb.com/img/game/003000/003156.png',
+		onGCD: true,
+	},
+
+	FUGA:
+	{
+		id:7483,
+		name'Fuga',
+		icon:'https://secure.xivdb.com/img/game/003000/003157.png',
+		onGCD: true,
+	},
+	
+	IAIJUTSU:
+	{
+		id:7867,
+		name:'Iaijutsu',
+		icon:'https://secure.xivdb.com/img/game/003000/003159.png'
+		onGCD: true,
+	},
+	
+	HIGANBANA:
+	{
+		id: 7489,
+		name:'Higanbana',
+		icon:'https://secure.xivdb.com/img/game/003000/003160.png'
+		onGCD: true,
+	},
+
+	GEKKO:
+	{
+		id:7481,
+		name: 'Gekko',
+		icon: 'https://secure.xivdb.com/img/game/003000/003158.png',
+		onGCD: true,
+	},
+
+	MANGETSU: 
+	{
+		id: 7484,
+		name: 'Mangetsu',
+		icon: 'https://secure.xivdb.com/img/game/003000/003163.png',
+		onGCD: true,
+	},
+
+	TENKA_GOKEN: 
+	{
+		id: 7488,
+		name: 'Tenka Goken',
+		icon: 'https://secure.xivdb.com/img/game/003000/003161.png',
+		onGCD: true,
+	},
+
+	KASHA:
+	{
+		id:7482,
+		name:'Kasha',
+		icon:'https://secure.xivdb.com/img/game/003000/003164.png',
+		onGCD: true,
+	},
+
+	OKA:
+	{
+		id: 7485,
+		name: 'Oka',
+		icon: 'https://secure.xivdb.com/img/game/003000/003165.png',
+		onGCD: true,
+	},
+
+	MIDARE_SETSUGEKKA:
+	{
+		id:7487,
+		name:'Midare Setsugekka',
+		icon: 'https://secure.xivdb.com/img/game/003000/003162.png'
+		onGCD: true,
+	},
+
+	YUKIKAZE:
+	{
+		id:7480,
+		name: 'Yukikaze',
+		icon: 'https://secure.xivdb.com/img/game/003000/003166.png',
+		onGCD: true,
+	},
+
+	//-----
+	//Player OGCDs
+	//-----
+	
+	THIRD_EYE:
+	{
+		id:7498,
+		name:'Third Eye',
+		icon:'https://secure.xivdb.com/img/game/003000/003153.png'
+		onGCD: false,
+		cooldown: 15,
+	},
+
+	AGEHA:
+	{
+		id:7500,
+		name: 'Ageha',
+		icon: 'https://secure.xivdb.com/img/game/003000/003154.png',
+		onGCD: false,
+		cooldown: 60;
+	},
+
+	MEIKYO_SHISUI:
+	{
+		id: 7499,
+		name: 'Meikyo Shisui',
+		icon: 'https://secure.xivdb.com/img/game/003000/003167.png',
+		onGCD: false,
+		cooldown: 80,
+	},
+
+	HISSATSU_KAITEN:
+	{
+		id: 7494
+		name: 'Hissatsu: Kaiten',
+		icon: 'https://secure.xivdb.com/img/game/003000/003168.png'
+		onGCD: false,
+		cooldown: 5,
+	},
+
+	HISSATSU_GYOTEN:
+	{
+		id: 7492,
+		name: 'Hissatsu: Gyoten',
+		icon: 'https://secure.xivdb.com/img/game/003000/003169.png'
+		onGCD: false,
+		cooldown: 10,
+	},
+
+	HISSATSU_YATEN:
+	{	
+		id:7493,
+		name: 'Hissatsu: Yaten',
+		icon: 'https://secure.xivdb.com/img/game/003000/003170.png'
+		onGCD: false,
+		cooldown: 10,
+	},
+
+	MERCIFUL_EYES:
+	{
+		id:7502,
+		name: 'Merciful Eyes',
+		icon: 'https://secure.xivdb.com/img/game/003000/003171.png'
+		onGCD: false,
+		cooldown: 1,
+	},
+
+	MEDITATE:
+	{
+		id: 7497,
+		name: 'Meditate',
+		icon: 'https://secure.xivdb.com/img/game/003000/003172.png',
+		onGCD: false,
+		cooldown: 60,
+	},
+
+	HISSATSU_SHINTEN:
+	{
+		id:7490,
+		name: 'Hissatsu: Shinten',
+		icon: 'https://secure.xivdb.com/img/game/003000/003173.png',
+		onGCD: false,
+		cooldown: 1,
+	},
+
+	HISSATSU_KYUTEN:
+	{
+		id:7491,
+		name: 'Hissatsu: Kyuten',
+		icon: 'https://secure.xivdb.com/img/game/003000/003174.png',
+		onGCD: false,
+		cooldown: 1,
+	},
+
+	HISSATSU_SEIGAN:
+	{	
+		id:7501,
+		name: 'Hissatsu: Seigan',
+		icon: 'https://secure.xivdb.com/img/game/003000/003175.png'
+		onGCD: false,
+		cooldown: 1,
+	},
+
+	HAGAKURE:
+	{
+		id: 7495
+		name: 'Hagakure'
+		icon: 'https://secure.xivdb.com/img/game/003000/003176.png'
+		onGCD: false,
+		cooldown: 40,
+	},
+
+	HISSATSU_GUREN:
+	{
+		id:7496,
+		name: 'Hisstasu: Guren',
+		icon: 'https://secure.xivdb.com/img/game/003000/003177.png',
+		onGCD: false,
+		cooldown: 120,
+	},
---
+++ src/data/STATUSES/SAM.js
@@ -0,0 +1,30 @@
+export default {
+	
+	THIRD_EYE: 
+	{
+		id: 7498,
+		name: 'Third Eye',
+		icon: 'https://secure.xivdb.com/img/game/003000/003153.png',
+	},
+
+
+	JINPU:
+	{
+		id:7478,
+		name: 'Jinpu',
+		icon: 'https://secure.xivdb.com/img/game/003000/003152.png',
+	},
+
+	SHIFU:
+	{
+		id:7479,
+		name: 'Shifu',
+		icon: 'https://secure.xivdb.com/img/game/003000/003156.png',
+	},
+
+	MEDITATE:
+	{
+		id:7497,
+		name:'Meditate',
+		icon: 'https://secure.xivdb.com/img/game/003000/003172.png',
+	},
