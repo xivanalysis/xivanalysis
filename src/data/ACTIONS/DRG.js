034c3b464d0a34196eb7c7492decc239ba209fc7
Astra <sweetie.puffhannah@gmail.com>
2018-07-23 16:28:32 +1000

    Fixed LNC.js typo, Added LNC and DRG actions.

--- src/data/ACTIONS/DRG.js
+++ src/data/ACTIONS/DRG.js
@@ -2,26 +2,105 @@ export default{
 	// -----
 	// Player GCDs
 	// -----
-	HEAVY_THRUST: {
-		id: 79,
-		name: 'Heavy Thrust',
-		icon: 'https://secure.xivdb.com/img/game/000000/000311.png',
+	FANG_AND_CLAW: {
+		id: 3554,
+		name: 'Fang and Claw',
+		icon: 'https://secure.xivdb.com/img/game/002000/002582.png',
 		onGcd: true,
 	},
-	IMPULSE_DRIVE: {
-		id: 81,
-		name: 'Impulse Drive',
-		icon: 'https://secure.xivdb.com/img/game/000000/000313.png',
+
+	WHEELING_THRUST: {
+		id: 3556,
+		name: 'Wheeling Thrust',
+		icon: 'https://secure.xivdb.com/img/game/002000/002584.png',
 		onGcd: true,
 	},
-	DISEMBOWEL: {
-		id: 87,
-		name: 'Disembowel',
-		icon: 'https://secure.xivdb.com/img/game/000000/000317.png',
+
+	DOOM_SPIKE: {
+		id: 86,
+		name: 'Doom Spike',
+		icon: 'https://secure.xivdb.com/img/game/000000/000306.png',
+		onGcd: true,
+	},
+
+	SONIC_THRUST: {
+		id: 7397,
+		name: 'Sonic Thrust',
+		icon: 'https://secure.xivdb.com/img/game/002000/002586.png',
 		onGcd: true,
 	},
 	// -----
 	// Player OGCDs
 	// -----
+	JUMP: {
+		id: 92,
+		name: 'Jump',
+		icon: 'https://secure.xivdb.com/img/game/002000/002576.png',
+		cooldown: 30,
+	},
+
+	ELUSIVE_JUMP: {
+		id: 94,
+		name: 'Elusive Jump',
+		icon: 'https://secure.xivdb.com/img/game/002000/002577.png',
+		cooldown: 30,
+	},
+
+	SPINESHATTER_DIVE: {
+		id: 95,
+		name: 'Spineshatter Dive',
+		icon: 'https://secure.xivdb.com/img/game/002000/002580.png',
+		cooldown: 60,
+	},
+
+	DRAGONFIRE_DIVE: {
+		id: 96,
+		name: 'Dragonfire Dive',
+		icon: 'https://secure.xivdb.com/img/game/002000/002578.png',
+		cooldown: 120,
+
+	},
+
+	BATTLE_LITANY: {
+		id: 3557,
+		name: 'Battle Litany',
+		icon: 'https://secure.xivdb.com/img/game/002000/002585.png',
+		cooldown: 180,
+	},
+
+	BLOOD_OF_THE_DRAGON: {
+		id: 3553,
+		name: 'Blood Of The Dragon',
+		icon: 'https://secure.xivdb.com/img/game/002000/002581.png',
+		cooldown: 30,
+	},
+
+	GEIRSKOGUL: {
+		id: 3555,
+		name: 'Geirskogul',
+		icon: 'https://secure.xivdb.com/img/game/002000/002583.png',
+		cooldown: 30,
+	},
+
+	DRAGON_SIGHT: {
+		id: 7398,
+		name: 'Dragon Sight',
+		icon: 'https://secure.xivdb.com/img/game/002000/002587.png',
+		cooldown: 120,
+	},
+
+	MIRAGE_DIVE: {
+		id: 7399,
+		name: 'Mirage Dive',
+		icon: 'https://secure.xivdb.com/img/game/002000/002588.png',
+		cooldown: 1,
+	},
+
+	NASTROND: {
+		id: 7400,
+		name: 'Nastrond',
+		icon: 'https://secure.xivdb.com/img/game/002000/002589.png',
+		cooldown: 10,
+	},
 
 }
---
+++ src/data/ACTIONS/LNC.js
@@ -0,0 +1,77 @@
+export default {
+	// -----
+	// Player GCDs
+	// -----
+	HEAVY_THRUST: {
+		id: 79,
+		name: 'Heavy Thrust',
+		icon: 'https://secure.xivdb.com/img/game/000000/000311.png',
+		onGcd: true,
+	},
+
+	IMPULSE_DRIVE: {
+		id: 81,
+		name: 'Impulse Drive',
+		icon: 'https://secure.xivdb.com/img/game/000000/000313.png',
+		onGcd: true,
+	},
+
+	DISEMBOWEL: {
+		id: 87,
+		name: 'Disembowel',
+		icon: 'https://secure.xivdb.com/img/game/000000/000317.png',
+		onGcd: true,
+	},
+
+	CHAOS_THRUST: {
+		id: 88,
+		name: 'Chaos Thrust',
+		icon: 'https://secure.xivdb.com/img/game/000000/000308.png',
+		onGcd: true,
+	},
+
+	TRUE_THRUST: {
+		id: 75,
+		name: 'True Thrust',
+		icon: 'https://secure.xivdb.com/img/game/000000/000310.png',
+		onGcd: true,
+	},
+
+	VORPAL_THRUST: {
+		id: 78,
+		name: 'Vorpal Thrust',
+		icon: 'https://secure.xivdb.com/img/game/000000/000312.png',
+		onGcd: true,
+	},
+
+	FULL_THRUST: {
+		id: 84,
+		name: 'Full Thrust',
+		icon: 'https://secure.xivdb.com/img/game/000000/000314.png',
+		onGcd: true,
+	},
+
+	PIERCING_TALON: {
+		id: 90,
+		name: 'Piercing Talon',
+		icon: 'https://secure.xivdb.com/img/game/000000/000315.png',
+		onGcd: true,
+	},
+
+	// -----
+	// Player OGCDs
+	// -----
+	LIFE_SURGE: {
+		id: 83,
+		name: 'Life Surge',
+		icon: 'https://secure.xivdb.com/img/game/000000/000304.png',
+		cooldown: 50,
+	},
+
+	BLOOD_FOR_BLOOD: {
+		id: 85,
+		name: 'Blood for Blood',
+		icon: 'https://secure.xivdb.com/img/game/000000/000309.png',
+		cooldown: 80,
+	},
+}
