fd79905e7b0f42079b7e0adced2feef8912c7a9c
Alex Levine <lord.xela@gmail.com>
2018-07-23 03:47:03 +0100

    Removed an unused function

--- src/parser/jobs/nin/Ninki.js
+++ src/parser/jobs/nin/Ninki.js
@@ -73,17 +73,6 @@ export default class Ninki extends Module {
 	}
 */
 
-	_grammaticalJoin(array) {
-		switch (array.length) {
-			case 1:
-				return array[0]
-			case 2:
-				return array[0] + ' and ' + array[1]
-			default:
-				return array.slice(0, array.length - 1).join(', ') + ' and ' + array[array.length - 1]
-		}
-	}
-
 	_onComplete() {
 		if (this._wastedNinki >= 20) {
 			let why = []
