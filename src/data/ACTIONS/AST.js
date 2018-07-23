d522b5b138261e92ef31aa6b70d71e66d902e75d
suShirou <>
2018-07-23 18:12:24 +0800

    AST data - actions and statuses to start with

---
+++ src/data/ACTIONS/AST.js
@@ -0,0 +1,29 @@
+export default {
+	ASCEND: {
+		id: 3603,
+		name: 'Ascend',
+		icon: 'https://secure.xivdb.com/img/game/003000/003132.png',
+		onGcd: true,
+		castTime: 8,
+	},
+
+	LIGHTSPEED: {
+		id: 3606,
+		name: 'LIGHTSPEED',
+		icon: 'https://secure.xivdb.com/img/game/003000/003135.png',
+		cooldown: 120,
+	},
+
+	COMBUST_II: {
+		id: 3608,
+		name: 'Combust II',
+		icon: 'https://secure.xivdb.com/img/game/003000/003125.png',
+		onGcd: true,
+	},
+
+	STELLAR_DETONATION: {
+		id: 8324,
+		name: 'Stellar Detonation',
+		icon: 'https://secure.xivdb.com/img/game/003000/003144.png',
+	}
+}
