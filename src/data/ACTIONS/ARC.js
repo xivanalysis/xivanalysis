758d9f1a64b514d5534a9ca50d9f8738d7f14dcb
Yumiya Nagatsuki <yumiyanagatsuki@bitbucket.org>
2018-07-24 20:08:12 -0300

    Added ARC/BRD skills and statuses; also fixed a typo on "CRITIAL UP"

---
+++ src/data/ACTIONS/ARC.js
@@ -0,0 +1,52 @@
+export default {
+	HEAVY_SHOT: {
+		id: 97,
+		name: 'Heavy Shot',
+		icon: 'https://secure.xivdb.com/img/game/000000/000358.png',
+	},
+	VENOMOUS_BITE: {
+		id: 100,
+		name: 'Venomous Bite',
+		icon: 'https://secure.xivdb.com/img/game/000000/000363.png',
+	},
+	QUICK_NOCK: {
+		id: 106,
+		name: 'Quick Nock',
+		icon: 'https://secure.xivdb.com/img/game/000000/000360.png',
+	},
+	BLOODLETTER: {
+		id: 110,
+		name: 'Bloodletter',
+		icon: 'https://secure.xivdb.com/img/game/000000/000361.png',
+	},
+	STRAIGHT_SHOT: {
+		id: 98,
+		name: 'Straight Shot',
+		icon: 'https://secure.xivdb.com/img/game/000000/000359.png',
+	},
+	BARRAGE: {
+		id: 107,
+		name: 'Barrage',
+		icon: 'https://secure.xivdb.com/img/game/000000/000353.png',
+	},
+	MISERYS_END: {
+		id: 103,
+		name: 'Misery\'s End',
+		icon: 'https://secure.xivdb.com/img/game/000000/000364.png',
+	},
+	WINDBITE: {
+		id: 113,
+		name: 'Windbite',
+		icon: 'https://secure.xivdb.com/img/game/000000/000367.png',
+	},
+	RAGING_STRIKES: {
+		id: 101,
+		name: 'Raging Strikes',
+		icon: 'https://secure.xivdb.com/img/game/000000/000352.png',
+	},
+	REPELLING_SHOT: {
+		id: 112,
+		name: 'Repelling Shot',
+		icon: 'https://secure.xivdb.com/img/game/000000/000366.png',
+	}
+}
---
+++ src/data/ACTIONS/BRD.js
@@ -0,0 +1,83 @@
+export default {
+	MAGES_BALLAD: {
+		id: 114,
+		name: 'Mage\'s Ballad',
+		icon: 'https://secure.xivdb.com/img/game/002000/002602.png',
+	},
+	FOE_REQUIEM: {
+		id: 115,
+		name: 'Foe Requiem',
+		icon: 'https://secure.xivdb.com/img/game/002000/002604.png',
+	},
+	ARMYS_PAEON: {
+		id: 116,
+		name: 'Army\'s Paeon',
+		icon: 'https://secure.xivdb.com/img/game/002000/002603.png',
+	},
+	RAIN_OF_DEATH: {
+		id: 117,
+		name: 'Rain Of Death',
+		icon: 'https://secure.xivdb.com/img/game/002000/002605.png',
+	},
+	BATTLE_VOICE: {
+		id: 118,
+		name: 'Battle Voice',
+		icon: 'https://secure.xivdb.com/img/game/002000/002601.png',
+	},
+	EMPYREAL_ARROW: {
+		id: 3558,
+		name: 'Empyreal Arrow',
+		icon: 'https://secure.xivdb.com/img/game/002000/002606.png',
+	},
+	THE_WANDERERS_MINUET: {
+		id: 3559,
+		name: 'The Wanderer\'s Minuet',
+		icon: 'https://secure.xivdb.com/img/game/002000/002607.png',
+	},
+	IRON_JAWS: {
+		id: 3560,
+		name: 'Iron Jaws',
+		icon: 'https://secure.xivdb.com/img/game/002000/002608.png',
+	},
+	THE_WARDENS_PAEAN: {
+		id: 3561,
+		name: 'The Warden\'s Paean',
+		icon: 'https://secure.xivdb.com/img/game/002000/002609.png',
+	},
+	SIDEWINDER: {
+		id: 3562,
+		name: 'Sidewinder',
+		icon: 'https://secure.xivdb.com/img/game/002000/002610.png',
+	},
+	PITCH_PERFECT: {
+		id: 7404,
+		name: 'Pitch Perfect',
+		icon: 'https://secure.xivdb.com/img/game/002000/002611.png',
+	},
+	TROUBADOUR: {
+		id: 7405,
+		name: 'Troubadour',
+		icon: 'https://secure.xivdb.com/img/game/002000/002612.png',
+	},
+	CAUSTIC_BITE: {
+		id: 7406,
+		name: 'Caustic Bite',
+		icon: 'https://secure.xivdb.com/img/game/002000/002613.png',
+	},
+	STORMBITE: {
+		id: 7407,
+		name: 'Stormbite',
+		icon: 'https://secure.xivdb.com/img/game/002000/002614.png',
+	},
+	NATURES_MINNE: {
+		id: 7408,
+		name: 'Nature\'s Minne',
+		icon: 'https://secure.xivdb.com/img/game/002000/002615.png',
+	},
+	REFULGENT_ARROW: {
+		id: 7409,
+		name: 'Refulgent Arrow',
+		icon: 'https://secure.xivdb.com/img/game/002000/002616.png',
+	}
+}
+
