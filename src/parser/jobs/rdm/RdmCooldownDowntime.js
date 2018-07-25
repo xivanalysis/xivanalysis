06bb89587656bd711facd3aa9a9892796039eafb
Cephid <Kaiser12@gmail.com>
2018-07-24 22:59:55 -0500

    initial entry for RdmCooldownDowntime and base CooldownDowntime

---
+++ src/parser/jobs/rdm/RdmCooldownDowntime.js
@@ -0,0 +1,23 @@
+import ACTIONS from 'data/ACTIONS'
+import CooldownDowntime from 'parser/core/modules/CooldownDowntime'
+
+//tracking the important™ CDs
+const TRACKEDCDS = [
+	ACTIONS.FLECHE.id,
+	ACTIONS.CONTRE_SIXTE.id,
+	ACTIONS.ACCELERATION.id,
+	ACTIONS.CORPS_A_CORPS.id,
+	ACTIONS.DISPLACEMENT.id,
+]
+
+//Time that Jump deems ok for a OGCD to be down : ^)
+const DOWNTIME_OK_TIME = 2000
+
+export default class RdmCooldownDowntime extends CooldownDowntime {
+
+	constructor(...args) {
+		super(...args)
+		super.setAbilityList(TRACKEDCDS)
+		super.setDownTimeDuration(DOWNTIME_OK_TIME)
+	}
+}
--- src/parser/jobs/rdm/index.js
+++ src/parser/jobs/rdm/index.js
@@ -1,9 +1,11 @@
 import About from './About'
 import Gauge from './Gauge'
 import Dualcast from './Dualcast'
+import RdmCooldownDowntime from './RdmCooldownDowntime'
 
 export default [
 	About,
 	Gauge,
 	Dualcast,
+	RdmCooldownDowntime,
 ]
