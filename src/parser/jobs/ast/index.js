026dd70e9d08179681d385130d19feac732e72d1
suShirou <suShirou#3520>
2018-07-26 16:56:34 +0800

    Merged /buffextension to /master

--- src/parser/jobs/ast/index.js
+++ src/parser/jobs/ast/index.js
@@ -1,13 +1,12 @@
 // import About from './About'
 // import EarthlyStar from './EarthlyStar'
 // import ArcanaUptime from './ArcanaUptime'
-// import BuffExtensions from './BuffExtensions'
+import BuffExtensions from './BuffExtensions'
 import ArcanaUndrawUsage from './ArcanaUndrawUsage'
 import DoT from './DoT'
 
 export default [
 	// About,
 	ArcanaUndrawUsage,
-	DoT,
-	// BuffExtensions
+	BuffExtensions,	DoT,
 ]
