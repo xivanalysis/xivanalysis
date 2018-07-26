fbfb89b95c66d4f11d5cf90804bffb0274202072
suShirou <suShirou#3520>
2018-07-26 17:59:18 +0800

    Entity.js handling error case

--- src/parser/core/Entity.js
+++ src/parser/core/Entity.js
@@ -40,14 +40,21 @@ export default class Entity {
 	}
 
 	/**
-	 * @param {number} forTimestamp Timestamp (in ms) to be considered, or the current timestamp if null. Won't work right for timestamps after the currentTimestamp.
+	 * @param {number} forTimestamp Timestamp (in ms) to be considered. Won't work right for timestamps after the currentTimestamp.
 	 * @param {number} bufferTime Time (in ms) after buff's expiration where it will still be included. There's a bug in the combat log where if a spell consumes a buff that buff may disappear a short time before the heal or damage event it's buffing is logged. This can sometimes go up to hundreds of milliseconds.
 	 * @param {number} minimalActiveTime - Time (in ms) the buff must have been active before timestamp for it to be included.
 	 * @param {number} sourceID - source ID the buff must have come from, or any source if null.
 	 * @returns {Object} - An array of buff objects on the target at the given timestamp. The buff object will have all the properties of the associated applybuff event, along with a start timestamp, an end timestamp if the buff has fallen, and an isDebuff flag. If multiple buffs meet the specifications, there's no guarantee which you'll get (this could happen if multiple spells with the same statusId but from different sources are on the same target)
 	 */
 	getStatuses(forTimestamp, bufferTime = 0, minimalActiveTime = 0, sourceID = null) {
-		const currentTimestamp = forTimestamp
+
+
+		const currentTimestamp = forTimestamp < this.parser.currentTimestamp ? forTimestamp
+			: this.parser.currentTimestamp
+
+		if (forTimestamp < this.parser.currentTimestamp) {
+			console.warn('Entity:getStatuses timestamp given is later than current timestamp')
+		}
 
 		return this.buffs.filter(buff =>
 			(currentTimestamp - minimalActiveTime) >= buff.start &&
