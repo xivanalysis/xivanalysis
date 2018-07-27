d2325b81942c189fbbbf4dfa9f5c7b66cc721dd7
Henrique Cezar <skyikaza@protonmail.com>
2018-07-26 22:11:30 -0300

    Added proper About description.

--- src/parser/jobs/war/About.js
+++ src/parser/jobs/war/About.js
@@ -7,6 +7,9 @@ import CoreAbout from 'parser/core/modules/About'
 export default class About extends CoreAbout {
 	description = <Fragment>
 		<p>This analyzer aims to identify some of the low-hanging fruit that could be used to improve your WAR gameplay, as well as give a deeper insight into what happened during an encounter.</p>
+		<p>While it's currently fairly incomplete, you can always axe questions in the Discord server, and if you need to learn how to WAR, you can always check out <a href="http://thebalanceffxiv.com">The Balance</a>.</p>
+		<p>Any bugs, complaints, suggestions -- Join us on the XivA Discord, and ping me (Sayaka) to let me know.</p>
+
 		<Message warning icon>
 			<Icon name="warning sign"/>
 			<Message.Content>
