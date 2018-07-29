08d8c04978c523974207bacaa0b40b56d155088c
xslare <xslare@gmail.com>
2018-07-29 14:32:15 +0200

    made escape handling more clear, switched to reduce w/ aggregator from map, split getDowntimes and getDowntimeWindows

--- src/parser/core/modules/Downtime.js
+++ src/parser/core/modules/Downtime.js
@@ -16,7 +16,7 @@ export default class Downtime extends Module {
 
 		// If there's nothing, just stop now
 		if (!downtimePeriods.length) {
-			return 0
+			return undefined
 		}
 
 		// Merge the downtimes that overlap
@@ -35,20 +35,38 @@ export default class Downtime extends Module {
 		return finalDowntimes
 	}
 
-	// -----
-	// Style Precedent:
-	// accumulator for downtime function must continue to be called "uptime"
-	// -----
 	getDowntime(start = 0, end = this.parser.currentTimeStamp) {
+		const res = this._internalDowntime(start, end)
+		if (res === undefined) {
+			return 0
+		}
 		// Return the final number
-		return this._internalDowntime(start, end).reduce((uptime, invuln) => uptime + Math.min(invuln.end, end) - Math.max(invuln.start, start), 0)
+		return res.reduce((uptime, invuln) => uptime + Math.min(invuln.end, end) - Math.max(invuln.start, start), 0)
+	}
+
+	getDowntimes(start = 0, end = this.parser.currentTimeStamp, minimumDowntimeLength = -1) {
+		const res = this._internalDowntime(start, end)
+		if (res === undefined) {
+			return []
+		}
+		return res.reduce((aggregator, invuln) => {
+			if (Math.min(invuln.end, end) - Math.max(invuln.start, start) > Math.min(minimumDowntimeLength, 0)) {
+				aggregator.push(Math.min(invuln.end, end) - Math.max(invuln.start, start))
+			}
+			return aggregator
+		}, [])
 	}
 
 	getDowntimeWindows(start = 0, end = this.parser.currentTimestamp, minimumWindowSize = -1) {
-		return this._internalDowntime(start, end).map(invuln =>
-			Math.min(invuln.end, end) - Math.max(invuln.start, start) > Math.min(minimumWindowSize, 0)
-				? Math.min(invuln.end, end) - Math.max(invuln.start, start)
-				: 0
-		)
+		const res = this._internalDowntime(start, end)
+		if (res === undefined) {
+			return []
+		}
+		return res.reduce((aggregator, invuln) => {
+			if (Math.min(invuln.end, end) - Math.max(invuln.start, start) > Math.min(minimumWindowSize, 0)) {
+				aggregator.push({start: Math.max(invuln.start, start), end: Math.min(invuln.end, end)})
+			}
+			return aggregator
+		}, [])
 	}
 }
