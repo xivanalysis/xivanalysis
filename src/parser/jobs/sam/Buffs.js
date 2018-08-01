3bf0ed3a70f624b430398584be23c3401d1e3913
Ryan Beck <rjb17f@my.fsu.edu>
2018-08-01 00:31:37 -0400

    Added Jinpu/Shifu uptime

--- src/parser/jobs/sam/Buffs.js
+++ src/parser/jobs/sam/Buffs.js
@@ -10,7 +10,7 @@ import {Suggestion, SEVERITY} from 'parser/core/modules/Suggestions'
 const JINPU_DURATION = 30000
 const SHIFU_DURATION = 30000
 
-const BUFF_BUFFER = 10000 //Idk why I like this name so much
+const BUFF_BUFFER = 10000
 
 export default class Buffs extends Module {
 	static handle = 'buffs'
@@ -21,18 +21,18 @@ export default class Buffs extends Module {
 		'suggestions',
 	]
 
-	_ShifuUses = []
 	_JinpuUses = []
+	_ShifuUses = []
 
 	_earlyJinpuApplications = 0
-	_earlyShifuApplications = 0
+	_earlyShifyApplications = 0
 
 	constructor(...args) {
 		super(...args)
 
 		const filter = {
 			by: 'player',
-			abilityId: [STATUSES.SHIFU.id, STATUSES.JINPU.id],
+			abilityId: [STATUSES.JINPU.id, STATUSES.SHIFU.id],
 		}
 		this.addHook('applybuff', filter, this._onShifuApplication, this._onJinpuApplication)
 		this.addHook('refreshbuff', filter, this._onShifuApplication, this._onJinpuApplication)
@@ -56,28 +56,26 @@ export default class Buffs extends Module {
 	}
 
 	_onShifuApplication(event) {
-                this._ShifuUses.unshift(event)
+		this._ShifuUses.unshift(event)
 
-                if (this._ShifuUses.length < 2) {
-                        return
-                }
+		if (this._ShifuUses.length < 2) {
+			return
+		}
 
-                const current = this._ShifuUses[0].timestamp
-                const previous = this._ShifuUses[1].timestamp
-                const timeSinceLastApplication = current - previous
+		const current = this._ShifuUses[0].timestamp
+		const previous = this._ShifuUses[1].timestamp
+		const timeSinceLastApplication = current - previous
 
-                if (timeSinceLastApplication < SHIFU_DURATION - BUFF_BUFFER) {
-                        this._earlyShifuApplications++
-                }
-        }
-
-
+		if (timeSinceLastApplication < SHIFU_DURATION - BUFF_BUFFER) {
+			this._earlyShifuApplications++
+		}
+	}
 
 	_onComplete() {
 		this.checklist.add(new Rule({
-			name: 'Keep Jinpu & Shifu up',
-			description: 'Jinpu increases your damage by 10% while Shifu increases your speed by 10%. they both are a huge part of Samurai\'s damage.',
-			target: 90,
+			name: 'Keep Shifu and Jinpu up',
+			description: 'Jinpu increases your damage by 10% and Shifu increases your speed by 10%. Both buffs are key part of Samurai\'s damage.',
+			target: 95,
 			requirements: [
 				new Requirement({
 					name: <Fragment><ActionLink {...ACTIONS.JINPU} /> uptime</Fragment>,
@@ -85,42 +83,42 @@ export default class Buffs extends Module {
 				}),
 
 				new Requirement({
-                                        name: <Fragment><ActionLink {...ACTIONS.SHIFU} /> uptime</Fragment>,
-                                        percent: () => this.getShifuUptimePercent(),
-                                }),
+					name: <Fragment><ActionLink {...ACTIONS.SHIFU} /> uptime</Fragment>,
+					percent: () => this.getShifuUptimePercent(),
+				}),
 
 			],
 		}))
 
 		if (this._earlyJinpuApplications) {
 			this.suggestions.add(new Suggestion({
-				icon: ACTIONS.SHIFU.icon,
+				icon: ACTIONS.JINPU.icon,
 				content: <Fragment>
-						Avoid refreshing {ACTIONS.JINPU.name} significantly before its expiration -- That might be making you miss uptime on other buffs} uses.
+						Avoid refreshing {ACTIONS.JINPU.name} significantly before its expiration -- That might be making you possibly lose resources.
 				</Fragment>,
 				severity: SEVERITY.MEDIUM,
 				why: <Fragment>
 					{this._earlyJinpuApplications} reapplications that were {BUFF_BUFFER / 1000} or more seconds before expiration.
 				</Fragment>,
 			}))
-
-		 if (this._earlyShifuApplications) {
-                        this.suggestions.add(new Suggestion({
-                                icon: ACTIONS.SHIFU.icon,
-                                content: <Fragment>
-                                                Avoid refreshing {ACTIONS.SHIFU.name} significantly before its expiration -- That might be making you miss uptime on other buffs uses.
-                                </Fragment>,
-                                severity: SEVERITY.MEDIUM,
-                                why: <Fragment>
-                                        {this._earlyShifuApplications} reapplications that were {BUFF_BUFFER / 1000} or more seconds before expiration.
-                                </Fragment>,
-                        }))
-
 		}
-	
+
+		if (this._earlyShifuApplications) {
+			this.suggestions.add(new Suggestion({
+				icon: ACTIONS.SHIFU.icon,
+				content: <Fragment>
+                                                Avoid refreshing {ACTIONS.SHIFU.name} significantly before its expiration -- That might be making you possibly lose resources.
+				</Fragment>,
+				severity: SEVERITY.MEDIUM,
+				why: <Fragment>
+					{this._earlyShifuApplications} reapplications that were {BUFF_BUFFER / 1000} or more seconds before expiration.
+				</Fragment>,
+			}))
+		}
 
 	}
-	//uptime functions
+
+	//
 	getJinpuUptimePercent() {
 		const statusUptime = this.combatants.getStatusUptime(STATUSES.JINPU.id, this.parser.player.id)
 		const fightUptime = this.parser.fightDuration - this.invuln.getInvulnerableUptime()
@@ -129,10 +127,10 @@ export default class Buffs extends Module {
 	}
 
 	getShifuUptimePercent() {
-                const statusUptime = this.combatants.getStatusUptime(STATUSES.SHIFU.id, this.parser.player.id)
-                const fightUptime = this.parser.fightDuration - this.invuln.getInvulnerableUptime()
+		const statusUptime = this.combatants.getStatusUptime(STATUSES.SHIFU.id, this.parser.player.id)
+		const fightUptime = this.parser.fightDuration - this.invuln.getInvulnerableUptime()
 
-                return (statusUptime / fightUptime) * 100
-        }
+		return (statusUptime / fightUptime) * 100
+	}
 
 }
