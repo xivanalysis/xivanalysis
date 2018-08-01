33d485024cdae1fac1966f9ff979e2a7aa0f5efa
Fürst <timo.dornhoefer@web.de>
2018-08-01 05:19:51 +0200

    switched to the core cooldowndowntime.js and started transl stuff

--- src/parser/jobs/blm/About.js
+++ src/parser/jobs/blm/About.js
@@ -5,8 +5,11 @@ import {ActionLink} from 'components/ui/DbLink'
 import ACTIONS from 'data/ACTIONS'
 import CONTRIBUTORS, {ROLES} from 'data/CONTRIBUTORS'
 import CoreAbout from 'parser/core/modules/About'
+//import {i18nMark, Trans} from '@lingui/react'
 
 export default class About extends CoreAbout {
+
+	//static i18n_id = i18nMark('blm.about.title')
 	description = <Fragment>
 		<p>This analyser aims to identify how you're not actually casting <ActionLink {...ACTIONS.FIRE_IV} /> as much as you think you are.</p>
 		<Message warning icon>
