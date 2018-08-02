f58b610a19ace0fe366ca3f4b500cac0f57afd17
Khloe Leclair <xivkhloeleclair@gmail.com>
2018-08-01 20:49:09 -0400

    Convert `<TransMarkdown />` to a PureComponent. Define a custom link renderer so that we can set a `target` on rendered links to force them to open in a new window when desired.

--- src/components/ui/TransMarkdown.js
+++ src/components/ui/TransMarkdown.js
@@ -1,21 +1,29 @@
 import {withI18n} from '@lingui/react'
 import PropTypes from 'prop-types'
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 import ReactMarkdown from 'react-markdown'
 
-class TransMarkdown extends Component {
+class TransMarkdown extends PureComponent {
 	static propTypes = {
 		id: PropTypes.string.isRequired,
 		i18n: PropTypes.shape({
 			_: PropTypes.func.isRequired,
 		}).isRequired,
 		source: PropTypes.string.isRequired,
+		linkTarget: PropTypes.string,
 	}
+
 	render() {
-		const {id, i18n, source} = this.props
+		const {id, i18n, source, linkTarget} = this.props
 
 		return <ReactMarkdown
 			source={i18n._(id, {}, {defaults: source})}
+			renderers={{
+				link: props => React.createElement('a', {
+					target: linkTarget,
+					href: props.href,
+				}, props.children),
+			}}
 		/>
 	}
 }
