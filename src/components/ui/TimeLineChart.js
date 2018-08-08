f5912cda0f627eada4eb5aa57c1ffa62dcdd66b5
Saxon Landers <saxon@ackwell.com.au>
2018-08-09 01:03:18 +1000

    Moved options outside render

--- src/components/ui/TimeLineChart.js
+++ src/components/ui/TimeLineChart.js
@@ -2,27 +2,27 @@ import PropTypes from 'prop-types'
 import React, {PureComponent} from 'react'
 import {Line} from 'react-chartjs-2'
 
+const DEFAULT_OPTIONS = {
+	scales: {
+		xAxes: [{
+			type: 'time',
+			time: {
+				displayFormats: {
+					minute: 'm:ss',
+					second: 'm:ss',
+					millisecond: 'm:ss.SS',
+				},
+			},
+		}],
+	},
+}
+
 export default class TimeLineChart extends PureComponent {
 	static propTypes = {
 		data: PropTypes.object.isRequired,
 	}
 
 	render() {
-		const options = {
-			scales: {
-				xAxes: [{
-					type: 'time',
-					time: {
-						displayFormats: {
-							minute: 'm:ss',
-							second: 'm:ss',
-							millisecond: 'm:ss.SS',
-						},
-					},
-				}],
-			},
-		}
-
-		return <Line data={this.props.data} options={options}/>
+		return <Line data={this.props.data} options={DEFAULT_OPTIONS}/>
 	}
 }
