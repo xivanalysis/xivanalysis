3e23922f71e77334cc9e56fae0b83ea33368f416
Saxon Landers <saxon@ackwell.com.au>
2018-09-14 02:05:21 +1000

    Initial tooltip integration

--- src/Root.js
+++ src/Root.js
@@ -1,21 +1,25 @@
 import React, {Component} from 'react'
-import {Provider} from 'react-redux'
+import {Provider as ReduxProvider} from 'react-redux'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import App from 'components/App'
 import I18nLoader from 'components/I18nLoader'
+import {Provider as TooltipProvider} from 'components/ui/DbLink'
 import store from 'store'
+console.log(store)
 
 class Root extends Component {
 	render() {
 		return (
-			<Provider store={store}>
+			<ReduxProvider store={store}>
 				<I18nLoader>
-					<Router>
-						<App/>
-					</Router>
+					<TooltipProvider>
+						<Router>
+							<App/>
+						</Router>
+					</TooltipProvider>
 				</I18nLoader>
-			</Provider>
+			</ReduxProvider>
 		)
 	}
 }
