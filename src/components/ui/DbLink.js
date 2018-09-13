c9d7f1a0448b49820dfeb689f073770bfb63a240
Saxon Landers <saxon@ackwell.com.au>
2018-09-14 02:14:18 +1000

    Initial re-impl of tooltip wrapper

--- src/components/ui/DbLink.js
+++ src/components/ui/DbLink.js
@@ -1,12 +1,16 @@
+import {Trans} from '@lingui/react'
 import {
 	Provider as TooltipProvider,
-	Tooltip,
+	tooltipHOC,
 } from '@xivanalysis/tooltips'
 import PropTypes from 'prop-types'
 import React from 'react'
 import {connect} from 'react-redux'
+import {Popup, Icon} from 'semantic-ui-react'
 
 import {STATUS_ID_OFFSET} from 'data/STATUSES'
+
+import styles from './DbLink.module.css'
 
 // Wrapping the provider w/ connect to pick up lang changes
 export const Provider = connect(state => ({
@@ -18,7 +22,54 @@ export const Provider = connect(state => ({
 	</TooltipProvider>
 })
 
-// Re-exporting the tooltip by default to maintain the interface
+class TooltipBase extends React.PureComponent {
+	static propTypes = {
+		baseUrl: PropTypes.string,
+		loading: PropTypes.bool.isRequired,
+		data: PropTypes.object,
+		Content: PropTypes.node,
+	}
+
+	render() {
+		// Pull in data from props and state
+		const {
+			baseUrl,
+			loading,
+			data,
+			Content,
+		} = this.props
+
+		const showIcon = true
+		const children = null
+		const showTooltip = true
+
+		if (loading) {
+			return <span>
+				{showIcon && <Icon loading name="circle notch" />}
+				{children || <Trans id="core.dblink.loading">Loading...</Trans>}
+			</span>
+		}
+
+		const link = <span className={styles.link}>
+			{showIcon && <img src={baseUrl + data.icon} alt="" />}
+			{children || data.name}
+		</span>
+
+		if (!showTooltip) {
+			return link
+		}
+
+		return <Popup
+			basic
+			style={{padding: '0px'}}
+			trigger={link}
+			content={<Content/>}
+		/>
+	}
+}
+
+const Tooltip = tooltipHOC(TooltipBase)
+
 export default Tooltip
 
 // Helpers, because ack is lazy. AND PROUD OF IT.
