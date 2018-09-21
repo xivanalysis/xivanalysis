8b8353710523b0024ef410c3249632cd9a5eed26
Saxon Landers <saxon@ackwell.com.au>
2018-09-21 16:58:59 +1000

    Allow trash fights to show up in the UI, added prelim zone data for alphascape

--- src/data/ZONES.js
+++ src/data/ZONES.js
@@ -91,6 +91,30 @@ const ZONES = {
 		banner: 'https://xivapi.com/i/112000/112272.png',
 	},
 
+	O9S: {
+		logId: 802,
+		dbId: 30073,
+		banner: 'https://xivapi.com/i/112000/112316.png',
+	},
+
+	O10S: {
+		logId: 803,
+		dbId: 30074,
+		banner: 'https://xivapi.com/i/112000/112317.png',
+	},
+
+	O11S: {
+		logId: 804,
+		dbId: 30075,
+		banner: 'https://xivapi.com/i/112000/112318.png',
+	},
+
+	O12S: {
+		logId: 805,
+		dbId: 30076,
+		banner: 'https://xivapi.com/i/112000/112319.png',
+	},
+
 	// Ultimate
 	UCOB: {
 		logId: 733,
