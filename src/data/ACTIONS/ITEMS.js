e588eb3f384bf5746604c21b2a8dc4fecca36406
suShirou <suShirou#3250>
2018-09-24 13:28:34 +0800

    g3 pots

--- src/data/ACTIONS/ITEMS.js
+++ src/data/ACTIONS/ITEMS.js
@@ -61,11 +61,42 @@ const ITEMS = {
 		name: 'Grade 2 Infusion of Mind',
 		icon: 'https://xivapi.com/i/020000/020705.png',
 	},
+
+	G3_INFUSION_STR: {
+		id: 22451,
+		name: 'Grade 3 Infusion of Strength',
+		icon: 'https://xivapi.com/i/020000/020701.png',
+	},
+
+	G3_INFUSION_DEX: {
+		id: 24261,
+		name: 'Grade 3 Infusion of Dexterity',
+		icon: 'https://xivapi.com/i/020000/020702.png',
+	},
+
+	G3_INFUSION_VIT: {
+		id: 24262,
+		name: 'Grade 3 Infusion of Vitality',
+		icon: 'https://xivapi.com/i/020000/020703.png',
+	},
+
+	G3_INFUSION_INT: {
+		id: 24264,
+		name: 'Grade 3 Infusion of Intelligence',
+		icon: 'https://xivapi.com/i/020000/020704.png',
+	},
+
+	G3_INFUSION_MND: {
+		id: 24265,
+		name: 'Grade 3 Infusion of Mind',
+		icon: 'https://xivapi.com/i/020000/020705.png',
+	},
+
 	SUPER_ETHER: {
-		id: 23168,
+		id: 23168 - 1000000, // what is consistency?
 		name: 'Super-Ether',
 		icon: 'https://xivapi.com/i/020000/020627.png',
-	}
+	},
 }
 
 // Items have an ID 1m higher than the xivdb ID
