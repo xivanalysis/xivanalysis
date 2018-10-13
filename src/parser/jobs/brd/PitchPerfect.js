a2be77a117fd42392a2480bb1431bbe4610a0783
Yumiya Nagatsuki <yumiyanagatsuki@bitbucket.org>
2018-10-13 04:07:00 -0300

    Finished Pitch Perfect module

--- src/parser/jobs/brd/PitchPerfect.js
+++ src/parser/jobs/brd/PitchPerfect.js
@@ -3,9 +3,9 @@
  */
 import Module from 'parser/core/Module'
 import ACTIONS, {getAction} from 'data/ACTIONS'
-//import {getStatus} from 'data/STATUSES'
 import STATUSES from 'data/STATUSES'
 import math from 'mathjsCustom'
+import {matchClosest} from 'utilities'
 
 // Relevant crit buffs
 const CRIT_MODIFIERS = [
@@ -48,27 +48,17 @@ export default class PitchPerfect extends Module {
 		'hitType', // eslint-disable-line xivanalysis/no-unused-dependencies
 	]
 
+	// Represents a map of IDs and statuses for each enemy in this parse
 	_enemies = {}
+
+	// Represents the player statuses
 	_player = {
-		critValues: [],
 		statuses: {},
-		get crit() { return 0 },
 		hasStatus(statusId) { return this.statuses[statusId] || false },
 	}
 
 	normalise(events) {
 
-		// tl;dr: 'K' is an approximation to damageToPotency ratio, ignoring the natural 5% spread because we don't need this kind of precision
-		const k = {
-			values: [],
-			// This method returns the mean of the data subset within {DEVIATION_PRECISION} standard deviations of the mean of the data set
-			get() {
-				const mean = math.mean(this.values)
-				const standardDeviation = math.std(this.values)
-
-				return math.mean(this.values.filter(v => v > mean - DEVIATION_PRECISION * standardDeviation && v < mean + DEVIATION_PRECISION * standardDeviation))
-			},
-		}
 		const damageInstances = []
 		const ppEvents = []
 
@@ -91,31 +81,31 @@ export default class PitchPerfect extends Module {
 				&& event.ability
 				&& !event.tick
 			) {
+				// Fixing the multiplier
+				let fixedMultiplier = event.debugMultiplier
+				if (
+					event.ability.guid !== ACTIONS.THE_WANDERERS_MINUET.id
+					&& event.ability.guid !== ACTIONS.MAGES_BALLAD.id
+					&& event.ability.guid !== ACTIONS.ARMYS_PAEON.id
+				) {
+					// Band-aid fix for disembowel (why, oh, why)
+					if (this._getEnemy(event.targetID).hasStatus(STATUSES.PIERCING_RESISTANCE_DOWN.id)) {
+						fixedMultiplier = Math.trunc((fixedMultiplier + DISEMBOWEL_STRENGTH) * 100) / 100
+					}
+					// AND ALSO FOR RANGED TRAIT, BECAUSE APPARENTLY IT'S PHYSICAL DAMAGE ONLY REEEEEEEEEE
+					fixedMultiplier = Math.trunc((fixedMultiplier + TRAIT_STRENGTH) * 100) / 100
+				}
 
 				// If it's a Pitch Perfect damage
 				if (event.ability.guid === ACTIONS.PITCH_PERFECT.id) {
 
-					ppEvents.push({event: event, rawDamage: event.amount / event.debugMultiplier})
+					ppEvents.push({event: event, rawDamage: event.amount / fixedMultiplier})
 
 				// Otherwise, if it doesn't have a conditional potency (Sidewinder and Pitch Perfect), it will be used to calculate 'K'
 				} else if (event.ability.guid !== ACTIONS.SIDEWINDER.id) {
 
 					// ...let's not count Spears for now
 					if (!this._player.hasStatus(STATUSES.THE_SPEAR.id)) {
-
-						let fixedMultiplier = event.debugMultiplier
-						if (
-							event.ability.guid !== ACTIONS.THE_WANDERERS_MINUET.id
-							&& event.ability.guid !== ACTIONS.MAGES_BALLAD.id
-							&& event.ability.guid !== ACTIONS.ARMYS_PAEON.id
-						) {
-							// Band-aid fix for disembowel (why, oh, why)
-							if (this._getEnemy(event.targetID).hasStatus(STATUSES.PIERCING_RESISTANCE_DOWN.id)) {
-								fixedMultiplier = Math.trunc((fixedMultiplier + DISEMBOWEL_STRENGTH) * 100) / 100
-							}
-							// AND ALSO FOR RANGED TRAIT, BECAUSE APPARENTLY IT'S PHYSICAL DAMAGE ONLY REEEEEEEEEE
-							fixedMultiplier = Math.trunc((fixedMultiplier + TRAIT_STRENGTH) * 100) / 100
-						}
 
 						const critTier = this._parseCritBuffs(event)
 
@@ -129,39 +119,38 @@ export default class PitchPerfect extends Module {
 			}
 		}
 
-		for (const prop of Object.keys(damageInstances)) {
-			for (const instance of damageInstances[prop]) {
+		// We use the damage events to determine 'K'
+		// tl;dr: 'K' is an approximation to damage to potency ratio, ignoring the natural 5% spread because we don't need this kind of precision
+		const k = this._getK(damageInstances)
 
-				// Let's not count auto attacks, because they have a different formula and aren't affected by the 20% trait
-				if (instance.event.ability.guid !== ACTIONS.SHOT.id) {
+		// We now use 'K' to guesstimate PP potency:
+		for (const pp of ppEvents) {
 
-					const skill = getAction(instance.event.ability.guid)
-					// We reverse calculate the base damage, before buffs/debuffs and crit/dhit modifiers
-					let rawDamage = instance.rawDamage
+			// We already have the unbuffed damage, we now need to strip PP off crit/dhit mods:
+			let rawDamage = pp.rawDamage
 
-					if (instance.event.criticalHit) {
-						rawDamage = Math.trunc(rawDamage / this._getCritMod(damageInstances))
-					}
-
-					if (instance.event.directHit) {
-						rawDamage = Math.trunc(rawDamage / DHIT_MOD)
-					}
-
-					if (skill.potency) {
-						k.values.push(Math.round(rawDamage * 100 / skill.potency))
-					}
-
-				}
+			if (pp.event.criticalHit) {
+				rawDamage = Math.trunc(rawDamage / this._getCritMod(damageInstances))
 			}
+
+			if (pp.event.directHit) {
+				rawDamage = Math.trunc(rawDamage / DHIT_MOD)
+			}
+
+			// We get the approximated potency and then match to the closest real potency
+			const approximatedPotency = rawDamage * 100 / k
+			const potency = matchClosest(ACTIONS.PITCH_PERFECT.potency, approximatedPotency)
+
+			// We then add the amount of stacks to the event
+			pp.event.stacks = ACTIONS.PITCH_PERFECT.potency.indexOf(potency) + 1
+
 		}
 
-		console.log(k.values)
-		console.log(k.get())
-		console.log(damageInstances)
-
+		// Return all this shit
 		return events
 	}
 
+	// Returns the enemy statuses state given the ID
 	_getEnemy(targetId) {
 		if (!this._enemies[targetId]) {
 			this._enemies[targetId] = {
@@ -173,7 +162,9 @@ export default class PitchPerfect extends Module {
 		return this._enemies[targetId]
 	}
 
+	// Returns the accumulated crit modifier from all the currently active crit buffs/debuffs
 	_parseCritBuffs(event) {
+		// We need to get the specific enemy in case it's Chain Stratagem
 		const enemy = this._getEnemy(event.targetID)
 		let accumulatedCritBuffs = 0
 
@@ -188,7 +179,6 @@ export default class PitchPerfect extends Module {
 			}
 
 			if (hasStatus) {
-
 				accumulatedCritBuffs += modifier.strenght
 			}
 
@@ -209,6 +199,7 @@ export default class PitchPerfect extends Module {
 			rates.push({rate: rate, amount: sampleSize})
 		}
 		const critRate = rates.reduce((acc, value) => acc + value.rate * value.amount, 0)/rates.reduce((acc, value) => acc + value.amount, 0)
+
 		// Time to guesstimate the critical hit rate attribute
 		const chr = (((critRate * 1000) - 50) * 2170/200) + 364
 
@@ -217,5 +208,46 @@ export default class PitchPerfect extends Module {
 
 	}
 	/* eslint-enable no-magic-numbers */
+
+	_getK(damageInstances) {
+		// 'K' is an approximation to damage to potency ratio
+		const values = []
+
+		// We iterate over all damage events, across all crit buff tiers
+		for (const critTier of Object.keys(damageInstances)) {
+			for (const instance of damageInstances[critTier]) {
+
+				// Let's not count auto attacks, because they have a different formula and aren't affected by the 20% trait
+				if (instance.event.ability.guid !== ACTIONS.SHOT.id) {
+
+					const skill = getAction(instance.event.ability.guid)
+
+					// We have already calculated the unbuffed damage, now we need to strip crit/dhit modifiers
+					let rawDamage = instance.rawDamage
+
+					if (instance.event.criticalHit) {
+						rawDamage = Math.trunc(rawDamage / this._getCritMod(damageInstances))
+					}
+
+					if (instance.event.directHit) {
+						rawDamage = Math.trunc(rawDamage / DHIT_MOD)
+					}
+
+					// If we have the potency information for the current skill, we add it's potency ratio to the array of potential 'K' values
+					if (skill.potency) {
+						values.push(Math.round(rawDamage * 100 / skill.potency))
+					}
+
+				}
+			}
+		}
+
+		// This method returns the mean of the data subset within {DEVIATION_PRECISION} standard deviations of the mean of the data set
+		const mean = math.mean(values)
+		const standardDeviation = math.std(values)
+
+		return math.mean(values.filter(v => v > mean - DEVIATION_PRECISION * standardDeviation && v < mean + DEVIATION_PRECISION * standardDeviation))
+
+	}
 
 }
--- src/parser/jobs/brd/index.js
+++ src/parser/jobs/brd/index.js
@@ -3,6 +3,7 @@ import Barrage from './Barrage'
 import DoTs from './DoTs'
 import OGCDDowntime from './OGCDDowntime'
 import PitchPerfect from './PitchPerfect'
+import PitchPerfectUsage from './PitchPerfectUsage'
 import SongUptime from './SongUptime'
 import StraightShot from './StraightShot'
 import Util from './Util'
@@ -14,6 +15,7 @@ export default [
 	DoTs,
 	OGCDDowntime,
 	PitchPerfect,
+	PitchPerfectUsage,
 	SongUptime,
 	StraightShot,
 	Util,
