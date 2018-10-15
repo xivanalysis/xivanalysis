b75ac2cefff7213daa8ac63de49a38dd44295651
ackwell <saxon@ackwell.com.au>
2018-10-16 09:28:47 +1100

    Fix sentry release config

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import ReactDOM from 'react-dom'
 import Root from './Root'
 
 // If we're in prod, boot up sentry
-const {NODE_ENV, VERSION, REACT_APP_RAVEN_DSN} = process.env
+const {NODE_ENV, REACT_APP_VERSION, REACT_APP_RAVEN_DSN} = process.env
 if (NODE_ENV === 'production' && REACT_APP_RAVEN_DSN) {
 	Raven.config(REACT_APP_RAVEN_DSN, {
 		environment: NODE_ENV,
-		release: VERSION,
+		release: REACT_APP_VERSION,
 
 		whitelistUrls: [
 			// All our project's JavaScript should be loaded from /static/
