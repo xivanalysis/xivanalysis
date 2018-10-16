a510065933296624c8f09dc37c3ea09c2e731e16
Alex Levine <lord.xela@gmail.com>
2018-10-16 14:08:03 +0100

    Reworked MCH to use the new module format

--- src/parser/jobs/mch/index.js
+++ src/parser/jobs/mch/index.js
@@ -1,18 +1,26 @@
-import About from './About'
-import Ammo from './Ammo'
-import Cooldowns from './Cooldowns'
-import Heat from './Heat'
-import HotShot from './HotShot'
-import Procs from './Procs'
-import Turrets from './Turrets'
-import Wildfire from './Wildfire'
-export default [
-	About,
-	Ammo,
-	Cooldowns,
-	Heat,
-	HotShot,
-	Procs,
-	Turrets,
-	Wildfire,
-]
+import {Trans} from '@lingui/react'
+import React from 'react'
+import {Icon, Message} from 'semantic-ui-react'
+
+import CONTRIBUTORS, {ROLES} from 'data/CONTRIBUTORS'
+
+export default {
+	modules: () => import('./modules' /* webpackChunkName: "jobs-mch" */),
+	description: <>
+		<Trans id="mch.about.description"><p>This module aims to help you improve your MCH gameplay by analyzing things that are difficult to spot in raw logs and pointing out ways to tighten up your rotation, better manage procs, and get the most out of your Wildfire burst windows.</p></Trans>
+		<Message warning icon>
+			<Icon name="warning sign"/>
+			<Message.Content>
+				<Trans id="mch.about.description.warning"><strong>The module is a work in progress.</strong> It currently covers very few aspects of MCH gameplay, and while the existing features below should be reasonably accurate, this system <em>is</em> still in development, and may get a little mixed up sometimes. If you notice any issues or have any concerns/feature requests, please drop by our Discord channel or report a bug on our github repository!</Trans>
+			</Message.Content>
+		</Message>
+	</>,
+	supportedPatches: {
+		from: '4.2',
+		to: '4.4',
+	},
+	contributors: [
+		{user: CONTRIBUTORS.TOASTDEIB, role: ROLES.MAINTAINER},
+	],
+	changelog: [],
+}
