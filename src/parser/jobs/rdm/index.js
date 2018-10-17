a3ea1c1c88a7ca7a1d77a3b3a7d7e434a924a746
Cephid <Kaiser12@gmail.com>
2018-10-16 22:45:46 -0500

    Migrated RDM to new Modules setup

--- src/parser/jobs/rdm/index.js
+++ src/parser/jobs/rdm/index.js
@@ -1,15 +1,37 @@
-import About from './About'
-import Gauge from './Gauge'
-import Dualcast from './Dualcast'
-import GeneralCDDowntime from './GeneralCDDowntime'
-import Procs from './Procs'
-import Combos from './Combos'
+import React, {Fragment} from 'react'
+import {Icon, Message} from 'semantic-ui-react'
+import CONTRIBUTORS, {ROLES} from 'data/CONTRIBUTORS'
+import ACTIONS from 'data/ACTIONS'
+import {ActionLink} from 'components/ui/DbLink'
+import TransMarkdown from 'components/ui/TransMarkdown'
+import {Trans, i18nMark} from '@lingui/react'
 
-export default [
-	About,
-	Gauge,
-	Dualcast,
-	GeneralCDDowntime,
-	Procs,
-	Combos,
-]
+const description = <Fragment>
+	<p>This analyzer aims to give you the information you need to turn your <span className="text-success">parses</span> into <span className="text-orange">parses</span></p>
+	<p>If you would like to learn more about RDM, check the guides over at <a href="https://thebalanceffxiv.com/">The Balance</a>, and have a chat in the <code>#rdm_questions</code> channel.</p>
+
+</Fragment>
+
+export default {
+	modules: () => import('./modules' /* webpackChunkName: "jobs-rdm" */),
+
+	description: <>
+		<TransMarkdown id={i18nMark('rdm.about.description')} source={description} />
+		<Message warning icon>
+			<Icon name="warning sign"/>
+			<Message.Content>
+				<Trans id="rdm.about.description.warning">Openers, advanced <ActionLink {...ACTIONS.CORPS_A_CORPS}/>, <ActionLink {...ACTIONS.DISPLACEMENT}/>, and <ActionLink {...ACTIONS.MANAFICATION}/> rules are currently not supported at this time.</Trans>
+			</Message.Content>
+		</Message>
+</>,
+	supportedPatches: {
+		from: '4.2',
+		to: '4.4',
+	},
+	contributors: [
+		{user: CONTRIBUTORS.LEYLIA, role: ROLES.MAINTAINER},
+		{user: CONTRIBUTORS.JUMP, role: ROLES.THEORYCRAFT},
+	],
+
+	changelog: [],
+}
