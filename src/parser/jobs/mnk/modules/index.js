752e5ced40e9b46b27470c0239717bdd3f505876
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2018-10-20 23:06:14 +1100

    Move MNK modules to new directory structure

---
+++ src/parser/jobs/mnk/modules/index.js
@@ -0,0 +1,21 @@
+import BuffUptime from './BuffUptime'
+import Demolish from './Demolish'
+import Fists from './Fists'
+import Forms from './Forms'
+import GreasedLightning from './GreasedLightning'
+import InternalRelease from './InternalRelease'
+import MnkAoE from './MnkAoE'
+import RiddleOfFire from './RiddleOfFire'
+import Speedmod from './Speedmod'
+
+export default [
+	BuffUptime,
+	Demolish,
+	Fists,
+	Forms,
+	GreasedLightning,
+	InternalRelease,
+	MnkAoE,
+	RiddleOfFire,
+	Speedmod,
+]
