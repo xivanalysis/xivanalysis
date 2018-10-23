7ab80b89d3d696caf338b23edf0d3b9609f7c5d5
Saxon Landers <saxon@ackwell.com.au>
2018-10-23 23:49:02 +1100

    Cleanup

--- src/parser/AVAILABLE_MODULES.js
+++ src/parser/AVAILABLE_MODULES.js
@@ -2,20 +2,21 @@ import BOSSES from 'data/BOSSES'
 import JOBS from 'data/JOBS'
 
 import CORE from './core'
-import MNK from './jobs/mnk'
-import NIN from './jobs/nin'
-import SMN from './jobs/smn'
-import WAR from './jobs/war'
-import MCH from './jobs/mch'
-import DRG from './jobs/drg'
-import RDM from './jobs/rdm'
+
 import AST from './jobs/ast'
 import BLM from './jobs/blm'
 import BRD from './jobs/brd'
+import DRG from './jobs/drg'
 import DRK from './jobs/drk'
+import MCH from './jobs/mch'
+import MNK from './jobs/mnk'
+import NIN from './jobs/nin'
 import PLD from './jobs/pld'
+import RDM from './jobs/rdm'
 import SAM from './jobs/sam'
 import SCH from './jobs/sch'
+import SMN from './jobs/smn'
+import WAR from './jobs/war'
 import WHM from './jobs/whm'
 
 import BAHAMUT_PRIME from './bosses/bahamutPrime'
@@ -25,21 +26,25 @@ export default {
 	CORE,
 
 	JOBS: {
-		[JOBS.MONK.logType]: MNK,
-		[JOBS.NINJA.logType]: NIN,
-		[JOBS.SUMMONER.logType]: SMN,
-		[JOBS.BLACK_MAGE.logType]: BLM,
-		[JOBS.RED_MAGE.logType]: RDM,
-		[JOBS.WARRIOR.logType]: WAR,
-		[JOBS.WHITE_MAGE.logType]: WHM,
 		[JOBS.PALADIN.logType]: PLD,
-		[JOBS.SCHOLAR.logType]: SCH,
-		[JOBS.BARD.logType]: BRD,
-		[JOBS.SAMURAI.logType]: SAM,
-		[JOBS.ASTROLOGIAN.logType]: AST,
+		[JOBS.WARRIOR.logType]: WAR,
 		[JOBS.DARK_KNIGHT.logType]: DRK,
-		[JOBS.MACHINIST.logType]: MCH,
+
+		[JOBS.WHITE_MAGE.logType]: WHM,
+		[JOBS.SCHOLAR.logType]: SCH,
+		[JOBS.ASTROLOGIAN.logType]: AST,
+
+		[JOBS.MONK.logType]: MNK,
 		[JOBS.DRAGOON.logType]: DRG,
+		[JOBS.NINJA.logType]: NIN,
+		[JOBS.SAMURAI.logType]: SAM,
+
+		[JOBS.BARD.logType]: BRD,
+		[JOBS.MACHINIST.logType]: MCH,
+
+		[JOBS.BLACK_MAGE.logType]: BLM,
+		[JOBS.SUMMONER.logType]: SMN,
+		[JOBS.RED_MAGE.logType]: RDM,
 	},
 
 	BOSSES: {
