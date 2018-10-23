8c98833cd27bfec26529d33f37b9cdcf8e48b385
Saxon Landers <saxon@ackwell.com.au>
2018-10-23 22:20:07 +1100

    Just gonna squeeeeze in here with this soz sky

--- src/parser/core/modules/ChangeLog.js
+++ src/parser/core/modules/ChangeLog.js
@@ -1,52 +1,55 @@
 import {i18nMark} from '@lingui/react'
-import React, {Fragment} from 'react'
-import {Header, Segment} from 'semantic-ui-react'
+import React from 'react'
+import {Segment, Popup} from 'semantic-ui-react'
 
-import ContributorLabel from 'components/ui/ContributorLabel'
 import Module, {DISPLAY_ORDER} from 'parser/core/Module'
+import ContributorLabel from 'components/ui/ContributorLabel'
 
 import styles from './ChangeLog.module.css'
 
-// TODO: Make it work with Core changelog.
-let _changeLog = []
-
 export default class ChangeLog extends Module {
-
 	static handle = 'changelog'
 	static displayOrder = DISPLAY_ORDER.BOTTOM
 
 	static title = 'Changelog'
 	static i18n_id = i18nMark('core.changelog.title')
 
-	constructor(...args) {
-		super(...args)
-
-		_changeLog = [...this.parser.meta.changelog]
-	}
-
 	output() {
-		if (_changeLog.length === 0) {
+		const {changelog} = this.parser.meta
+
+		if (changelog.length === 0) {
 			return false
 		}
 
 		return <div>
-			{_changeLog.map((item, key) => {
+			{changelog.map((item, index) => {
 				// Fixes the issue with the 'attach' property. Basically, if it's the first one it's just gonna attach to the top, if not, it'll attach to the segment/header above it.
-				const attach = (key === 0) ? 'top' : true
+				let attach = {
+					0: 'top',
+					[changelog.length - 1]: 'bottom',
+				}[index] || true
+				if (changelog.length === 1) {
+					attach = false
+				}
 
-				return <Fragment key={key}>
-					<Header as="h5" attached={attach}>
-						{item.date.toLocaleDateString()}
-						<span className={styles.contributor}>{item.contributors.map(contributor => {
-							return <div key={typeof contributor === 'string' ? contributor : contributor.name}>
-								<ContributorLabel contributor={contributor} />
-							</div>
-						})}</span>
-					</Header>
-					<Segment attached className={styles.changes}>
-						{item.changes}
-					</Segment>
-				</Fragment>
+				const dateString = item.date.toLocaleDateString()
+
+				const change = <Segment attached={attach} className={styles.change}>
+					<strong className={styles.date}>{dateString}</strong>
+					<span className={styles.message}>{item.changes}</span>
+					<div className={styles.contributors}>
+						{item.contributors.map(contributor => <ContributorLabel key={contributor.name} contributor={contributor} />)}
+					</div>
+				</Segment>
+
+				// Popup in case someone made the commit message too wide
+				return <Popup
+					key={index}
+					trigger={change}
+					header={dateString}
+					content={item.changes}
+					position="top center"
+				/>
 			})}
 		</div>
 	}
