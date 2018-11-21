fc081c016875d09421a8bbacf003dcfc60c1edd4
Saxon Landers <saxon@ackwell.com.au>
2018-11-21 23:25:47 +1100

    Tweaks~

--- src/parser/jobs/smn/index.js
+++ src/parser/jobs/smn/index.js
@@ -2,8 +2,11 @@ import {Trans, i18nMark} from '@lingui/react'
 import React from 'react'
 import {Icon, Message} from 'semantic-ui-react'
 
+import {ActionLink, StatusLink} from 'components/ui/DbLink'
 import TransMarkdown from 'components/ui/TransMarkdown'
+import ACTIONS from 'data/ACTIONS'
 import CONTRIBUTORS, {ROLES} from 'data/CONTRIBUTORS'
+import STATUSES from 'data/STATUSES'
 
 const description = `
 This analyser aims to identify some of the low-hanging fruit that could be used to improve your SMN gameplay, as well as give a deeper insight into what happened during an encounter.
@@ -35,5 +38,15 @@ export default {
 		{user: CONTRIBUTORS.FRYTE, role: ROLES.THEORYCRAFT},
 	],
 
-	changelog: [],
+	changelog: [{
+		date: new Date('2018-11-21'),
+		changes: <>
+			A few small tweaks and adjustments:&nbsp;
+			<ul>
+				<li>Prevented <ActionLink {...ACTIONS.DEATHFLARE}/>s lost due to death from counting towards lost-deathflare suggestion.</li>
+				<li>Adjusted <StatusLink {...STATUSES.SHADOW_FLARE}/> uptime calculation to  exclude time before the first cast.</li>
+			</ul>
+		</>,
+		contributors: [CONTRIBUTORS.ACKWELL],
+	}],
 }
