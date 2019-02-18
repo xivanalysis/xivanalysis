9e223b0889a191cc7b1a26dd4b941aacede0be02
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2019-02-18 17:56:04 +1100

    Ignore some magic on RDM

--- src/parser/jobs/rdm/modules/GeneralCDDowntime.js
+++ src/parser/jobs/rdm/modules/GeneralCDDowntime.js
@@ -3,6 +3,7 @@ import CooldownDowntime from 'parser/core/modules/CooldownDowntime'
 
 export default class GeneralCDDowntime extends CooldownDowntime {
 	//Time that Jump deems ok for a OGCD to be down : ^)
+	// eslint-disable-next-line no-magic-numbers
 	allowedDowntime = 4000
 	trackedCds = [
 		ACTIONS.FLECHE.id,
