1322df71a6c911c2c3467d803a0597065fcae6fe
Saxon Landers <saxon@ackwell.com.au>
2019-04-15 23:11:13 +1000

    Extremely early stage analyser [skip ci]

---
+++ src/analyser/Analyser.ts
@@ -0,0 +1,33 @@
+import {Actor, Events} from '@xivanalysis/parser-core'
+
+// TODO: should this be in the parser?
+const isAddActor = (event: Events.Base): event is Events.AddActor =>
+	event.type === Events.Type.ADD_ACTOR
+
+const generateActorFinder = (id: Actor['id']) =>
+	(event: Events.Base): event is Events.AddActor =>
+		isAddActor(event) && event.actor.id === id
+
+export class Analyser {
+	private readonly events: Events.Base[]
+	private readonly actor: Actor
+
+	constructor(opts: {
+		events: Events.Base[],
+		actorId: Actor['id'],
+	}) {
+		this.events = opts.events
+
+		// Find the AddActor event for the provided actor
+		// If they aren't added, we can't analyse them, so throw
+		const event = opts.events.find(generateActorFinder(opts.actorId))
+		if (!event) {
+			// TODO: Proper error
+			throw new Error('Could not find actor matching the ID specified.')
+		}
+		this.actor = event.actor
+
+		// TODO: HOW DO I SIGNAL WHAT BOSS IT IS?
+		//       Should I split "boss" modules by zone instead?
+	}
+}
