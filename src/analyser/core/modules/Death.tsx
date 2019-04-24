9987840fbd6228db479fd8d71638031ece71b107
Saxon Landers <saxon@ackwell.com.au>
2019-04-24 23:45:44 +1000

    Add WIP implementation of death module

---
+++ src/analyser/core/modules/Death.tsx
@@ -0,0 +1,64 @@
+import {Plural, Trans} from '@lingui/react'
+import {Events} from '@xivanalysis/parser-core'
+import {EventTypes} from 'analyser/Analyser'
+import {dependency} from 'analyser/dependency'
+import {Module} from 'analyser/Module'
+import ACTIONS from 'data/ACTIONS'
+import React from 'react'
+import {Severity, Suggestion, Suggestions} from './Suggestions'
+
+export class Death extends Module {
+	static handle = 'death'
+	@dependency suggestions!: Suggestions
+
+	private count = 0
+	// private deadTime = 0
+	// private timestamp?: number
+
+	protected init() {
+		this.addHook(Events.Type.DEATH, {targetId: this.analyser.actor.id}, this.onDeath)
+		// TODO: Hook end of death (raise, prepare, etc)
+		this.addHook(EventTypes.COMPLETE, this.onComplete)
+	}
+
+	private onDeath(event: Events.Death) {
+		if (!this.shouldCountDeath()) { return }
+
+		this.count++
+		// this.timestamp = event.timestamp
+	}
+
+	private onComplete() {
+		// TODO: Timeline :nauseated_face:
+
+		// TODO: Refund a death on wipes
+		// Will need to consider how the _fuck_ i'm gonna mark kills/wipes
+
+		if (this.count <= 0) {
+			return
+		}
+
+		this.suggestions.add(new Suggestion({
+			icon: ACTIONS.RAISE.icon,
+			content: <Trans id="core.deaths.content">
+				Don't die. Between downtime, lost gauge resources, and resurrection debuffs, dying is absolutely <em>crippling</em> to damage output.
+			</Trans>,
+			severity: Severity.MORBID,
+			why: <Plural
+				id="core.deaths.why"
+				value={this.count}
+				_1="# death"
+				other="# deaths"
+			/>,
+		}))
+	}
+
+	/**
+	 * If a death at the time of calling should be considered the player's fault.
+	 * Fights that force a player death (*cough*ucob*cough*) should return `false`
+	 * from this function at the time of the forced death.
+	 */
+	protected shouldCountDeath() {
+		return true
+	}
+}
--- src/analyser/core/modules/index.ts
+++ src/analyser/core/modules/index.ts
@@ -1,9 +1,11 @@
 import {Actors} from './Actors'
+import {Death} from './Death'
 import {EventView} from './EventView'
 import {Suggestions} from './Suggestions'
 
 export default [
 	Actors,
+	Death,
 	EventView,
 	Suggestions,
 ]
