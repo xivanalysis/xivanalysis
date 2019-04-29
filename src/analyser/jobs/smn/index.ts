103507143c6e6ab7363e51b4715dc4323b68ec52
Saxon Landers <saxon@ackwell.com.au>
2019-04-29 21:44:40 +1000

    Fix derp

--- src/analyser/jobs/smn/index.ts
+++ src/analyser/jobs/smn/index.ts
@@ -1,5 +1,5 @@
 import {Meta} from 'analyser/Meta'
 
 export default new Meta({
-	modules: () => import('./modules' /* webpackChunkName: "jobs-smn"" */),
+	modules: () => import('./modules' /* webpackChunkName: "jobs-smn" */),
 })
