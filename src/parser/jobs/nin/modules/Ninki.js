7dedda72fcdb18dae6f09357e5f7a56ad8527cb9
Toastdeib <lord.xela@gmail.com>
2019-06-16 16:23:26 -0400

    Initial pass at ShB changes

--- src/parser/jobs/nin/modules/Ninki.js
+++ src/parser/jobs/nin/modules/Ninki.js
@@ -5,7 +5,7 @@ import React, {Fragment} from 'react'
 import {ActionLink} from 'components/ui/DbLink'
 import ACTIONS from 'data/ACTIONS'
 import Module, {DISPLAY_MODE} from 'parser/core/Module'
-import {TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
+import {Suggestion, TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
 import Color from 'color'
 import JOBS from 'data/JOBS'
 import TimeLineChart from 'components/ui/TimeLineChart'
@@ -14,14 +14,22 @@ import TimeLineChart from 'components/ui/TimeLineChart'
 const MAX_NINKI = 100
 
 const NINKI_BUILDERS = {
-	[ACTIONS.MUG.id]: 30,
-	[ACTIONS.ATTACK.id]: 6,
+	[ACTIONS.MUG.id]: 40,
+	[ACTIONS.MEISUI.id]: 40,
+	[ACTIONS.SPINNING_EDGE.id]: 8,
+	[ACTIONS.GUST_SLASH.id]: 8,
+	[ACTIONS.AEOLIAN_EDGE.id]: 8,
+	[ACTIONS.SHADOW_FANG.id]: 8,
+	[ACTIONS.ARMOR_CRUSH.id]: 8,
+	[ACTIONS.DEATH_BLOSSOM.id]: 8, // TODO - Confirm whether AoE weaponskills count
+	[ACTIONS.HAKKE_MUJINSATSU.id]: 8,
+	[ACTIONS.THROWING_DAGGER.id]: 8,
 }
 
 const NINKI_SPENDERS = {
 	[ACTIONS.HELLFROG_MEDIUM.id]: 80,
 	[ACTIONS.BHAVACAKRA.id]: 80,
-	[ACTIONS.TEN_CHI_JIN.id]: 80,
+	[ACTIONS.BUNSHIN.id]: 80,
 }
 
 export default class Ninki extends Module {
@@ -29,7 +37,6 @@ export default class Ninki extends Module {
 	static title = t('nin.ninki.title')`Ninki Timeline`
 	static displayMode = DISPLAY_MODE.FULL
 	static dependencies = [
-		'cooldowns',
 		'suggestions',
 	]
 
@@ -37,13 +44,21 @@ export default class Ninki extends Module {
 	_ninkiHistory = []
 	_wasteBySource = {
 		[ACTIONS.MUG.id]: 0,
-		[ACTIONS.ATTACK.id]: 0,
+		[ACTIONS.MEISUI.id]: 0,
+		[ACTIONS.SPINNING_EDGE.id]: 0,
+		[ACTIONS.GUST_SLASH.id]: 0,
+		[ACTIONS.AEOLIAN_EDGE.id]: 0,
+		[ACTIONS.SHADOW_FANG.id]: 0,
+		[ACTIONS.ARMOR_CRUSH.id]: 0,
+		[ACTIONS.DEATH_BLOSSOM.id]: 0, // TODO - Confirm whether AoE weaponskills count
+		[ACTIONS.HAKKE_MUJINSATSU.id]: 0,
+		[ACTIONS.THROWING_DAGGER.id]: 0,
 	}
 	_erroneousFrogs = 0 // This is my new band name
 
 	constructor(...args) {
 		super(...args)
-		this.addHook('cast', {by: 'player', abilityId: Object.keys(this._wasteBySource).map(Number)}, this._onBuilderCast)
+		this.addHook('cast', {by: 'player', abilityId: Object.keys(NINKI_BUILDERS).map(Number)}, this._onBuilderCast)
 		this.addHook('cast', {by: 'player', abilityId: Object.keys(NINKI_SPENDERS).map(Number)}, this._onSpenderCast)
 		this.addHook('aoedamage', {by: 'player', abilityId: ACTIONS.HELLFROG_MEDIUM.id}, this._onHellfrogAoe)
 		this.addHook('death', {to: 'player'}, this._onDeath)
@@ -69,10 +84,8 @@ export default class Ninki extends Module {
 	}
 
 	_onHellfrogAoe(event) {
-		if (event.hits.length === 1 &&
-			(this.cooldowns.getCooldownRemaining(ACTIONS.BHAVACAKRA.id) <= 0 ||
-			this.cooldowns.getCooldownRemaining(ACTIONS.TEN_CHI_JIN.id) <= 0)) {
-			// If we have an Hellfrog AoE event with only one target while Bhava and/or TCJ are available, it was probably a bad life choice
+		if (event.hits.length === 1) {
+			// If we have a Hellfrog AoE event with only one target, it should've been a Bhava instead
 			this._erroneousFrogs++
 		}
 	}
@@ -89,11 +102,11 @@ export default class Ninki extends Module {
 	}
 
 	_onComplete() {
-		const totalWaste = this._wasteBySource[ACTIONS.MUG.id] + this._wasteBySource[ACTIONS.ATTACK.id]
+		const totalWaste = Object.values(this._wasteBySource).reduce((reducer, value) => reducer + value)
 		this.suggestions.add(new TieredSuggestion({
 			icon: 'https://xivapi.com/i/005000/005411.png',
 			content: <Trans id="nin.ninki.suggestions.waste.content">
-				Avoid using <ActionLink {...ACTIONS.MUG}/> when above 60 Ninki and holding your Ninki spenders when near or at cap (with a few small exceptions) in order to maximize the number of spenders you can use over the course of a fight.
+				Avoid using <ActionLink {...ACTIONS.MUG}/> and <ActionLink {...ACTIONS.MEISUI}/> when above 50 Ninki and holding your Ninki spenders when near or at cap (with a few small exceptions) in order to maximize the number of spenders you can use over the course of a fight.
 			</Trans>,
 			tiers: {
 				30: SEVERITY.MINOR,
@@ -105,16 +118,12 @@ export default class Ninki extends Module {
 			</Trans>,
 		}))
 
-		this.suggestions.add(new TieredSuggestion({
+		this.suggestions.add(new Suggestion({
 			icon: ACTIONS.HELLFROG_MEDIUM.icon,
 			content: <Trans id="nin.ninki.suggestions.frog.content">
-				Avoid using <ActionLink {...ACTIONS.HELLFROG_MEDIUM}/> when you have one of your other spenders available (unless there are multiple targets), as it has the lowest potency of the three by a significant margin when used on only one.
+				Avoid using <ActionLink {...ACTIONS.HELLFROG_MEDIUM}/> when you only have one target, as <ActionLink {...ACTIONS.BHAVACAKRA}/> has higher potency and can be used freely.
 			</Trans>,
-			tiers: {
-				1: SEVERITY.MINOR,
-				3: SEVERITY.MEDIUM,
-			},
-			value: this._erroneousFrogs,
+			severity: SEVERITY.MEDIUM,
 			why: <Trans id="nin.ninki.suggestions.frog.why">
 				You used Hellfrog Medium <Plural value={this._erroneousFrogs} one="# time" other="# times"/> when other spenders were available.
 			</Trans>,
