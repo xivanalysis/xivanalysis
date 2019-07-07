4314daa305c969454aa4c8d6488de29a4aba7241
Rachel Forshee <rachel.i.forshee@gmail.com>
2019-07-07 14:13:07 -0700

    First pass at GNB support modules.

--- src/parser/jobs/gnb/index.tsx
+++ src/parser/jobs/gnb/index.tsx
@@ -1,24 +1,36 @@
 import {t} from '@lingui/macro'
+import React from 'react'
+
 import TransMarkdown from 'components/ui/TransMarkdown'
 import CONTRIBUTORS, {ROLES} from 'data/CONTRIBUTORS'
 import {Meta} from 'parser/core/Meta'
 import {changelog} from './changelog'
 
-const description = t('gnb.about.description')`
-You can write the description for the job here. It supports a superset of
-[markdown](https://commonmark.org/).
+const description = t('gnb.about.description')`This analyzer looks for the low-hanging, easy to spot issues in your gameplay that can be fixed to improve your damage across a fight as Gunbreaker.
+If you're looking to learn about how exactly the job plays and functions from the ground up, take a look at a few basic guides:
+
+* [General tanking guide by Aletin](https://goo.gl/nYzAnq)
+* [Gunbreaker guide by Aletin](https://docs.google.com/document/d/1gfsMbHP55N5e0UKyc9zfpAdL_uVco40YdBlVNGvyJ0k/edit?usp=sharing)
+
+Known issues:
+
+* Uncomboed Solid Barrel will count as loading an additional Cartridge.
+
+If you have any suggestions about the module, feel free to ping me on Discord at Lhea#7581.
 `
 
 export default new Meta({
 	modules: () => import('./modules' /* webpackChunkName: "jobs-gnb" */),
 
 	Description: () => <TransMarkdown source={description}/>,
+
 	// supportedPatches: {
-	// 	from: '✖',
-	// 	to: '✖',
-	// }
+	// 	from: '5.0',
+	// 	to: '5.0',
+	// },
+
 	contributors: [
-		// {user: CONTRIBUTORS.YOU, role: ROLES.MAINTAINER},
+		{user: CONTRIBUTORS.LHEA, role: ROLES.DEVELOPER},
 	],
 
 	changelog,
