98e5507af69aa39650d277b6bf7099fdce7beae4
Kevin Kragenbrink <kevin.kragenbrink@crowdstrike.com>
2019-07-09 08:14:39 -0700

    simplifying code for dia tracking, now that there is only one dot

--- src/parser/jobs/whm/modules/DoTs.js
+++ src/parser/jobs/whm/modules/DoTs.js
@@ -5,24 +5,26 @@ import ACTIONS from 'data/ACTIONS'
 import STATUSES from 'data/STATUSES'
 import Module from 'parser/core/Module'
 import {TieredRule, TARGET, Requirement} from 'parser/core/modules/Checklist'
-import {Suggestion, SEVERITY} from 'parser/core/modules/Suggestions'
+import {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
 import {Trans} from '@lingui/react'
 
-const STATUS_DURATION = {
-	[STATUSES.DIA.id]: 30000,
+const STATUS_DURATION = 30000
+
+const RULE_TIERS = {
+	90: TARGET.WARN,
+	95: TARGET.SUCCESS,
 }
 
-const CLIP_MAX_MINOR = {
-	[STATUSES.DIA.id]: 9000, // less than 3 gcds wasted
-}
-const CLIP_MAX_MEDIUM = {
-	[STATUSES.DIA.id]: 30000,
+const SUGGESTION_TIERS = {
+	500: SEVERITY.MINOR,
+	9000: SEVERITY.MEDIUM,
+	30000: SEVERITY.MAJOR,
 }
 
 const DOT_CLIPPING_THRESHOLD = 500 // ms of dot clipping to warrant a suggestion
 
 export default class DoTs extends Module {
-    static handle = 'dots'
+	static handle = 'dots'
 	static dependencies = [
 		'checklist',
 		'enemies',
@@ -31,9 +33,7 @@ export default class DoTs extends Module {
 	]
 
 	_lastApplication = {}
-	_clip = {
-		[STATUSES.DIA.id]: 0,
-	}
+	_clip = 0
 
 	constructor(...args) {
 		super(...args)
@@ -47,29 +47,27 @@ export default class DoTs extends Module {
 	}
 
 	_onDotApply(event) {
-		const statusId = event.ability.guid
-
 		// Make sure we're tracking for this target
 		const applicationKey = `${event.targetID}|${event.targetInstance}`
-		const lastApplication = this._lastApplication[applicationKey] = this._lastApplication[applicationKey] || {}
+		let lastApplication = this._lastApplication[applicationKey] = this._lastApplication[applicationKey] || 0
 
 		// If it's not been applied yet, or we're rushing, set it and skip out
-		if (!lastApplication[statusId]) {
-			lastApplication[statusId] = event.timestamp
+		if (!lastApplication) {
+			lastApplication = event.timestamp
 			return
 		}
 
 		// Base clip calc
-		let clip = STATUS_DURATION[statusId] - (event.timestamp - lastApplication[statusId])
+		let clip = STATUS_DURATION - (event.timestamp - lastApplication)
 
 		// Also remove invuln time in the future that casting later would just push dots into
 		// TODO: This relies on a full set of invuln data ahead of time. Can this be trusted?
-		clip -= this.invuln.getInvulnerableUptime('all', event.timestamp, event.timestamp + STATUS_DURATION[statusId] + clip)
+		clip -= this.invuln.getInvulnerableUptime('all', event.timestamp, event.timestamp + STATUS_DURATION + clip)
 
 		// Capping clip at 0 - less than that is downtime, which is handled by the checklist requirement
-		this._clip[statusId] += Math.max(0, clip)
+		this._clip += Math.max(0, clip)
 
-		lastApplication[statusId] = event.timestamp
+		lastApplication = event.timestamp
 	}
 
 	_onComplete() {
@@ -77,9 +75,9 @@ export default class DoTs extends Module {
 		this.checklist.add(new TieredRule({
 			name: <Trans id="whm.dots.rule.name"> Keep your DoTs up </Trans>,
 			description: <Trans id="whm.dots.rule.description">
-				As a White Mage, Dia is significant portion of your sustained damage. Aim to keep them it at all times.
+				As a White Mage, Dia is significant portion of your sustained damage. Aim to keep them it up at all times.
 			</Trans>,
-			tiers: {90: TARGET.WARN, 95: TARGET.SUCCESS},
+			tiers: RULE_TIERS,
 			requirements: [
 				new Requirement({
 					name: <Trans id="whm.dots.requirement.uptime-dia.name"><ActionLink {...ACTIONS.DIA} /> uptime</Trans>,
@@ -89,17 +87,15 @@ export default class DoTs extends Module {
 		}))
 
 		//Suggestion for DoT clipping
-		if (this._clip[STATUSES.DIA.id] > DOT_CLIPPING_THRESHOLD) {
-			const isMinor = this._clip[STATUSES.DIA.id] <= CLIP_MAX_MINOR[STATUSES.DIA.id]
-			const isMedium = this._clip[STATUSES.DIA.id] <= CLIP_MAX_MEDIUM[STATUSES.DIA.id]
-			this.suggestions.add(new Suggestion({
+		if (this._clip > DOT_CLIPPING_THRESHOLD) {
+			this.suggestions.add(new TieredSuggestion({
 				icon: ACTIONS.DIA.icon,
 				content: <Trans id="whm.dots.suggestion.clip-dia.content">
-					Avoid refreshing DoTs significantly before their expiration, this will allow you to cast more Glare. (Note: We do not yet consider using Dia for initial damage on the move)
+					Avoid refreshing Dia significantly before its expiration, this will allow you to cast more Glare.
 				</Trans>,
-				severity: isMinor ? SEVERITY.MINOR : isMedium ? SEVERITY.MEDIUM : SEVERITY.MAJOR,
+				tiers: SUGGESTION_TIERS,
 				why: <Trans id="whm.dots.suggestion.clip-dia.why">
-					{this.parser.formatDuration(this._clip[STATUSES.DIA.id])} of {STATUSES.DIA.name} lost to early refreshes.
+					{this.parser.formatDuration(this._clip)} of {STATUSES.DIA.name} lost to early refreshes.
 				</Trans>,
 			}))
 		}
