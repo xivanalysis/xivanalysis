e18e9dfc26f0679e9402001de8825c43f45d27e4
suShirou <suShirow>
2019-07-16 12:24:30 +0800

    Revert "Removed RR from add stats"
    This reverts commit cce6980a88d2161672ed65c3a44b95b5056c42b9.

--- src/parser/jobs/brd/modules/AdditionalStats.js
+++ src/parser/jobs/brd/modules/AdditionalStats.js
@@ -22,6 +22,11 @@ const CRIT_MODIFIERS = [
 		strength: 0.02,
 	},
 	{
+		id: STATUSES.THE_SPEAR.id,
+		//fuck royal road
+		strength: 0.10,
+	},
+	{
 		id: STATUSES.STRAIGHT_SHOT.id,
 		strength: 0.10,
 	},
