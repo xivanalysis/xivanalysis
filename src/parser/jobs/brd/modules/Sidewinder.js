270d69798617f3a129f5bdbde18e50f594cd624b
Ririan <52671181+Ririan@users.noreply.github.com>
2019-07-21 07:28:31 -0500

    Add Sidewinder Module
    This sidewinder module checks for casts of Sidewinder and Shadowbite on targets without both DoTs applied and warns the user through a suggestion, and generates a list with timestamps of when either Sidewinder or Shadowbite was cast, and with what DoT (or no DoT) it was cast on.

---
+++ src/parser/jobs/brd/modules/Sidewinder.js
@@ -0,0 +1,179 @@
+/**
+ * @author Ririan
+ */
+import React from 'react'
+import {Trans} from '@lingui/react'
+import {t} from '@lingui/macro'
+import {List} from 'semantic-ui-react'
+import Module from 'parser/core/Module'
+import {getDataBy} from 'data'
+import {TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
+import STATUSES from 'data/STATUSES'
+import ACTIONS from 'data/ACTIONS'
+import {ActionLink, StatusLink} from 'components/ui/DbLink'
+
+// All of Bards DoTs
+const DOTS = [
+	STATUSES.CAUSTIC_BITE.id,
+	STATUSES.STORMBITE.id,
+	STATUSES.VENOMOUS_BITE.id,
+	STATUSES.WINDBITE.id,
+]
+
+export default class Sidewinder extends Module {
+	static handle = 'sidewinder'
+	static title = t('brd.sidewinder.title')`Sidewinders and Shadowbites`
+	static dependencies = [
+		'suggestions',
+	]
+
+	_amountOfBadSidewinders = 0
+	_amountOfBadShadowbites = 0
+
+	_badCasts = []
+
+	_enemies = {}
+
+	constructor(...args) {
+		super(...args)
+
+		this.addHook('cast', {
+			by: 'player',
+			abilityId: [ACTIONS.SIDEWINDER.id, ACTIONS.SHADOWBITE.id],
+		}, this._onSidewinderCast)
+
+		this.addHook('applydebuff', {
+			by: 'player',
+			abilityId: DOTS,
+		}, this._onApplyDot)
+
+		this.addHook('removedebuff', {
+			by: 'player',
+			abilityId: DOTS,
+		}, this._onRemoveDot)
+		this.addHook('complete', this._onComplete)
+	}
+
+	_onSidewinderCast(event) {
+		const enemyDots = this._getEnemyDots(event.targetID)
+		const abilityId = event.ability.guid
+		if (enemyDots.length < 2) {
+			if (abilityId === ACTIONS.SIDEWINDER.id) {
+				this._amountOfBadSidewinders++
+			} else {
+				this._amountOfBadShadowbites++
+			}
+
+			this._badCasts.push({abilityId: abilityId, dotsApplied: enemyDots.length, appliedDot: enemyDots[0], timestamp: event.timestamp})
+		}
+	}
+
+	_onApplyDot(event) {
+		const enemyDots = this._getEnemyDots(event.targetID)
+		enemyDots.push(event.ability.guid)
+	}
+
+	_onRemoveDot(event) {
+		const enemyDots = this._getEnemyDots(event.targetID)
+		const dotIndex = this._findDot(enemyDots)
+		if (dotIndex !== -1) {
+			enemyDots.splice(dotIndex, 1)
+		}
+	}
+
+	// Returns the enemy statuses state
+	_getEnemyDots(targetId) {
+		if (!this._enemies[targetId]) {
+			this._enemies[targetId] = []
+		}
+
+		return this._enemies[targetId]
+	}
+
+	_findDot(array) {
+		return array.findIndex(element => {
+			for (const dot of DOTS) {
+				if (element === dot) {
+					return true
+				}
+			}
+			return false
+		})
+	}
+
+	_onComplete() {
+		if (!this._badCasts.length) {
+			return
+		}
+		if (this._amountOfBadSidewinders) {
+			this.suggestions.add(new TieredSuggestion({
+				icon: ACTIONS.SIDEWINDER.icon,
+				content: <>
+					<Trans id="brd.sidewinder.suggestion.bad-sidewinder">
+				Only use <ActionLink {...ACTIONS.SIDEWINDER}/> when you have both <ActionLink {...ACTIONS.CAUSTIC_BITE}/> and <ActionLink {...ACTIONS.STORMBITE}/> active on the target. Remember that a DoT doesn't apply as soon as you cast it, so you have to wait for it to apply before casting <ActionLink showIcon={false} {...ACTIONS.SIDEWINDER}/>.
+					</Trans>
+				</>,
+				tiers: {
+					5: SEVERITY.MAJOR,
+					3: SEVERITY.MEDIUM,
+					1: SEVERITY.MINOR,
+				},
+				value: this._amountOfBadSidewinders,
+				why: <>
+					<Trans id="brd.sidewinder.suggestion.bad-shadowbite.reason">
+						{this._amountOfBadSidewinders} casts of <ActionLink showIcon={false} {...ACTIONS.SIDEWINDER}/> without both DoTs applied.
+					</Trans>
+				</>,
+			}))
+		}
+
+		if (this._amountOfBadShadowbites) {
+			this.suggestions.add(new TieredSuggestion({
+				icon: ACTIONS.SHADOWBITE.icon,
+				content: <>
+					<Trans id="brd.sidewinder.suggestion.bad-shadowbite">
+				Only use <ActionLink {...ACTIONS.SHADOWBITE}/> when you have both <ActionLink {...ACTIONS.CAUSTIC_BITE}/> and <ActionLink {...ACTIONS.STORMBITE}/> active on the target. Remember that a DoT doesn't apply as soon as you cast it, so you have to wait for it to apply before casting <ActionLink showIcon={false} {...ACTIONS.SHADOWBITE}/>.
+					</Trans>
+				</>,
+				tiers: {
+					5: SEVERITY.MAJOR,
+					3: SEVERITY.MEDIUM,
+					1: SEVERITY.MINOR,
+				},
+				value: this._amountOfBadShadowbites,
+				why: <>
+					<Trans id="brd.sidewinder.suggestion.bad-shadowbite.reason">
+						{this._amountOfBadShadowbites} casts of <ActionLink showIcon={false} {...ACTIONS.SHADOWBITE}/> without both DoTs applied.
+					</Trans>
+				</>,
+			}))
+		}
+	}
+
+	output() {
+		if (!this._badCasts.length) {
+			return
+		}
+		// Builds a list item for each incorrect cast
+		const items = this._badCasts.map(cast => {
+			const ability = getDataBy(ACTIONS, 'id', cast.abilityId)
+			if (cast.appliedDot) {
+				return <List.Item key={cast.timestamp}>
+					<List.Content>
+						<Trans id="brd.sidewinder.list.one-dot">{this.parser.formatTimestamp(cast.timestamp)} - <ActionLink {...ability}/> was cast with only <StatusLink {...getDataBy(STATUSES, 'id', cast.appliedDot)}/> applied.</Trans>
+					</List.Content>
+				</List.Item>
+			}
+			return <List.Item key={cast.timestamp}>
+				<List.Content>
+					<Trans id="brd.sidewinder.list.no-dots">{this.parser.formatTimestamp(cast.timestamp)} - <ActionLink {...ability}/> was cast with no DoTs applied.</Trans>
+				</List.Content>
+			</List.Item>
+		})
+		// Output is a List, where every item is an incorrect cast
+		return <List divided relaxed>
+			{items}
+		</List>
+	}
+
+}
