a8be1979cc91ef0d2991a151cacb27598118b06b
Ryan Beck <rjb17f@my.fsu.edu>
2019-07-20 22:56:35 -0400

    updated Kenki

--- src/parser/jobs/sam/modules/Kenki.js
+++ src/parser/jobs/sam/modules/Kenki.js
@@ -1,5 +1,5 @@
 import {t} from '@lingui/macro'
-import {Trans} from '@lingui/react'
+import {Trans, Plural} from '@lingui/react'
 import Color from 'color'
 import _ from 'lodash'
 import React from 'react'
