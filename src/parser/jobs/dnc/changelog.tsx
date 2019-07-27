fb0e5aada980c09b0ae9fdd24d96f58e32372b8f
Saxon Landers <saxon@ackwell.com.au>
2019-07-27 18:51:59 +1000

    Never said I was smart

--- src/parser/jobs/dnc/changelog.tsx
+++ src/parser/jobs/dnc/changelog.tsx
@@ -7,6 +7,11 @@ import STATUSES from 'data/STATUSES'
 export const changelog = [
 	{
 		date: new Date('2019-07-26'),
+		Changes: () => <>Baseline <StatusLink {...STATUSES.ESPRIT} /> gauge implementation.</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
+	{
+		date: new Date('2019-07-26'),
 		Changes: () => <>Suggestion for not using Devilment outside Technical Finish windows other than the opener.</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
 	},
@@ -18,11 +23,6 @@ export const changelog = [
 	{
 		date: new Date('2019-07-20'),
 		Changes: () => <>Suggestions for dance performances and checklist rule for <StatusLink {...STATUSES.STANDARD_FINISH} /> uptime.</>,
-		contributors: [CONTRIBUTORS.AKAIRYU],
-	},
-	{
-		date: new Date('2019-07-26'),
-		Changes: () => <>Baseline <StatusLink {...STATUSES.ESPRIT} /> gauge implementation.</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
 	},
 ]
