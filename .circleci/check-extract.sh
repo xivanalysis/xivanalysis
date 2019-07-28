cc086697519a5ab7512005473b4362dd052a45e7
Saxon Landers <saxon@ackwell.com.au>
2019-07-28 21:16:15 +1000

    xml is trash

--- .circleci/check-extract.sh
+++ .circleci/check-extract.sh
@@ -5,8 +5,7 @@ yarn extract;
 if [[ $(git diff --stat locale) != '' ]]; then
 	echo "Extract diff found.";
 	DIFF=$(git diff --color=never locale)
-	OUTPUT="
-	<?xml version=\"1.0\" encoding=\"utf-8\"?>
+	OUTPUT="<?xml version=\"1.0\" encoding=\"utf-8\"?>
 	<testsuites>
 		<testsuite package=\"xiva.extract\" time=\"0\" tests=\"1\" errors=\"1\" name=\"i18n message extraction\" >
 			<testcase time=\"0\" name=\"xiva.extract.failure\">
