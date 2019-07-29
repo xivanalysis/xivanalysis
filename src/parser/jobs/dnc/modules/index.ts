e02c677009c1494f141758c00262a6026378a85e
2Broken <asunatheflashtastic@yahoo.com>
2019-07-28 19:55:25 -0700

    move Procs import into correct order

--- src/parser/jobs/dnc/modules/index.ts
+++ src/parser/jobs/dnc/modules/index.ts
@@ -1,8 +1,8 @@
 import DirtyDancing from './DirtyDancing'
-import Procs from './Procs'
 import EspritGauge from './Esprit'
 import FeatherGauge from './FeatherGauge'
 import OGCDDowntime from './OCDDowntime'
+import Procs from './Procs'
 
 export default [
 	Procs,
