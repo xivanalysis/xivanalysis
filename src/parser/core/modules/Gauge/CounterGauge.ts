59cc1d4123ff53466d81697dae9a559f54c8182e
Saxon Landers <saxon@ackwell.com.au>
2019-08-05 01:05:27 +1000

    Rudimentary scaffolding for counter

--- src/parser/core/modules/Gauge/CounterGauge.ts
+++ src/parser/core/modules/Gauge/CounterGauge.ts
@@ -1,6 +1,26 @@
 import {AbstractGauge} from './AbstractGauge'
 
 export class CounterGauge extends AbstractGauge {
+	private value: number
+	private minimum: number
+	private maximum: number
+
+	constructor(opts: {
+		value?: number,
+		minimum?: number,
+		maximum?: number,
+	}) {
+		super()
+
+		this.minimum = opts.minimum || 0
+		this.value = opts.value || this.minimum
+		this.maximum = opts.maximum || 100
+	}
+
 	/** @inheritdoc */
-	reset() {}
+	reset() {
+		// NOTE: This assumes counters always reset to their minimum value.
+		// Should that not be the case, probbaly needs a `resetTo` value.
+		this.value = this.minimum
+	}
 }
