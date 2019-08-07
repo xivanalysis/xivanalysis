a098d5feb722710262294937a1935616f3a9551a
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2019-08-06 19:05:00 -0500

    Track Chain Stratagem
    A simple addition to track the SCH raid buff :)
    
    If you need me to update all the files in `locale`, I'd be happy to add
    stubs for those as well.

--- src/parser/jobs/sch/modules/index.js
+++ src/parser/jobs/sch/modules/index.js
@@ -1,9 +1,11 @@
 import Aetherflow from './Aetherflow'
 import DoTs from './DoTs'
 import Weaving from './Weaving'
+import ChainStrat from './ChainStrat'
 
 export default [
 	Weaving,
 	Aetherflow,
 	DoTs,
+	ChainStrat,
 ]
