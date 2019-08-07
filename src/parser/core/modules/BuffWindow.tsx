5b0081770669789b614ac342d89de2322f1facd4
Azariah Aeroborn <26804378+AzariahAeroborn@users.noreply.github.com>
2019-08-07 17:36:26 -0400

    Address review feedback

--- src/parser/core/modules/BuffWindow.tsx
+++ src/parser/core/modules/BuffWindow.tsx
@@ -4,7 +4,9 @@ import {Plural, Trans} from '@lingui/react'
 import {ActionLink} from 'components/ui/DbLink'
 import {RotationTable, RotationTableTargetData} from 'components/ui/RotationTable'
 import ACTIONS from 'data/ACTIONS'
-import {ActionData, getDataBy, StatusData} from 'data/getDataBy'
+import {Action} from 'data/ACTIONS/ACTIONS'
+import {getDataBy} from 'data/getDataBy'
+import {Status} from 'data/STATUSES/STATUSES'
 import {BuffEvent, CastEvent} from 'fflogs'
 import _ from 'lodash'
 import Module, {dependency} from 'parser/core/Module'
@@ -47,33 +49,54 @@ interface BuffWindowExpectedGCDs {
 }
 
 interface BuffWindowRequiredGCDs {
-	iconAction: ActionData
-	actions: ActionData[]
+	iconAction: Action
+	actions: Action[]
 	suggestionContent: JSX.Element | string
 	severityTiers: SeverityTiers
 }
 
 interface BuffWindowTrackedCooldown {
-	action: ActionData
+	action: Action
 	expectedPerWindow: number,
 	suggestionContent: JSX.Element | string
 	severityTiers: SeverityTiers
 }
 
-export default class BuffWindowModule extends Module {
+export abstract class BuffWindowModule extends Module {
 	static handle: string = 'buffwindow'
 	static title: MessageDescriptor = t('core.buffwindow.title')`Buff Window`
 
-	// Implementing modules should pass the ACTION and STATUS object respectively that represents the buff window
-	protected buffAction?: ActionData
-	protected buffStatus?: StatusData
+	/**
+	 * Implementing modules MUST define the ACTION object for the action that initiates the buff window
+	 */
+	abstract buffAction: Action
+	/**
+	 * Implementing modules MUST define the STATUS object for the status that represents the buff window
+	 */
+	abstract buffStatus: Status
 
-	// From a practical perspective, expectedGCDs should be considered required for implementing modules.
-	// Other properties can be added as desired.  trackedBadCooldowns will show suggestions if used too many times, but will not show in the rotation tables
+	/**
+	 * Most implementing modules will pass an expectedGCDs object to indicate the number of GCDs expected within the buff window
+	 * 	 and the suggestion "content" copy and severity tiers for failing to meet the number of expected GCDs.
+	 */
 	protected expectedGCDs?: BuffWindowExpectedGCDs
+	/**
+	 * Optionally, you can also specify requiredGCDs to indicate that all GCDs used within the buff window MUST be one of the skills
+	 *   designated in the requiredGCDs.actions array
+	 */
 	protected requiredGCDs?: BuffWindowRequiredGCDs
-	protected trackedCooldowns?: BuffWindowTrackedCooldown[]
-	protected trackedBadCooldowns?: BuffWindowTrackedCooldown[]
+	/**
+	 * Optionally, you can also specify additional cooldowns to track usage of, indicating the number of expected usages per window.
+	 * - trackedCooldowns will require a MINIMUM of trackedCooldown.expectedPerWindow uses of the specified action in each window
+	 *     and will provide data in the RotationTable about the number used as well as a suggestion based on missed uses
+	 */
+	protected trackedCooldowns: BuffWindowTrackedCooldown[] = []
+	/**
+	 * Optionally, you can also specify additional cooldowns to track usage of, indicating the number of expected usages per window.
+	 *  - trackedBadCooldowns will require NO MORE THAN trackedBadCooldown.expectedPerWindow uses of the specified action in each window
+	 *     (usually, this number should be 0), and will provide a suggestion if the BadCooldown is being used more than the expected threshold
+	 */
+	protected trackedBadCooldowns: BuffWindowTrackedCooldown[] = []
 
 	@dependency private suggestions!: Suggestions
 	@dependency private timeline!: Timeline
@@ -97,7 +120,7 @@ export default class BuffWindowModule extends Module {
 	}
 
 	private onCast(event: CastEvent) {
-		const action: ActionData | undefined = getDataBy(ACTIONS, 'id', event.ability.guid)
+		const action: Action | undefined = getDataBy(ACTIONS, 'id', event.ability.guid)
 
 		if (!action || action.autoAttack) {
 			// Disregard auto attacks for tracking rotations / events during buff windows
@@ -131,8 +154,12 @@ export default class BuffWindowModule extends Module {
 		}
 	}
 
-	// For consumers that have the same number of expected GCDs per window, this will use the expectedPerWindow property
-	//   on expectedGCDs as the baseline.  This method can be overridden if the logic of expected GCDs per window is variable
+	/**
+	 * For consumers that have the same number of expected GCDs per window, this will use the expectedPerWindow property
+	 *   on expectedGCDs as the baseline
+	 * This method MAY be overridden if the logic of expected GCDs per window is variable
+	 * @param buffWindow
+	 */
 	protected getBaselineExpectedGCDs(buffWindow: BuffWindowState): number {
 		if ( this.expectedGCDs ) {
 			return this.expectedGCDs.expectedPerWindow
@@ -140,15 +167,22 @@ export default class BuffWindowModule extends Module {
 		return 0
 	}
 
-	// Override point for class-specific rushing logic per BuffWindow - default no effect
-	// Return a positive number to increase expected GCDs for this window, and a negative number to decrease
+	/**
+	 * This method MAY be overridden to provide class-specific rushing logic per BuffWindow - default is no effect
+	 * Return a positive number to INCREASE expected GCDs for this window, or a negative number to DECREASE
+	 * @param buffWindow
+	 */
 	protected changeExpectedGCDsClassLogic(buffWindow: BuffWindowState): number {
 		return 0
 	}
 
-	// Rushing handling for end of fight rushing.  Can be overridden if class rules for end of fight rushing vary
+	/**
+	 * Handles rushing logic to reduce expected GCDs in a window for end of fight rushing
+	 * This method MAY be overridden if class rules for end of fight rushing vary
+	 * @param buffWindow
+	 */
 	protected reduceExpectedGCDsEndOfFight(buffWindow: BuffWindowState): number {
-		if ( this.buffStatus && this.buffStatus.duration ) {
+		if ( this.buffStatus.duration ) {
 			// Check to see if this window is rushing due to end of fight - reduce expected GCDs accordingly
 			const windowDurationMillis = this.buffStatus.duration * 1000
 			const fightTimeRemaining = this.parser.fight.end_time - buffWindow.start
@@ -163,15 +197,23 @@ export default class BuffWindowModule extends Module {
 		return 0
 	}
 
-	// For consumers that have tracked cooldowns that expect the same number of that cooldown per window, this will use the
-	//   expectedPerWindow property on that cooldown as the baseline.  This method can be overridden if the logic of
-	//   expected tracked cooldowns per window is variable
+	/**
+	 * For consumers that have tracked cooldowns that expect the same number of usages per window, this will use the
+	 *   expectedPerWindow property on that cooldown as the baseline
+	 * This method MAY be overridden if the logic of expected tracked cooldowns per window is variable
+	 * @param buffWindow
+	 * @param cooldown
+	 */
 	protected getBaselineExpectedTrackedCooldown(buffWindow: BuffWindowState, cooldown: BuffWindowTrackedCooldown): number {
 		return cooldown.expectedPerWindow || 0
 	}
 
-	// Override point for class-specific logic to change expected uses of a tracked cooldown per BuffWindow - default no effect
-	// Return a positive number to increase expected GCDs for this window, and a negative number to decrease
+	/**
+	 * This method MAY be overridden to provide class-specific logic to change expected uses of a tracked cooldown per BuffWindow - default no effect
+	 * Return a positive number to INCREASE expected tracked cooldown usages for this window, or a negative number to DECREASE
+	 * @param buffWindow
+	 * @param cooldown
+	 */
 	protected changeExpectedTrackedCooldownClassLogic(buffWindow: BuffWindowState, cooldown: BuffWindowTrackedCooldown): number {
 		return 0
 	}
@@ -185,7 +227,7 @@ export default class BuffWindowModule extends Module {
 	}
 
 	private onComplete() {
-		if ( this.buffAction && this.expectedGCDs ) {
+		if ( this.expectedGCDs ) {
 			const missedGCDs = this.buffWindows
 				.reduce((sum, curWindow) => {
 					const expectedGCDs = this.getBuffWindowExpectedGCDs(curWindow)
@@ -203,7 +245,7 @@ export default class BuffWindowModule extends Module {
 			}))
 		}
 
-		if ( this.buffAction && this.requiredGCDs ) {
+		if ( this.requiredGCDs ) {
 			const allowedGCDsById = this.requiredGCDs.actions.map(a => a.id)
 			const invalidGCDs = this.buffWindows
 				.reduce((sum, curWindow) => sum + Math.max(0, curWindow.gcds - curWindow.getActionCountByIds(allowedGCDsById)), 0)
@@ -219,43 +261,35 @@ export default class BuffWindowModule extends Module {
 			}))
 		}
 
-		if ( this.trackedCooldowns ) {
-			this.trackedCooldowns.forEach(cooldown => {
-				if ( this.buffAction ) {
-					const missedCooldowns = this.buffWindows
-						.reduce((sum, curWindow) => sum + Math.max(0, cooldown.expectedPerWindow - curWindow.getActionCountByIds([cooldown.action.id])), 0)
+		this.trackedCooldowns.forEach(cooldown => {
+			const missedCooldowns = this.buffWindows
+				.reduce((sum, curWindow) => sum + Math.max(0, cooldown.expectedPerWindow - curWindow.getActionCountByIds([cooldown.action.id])), 0)
 
-					this.suggestions.add(new TieredSuggestion({
-						icon: cooldown.action.icon,
-						content: cooldown.suggestionContent,
-						tiers: cooldown.severityTiers,
-						value: missedCooldowns,
-						why: <Trans id="core.buffwindow.suggestions.trackedcooldown.why">
-							{missedCooldowns} <Plural value={missedCooldowns} one="use of" other="uses of"/> <ActionLink showIcon={false} {...cooldown.action}/> <Plural value={missedCooldowns} one="was" other="were"/> missed during {this.buffAction.name} windows.
-						</Trans>,
-					}))
-				}
-			})
-		}
+			this.suggestions.add(new TieredSuggestion({
+				icon: cooldown.action.icon,
+				content: cooldown.suggestionContent,
+				tiers: cooldown.severityTiers,
+				value: missedCooldowns,
+				why: <Trans id="core.buffwindow.suggestions.trackedcooldown.why">
+					{missedCooldowns} <Plural value={missedCooldowns} one="use of" other="uses of"/> <ActionLink showIcon={false} {...cooldown.action}/> <Plural value={missedCooldowns} one="was" other="were"/> missed during {this.buffAction.name} windows.
+				</Trans>,
+			}))
+		})
 
-		if ( this.trackedBadCooldowns ) {
-			this.trackedBadCooldowns.forEach(badCooldown => {
-				if ( this.buffAction ) {
-					const badCooldowns = this.buffWindows
-						.reduce((sum, curWindow) => sum + Math.max(0, curWindow.getActionCountByIds([badCooldown.action.id])), 0)
+		this.trackedBadCooldowns.forEach(badCooldown => {
+			const badCooldowns = this.buffWindows
+				.reduce((sum, curWindow) => sum + Math.max(0, curWindow.getActionCountByIds([badCooldown.action.id])), 0)
 
-					this.suggestions.add(new TieredSuggestion({
-						icon: badCooldown.action.icon,
-						content: badCooldown.suggestionContent,
-						tiers: badCooldown.severityTiers,
-						value: badCooldowns,
-						why: <Trans id="core.buffwindow.suggestions.badcooldown.why">
-							{badCooldowns} <Plural value={badCooldowns} one="use of" other="uses of"/> <ActionLink showIcon={false} {...badCooldown.action}/> during {this.buffAction.name} windows.
-						</Trans>,
-					}))
-				}
-			})
-		}
+			this.suggestions.add(new TieredSuggestion({
+				icon: badCooldown.action.icon,
+				content: badCooldown.suggestionContent,
+				tiers: badCooldown.severityTiers,
+				value: badCooldowns,
+				why: <Trans id="core.buffwindow.suggestions.badcooldown.why">
+					{badCooldowns} <Plural value={badCooldowns} one="use of" other="uses of"/> <ActionLink showIcon={false} {...badCooldown.action}/> during {this.buffAction.name} windows.
+				</Trans>,
+			}))
+		})
 	}
 
 	output() {
@@ -273,14 +307,12 @@ export default class BuffWindowModule extends Module {
 				accessor: 'badgcd',
 			})
 		}
-		if ( this.trackedCooldowns ) {
-			this.trackedCooldowns.forEach((cooldown) => {
-				rotationTargets.push({
-					header: <ActionLink showName={false} {...cooldown.action}/>,
-					accessor: cooldown.action.name,
-				})
+		this.trackedCooldowns.forEach((cooldown) => {
+			rotationTargets.push({
+				header: <ActionLink showName={false} {...cooldown.action}/>,
+				accessor: cooldown.action.name,
 			})
-		}
+		})
 
 		const rotationData = this.buffWindows
 			.map(buffWindow => {
