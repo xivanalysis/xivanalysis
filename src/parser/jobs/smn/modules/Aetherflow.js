cb3555698446c107f4509ccda9331e85e2ef738c
KelosPelmand <53376212+KelosPelmand@users.noreply.github.com>
2019-08-08 07:38:37 -0400

    Now check for DoTs on Ruin spells like Fester does. (#477)

--- src/parser/jobs/smn/modules/Aetherflow.js
+++ src/parser/jobs/smn/modules/Aetherflow.js
@@ -2,29 +2,14 @@ import {t} from '@lingui/macro'
 import {Trans, Plural} from '@lingui/react'
 import React from 'react'
 
-import {ActionLink, StatusLink} from 'components/ui/DbLink'
+import {ActionLink} from 'components/ui/DbLink'
 import ACTIONS from 'data/ACTIONS'
-import STATUSES from 'data/STATUSES'
 import Module from 'parser/core/Module'
 import {TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
 
 import DISPLAY_ORDER from './DISPLAY_ORDER'
 
-// Statuses that need to be up for Fester & Bane to actually do something good
-const SMN_DOT_STATUSES = [
-	STATUSES.BIO_III.id,
-	STATUSES.MIASMA_III.id,
-]
-
-const FESTER_POT_PER_DOT = 150
-
 // Severities
-// In potency
-const FESTER_SEVERITY = {
-	1: SEVERITY.MEDIUM,
-	600: SEVERITY.MAJOR,
-}
-
 const MULTI_HIT_SKILL_DATA = {
 	[ACTIONS.PAINFLARE.id]: {
 		minTargets: 2,
@@ -48,50 +33,19 @@ export default class Aetherflow extends Module {
 	static dependencies = [
 		// Ensure AoE runs cleanup before us
 		'aoe', // eslint-disable-line @xivanalysis/no-unused-dependencies
-		'enemies',
 		'suggestions',
 	]
 	static displayOrder = DISPLAY_ORDER.AETHERFLOW
 
 	_badMultiHitCasts = {}
-	_badDotReqCasts = {}
-
 	constructor(...args) {
 		super(...args)
-
-		this.addHook('cast', {
-			by: 'player',
-			abilityId: [ACTIONS.FESTER.id, ACTIONS.BANE.id],
-		}, this._onDotReqCast)
 
 		this.addHook('aoedamage', {
 			by: 'player',
 			abilityId: Object.keys(MULTI_HIT_SKILL_DATA).map(Number),
 		}, this._onAoeDamage)
 		this.addHook('complete', this._onComplete)
-	}
-
-	_onDotReqCast(event) {
-		const actionId = event.ability.guid
-
-		const target = this.enemies.getEntity(event.targetID)
-		if (!target) { return }
-		const statusesMissing = SMN_DOT_STATUSES.length - SMN_DOT_STATUSES.filter(statusId => target.hasStatus(statusId)).length
-
-		// Don't need to worry if they got them all up
-		if (statusesMissing === 0) { return }
-
-		// Make sure we're tracking this skill/num
-		if (!this._badDotReqCasts[actionId]) {
-			this._badDotReqCasts[actionId] = {}
-		}
-		if (!this._badDotReqCasts[actionId][statusesMissing]) {
-			this._badDotReqCasts[actionId][statusesMissing] = 0
-		}
-
-		// Add to the appropriate key
-		// Just tracking flat count for now. Expand to events if need info (for the timeline, yes pls)
-		this._badDotReqCasts[actionId][statusesMissing]++
 	}
 
 	_onAoeDamage(event) {
@@ -105,30 +59,6 @@ export default class Aetherflow extends Module {
 	}
 
 	_onComplete() {
-		// Suggestion for fester
-		const badFesters = this._badDotReqCasts[ACTIONS.FESTER.id] || {}
-		const festerKeys = Object.keys(badFesters).map(num => parseInt(num, 10))
-		// Feel this can be used as a better base metric for judging Fester whiff severity, imo < 600 is medium, > major
-		const totalFesterPotencyLost = festerKeys.reduce((carry, num) => carry + num * FESTER_POT_PER_DOT * badFesters[num], 0)
-
-		// Sorry Nem, I've simplified this a lot to smooth out the i18n process. We can have a look into improving the output later.
-		const numBadFesters = festerKeys.reduce((carry, num) => carry + badFesters[num], 0)
-
-		// Fester suggestion
-		this.suggestions.add(new TieredSuggestion({
-			icon: ACTIONS.FESTER.icon,
-			content: <Trans id="smn.aetherflow.suggestions.fester.content">
-				To get the most potency out of your <ActionLink {...ACTIONS.FESTER}/>s, ensure both <StatusLink {...STATUSES.BIO_III}/> and <StatusLink {...STATUSES.MIASMA_III}/> are applied to your target. Avoid casting Fester directly after DoT application, as the status takes a short period to apply.
-			</Trans>,
-			why: <Trans id="smn.aetherflow.suggestions.fester.why">
-				{totalFesterPotencyLost} potency lost to
-				<Plural value={numBadFesters} one="# cast" other="# casts"/>
-				of Fester on targets missing DoTs.
-			</Trans>,
-			tiers: FESTER_SEVERITY,
-			value: totalFesterPotencyLost,
-		}))
-
 		// Painflare suggestion, I want to say < 4 is medium because 4 is greater than a Deathflare
 		const numBadPainflares = this._badMultiHitCasts[ACTIONS.PAINFLARE.id] || 0
 		this.suggestions.add(new TieredSuggestion({
