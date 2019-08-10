7421e9c1d795759c42945766875ad6190631e603
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2019-08-10 00:16:25 -0500

    Add fairy gauge tracking
    Could be expanded later to keep track of 'fairy uptime', but I think
    this is good enough for now.

--- src/parser/jobs/sch/modules/index.js
+++ src/parser/jobs/sch/modules/index.js
@@ -2,10 +2,12 @@ import Aetherflow from './Aetherflow'
 import DoTs from './DoTs'
 import Weaving from './Weaving'
 import ChainStrat from './ChainStrat'
+import FaerieGauge from './FaerieGauge'
 
 export default [
 	Weaving,
 	Aetherflow,
 	DoTs,
 	ChainStrat,
+	FaerieGauge,
 ]
