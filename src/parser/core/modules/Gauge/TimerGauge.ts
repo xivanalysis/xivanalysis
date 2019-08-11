264dc3087169791171b08c888c6c7b0378d87f68
Saxon Landers <saxon@ackwell.com.au>
2019-08-12 00:17:52 +1000

    Remove timer gauge

--- src/parser/core/modules/Gauge/TimerGauge.ts
+++
@@ -1,6 +0,0 @@
-import {AbstractGauge} from './AbstractGauge'
-
-export class TimerGauge extends AbstractGauge {
-	/** @inheritdoc */
-	reset() {}
-}
--- src/parser/core/modules/Gauge/index.ts
+++ src/parser/core/modules/Gauge/index.ts
@@ -1,3 +1,2 @@
 export {Gauge} from './Gauge'
 export {CounterGauge} from './CounterGauge'
-export {TimerGauge} from './TimerGauge'
