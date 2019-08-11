262b6c8b3299b6c1287188fbc3591510e8c4e809
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2019-08-11 09:28:04 -0500

    Update statuses for SCH
    Just a small fix that adds Catalyze and adds durations for statues that lack them.

--- src/data/STATUSES/SCH.js
+++ src/data/STATUSES/SCH.js
@@ -3,18 +3,28 @@ export default {
 		id: 1895,
 		name: 'Biolysis',
 		icon: 'https://xivapi.com/i/012000/012812.png',
+		duration: 30,
 	},
 
 	CHAIN_STRATAGEM: {
 		id: 1221,
 		name: 'Chain Stratagem',
 		icon: 'https://xivapi.com/i/012000/012809.png',
+		duration: 15,
 	},
 
 	GALVANIZE: {
 		id: 297,
 		name: 'Galvanize',
 		icon: 'https://xivapi.com/i/012000/012801.png',
+		duration: 30,
+	},
+
+	CATALYZE: {
+		id: 1918,
+		name: 'Catalyze',
+		icon: 'https://xivapi.com/i/012000/012814.png',
+		duration: 30,
 	},
 
 	RECITATION: {
