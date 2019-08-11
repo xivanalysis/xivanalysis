ebbe85fc6f1b528eec1124ebdf0803a73f8e327b
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2019-08-10 23:50:43 -0500

    DNC: Show procs on timeline (#539)

--- src/parser/jobs/dnc/modules/Combos.ts
+++ src/parser/jobs/dnc/modules/Combos.ts
@@ -1,6 +1,8 @@
 import ACTIONS from 'data/ACTIONS'
+import STATUSES from 'data/STATUSES'
 import {dependency} from 'parser/core/Module'
 import {AoeEvent} from 'parser/core/modules/AoE'
+import Combatants from 'parser/core/modules/Combatants'
 import CoreCombos from 'parser/core/modules/Combos'
 import DirtyDancing from './DirtyDancing'
 
@@ -11,6 +13,7 @@ export default class Combos extends CoreCombos {
 	static suggestionIcon = ACTIONS.CASCADE.icon
 
 	@dependency private dancing!: DirtyDancing
+	@dependency private combatants!: Combatants
 
 	// Override check for allowable breaks. If two dances were started during the initial context's GCD timeout window,
 	// (ie, both Standard and Technical were danced), then we'll allow it
@@ -19,6 +22,10 @@ export default class Combos extends CoreCombos {
 		if (context.length !== 1) {
 			return false
 		}
-		return this.dancing.dancesInRange(context[0].timestamp, context[0].timestamp + GCD_TIMEOUT_MILLIS) === 2
+		// Technical windows could also go Tech -> Saber -> Fountainfall -> Saber -> Standard -> Fountain drops,
+		// so just disable any drops that happened in a Technical window (still need the dances in range check since
+		// Cascade -> Standard -> Technical leaves the buff falling off before Technical Finish buff applies
+		return this.dancing.dancesInRange(context[0].timestamp, context[0].timestamp + GCD_TIMEOUT_MILLIS) === 2 ||
+			this.combatants.selected.hasStatus(STATUSES.TECHNICAL_FINISH.id, event.timestamp)
 	}
 }
--- src/parser/jobs/dnc/modules/Procs.js
+++ src/parser/jobs/dnc/modules/Procs.js
@@ -2,16 +2,27 @@ import {Trans, Plural} from '@lingui/react'
 import React from 'react'
 
 import {ActionLink} from 'components/ui/DbLink'
+import {getDataBy} from 'data'
 import ACTIONS from 'data/ACTIONS'
 import STATUSES from 'data/STATUSES'
 import Module from 'parser/core/Module'
 import {TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
+import {Group, Item} from 'parser/core/modules/Timeline'
+
+const PROC_STATUSES = [
+	STATUSES.FLOURISHING_FAN_DANCE.id,
+	STATUSES.FLOURISHING_CASCADE.id,
+	STATUSES.FLOURISHING_FOUNTAIN.id,
+	STATUSES.FLOURISHING_SHOWER.id,
+	STATUSES.FLOURISHING_WINDMILL.id,
+]
 
 export default class Procs extends Module {
 	static handle = 'procs'
 	static dependencies = [
 		'downtime',
 		'suggestions',
+		'timeline',
 	]
 
 	_casts = { //the listing order is arbitrary
@@ -29,14 +40,47 @@ export default class Procs extends Module {
 		[STATUSES.FLOURISHING_WINDMILL.id]: 0,
 	}
 
+	_buffWindows = {
+		[STATUSES.FLOURISHING_FAN_DANCE.id]: {
+			current: null,
+			history: [],
+		},
+		[STATUSES.FLOURISHING_CASCADE.id]: {
+			current: null,
+			history: [],
+		},
+		[STATUSES.FLOURISHING_FOUNTAIN.id]: {
+			current: null,
+			history: [],
+		},
+		[STATUSES.FLOURISHING_SHOWER.id]: {
+			current: null,
+			history: [],
+		},
+		[STATUSES.FLOURISHING_WINDMILL.id]: {
+			current: null,
+			history: [],
+		},
+	}
+	_group = null
+
 	_overwrittenProcs = 0
 	constructor(...args) {
 		super(...args)
 
 		this.addHook('cast', {by: 'player', abilityId: Object.keys(this._casts).map(Number)}, this._onCast)
-		this.addHook('refreshbuff', {by: 'player', abilityId: Object.keys(this._removedProcs).map(Number)}, this._procOverwritten)
-		this.addHook('removebuff', {by: 'player', abilityId: Object.keys(this._removedProcs).map(Number)}, this._onProcRemoved)
+		this.addHook('applybuff', {by: 'player', abilityId: PROC_STATUSES}, this._onProcGained)
+		this.addHook('refreshbuff', {by: 'player', abilityId: PROC_STATUSES}, this._procOverwritten)
+		this.addHook('removebuff', {by: 'player', abilityId: PROC_STATUSES}, this._onProcRemoved)
 		this.addHook('complete', this._onComplete)
+
+		this._group = new Group({
+			id: 'procbuffs',
+			content: 'Procs',
+			order: 0,
+			nestedGroups: [],
+		})
+		this.timeline.addGroup(this._group) // Group for showing procs on the timeline
 	}
 
 	_onCast(event) {
@@ -44,13 +88,38 @@ export default class Procs extends Module {
 			this._casts[event.ability.guid]++
 		}
 	}
+	_onProcGained(event) {
+		const statusId = event.ability.guid
+		const tracker = this._buffWindows[statusId]
+
+		if (!tracker) {
+			return
+		}
+
+		tracker.current = {
+			start: event.timestamp,
+		}
+	}
 	_procOverwritten() {
 		this._overwrittenProcs++
 	}
 	_onProcRemoved(event) {
+		const statusId = event.ability.guid
+
 		if (!this.downtime.isDowntime(event.timestamp)) {
 			this._removedProcs[event.ability.guid]++
 		}
+
+		// If this proc is active, stop the buff window
+		const tracker = this._buffWindows[statusId]
+
+		if (!tracker.current) {
+			return
+		}
+
+		tracker.current.stop = event.timestamp
+		tracker.history.push(tracker.current)
+		tracker.current = null
 	}
 
 	_onComplete() { // tracking dropped procs
@@ -92,6 +161,41 @@ export default class Procs extends Module {
 			value: this._overwrittenProcs,
 		}))
 
+		PROC_STATUSES.forEach(buff => {
+			const status = getDataBy(STATUSES, 'id', buff)
+			const groupId = this.getGroupIdForStatus(status)
+			const fightStart = this.parser.fight.start_time
+
+			// Finalise the buff if it was still active
+			if (this._buffWindows[buff].current) {
+				this._stopAndSave(buff)
+			}
+
+			// Add buff windows to the timeline
+			this._buffWindows[buff].history.forEach(window => {
+				this.timeline.addItem(new Item({
+					type: 'background',
+					start: window.start - fightStart,
+					end: window.stop - fightStart,
+					group: groupId,
+					content: <img src={status.icon} alt={status.name}/>,
+				}))
+			})
+		})
 	}
 
+	getGroupIdForStatus(status) {
+		const groupId = 'procbuffs-' + status.id
+
+		// Make sure a timeline group exists for this buff
+		if (!this._group.nestedGroups.includes(groupId)) {
+			this.timeline.addGroup(new Group({
+				id: groupId,
+				content: status.name,
+			}))
+			this._group.nestedGroups.push(groupId)
+		}
+
+		return groupId
+	}
 }
