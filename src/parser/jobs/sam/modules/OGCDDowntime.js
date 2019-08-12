7007d13838454b2d50ace55f73e604e559fc98a3
Ryan Beck <rjb17f@my.fsu.edu>
2019-08-12 14:06:27 -0400

    updating modules to move tracking to the core OGCD instead of my own Cavemen tech

---
+++ src/parser/jobs/sam/modules/OGCDDowntime.js
@@ -0,0 +1,14 @@
+import ACTIONS from 'data/ACTIONS'
+import CooldownDowntime from 'parser/core/modules/CooldownDowntime'
+
+export default class GeneralCDDowntime extends CooldownDowntime {
+	//Time that samurais have deemed ok for a OGCD to be down
+	// eslint-disable-next-line no-magic-numbers
+	allowedDowntime = 5000
+	trackedCds = [
+		ACTIONS.MEIKYO_SHISUI.id,
+		ACTIONS.KAESHI_SETSUGEKKA.id,
+		ACTIONS.KAESHI_GOKEN.id,
+		ACTIONS.KAESHI_HIGANBANA.id,
+	]
+}
