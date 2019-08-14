b24fce212deb0e827219a02e417480ad59953f3c
Saxon Landers <saxon@ackwell.com.au>
2019-08-14 23:52:42 +1000

    Fix GCD duration normalisation for actions with a short recast (#552)

--- src/parser/core/changelog.tsx
+++ src/parser/core/changelog.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import {ChangelogEntry} from './Meta'
 
 export const changelog: ChangelogEntry[] = [{
+	date: new Date('2019-08-14'),
+	Changes: () => <>Improve GCD calculations for actions with a recast less than 2.5 seconds. Jobs such as DNC should no longer occasionally recieve incredibly short GCD estimates.</>,
+	contributors: [CONTRIBUTORS.ACKWELL],
+}, {
 	date: new Date('2019-07-20'),
 	Changes: () => <>Add handling for GCD actions with recasts longer than the player GCD. This should impove GCD estimation accuracy and timeline display for DNC, GNB, and MCH.</>,
 	contributors: [CONTRIBUTORS.ACKWELL],
