c403c88f683dab308ca6bc6606039b051d3ead84
Panic Stevenson <Panic.Stevenson@microsoft.com>
2019-08-15 15:39:33 -0700

    Remove Scholar Ruin II Weaving Suggestion
    Remove the Scholar suggestion regarding Ruin II weaving as it is no
    longer accurate with the changes made to the job in Shadowbringers.
    
    The module is detrimental to players trying to analyze their fights
    currently because it overwrites the existing Weaving module that
    otherwise explains poor weaving, and gives misinformation that it is
    suboptimal to use Ruin II in a fight unless moving.
    
    Signed-off-by: Panic Stevenson <Panic.Stevenson@gmail.com>

--- src/parser/jobs/sch/modules/index.js
+++ src/parser/jobs/sch/modules/index.js
@@ -1,11 +1,9 @@
 import Aetherflow from './Aetherflow'
 import DoTs from './DoTs'
-import Weaving from './Weaving'
 import ChainStrat from './ChainStrat'
 import FaerieGauge from './FaerieGauge'
 
 export default [
-	Weaving,
 	Aetherflow,
 	DoTs,
 	ChainStrat,
