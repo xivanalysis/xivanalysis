c6164b5967646d13630b58e3a7c6f3019ea9f729
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2019-08-16 00:21:27 -0500

    Better clarification of how much waste there was

--- src/parser/jobs/sch/modules/DoTs.js
+++ src/parser/jobs/sch/modules/DoTs.js
@@ -5,19 +5,25 @@ import {ActionLink, StatusLink} from 'components/ui/DbLink'
 import ACTIONS from 'data/ACTIONS'
 import STATUSES from 'data/STATUSES'
 import CoreDoTs from 'parser/core/modules/DoTs'
-import {Rule, Requirement} from 'parser/core/modules/Checklist'
+import {TieredRule, TARGET, Requirement} from 'parser/core/modules/Checklist'
 import {TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
 import DISPLAY_ORDER from './DISPLAY_ORDER'
 
 // In seconds
-const CLIPPING_SEVERITY = {
-	6: SEVERITY.MINOR,
-	9: SEVERITY.MEDIUM,
-	12: SEVERITY.MAJOR,
+const SEVERITIES = {
+	CLIPPING: {
+		6: SEVERITY.MINOR,
+		9: SEVERITY.MEDIUM,
+		12: SEVERITY.MAJOR,
+	},
+	UPTIME: {
+		84: TARGET.WARN,
+		91: TARGET.SUCCESS,
+	},
 }
 
 export default class DoTs extends CoreDoTs {
-	static handle = 'dots'
+	static handle = 'Biolysis'
 	static displayOrder = DISPLAY_ORDER.DOTS
 	static dependencies = [
 		...DoTs.dependencies,
@@ -30,11 +36,12 @@ export default class DoTs extends CoreDoTs {
 	]
 
 	addChecklistRules() {
-		this.checklist.add(new Rule({
-			name: <Trans id="sch.dots.checklist.name">Keep your DoTs up</Trans>,
+		this.checklist.add(new TieredRule({
+			name: <Trans id="sch.dots.checklist.name">Keep your DoT up</Trans>,
 			description: <Trans id="sch.dots.checklist.description">
-				As a Scholar, DoTs are a significant portion of your sustained damage. Aim to keep them up at all times.
+				As a Scholar, Biolysis is a notable porition of your damage. Aim to keep it up at all times.
 			</Trans>,
+			tiers: SEVERITIES.UPTIME,
 			requirements: [
 				new Requirement({
 					name: <Trans id="sch.dots.checklist.requirement.bio-ii.name"><ActionLink {...ACTIONS.BIOLYSIS} /> uptime</Trans>,
@@ -51,10 +58,10 @@ export default class DoTs extends CoreDoTs {
 			content: <Trans id="sch.dots.suggestions.clipping.content">
 				Avoid refreshing Biolysis significantly before its expiration, except when at the end of the fight. Unnecessary refreshes use up your mana more than necessary, and may cause you to go out of mana.
 			</Trans>,
-			tiers: CLIPPING_SEVERITY,
+			tiers: SEVERITIES.CLIPPING,
 			value: clipPerMinute,
 			why: <Trans id="sch.dots.suggestions.clipping.why">
-				{this.parser.formatDuration(clip[STATUSES.BIOLYSIS.id])} of <StatusLink {...STATUSES.BIOLYSIS}/> lost to early refreshes.
+				An average of {this.parser.formatDuration(this.getClippingAmount(STATUSES.BIOLYSIS.id) * 1000)} of <StatusLink {...STATUSES.BIOLYSIS}/> clipped every minute, for a total of {this.parser.formatDuration(clip[STATUSES.BIOLYSIS.id])} lost to early refreshes.
 			</Trans>,
 		}))
 	}
