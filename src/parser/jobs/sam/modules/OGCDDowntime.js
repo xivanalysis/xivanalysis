72ec0d91e8b9be4fad2a97ce8377eae47e6d5c9f
Ryan Beck <rjb17f@my.fsu.edu>
2019-08-17 15:26:13 -0400

    offset added

--- src/parser/jobs/sam/modules/OGCDDowntime.js
+++ src/parser/jobs/sam/modules/OGCDDowntime.js
@@ -1,6 +1,9 @@
 import ACTIONS from 'data/ACTIONS'
 import CooldownDowntime from 'parser/core/modules/CooldownDowntime'
-
+//meikyo is used after the 7th gcd
+const FIRSTUSEOFFSETMEIKYO = 1750
+//tsubame is used after the first midare, on the 11th GCD
+const FIRSTUSEOFFSETTSUBAME = 27500
 export default class OGCDDowntime extends CooldownDowntime {
 	//Time that samurais have deemed ok for a OGCD to be down
 	// eslint-disable-next-line no-magic-numbers
@@ -11,4 +14,12 @@ export default class OGCDDowntime extends CooldownDowntime {
 		ACTIONS.KAESHI_GOKEN.id,
 		ACTIONS.KAESHI_HIGANBANA.id,
 	]
+
+	firstUseOffsetPerOgcd = {
+		[ACTIONS.MEIKYO_SHISUI.id]: FIRSTUSEOFFSETMEIKYO,
+		[ACTIONS.KAESHI_SETSUGEKKA.id]: FIRSTUSEOFFSETTSUBAME,
+		[ACTIONS.KAESHI_GOKEN.id]: FIRSTUSEOFFSETTSUBAME,
+		[ACTIONS.KAESHI_HIGANBANA.id]: FIRSTUSEOFFSETTSUBAME,
+	}
+
 }
