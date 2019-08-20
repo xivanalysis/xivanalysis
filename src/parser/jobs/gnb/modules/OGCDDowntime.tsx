ce755665fe60c6f5e4900ca390e8522d23c31a43
qaphla <3596194+qaphla@users.noreply.github.com>
2019-08-20 18:09:52 -0400

    Adds damage self-buff to GNB oGCD tracking.
    Adds the self-buff No Mercy to the list of oGCDs whose downtime is tracked.
    Allowable downtime was left untouched, as No Mercy usage should be close to
    on cooldown.
    
    First use offset for No Mercy was set to 10 seconds to account for the 4
    weaponskills used beforehand.

--- src/parser/jobs/gnb/modules/OGCDDowntime.tsx
+++ src/parser/jobs/gnb/modules/OGCDDowntime.tsx
@@ -1,9 +1,21 @@
 import ACTIONS from 'data/ACTIONS'
 import CooldownDowntime from 'parser/core/modules/CooldownDowntime'
 
+/* This offset allows for four weaponskills to be used before No Mercy in the
+ * opener, as is currently recommended. Should be updated if openers change to
+ * have later No Mercy.
+ */
+const FIRST_USE_OFFSET_NO_MERCY = 10000
+
 export default class AbilityDowntime extends CooldownDowntime {
 	allowedDowntime = 2500
+
+	firstUseOffsetPerOgcd = {
+		[ACTIONS.NO_MERCY.id]: FIRST_USE_OFFSET_NO_MERCY,
+	}
+
 	trackedCds = [
+		ACTIONS.NO_MERCY.id,
 		ACTIONS.BLASTING_ZONE.id, // Only track this one for now, assume level 80
 		ACTIONS.ROUGH_DIVIDE.id,
 		ACTIONS.BOW_SHOCK.id,
