de587febbb2ca4795ffd5a637a5aae8843b7249c
Azariah Aeroborn <26804378+AzariahAeroborn@users.noreply.github.com>
2019-08-24 12:22:27 -0400

    Remove shared role actions & statuses that no longer exist in 5.0

--- src/data/STATUSES/ROLE.js
+++ src/data/STATUSES/ROLE.js
@@ -1,22 +1,12 @@
-export default {
-	CLERIC_STANCE: {
-		id: 145,
-		name: 'Cleric Stance',
-		icon: 'https://xivapi.com/i/010000/010402.png',
-		duration: 15,
-	},
+import ACTIONS from 'data/ACTIONS'
 
+export default {
 	SWIFTCAST: {
 		id: 167,
 		name: 'Swiftcast',
 		icon: 'https://xivapi.com/i/010000/010454.png',
 		duration: 10,
-	},
-
-	PROTECT: {
-		id: 146,
-		name: 'Protect',
-		icon: 'https://xivapi.com/i/010000/010405.png',
+		action: ACTIONS.SWIFTCAST,
 	},
 
 	LUCID_DREAMING: {
@@ -25,20 +15,9 @@ export default {
 		icon: 'https://xivapi.com/i/013000/013909.png',
 	},
 
-	LARGESSE: {
-		id: 1207,
-		name: 'Largesse',
-		icon: 'https://xivapi.com/i/013000/013910.png',
-	},
-
 	SURECAST: {
 		id: 160,
 		name: 'Surecast',
 		icon: 'https://xivapi.com/i/010000/010452.png',
-	},
-	EYE_FOR_AN_EYE: {
-		id: 185,
-		name: 'Eye for an Eye',
-		icon: 'https://xivapi.com/i/013000/013918.png',
 	},
 }
