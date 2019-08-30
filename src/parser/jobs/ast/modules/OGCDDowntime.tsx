2335a57b7794e0a0170824184b9d2fb2eb628042
suShirou <suShirow>
2019-08-30 17:56:12 +0800

    Isolated Divination in OGCDDowntime.tsx

--- src/parser/jobs/ast/modules/OGCDDowntime.tsx
+++ src/parser/jobs/ast/modules/OGCDDowntime.tsx
@@ -1,4 +1,5 @@
 import {Trans} from '@lingui/react'
+import {ActionLink} from 'components/ui/DbLink'
 import ACTIONS from 'data/ACTIONS'
 import CooldownDowntime from 'parser/core/modules/CooldownDowntime'
 import React from 'react'
@@ -6,8 +7,8 @@ import React from 'react'
 const TARGETPERCENT = 95
 const FIRST_USE_OFFSET_ALL = 15000
 const ALLOWED_DOWNTIME_ALL = 2500
-// Diviniation at 10s for TA and other opener bursts
-const FIRST_USE_OFFSET_DIVINATION = 10000
+// Diviniation at 9.5-12.5s for TA and other opener bursts
+const FIRST_USE_OFFSET_DIVINATION = 12500
 // Damage doesn't come right away in every fight
 const FIRST_USE_OFFSET_HEALS = 30000
 
@@ -16,23 +17,17 @@ const ALLOWED_DOWNTIME_CELESTIAL_OPPOSITION = 15000
 
 export default class OGCDDowntime extends CooldownDowntime {
 	allowedDowntime = ALLOWED_DOWNTIME_ALL
-	allowedDowntimePerOgcd = {
-		[ACTIONS.CELESTIAL_OPPOSITION.id]: ALLOWED_DOWNTIME_CELESTIAL_OPPOSITION,
-	}
+
 	firstUseOffset = FIRST_USE_OFFSET_ALL
 	firstUseOffsetPerOgcd = {
 		[ACTIONS.DIVINATION.id]: FIRST_USE_OFFSET_DIVINATION,
-		[ACTIONS.CELESTIAL_INTERSECTION.id]: FIRST_USE_OFFSET_HEALS,
-		[ACTIONS.CELESTIAL_OPPOSITION.id]: FIRST_USE_OFFSET_HEALS,
 	}
 
 	trackedCds = [
 		ACTIONS.DIVINATION.id,
-		ACTIONS.CELESTIAL_INTERSECTION.id,
-		ACTIONS.CELESTIAL_OPPOSITION.id,
-		ACTIONS.NEUTRAL_SECT.id,
-		ACTIONS.COLLECTIVE_UNCONSCIOUS.id,
 	]
 	target = TARGETPERCENT
-	description = <Trans id="ast.ogcd-downtime.ogcd-cd-metric">Maximize usage of OGCDs to contribute to raid damage and reduce your need to GCD heal.</Trans>
+	description = <Trans id="ast.ogcd-downtime.divination"><ActionLink {...ACTIONS.DIVINATION} /> provides Astrologian with a strong amount of raid DPS when used together with arcanum.
+			Damage percentage bonuses stack multiplicatively, so it's most optimal to stack it with cards from <ActionLink {...ACTIONS.MINOR_ARCANA} />. Try to time the usage to match high output phases of other party members.
+		</Trans>
 }
