bdb1eddd87caf72b08ba35e145b9966d3f83fb6a
suShirou <suShirow>
2019-09-25 12:48:34 +0800

    remove unused code; toasty suggestions;

--- src/parser/jobs/ast/modules/Synastry.tsx
+++ src/parser/jobs/ast/modules/Synastry.tsx
@@ -5,6 +5,7 @@ import STATUSES from 'data/STATUSES'
 import {CastEvent} from 'fflogs'
 import Module, {dependency} from 'parser/core/Module'
 import Combatants from 'parser/core/modules/Combatants'
+import Cooldowns from 'parser/core/modules/Cooldowns'
 import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
 import React from 'react'
 
@@ -26,52 +27,34 @@ export default class Synastry extends Module {
 
 	@dependency private combatants!: Combatants
 	@dependency private suggestions!: Suggestions
+	@dependency private cooldowns!: Cooldowns
 
-	private lastUse = 0
-	private uses = 0
-	private totalHeld = 0
 	private nonSynastryHeals = 0
 
 	protected init() {
-		this.addHook('cast', {abilityId: ACTIONS.SYNASTRY.id, by: 'player'}, this.onCast)
 		this.addHook('cast', {abilityId: GCD_ST_HEAL, by: 'player'}, this.onSingleTargetHealCast)
 		this.addHook('complete', this.onComplete)
 	}
 
 	private onSingleTargetHealCast(event: CastEvent) {
 		// Ignore if Synastry is still on CD or we already have it up
-		if (this.combatants.selected.hasStatus(STATUSES.SYNASTRY_SELF.id)
-		|| this.lastUse + (ACTIONS.SYNASTRY.cooldown * 1000) > event.timestamp) {
+		if (this.cooldowns.getCooldownRemaining(ACTIONS.SYNASTRY.id) > 0) {
 			return
 		}
 		this.nonSynastryHeals++
 	}
 
-	private onCast(event: CastEvent) {
-		this.uses++
-		if (this.lastUse === 0) { this.lastUse = this.parser.fight.start_time }
-
-		const held = event.timestamp - this.lastUse - (ACTIONS.SYNASTRY.cooldown * 1000)
-		if (held > 0) {
-			this.totalHeld += held
-		}
-		// update the last use
-		this.lastUse = event.timestamp
-	}
-
 	onComplete() {
-		if (this.nonSynastryHeals) {
-			this.suggestions.add(new TieredSuggestion({
-				icon: ACTIONS.SYNASTRY.icon,
-				content: <Trans id="ast.synastry.suggestion.content">
-					Try to use <ActionLink {...ACTIONS.SYNASTRY} /> if you need to cast a single-target GCD heal. The GCD heal itself is already an efficiency loss, so it's better to make it as strong as possible if Synastry is not needed soon.
-				</Trans>,
-				tiers: SEVERITIES.WASTED_ST_HEAL_TIERS,
-				value: this.nonSynastryHeals,
-				why: <Trans id="ast.synastry.suggestion.why">
-					<Plural value={this.nonSynastryHeals} one="# single-target GCD heal was cast" other="# single-target GCD heals were cast" /> without synastry even though it was available.
-				</Trans>,
-			}))
-		}
+		this.suggestions.add(new TieredSuggestion({
+			icon: ACTIONS.SYNASTRY.icon,
+			content: <Trans id="ast.synastry.suggestion.content">
+				Try to use <ActionLink {...ACTIONS.SYNASTRY} /> if you need to cast a single-target GCD heal. The GCD heal itself is already an efficiency loss, so it's better to make it as strong as possible if Synastry is not needed soon.
+			</Trans>,
+			tiers: SEVERITIES.WASTED_ST_HEAL_TIERS,
+			value: this.nonSynastryHeals,
+			why: <Trans id="ast.synastry.suggestion.why">
+				<Plural value={this.nonSynastryHeals} one="# single-target GCD heal was cast" other="# single-target GCD heals were cast" /> without synastry even though it was available.
+			</Trans>,
+		}))
 	}
 }
