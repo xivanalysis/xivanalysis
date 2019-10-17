d3e0bee89aa40949f694fe79f330be3d2cb0ce49
KelosPelmand <53376212+KelosPelmand@users.noreply.github.com>
2019-10-16 20:09:25 -0400

    Added override to perform end of fight Painflare check.

--- src/parser/jobs/smn/modules/MultiHitSkills.js
+++ src/parser/jobs/smn/modules/MultiHitSkills.js
@@ -2,6 +2,11 @@ import ACTIONS from 'data/ACTIONS'
 import {AoEUsages} from 'parser/core/modules/AoEUsages'
 
 export default class MultiHit extends AoEUsages {
+	static dependencies = [
+		'gauge',
+		//need to include base class dependency or it will not be pulled in
+		'suggestions', // eslint-disable-line @xivanalysis/no-unused-dependencies
+	]
 
 	trackedAbilities = [
 		{
@@ -20,4 +25,11 @@ export default class MultiHit extends AoEUsages {
 			minTargets: 3,
 		},
 	]
+
+	adjustMinTargets(event /*: AoeEvent*/, minTargets /*: number */) {
+		if (event.ability.guid === ACTIONS.PAINFLARE.id && this.gauge.isRushingAetherflow()) {
+			return 1
+		}
+		return minTargets
+	}
 }
