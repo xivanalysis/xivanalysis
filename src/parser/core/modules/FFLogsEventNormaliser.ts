e7c366f9d0496d6f77023d0d9749c8deae3fc89e
Saxon Landers <saxon@ackwell.com.au>
2019-10-20 13:43:23 +1100

    Throw error if accessing normalised data before normalisation (#681)
    [skip ci]

--- src/parser/core/modules/FFLogsEventNormaliser.ts
+++ src/parser/core/modules/FFLogsEventNormaliser.ts
@@ -14,21 +14,26 @@ const BASELINE_EVENTS: Array<Event['type']> = [
 export class FFLogsEventNormaliser extends Module {
 	static handle: string = 'fflogsEvents'
 
-	private _hasCalculatedEvents: boolean = false
+	private _hasCalculatedEvents?: boolean
 
 	get hasCalculatedEvents() {
+		if (this._hasCalculatedEvents == null) {
+			throw new Error('Attempted to check presence of calculated events before normaliser execution.')
+		}
 		return this._hasCalculatedEvents
 	}
 
 	get damageEventName() {
-		return (this._hasCalculatedEvents ? 'calculateddamage': 'damage')
+		return (this.hasCalculatedEvents ? 'calculateddamage': 'damage')
 	}
 
 	get healEventName() {
-		return (this._hasCalculatedEvents ? 'calculatedheal': 'heal')
+		return (this.hasCalculatedEvents ? 'calculatedheal': 'heal')
 	}
 
 	normalise(events: Event[]): Event[] {
+		this._hasCalculatedEvents = false
+
 		for (const event of events) {
 			// Check to see if this is a calculated damage/heal event and set the _hasCalculated events flag if it is
 			// Once we've seen one, return
