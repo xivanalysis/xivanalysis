f01ed59aeba2a3c527d2218c6fc4440f7117ff1e
Evan Shimizu <ebshimizu@gmail.com>
2019-10-28 00:13:08 -0400

    reformatted new files

--- src/parser/jobs/drg/modules/Jumps.js
+++ src/parser/jobs/drg/modules/Jumps.js
@@ -1,14 +1,7 @@
 import {t} from '@lingui/macro'
 import {Trans, Plural} from '@lingui/react'
 import React, {Fragment} from 'react'
-import {
-	Table,
-	Button,
-	Accordion,
-	Header,
-	Message,
-	Icon,
-} from 'semantic-ui-react'
+import {Table, Button, Accordion, Header,	Message, Icon} from 'semantic-ui-react'
 
 import Module from 'parser/core/Module'
 import ACTIONS from 'data/ACTIONS'
@@ -18,18 +11,17 @@ import {Rule, Requirement} from 'parser/core/modules/Checklist'
 import {getDataBy} from 'data'
 import DISPLAY_ORDER from './DISPLAY_ORDER'
 import {TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
-// import {TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
 
 export default class Jumps extends Module {
-	static handle = 'jumps';
-	static title = t('drg.jump.title')`Jumps`;
+	static handle = 'jumps'
+	static title = t('drg.jump.title')`Jumps`
 	static dependencies = [
 		'checklist',
 		'invuln',
 		'combatants',
 		'timeline',
 		'suggestions',
-	];
+	]
 
 	// theoretically this is different depending on if you're not 80 yet, but
 	// it's probably ok to assume this is for current endgame?
@@ -52,24 +44,12 @@ export default class Jumps extends Module {
 			max: 0,
 			cd: ACTIONS.DRAGONFIRE_DIVE.cooldown * 1000,
 		},
-	};
+	}
 
 	constructor(...args) {
 		super(...args)
 
-		this.addHook(
-			'cast',
-			{
-				by: 'player',
-				abilityId: [
-					ACTIONS.JUMP.id,
-					ACTIONS.HIGH_JUMP.id,
-					ACTIONS.SPINESHATTER_DIVE.id,
-					ACTIONS.DRAGONFIRE_DIVE.id,
-				],
-			},
-			this._onJump
-		)
+		this.addHook('cast', {by: 'player',	abilityId: [ACTIONS.JUMP.id, ACTIONS.HIGH_JUMP.id, ACTIONS.SPINESHATTER_DIVE.id, ACTIONS.DRAGONFIRE_DIVE.id]}, this._onJump)
 		this.addHook('complete', this._onComplete)
 	}
 
@@ -100,12 +80,7 @@ export default class Jumps extends Module {
 		const windows = this.invuln.getInvulns()
 
 		for (const actionId in this._jumpData) {
-			this._jumpData[actionId].max = this._computeMaxJumps(
-				this._jumpData[actionId].first,
-				this.parser.fight.end_time,
-				this._jumpData[actionId].cd,
-				windows
-			)
+			this._jumpData[actionId].max = this._computeMaxJumps(this._jumpData[actionId].first, this.parser.fight.end_time, this._jumpData[actionId].cd, windows)
 		}
 	}
 
@@ -120,10 +95,7 @@ export default class Jumps extends Module {
 			active.push(STATUSES.BATTLE_LITANY.id)
 		}
 
-		if (
-			this.combatants.selected.hasStatus(STATUSES.RIGHT_EYE.id) ||
-			this.combatants.selected.hasStatus(STATUSES.RIGHT_EYE_SOLO)
-		) {
+		if (this.combatants.selected.hasStatus(STATUSES.RIGHT_EYE.id) || this.combatants.selected.hasStatus(STATUSES.RIGHT_EYE_SOLO)) {
 			active.push(STATUSES.RIGHT_EYE.id)
 		}
 
@@ -167,21 +139,14 @@ export default class Jumps extends Module {
 	}
 
 	createTimelineButton(timestamp) {
-		return (
-			<Button
-				circular
-				compact
-				icon="time"
-				size="small"
-				onClick={() =>
-					this.timeline.show(
-						timestamp - this.parser.fight.start_time,
-						timestamp - this.parser.fight.start_time
-					)
-				}
-				content={this.parser.formatTimestamp(timestamp)}
-			/>
-		)
+		return <Button
+			circular
+			compact
+			icon="time"
+			size="small"
+			onClick={() => this.timeline.show(timestamp - this.parser.fight.start_time,	timestamp - this.parser.fight.start_time)}
+			content={this.parser.formatTimestamp(timestamp)}
+		/>
 	}
 
 	_jumpAnalysis(key) {
@@ -189,65 +154,38 @@ export default class Jumps extends Module {
 
 		// this should be correct, + a bit of an epsilon. should be ok for these estimation purposes?
 		const duration = this.parser.fight.end_time - this._jumpData[key].first
-		const leeway =
-			duration - (this._jumpData[key].max - 1) * this._jumpData[key].cd
+		const leeway = duration - (this._jumpData[key].max - 1) * this._jumpData[key].cd
 		let totalDrift = 0
 
 		const rows = history.map((event, idx) => {
-			const buffCell = (
-				<Table.Cell>
-					{event.buffs.map(id => {
-						return (
-							<StatusLink
-								key={id}
-								showName={false}
-								iconSize="35px"
-								{...getDataBy(STATUSES, 'id', id)}
-							/>
-						)
-					})}
-				</Table.Cell>
-			)
+			const buffs = event.buffs.map(id => {
+				return <StatusLink	key={id} showName={false}	iconSize="35px"	{...getDataBy(STATUSES, 'id', id)} />
+			})
 
 			const delay = idx > 0 ? event.time - history[idx - 1].time : 0
 			const drift = idx > 0 ? delay - this._jumpData[key].cd : 0
 			totalDrift += drift
 
-			return (
-				<Table.Row key={event.time}>
-					<Table.Cell>{this.createTimelineButton(event.time)}</Table.Cell>
-					<Table.Cell>{this.parser.formatDuration(delay)}</Table.Cell>
-					<Table.Cell>{this.parser.formatDuration(drift)}</Table.Cell>
-					{buffCell}
-				</Table.Row>
-			)
+			return <Table.Row key={event.time}>
+				<Table.Cell>{this.createTimelineButton(event.time)}</Table.Cell>
+				<Table.Cell>{this.parser.formatDuration(delay)}</Table.Cell>
+				<Table.Cell>{this.parser.formatDuration(drift)}</Table.Cell>
+				<Table.Cell>{buffs}</Table.Cell>
+			</Table.Row>
 		})
 
-		// drift column
-		// also should display how much leeway there is to fit all possible jumps in
-
 		return {
-			table: (
-				<Table>
-					<Table.Header>
-						<Table.Row key={key + 'header'}>
-							<Table.HeaderCell>
-								<Trans id="drg.jumptable.time">Time</Trans>
-							</Table.HeaderCell>
-							<Table.HeaderCell>
-								<Trans id="drg.jumptable.delay">CD</Trans>
-							</Table.HeaderCell>
-							<Table.HeaderCell>
-								<Trans id="drg.jumptable.drift">Drift</Trans>
-							</Table.HeaderCell>
-							<Table.HeaderCell>
-								<Trans id="drg.jumptable.statuses">Personal Buffs</Trans>
-							</Table.HeaderCell>
-						</Table.Row>
-					</Table.Header>
-					<Table.Body>{rows}</Table.Body>
-				</Table>
-			),
+			table: <Table>
+				<Table.Header>
+					<Table.Row key={key + 'header'}>
+						<Table.HeaderCell><Trans id="drg.jumptable.time">Time</Trans></Table.HeaderCell>
+						<Table.HeaderCell><Trans id="drg.jumptable.delay">CD</Trans></Table.HeaderCell>
+						<Table.HeaderCell><Trans id="drg.jumptable.drift">Drift</Trans></Table.HeaderCell>
+						<Table.HeaderCell><Trans id="drg.jumptable.statuses">Personal Buffs</Trans></Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body>{rows}</Table.Body>
+			</Table>,
 			leeway,
 			totalDrift,
 		}
@@ -256,31 +194,20 @@ export default class Jumps extends Module {
 	_jumpSuggest(action, transid) {
 		const suggestID = `drg.jumps.${transid}-suggestion.content`
 		const whyID = `drg.jumps.${transid}-suggestion.why`
-		const missed =
-			this._jumpData[action.id].max - this._jumpData[action.id].history.length
+		const missed = this._jumpData[action.id].max - this._jumpData[action.id].history.length
 		const missJumpPct = Math.floor(100 - this._jumpPct(action.id))
 
 		this.suggestions.add(
 			new TieredSuggestion({
 				icon: action.icon,
-				content: (
-					<Trans id={suggestID}>
-						You should be using your <ActionLink {...action} /> as close to on
-						cooldown as possible.
-					</Trans>
-				),
+				content: <Trans id={suggestID}>You should be using your <ActionLink {...action} /> as close to on cooldown as possible.</Trans>,
 				tiers: {
 					1: SEVERITY.MINOR,
 					15: SEVERITY.MEDIUM,
 					30: SEVERITY.MAJOR,
 				},
 				value: missJumpPct,
-				why: (
-					<Trans id={whyID}>
-						You could have used <ActionLink {...action} /> {missed} more
-						<Plural value={missed} one="time" other="times" />.
-					</Trans>
-				),
+				why: <Trans id={whyID}>You could have used <ActionLink {...action} /> {missed} more <Plural value={missed} one="time" other="times" />.</Trans>,
 			})
 		)
 	}
@@ -293,40 +220,20 @@ export default class Jumps extends Module {
 
 		this.checklist.add(
 			new Rule({
-				name: (
-					<Trans id="drg.jump.checklist.name">Use Your Jumps on Cooldown</Trans>
-				),
-				description: (
-					<Fragment>
-						<Trans id="drg.jump.checklist.description">
-							Your jumps should be used as close to on cooldown as possible.
-						</Trans>
-					</Fragment>
-				),
+				name: <Trans id="drg.jump.checklist.name">Use Your Jumps on Cpoldown</Trans>,
+				description: <Trans id="drg.jump.checklist.description">Your jumps should be used as close to on cooldown as possible.</Trans>,
 				displayOrder: DISPLAY_ORDER.HIGH_JUMP,
 				requirements: [
 					new Requirement({
-						name: (
-							<Trans id="drg.jump.checklist.jump-req.name">
-								<ActionLink {...ACTIONS.HIGH_JUMP} /> uses (% of max)
-							</Trans>
-						),
+						name: <Trans id="drg.jump.checklist.jump-req.name"><ActionLink {...ACTIONS.HIGH_JUMP} /> uses (% of max)</Trans>,
 						percent: () => this._jumpPct(ACTIONS.HIGH_JUMP.id),
 					}),
 					new Requirement({
-						name: (
-							<Trans id="drg.jump.checklist.ssd-req.name">
-								<ActionLink {...ACTIONS.SPINESHATTER_DIVE} /> uses (% of max)
-							</Trans>
-						),
+						name: <Trans id="drg.jump.checklist.ssd-req.name"><ActionLink {...ACTIONS.SPINESHATTER_DIVE} /> uses (% of max)</Trans>,
 						percent: () => this._jumpPct(ACTIONS.SPINESHATTER_DIVE.id),
 					}),
 					new Requirement({
-						name: (
-							<Trans id="drg.jump.checklist.dfd-req.name">
-								<ActionLink {...ACTIONS.DRAGONFIRE_DIVE} /> uses (% of max)
-							</Trans>
-						),
+						name: <Trans id="drg.jump.checklist.dfd-req.name"><ActionLink {...ACTIONS.DRAGONFIRE_DIVE} /> uses (% of max)</Trans>,
 						percent: () => this._jumpPct(ACTIONS.DRAGONFIRE_DIVE.id),
 					}),
 				],
@@ -339,44 +246,33 @@ export default class Jumps extends Module {
 		const data = this._jumpAnalysis(action.id)
 		const noteTransID = `drg.jumps.${transid}-section-note`
 
-		return (
-			<Fragment>
-				<Message attached="top" info>
-					<Trans id={noteTransID}>
-						<Icon name={'info'} /> You used <ActionLink {...action} />{' '}
-						<strong>{this._jumpData[action.id].history.length}</strong> of{' '}
-						<strong>{this._jumpData[action.id].max}</strong> possible times.
-					</Trans>
-				</Message>
-				<Accordion
-					styled
-					fluid
-					exclusive={false}
-					panels={[
-						{
-							title: {
-								key: `title-${transid}`,
-								content: (
-									<Fragment>
-										<Trans id="drg.jumps.jump-panel-details">Details</Trans>
-									</Fragment>
-								),
-							},
-							content: {
-								key: `content-${transid}`,
-								content: data.table,
-							},
+		return <Fragment>
+			<Message attached="top" info>
+				<Trans id={noteTransID}>
+					<Icon name={'info'} /> You used <ActionLink {...action} /> <strong>{this._jumpData[action.id].history.length}</strong> of	<strong>{this._jumpData[action.id].max}</strong> possible times.
+				</Trans>
+			</Message>
+			<Accordion
+				styled
+				fluid
+				exclusive={false}
+				panels={[
+					{
+						title: {
+							key: `title-${transid}`,
+							content: <Trans id="drg.jumps.jump-panel-details">Details</Trans>,
 						},
-					]}
-				/>
-				<Message attached="bottom" info>
-					<Icon name={'clock'} /> Your casts drifted by{' '}
-					<strong>{this.parser.formatDuration(data.totalDrift)}</strong>. In
-					order to get the most uses, you needed a maximum drift of{' '}
-					<strong>{this.parser.formatDuration(data.leeway)}</strong>.
-				</Message>
-			</Fragment>
-		)
+						content: {
+							key: `content-${transid}`,
+							content: data.table,
+						},
+					},
+				]}
+			/>
+			<Message attached="bottom" info>
+				<Icon name={'clock'} /> Your casts drifted by <strong>{this.parser.formatDuration(data.totalDrift)}</strong>. In order to get the most uses, you needed a maximum drift of <strong>{this.parser.formatDuration(data.leeway)}</strong>.
+			</Message>
+		</Fragment>
 	}
 
 	output() {
@@ -384,26 +280,14 @@ export default class Jumps extends Module {
 			<Fragment>
 				<Message>
 					<Trans id="drg.jumps.analysis.message">
-						You should be using your jumps on cooldown. The number of jumps
-						you'll be able to fit will depend on the length of the fight and
-						where the boss invulnerability windows are. This analysis estimates
-						the number of maximum possible jumps, and shows you how far your
-						casts are allowed to drift overall to meet the maximum. This
-						analysis might produce very tight maximum drifts, so if you miss one
-						jump, don't worry too much about it.
+						You should be using your jumps on cooldown. The number of jumps	you'll be able to fit will depend on the length of the fight and where the boss invulnerability windows are. This analysis estimates	the number of maximum possible jumps, and shows you how far your casts are allowed to drift overall to meet the maximum. This	analysis might produce very tight maximum drifts, so if you miss one jump, don't worry too much about it.
 					</Trans>
 				</Message>
-				<Header size="small">
-					<Trans id="drg.jumps.accordion.hj-header">High Jump</Trans>
-				</Header>
+				<Header size="small"><Trans id="drg.jumps.accordion.hj-header">High Jump</Trans></Header>
 				{this._jumpAnalysisSection(ACTIONS.HIGH_JUMP, 'hj')}
-				<Header size="small">
-					<Trans id="drg.jumps.accordion.ssd-header">Spineshatter Dive</Trans>
-				</Header>
+				<Header size="small"><Trans id="drg.jumps.accordion.ssd-header">Spineshatter Dive</Trans></Header>
 				{this._jumpAnalysisSection(ACTIONS.SPINESHATTER_DIVE, 'ssd')}
-				<Header size="small">
-					<Trans id="drg.jumps.accordion.dfd-header">Dragonfire Dive</Trans>
-				</Header>
+				<Header size="small"><Trans id="drg.jumps.accordion.dfd-header">Dragonfire Dive</Trans></Header>
 				{this._jumpAnalysisSection(ACTIONS.DRAGONFIRE_DIVE, 'dfd')}
 			</Fragment>
 		)
--- src/parser/jobs/drg/modules/Positionals.js
+++ src/parser/jobs/drg/modules/Positionals.js
@@ -28,41 +28,29 @@ const TRUE_NORTH_CHARGES = 2
 const TRUE_NORTH_CD = ACTIONS.TRUE_NORTH.cooldown * 1000
 
 export default class Positionals extends Module {
-	static handle = 'positionals';
-	static title = t('drg.positionals.title')`Positionals`;
-	static dependencies = ['jumps', 'combatants', 'suggestions'];
+	static handle = 'positionals'
+	static title = t('drg.positionals.title')`Positionals`
+	static dependencies = [
+		'jumps',
+		'combatants',
+		'suggestions',
+	]
 
 	// tracking raiden thrust procs
-	_rtCombos = [];
-	_currentCombo = null;
+	_rtCombos = []
+	_currentCombo = null
 
 	// true north tracking
-	_tnCharges = 2;
-	_nextChargeIn = [];
+	_tnCharges = 2
+	_nextChargeIn = []
 
 	constructor(...args) {
 		super(...args)
 
-		this.addHook(
-			'cast',
-			{by: 'player', abilityId: ROTATION_IDS},
-			this._onGCD
-		)
-		this.addHook(
-			'cast',
-			{by: 'player', abilityId: PROC_IDS},
-			this._onProcGCD
-		)
-		this.addHook(
-			'applybuff',
-			{by: 'player', abilityId: STATUSES.RAIDEN_THRUST_READY.id},
-			this._procSuccess
-		)
-		this.addHook(
-			'cast',
-			{by: 'player', abilityId: ACTIONS.TRUE_NORTH.id},
-			this._tnUsed
-		)
+		this.addHook('cast', {by: 'player', abilityId: ROTATION_IDS}, this._onGCD)
+		this.addHook('cast', {by: 'player', abilityId: PROC_IDS}, this._onProcGCD)
+		this.addHook('applybuff', {by: 'player', abilityId: STATUSES.RAIDEN_THRUST_READY.id}, this._procSuccess)
+		this.addHook('cast', {by: 'player', abilityId: ACTIONS.TRUE_NORTH.id}, this._tnUsed)
 		this.addHook('complete', this._onComplete)
 	}
 
@@ -91,9 +79,7 @@ export default class Positionals extends Module {
 				success: false,
 				used: false,
 				trueNorthCharges: this._tnCharges,
-				trueNorthCast1: this.combatants.selected.hasStatus(
-					STATUSES.TRUE_NORTH.id
-				),
+				trueNorthCast1: this.combatants.selected.hasStatus(STATUSES.TRUE_NORTH.id),
 				time: event.timestamp,
 			}
 		} else if (this._currentCombo) {
@@ -107,9 +93,7 @@ export default class Positionals extends Module {
 				this._currentCombo.time = event.timestamp
 
 				// check for true north still active
-				this._currentCombo.trueNorthCast2 = this.combatants.selected.hasStatus(
-					STATUSES.TRUE_NORTH.id
-				)
+				this._currentCombo.trueNorthCast2 = this.combatants.selected.hasStatus(STATUSES.TRUE_NORTH.id)
 			}
 		}
 
@@ -160,150 +144,92 @@ export default class Positionals extends Module {
 		this.suggestions.add(
 			new TieredSuggestion({
 				icon: ACTIONS.RAIDEN_THRUST.icon,
-				content: (
-					<Trans id="drg.positionals.suggestions.content">
-						Performing the proper positionals for{' '}
-						<ActionLink {...ACTIONS.FANG_AND_CLAW} /> and{' '}
-						<ActionLink {...ACTIONS.WHEELING_THRUST} /> will do more damage and
-						let you use <ActionLink {...ACTIONS.RAIDEN_THRUST} />. Try to land
-						these positionals, and use <ActionLink {...ACTIONS.TRUE_NORTH} /> if
-						it's available.
-					</Trans>
-				),
+				content: <Trans id="drg.positionals.suggestions.content">Performing the proper positionals for	<ActionLink {...ACTIONS.FANG_AND_CLAW} /> and <ActionLink {...ACTIONS.WHEELING_THRUST} /> will do more damage and	let you use <ActionLink {...ACTIONS.RAIDEN_THRUST} />. Try to land these positionals, and use <ActionLink {...ACTIONS.TRUE_NORTH} /> if	it's available.	</Trans>,
 				tiers: {
 					1: SEVERITY.MINOR,
 					10: SEVERITY.MEDIUM,
 				},
 				value: missed,
-				why: (
-					<Trans id="drg.positionals.suggestions.why">
-						You missed {missed}{' '}
-						<Plural value={missed} one="proc" other="procs" /> of Raiden Thrust.
-					</Trans>
-				),
+				why: <Trans id="drg.positionals.suggestions.why">You missed {missed} <Plural value={missed} one="proc" other="procs" /> of Raiden Thrust.</Trans>,
 			})
 		)
 	}
 
 	_checkIcon(success, size = 'large') {
-		return success ? (
-			<Icon color="green" name="check" size={size} />
-		) : (
-			<Icon color="red" name="x" size={size} />
-		)
+		return success ? <Icon color="green" name="check" size={size} /> : <Icon color="red" name="x" size={size} />
 	}
 
 	_procTable() {
 		return this._rtCombos.map(combo => {
 			const action = getDataBy(ACTIONS, 'id', combo.next)
 
-			return (
-				<Table.Row key={combo.time}>
-					<Table.Cell>{this.jumps.createTimelineButton(combo.time)}</Table.Cell>
-					<Table.Cell>
-						<ActionLink {...action} />
-					</Table.Cell>
-					<Table.Cell
-						textAlign="center"
-						positive={combo.success}
-						negative={!combo.success}
-					>
-						{this._checkIcon(combo.success)}
-					</Table.Cell>
-					<Table.Cell textAlign="center">
-						{combo.trueNorthCast1 ? <Icon color="green" name="check" /> : ''}
-					</Table.Cell>
-					<Table.Cell textAlign="center">
-						{combo.trueNorthCast2 ? <Icon color="green" name="check" /> : ''}
-					</Table.Cell>
-					<Table.Cell textAlign="center">
-						{this._checkIcon(combo.trueNorthCharges > 0, '')} (
-						{combo.trueNorthCharges}{' '}
-						<Plural
-							value={combo.trueNorthCharges}
-							one="charge"
-							other="charges"
-						/>
-						)
-					</Table.Cell>
-				</Table.Row>
-			)
+			return <Table.Row key={combo.time}>
+				<Table.Cell>{this.jumps.createTimelineButton(combo.time)}</Table.Cell>
+				<Table.Cell><ActionLink {...action} /></Table.Cell>
+				<Table.Cell	textAlign="center" positive={combo.success}	negative={!combo.success}>
+					{this._checkIcon(combo.success)}
+				</Table.Cell>
+				<Table.Cell textAlign="center">
+					{combo.trueNorthCast1 ? <Icon color="green" name="check" /> : ''}
+				</Table.Cell>
+				<Table.Cell textAlign="center">
+					{combo.trueNorthCast2 ? <Icon color="green" name="check" /> : ''}
+				</Table.Cell>
+				<Table.Cell textAlign="center">
+					{this._checkIcon(combo.trueNorthCharges > 0, '')} ({combo.trueNorthCharges} <Plural	value={combo.trueNorthCharges} one="charge" other="charges"	/>)
+				</Table.Cell>
+			</Table.Row>
 		})
 	}
 
 	output() {
 		const missed = this._rtCombos.filter(combo => !combo.success).length
 
-		return (
-			<Fragment>
-				<Message>
-					<Trans id="drg.positionals.analysis.message">
-						Being in the correct position when using{' '}
-						<ActionLink {...ACTIONS.WHEELING_THRUST} /> and{' '}
-						<ActionLink {...ACTIONS.FANG_AND_CLAW} /> will allow you to use{' '}
-						<ActionLink {...ACTIONS.RAIDEN_THRUST} /> instead of{' '}
-						<ActionLink {...ACTIONS.TRUE_THRUST} />. You should be trying to
-						proc this ability as much as possible, relying on{' '}
-						<ActionLink {...ACTIONS.TRUE_NORTH} /> in situations where you
-						cannot reach the proper position. Landing every single one of these
-						positionals boosts your damage output by ~4%. The table below
-						displays all positionals, and whether or not{' '}
-						<ActionLink {...ACTIONS.TRUE_NORTH} /> was available to use.
-					</Trans>
-				</Message>
-				<Message info>
-					<Trans id="drg.positionals.analysis.missed">
-						<Icon name="info" />
-						You missed {missed} of {this._rtCombos.length} possible{' '}
-						<ActionLink {...ACTIONS.RAIDEN_THRUST} /> procs.
-						{missed > 0 ? (
-							<>
-								Of these missed procs,{' '}
-								<strong>
-									{
-										this._rtCombos.filter(
-											combo => !combo.success && combo.trueNorthCharges > 0
-										).length
-									}
-								</strong>{' '}
-								could be handled with <ActionLink {...ACTIONS.TRUE_NORTH} />{' '}
-							</>
-						) : (
-							''
-						)}
-					</Trans>
-				</Message>
-				<Table>
-					<Table.Header>
-						<Table.Row key="pos-header">
-							<Table.HeaderCell rowSpan="2">
-								<Trans id="drg.positionals.table.time">Time</Trans>
-							</Table.HeaderCell>
-							<Table.HeaderCell rowSpan="2">
-								<Trans id="drg.positionals.table.procact">Final Action</Trans>
-							</Table.HeaderCell>
-							<Table.HeaderCell rowSpan="2">
-								<Trans id="drg.positionals.table.success">Success</Trans>
-							</Table.HeaderCell>
-							<Table.HeaderCell colSpan="3" textAlign="center">
-								<Trans id="drg.positionals.table.truenorth">True North</Trans>
-							</Table.HeaderCell>
-						</Table.Row>
-						<Table.Row>
-							<Table.HeaderCell textAlign="center">
-								<Trans id="drg.positionals.table.tn1">Cast 1</Trans>
-							</Table.HeaderCell>
-							<Table.HeaderCell textAlign="center">
-								<Trans id="drg.positionals.table.tn2">Cast 2</Trans>
-							</Table.HeaderCell>
-							<Table.HeaderCell textAlign="center">
-								<Trans id="drg.positionals.table.tnavail">Available</Trans>
-							</Table.HeaderCell>
-						</Table.Row>
-					</Table.Header>
-					{this._procTable()}
-				</Table>
-			</Fragment>
-		)
+		return <Fragment>
+			<Message>
+				<Trans id="drg.positionals.analysis.message">Being in the correct position when using <ActionLink {...ACTIONS.WHEELING_THRUST} /> and	<ActionLink {...ACTIONS.FANG_AND_CLAW} /> will allow you to use <ActionLink {...ACTIONS.RAIDEN_THRUST} /> instead of <ActionLink {...ACTIONS.TRUE_THRUST} />. You should be trying to proc this ability as much as possible, relying on <ActionLink {...ACTIONS.TRUE_NORTH} /> in situations where you cannot reach the proper position. Landing every single one of these positionals boosts your damage output by ~4%. The table below	displays all positionals, and whether or not <ActionLink {...ACTIONS.TRUE_NORTH} /> was available to use.</Trans>
+			</Message>
+			<Message info>
+				<Trans id="drg.positionals.analysis.missed"><Icon name="info" /> You missed {missed} of {this._rtCombos.length} possible <ActionLink {...ACTIONS.RAIDEN_THRUST} /> procs.
+					{missed > 0 ? (
+						<>
+							Of these missed procs, <strong>{this._rtCombos.filter(combo => !combo.success && combo.trueNorthCharges > 0).length}</strong> could be handled with <ActionLink {...ACTIONS.TRUE_NORTH} />
+						</>
+					) : (
+						''
+					)}
+				</Trans>
+			</Message>
+			<Table>
+				<Table.Header>
+					<Table.Row key="pos-header">
+						<Table.HeaderCell rowSpan="2">
+							<Trans id="drg.positionals.table.time">Time</Trans>
+						</Table.HeaderCell>
+						<Table.HeaderCell rowSpan="2">
+							<Trans id="drg.positionals.table.procact">Final Action</Trans>
+						</Table.HeaderCell>
+						<Table.HeaderCell rowSpan="2">
+							<Trans id="drg.positionals.table.success">Success</Trans>
+						</Table.HeaderCell>
+						<Table.HeaderCell colSpan="3" textAlign="center">
+							<Trans id="drg.positionals.table.truenorth">True North</Trans>
+						</Table.HeaderCell>
+					</Table.Row>
+					<Table.Row>
+						<Table.HeaderCell textAlign="center">
+							<Trans id="drg.positionals.table.tn1">Cast 1</Trans>
+						</Table.HeaderCell>
+						<Table.HeaderCell textAlign="center">
+							<Trans id="drg.positionals.table.tn2">Cast 2</Trans>
+						</Table.HeaderCell>
+						<Table.HeaderCell textAlign="center">
+							<Trans id="drg.positionals.table.tnavail">Available</Trans>
+						</Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
+				{this._procTable()}
+			</Table>
+		</Fragment>
 	}
 }
