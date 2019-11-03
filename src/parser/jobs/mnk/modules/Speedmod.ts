e7644612b6baa9ee90c66d875d9484ab3d1e2cd9
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2019-11-03 21:18:34 +1100

    Remove unused init

--- src/parser/jobs/mnk/modules/Speedmod.ts
+++ src/parser/jobs/mnk/modules/Speedmod.ts
@@ -8,10 +8,6 @@ const SPEED_INCREASE_PER_STACK = 5
 export default class Speedmod extends CoreSpeedmod {
 	@dependency private gauge!: Gauge
 
-	protected init(): void {
-		super.init()
-	}
-
 	getJobAdditionalSpeedbuffScalar(event: TODO) {
 		return (100 - (this.gauge.getStacksAt(event.timestamp) * SPEED_INCREASE_PER_STACK)) / 100
 	}
