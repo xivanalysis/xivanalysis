6a47c0b1bfa4f71ee0d02656ee455ca506c1be22
Saxon Landers <saxon@ackwell.com.au>
2019-11-12 20:32:12 +1100

    Taking bets on if the user has perms for labels

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,13 @@
 import {danger, message} from 'danger'
 
+const Labels = {
+	Parser: {
+		CORE: {name: 'Parser - Core', description: '', color: '#aaaaaa'},
+	},
+}
+
 const modifiedMD = danger.git.modified_files.join('\n- ')
 message('Changed Files in this PR: \n- ' + modifiedMD)
+
+// Label test
+danger.github.utils.createOrAddLabel(Labels.Parser.CORE)
