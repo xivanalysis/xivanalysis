eefda6eccb3ad6ec5c91650ea962d6a0fd3c41b8
Saxon Landers <saxon@ackwell.com.au>
2019-11-24 20:56:35 +1100

    Remove test layer

--- src/data/STATUSES/layers.ts
+++ src/data/STATUSES/layers.ts
@@ -2,7 +2,7 @@ import {Layer} from 'data/layer'
 import {StatusRoot} from './root'
 
 export const layers: Array<Layer<StatusRoot>> = [
-	{patch: '5.01', data: {
-		BIO_II: {id: 1000},
-	}},
+	// Layers should be in their own files, and imported for use here.
+	// Example layer:
+	// {patch: '5.05', data: {BIO_II: {id: 9001}}}}
 ]
