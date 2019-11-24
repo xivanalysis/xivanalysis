6afd700d592ef3756f3ea7048d8daaa605354b00
Saxon Landers <saxon@ackwell.com.au>
2019-11-24 17:19:08 +1100

    Fleshed out module a bit

--- src/parser/core/modules/Data.ts
+++ src/parser/core/modules/Data.ts
@@ -1,16 +1,30 @@
-import {applyLayer} from 'data/layer'
-import collated, {layers, root} from 'data/STATUSES/newIndex'
+import {applyLayer, Layer, LayerData} from 'data/layer'
+import {layers as statusLayers, root as statusRoot} from 'data/STATUSES/newIndex'
 import Module from 'parser/core/Module'
 
 export class Data extends Module {
 	static handle = 'data'
 	static debug = true
 
-	protected init() {
-		const statuses = layers
+	private appliedCache = new Map<unknown, unknown>()
+
+	get statuses() {
+		return this.getAppliedData(statusRoot, statusLayers)
+	}
+
+	private getAppliedData<R>(root: R, layers: Array<Layer<R>>): R {
+		const cached = this.appliedCache.get(root)
+		if (cached) {
+			return cached as R
+		}
+
+		this.debug('generating applied data for', root)
+
+		const applied = layers
 			.filter(layer => this.parser.patch.compare(layer.patch) >= 0)
 			.reduce(applyLayer, root)
-		this.debug(statuses)
-		this.debug(collated)
+		this.appliedCache.set(root, applied)
+
+		return applied
 	}
 }
