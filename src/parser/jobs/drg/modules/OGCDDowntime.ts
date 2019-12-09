27fe0d58385c2294d25a3950e7454eeb377bf6ab
Evan Shimizu <ebshimizu@gmail.com>
2019-12-08 21:27:34 -0500

    drg: tighter ogcd first use offsets

--- src/parser/jobs/drg/modules/OGCDDowntime.ts
+++ src/parser/jobs/drg/modules/OGCDDowntime.ts
@@ -1,17 +1,45 @@
 import ACTIONS from 'data/ACTIONS'
 import {CooldownDowntime} from 'parser/core/modules/CooldownDowntime'
 
-const DEFAULT_FIRST_USE_OFFSET = 25000
+// at high skill speeds, Battle Litany is first, so the order is a bit fluid,
+// however all are used before the third GCD
+const BUFF_FIRST_USE_OFFSET = 5000
+
+// the high sks opener delays jumps to better line up with later windows,
+// at least that's what I'm told. These timings should account for both openers,
+// assuming the fast sks threshold of 2.35s
+const JUMP_FIRST_USE_OFFSET = 11750
+const SSD_FIRST_USE_OFFSET = 16450
+const DFD_FIRST_USE_OFFSET = 18800
+
+// always before Full Thrust, the 8th GCD
+const LIFE_SURGE_FIRST_USE_OFFSET = 17500
 
 export default class OGCDDowntime extends CooldownDowntime {
-	defaultFirstUseOffset = DEFAULT_FIRST_USE_OFFSET
 	trackedCds = [
-		{cooldowns: [ACTIONS.HIGH_JUMP]},
-		{cooldowns: [ACTIONS.SPINESHATTER_DIVE]},
-		{cooldowns: [ACTIONS.DRAGONFIRE_DIVE]},
-		{cooldowns: [ACTIONS.LIFE_SURGE]},
-		{cooldowns: [ACTIONS.LANCE_CHARGE]},
-		{cooldowns: [ACTIONS.DRAGON_SIGHT]},
-		{cooldowns: [ACTIONS.BATTLE_LITANY]},
+		{
+			cooldowns: [ACTIONS.HIGH_JUMP],
+			firstUseOffset: JUMP_FIRST_USE_OFFSET,
+		},
+		{
+			cooldowns: [ACTIONS.SPINESHATTER_DIVE],
+			firstUseOffset: SSD_FIRST_USE_OFFSET,
+		},
+		{
+			cooldowns: [ACTIONS.DRAGONFIRE_DIVE],
+			firstUseOffset: DFD_FIRST_USE_OFFSET,
+		},
+		{
+			cooldowns: [ACTIONS.LIFE_SURGE],
+			firstUseOffset: LIFE_SURGE_FIRST_USE_OFFSET,
+		},
+		{
+			cooldowns: [
+				ACTIONS.LANCE_CHARGE,
+				ACTIONS.DRAGON_SIGHT,
+				ACTIONS.BATTLE_LITANY,
+			],
+			firstUseOffset: BUFF_FIRST_USE_OFFSET,
+		},
 	]
 }
