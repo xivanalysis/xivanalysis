37bbf69f5c011e3a0182c7eda7bb2d081015c49e
Evan Shimizu <ebshimizu@gmail.com>
2019-12-12 20:21:11 -0500

    drg: adjusted first offset

--- src/parser/jobs/drg/modules/OGCDDowntime.ts
+++ src/parser/jobs/drg/modules/OGCDDowntime.ts
@@ -8,9 +8,9 @@ const BUFF_FIRST_USE_OFFSET = 5000
 // the high sks opener delays jumps to better line up with later windows,
 // at least that's what I'm told. These timings should account for both openers,
 // assuming the fast sks threshold of 2.35s
-const JUMP_FIRST_USE_OFFSET = 11750
-const SSD_FIRST_USE_OFFSET = 16450
-const DFD_FIRST_USE_OFFSET = 18800
+const JUMP_FIRST_USE_OFFSET = 14100		// before 7th gcd
+const SSD_FIRST_USE_OFFSET = 18800		// before 9th gcd
+const DFD_FIRST_USE_OFFSET = 21500		// before 10th gcd
 
 // always before Full Thrust, the 8th GCD
 const LIFE_SURGE_FIRST_USE_OFFSET = 17500
