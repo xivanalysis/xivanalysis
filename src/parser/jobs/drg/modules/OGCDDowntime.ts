c0fcbfa92ebd15cf008f587b30b094cf71b2dd8e
Evan Shimizu <ebshimizu@gmail.com>
2019-12-17 19:55:30 -0500

    looks like ~2s delay max before first gcd for most

--- src/parser/jobs/drg/modules/OGCDDowntime.ts
+++ src/parser/jobs/drg/modules/OGCDDowntime.ts
@@ -1,19 +1,20 @@
 import ACTIONS from 'data/ACTIONS'
 import {CooldownDowntime} from 'parser/core/modules/CooldownDowntime'
 
+// +2s start of fight buffer added for all first use
 // at high skill speeds, Battle Litany is first, so the order is a bit fluid,
 // however all are used before the third GCD
-const BUFF_FIRST_USE_OFFSET = 5000
+const BUFF_FIRST_USE_OFFSET = 7000
 
 // the high sks opener delays jumps to better line up with later windows,
-// at least that's what I'm told. These timings should account for both openers,
-// assuming the fast sks threshold of 2.35s
-const JUMP_FIRST_USE_OFFSET = 14100		// before 7th gcd
-const SSD_FIRST_USE_OFFSET = 18800		// before 9th gcd
-const DFD_FIRST_USE_OFFSET = 21500		// before 10th gcd
+// but isn't used with current sets. Timings listed here should work
+// for both openers, if the high sks build ever becomes relevant.
+const JUMP_FIRST_USE_OFFSET = 16100		// before 7th gcd
+const SSD_FIRST_USE_OFFSET = 20800		// before 9th gcd
+const DFD_FIRST_USE_OFFSET = 23500		// before 10th gcd
 
 // always before Full Thrust, the 8th GCD
-const LIFE_SURGE_FIRST_USE_OFFSET = 17500
+const LIFE_SURGE_FIRST_USE_OFFSET = 19500
 
 export default class OGCDDowntime extends CooldownDowntime {
 	defaultFirstUseOffset = BUFF_FIRST_USE_OFFSET
