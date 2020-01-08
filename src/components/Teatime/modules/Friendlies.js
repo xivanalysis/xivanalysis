0d67f50cdddd2a9427dcddcef861825eba586034
Kevin Coffey <kevin.coffey@datadoghq.com>
2020-01-08 08:44:18 -0500

    Sorting players consistently is nice (#3)

--- src/components/Teatime/modules/Friendlies.js
+++ src/components/Teatime/modules/Friendlies.js
@@ -1,11 +1,31 @@
+import stable from 'stable'
+
 import {ActorType} from 'fflogs'
 import Module from 'parser/core/Module'
 
+const ACTOR_TYPE_ORDER = {
+	[ActorType.PALADIN]: 0,
+	[ActorType.WARRIOR]: 1,
+	[ActorType.DARK_KNIGHT]: 2,
+	[ActorType.GUNBREAKER]: 3,
+	[ActorType.WHITE_MAGE]: 4,
+	[ActorType.SCHOLAR]: 5,
+	[ActorType.ASTROLOGIAN]: 6,
+	[ActorType.MONK]: 7,
+	[ActorType.DRAGOON]: 8,
+	[ActorType.NINJA]: 9,
+	[ActorType.SAMURAI]: 10,
+	[ActorType.BARD]: 11,
+	[ActorType.MACHINIST]: 12,
+	[ActorType.DANCER]: 13,
+	[ActorType.BLACK_MAGE]: 14,
+	[ActorType.SUMMONER]: 15,
+	[ActorType.RED_MAGE]: 16,
+}
+
 export default class Friendlies extends Module {
 	static handle = 'friendlies'
-	static dependencies = [
-		// 'data',
-	]
+	static dependencies = []
 
 	_playerFriendlies = []
 
@@ -16,6 +36,13 @@ export default class Friendlies extends Module {
 		const reportFriendlies = this.parser.report.friendlies
 		const fight_id = this.parser.fight.id
 		this._playerFriendlies = this._filterPlayers(reportFriendlies, fight_id)
+		stable.inplace(this._playerFriendlies, (a, b) => {
+			if (a.type === b.type) {
+				return a.id - b.id
+			}
+			return ACTOR_TYPE_ORDER[a.type] - ACTOR_TYPE_ORDER[b.type]
+		})
+
 	}
 
 	_filterPlayers(friendlies, currentFight) {
