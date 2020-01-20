72be5dd1b96eaf15fd6eb8f29fa091f38a230cd7
paige404 <59348003+paige404@users.noreply.github.com>
2020-01-20 06:46:23 -0500

    Add Blue Mage (#753)

---
+++ src/parser/jobs/blu/changelog.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import CONTRIBUTORS from 'data/CONTRIBUTORS'
+
+export const changelog = [
+	{
+		date: new Date('2019-12-30'),
+		Changes: () => <>
+			Added this changelog.
+		</>,
+		contributors: [CONTRIBUTORS.PAIGE_404],
+	},
+]
