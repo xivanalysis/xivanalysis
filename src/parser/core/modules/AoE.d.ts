439934b972f508a5202e0f7c8cc85697ea6cd4fb
Saxon Landers <saxon@ackwell.com.au>
2020-02-10 20:12:43 +1100

    [skip ci] Fix up some of the core ts defs

--- src/parser/core/modules/AoE.d.ts
+++ src/parser/core/modules/AoE.d.ts
@@ -1,5 +1,9 @@
-import {AbilityEvent} from 'fflogs'
+import {AbilityEvent, Event} from 'fflogs'
 import Module from 'parser/core/Module'
+import Enemies from './Enemies'
+import {FFLogsEventNormaliser} from './FFLogsEventNormaliser'
+import PrecastAction from './PrecastAction'
+import PrecastStatus from './PrecastStatus'
 
 interface Hit {
 	id: number
@@ -14,4 +18,11 @@ export interface AoeEvent extends AbilityEvent {
 	successfulHit: boolean
 }
 
-export default class AoE extends Module {}
+export default class AoE extends Module {
+	protected precastAction: PrecastAction
+	protected precastStatus: PrecastStatus
+	protected enemies: Enemies
+	protected fflogsEvents: FFLogsEventNormaliser
+
+	isValidHit(event: Event): boolean
+}
