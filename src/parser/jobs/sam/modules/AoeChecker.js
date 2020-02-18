bd6e89bdf27f0283a89e56553a5fae8cff0ba04b
Ryan Beck <rjb17f@my.fsu.edu>
2020-02-17 19:36:24 -0500

    broken.

--- src/parser/jobs/sam/modules/AoeChecker.js
+++ src/parser/jobs/sam/modules/AoeChecker.js
@@ -1,140 +1,69 @@
-import {t} from '@lingui/macro'
-import {Trans, Plural} from '@lingui/react'
-import React from 'react'
 import ACTIONS from 'data/ACTIONS'
-import Module from 'parser/core/Module'
-import {TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
+import STATUSES from 'data/STATUSES'
+import {AoEUsages} from 'parser/core/modules/AoeUsages'
+//const GAIN_AT_3 = new Set([ACTIONS.FUGA.id, ACTIONS.OKA.id, ACTIONS.MANGETSU.id, ACTIONS.HISSATSU_KYUTEN.id])
+//const GAIN_AT_2 = new Set([ACTIONS.HISSATSU_GUREN.id, ACTIONS.TENKA_GOKEN.id, ACTIONS.KAESHI_GOKEN.id])
 
-//All sets contain AoE moves to be checked, grouped by type (GCD, SEN, KENKI)
-const AOE_GCDS = new Set([ACTIONS.FUGA.id, ACTIONS.OKA.id, ACTIONS.MANGETSU.id])
-const AOE_SENS = new Set([ACTIONS.TENKA_GOKEN.id, ACTIONS.KAESHI_GOKEN.id])
-const AOE_KENKI = new Set([ACTIONS.HISSATSU_KYUTEN.id, ACTIONS.HISSATSU_GUREN.id])
-
-//These sets are based on break Points, and not type of resource used
-
-const GAIN_AT_3 = new Set([ACTIONS.FUGA.id, ACTIONS.OKA.id, ACTIONS.MANGETSU.id, ACTIONS.HISSATSU_KYUTEN.id])
-const GAIN_AT_2 = new Set([ACTIONS.HISSATSU_GUREN.id, ACTIONS.TENKA_GOKEN.id, ACTIONS.KAESHI_GOKEN.id])
-
-//saying const # don't work so screw it
-
-const GAIN3 = 3
-const GAIN2 = 2
-
-export default class AoeChecker extends Module {
-	static handle = 'aoechecker'
-	static title = t('sam.aoechecker.title')`AoeChecker`
+export default class AoeChecker extends AoEUsages {
 	static dependencies = [
-		'suggestions',
+		...AoEUsages.dependencies,
+		'combatants',
 	]
 
-	_badAoeGCDs = 0 //amount of aoe GCDs that didn't hit their break point for a gain
-	_badSenCasts = 0 //amount of Goken/Tsubame Goken that didn't hit the break point for damage gain
-	_badKenkiCasts = 0 //amount of Kenki Casts that didn't break the damage gain threshold
+	suggestionIcon = ACTIONS.FUGA.icon
 
-	constructor(...args) {
-		super(...args)
-		this.addHook('aoedamage', {by: 'player'}, this._onAoe)
-		this.addHook('complete', this._onComplete)
-	}
+	trackedAbilites = [
+		{
+			aoeAbility: ACTIONS.HISSATSU_GUREN,
+			stAbilties: [ACTIONS.HISSATSU_SENEI],
+			minTargets: 2,
+		},
 
-	_onAoe(event) {
+		{
+			aoeAbility: ACTIONS.HISSATSU_KYUTEN,
+			stAbilties: [ACTIONS.HISSTASU_SHINTEN],
+			minTargets: 3,
+		},
 
-		const action = event.ability.guid
+		{
+			aoeAbility: ACTIONS.TENKA_GOKEN,
+			stAbilites: [ACTIONS.MIDARE_SETSUGEKKA],
+			minTargets: 2,
+		},
 
-		//Step 1: Find break point
+		{
+			aoeAbility: ACTIONS.KAESHI_GOKEN,
+			stAbilites: [ACTIONS.KAESHI_SETSUGEKKA],
+			minTargets: 2,
+		},
 
-		if (GAIN_AT_2.has(action)) {
+		{
+			aoeAbility: ACTIONS.FUGA,
+			stAbilites: [ACTIONS.HAKAZE],
+			minTargets: 3,
 
-			//Step 2: Check break point
+		},
 
-			if (event.hits.length < GAIN2) {
-				//Step 3: Check type of resource used and increment
+		{
+			aoeAbility: ACTIONS.OKA,
+			stAbilites: [ACTIONS.KASHA],
+			minTargets: 3,
 
-				if (AOE_GCDS.has(action)) {
+		},
 
-					this._badAoeGCDs++
-				} else if (AOE_SENS.has(action)) {
+		{
+			aoeAbility: ACTIONS.MANGETSU,
+			stAbilites: [ACTIONS.GEKKO],
+			minTargets: 3,
 
-					this._badSenCasts++
-				} else if (AOE_KENKI.has(action)) {
+		},
+	]
 
-					this._badKenkiCasts++
-				}
-			}
-
-		} else if (GAIN_AT_3.has(action)) {
-
-			//Step 2: Check break point
-
-			if (event.hits.length < GAIN3) {
-				//Step 3: Check type of resource used and increment
-
-				if (AOE_GCDS.has(action)) {
-
-					this._badAoeGCDs++
-				} else if (AOE_SENS.has(action)) {
-
-					this._badSenCasts++
-				} else if (AOE_KENKI.has(action)) {
-
-					this._badKenkiCasts++
-				}
-			}
+	adjustMinTargets(event, minTargets) {
+		if (event.ability.guid === (ACTIONS.MANGETSU || ACTIONS.OKA) && !this.combatants.hasStatus(STATUSES.MEIKYO_SHISUI.id)) {
+			return 1
 		}
-	}
-
-	_onComplete() {
-
-		//SUGGESTION TIME!
-
-		this.suggestions.add(new TieredSuggestion({
-			icon: ACTIONS.HISSATSU_GUREN.icon,
-			content: <Trans id= "sam.aoechecker.suggestions.badkenki.content">
-			Using aoe moves when you don't reach the target threshold for it to be a gain is a loss.
-			</Trans>,
-			tiers: {
-				1: SEVERITY.MINOR,
-				3: SEVERITY.MEDIUM,
-				5: SEVERITY.MAJOR,
-			},
-			value: this._badKenkiCasts,
-			why: <Trans id = "sam.aoechecker.suggestions.aoekenki.why">
-					You misused kenki moves <Plural value ={this._badKenkiCasts} one="# time" other="# times" /> during the fight.
-			</Trans>,
-		}))
-
-		this.suggestions.add(new TieredSuggestion({
-			icon: ACTIONS.TENKA_GOKEN.icon,
-			content: <Trans id= "sam.aoechecker.suggestions.badsen.content">
-                        Using aoe moves when you don't reach the target threshold for it to be a gain is a loss.
-			</Trans>,
-			tiers: {
-				1: SEVERITY.MINOR,
-				3: SEVERITY.MEDIUM,
-				5: SEVERITY.MAJOR,
-			},
-			value: this._badSenCasts,
-			why: <Trans id = "sam.aoechecker.suggestions.badsen.why">
-                                        You misused sen moves <Plural value ={this._badSenCasts} one="# time" other="# times" /> during the fight.
-			</Trans>,
-		}))
-
-		this.suggestions.add(new TieredSuggestion({
-			icon: ACTIONS.FUGA.icon,
-			content: <Trans id= "sam.aoechecker.suggestions.badgcd.content">
-                        Using aoe moves when you don't reach the target threshold for it to be a gain is a loss.
-			</Trans>,
-			tiers: {
-				1: SEVERITY.MINOR,
-				3: SEVERITY.MEDIUM,
-				5: SEVERITY.MAJOR,
-			},
-			value: this._badAoeGCDs,
-			why: <Trans id = "sam.aoechecker.suggestions.badgcd.why">
-                                        You misused aoe GCDs <Plural value ={this._badAoeGCDs} one="# time" other="# times" /> during the fight.
-			</Trans>,
-		}))
-
+		return minTargets
 	}
 }
 
