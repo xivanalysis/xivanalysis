9682e4aceb666ffe949b66c3235b4ac44f9781fe
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2020-03-09 03:31:45 +1100

    MNK 5.2 support and smol feex for steppies (#789)
    * Update MNK modules to use new data fetchers and TS 3.7 niceness
    
    * Fix bug with Bootshine positional not checking correct form
    
    * Bump MNK to support 5.2
    
    * feex lingui
    
    * chop chop changelogging
    
    * Fix wording in dropped Forms suggestion
    
    * Fix whitespace in dropped RoF suggestion
    
    * Feex Data dependency and some bad conditions
    
    * Feex Data import and some bad boolean conditions
    
    * Avoid flagging formless GCDs when PB kills form
    
    * Jesus fuck I hate you lingui you can feex your extracted balls
    
    * Now Playing: Step Two - Parov Stelar

--- src/parser/jobs/mnk/modules/Weaving.ts
+++ src/parser/jobs/mnk/modules/Weaving.ts
@@ -1,15 +1,15 @@
 import ACTIONS from 'data/ACTIONS'
-import CoreWeaving from 'parser/core/modules/Weaving'
+import {CastEvent} from 'fflogs'
+import CoreWeaving, {WeaveInfo} from 'parser/core/modules/Weaving'
 
 const SSS_MAX_WEAVES = 4
 
 export default class Weaving extends CoreWeaving {
-	isBadWeave(weave: TODO /*, maxWeaves*/) {
-		if (weave.leadingGcdEvent &&
-			weave.leadingGcdEvent.ability &&
-			weave.leadingGcdEvent.ability.guid === ACTIONS.SIX_SIDED_STAR.id) {
-				return weave.weaves.length > SSS_MAX_WEAVES
-			}
+	isBadWeave(weave: WeaveInfo /*, maxWeaves*/) {
+		const gcd = weave.leadingGcdEvent as CastEvent
+		if (gcd?.ability && gcd?.ability?.guid === ACTIONS.SIX_SIDED_STAR.id) {
+			return weave.weaves.length > SSS_MAX_WEAVES
+		}
 
 		return super.isBadWeave(weave)
 	}
