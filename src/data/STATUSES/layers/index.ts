a6beee67d950c9d0c0391b39f7ab3c83e29a9434
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2020-03-09 17:19:00 +1100

    Add layers for patch 5.1 WAR status updates

--- src/data/STATUSES/layers/index.ts
+++ src/data/STATUSES/layers/index.ts
@@ -1,8 +1,12 @@
 import {Layer} from 'data/layer'
 import {StatusRoot} from '../root'
 
+import {patch510} from './patch5.1'
+
 export const layers: Array<Layer<StatusRoot>> = [
 	// Layers should be in their own files, and imported for use here.
 	// Example layer:
 	// {patch: '5.05', data: {BIO_II: {id: 9001}}}}
+
+	patch510,
 ]
