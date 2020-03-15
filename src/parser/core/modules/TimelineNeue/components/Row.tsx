834b21d76fde322a18caa42d52e9677f4cd16389
Saxon Landers <saxon@ackwell.com.au>
2020-03-15 23:01:56 +1100

    Rewrite practically the entire Row system

--- src/parser/core/modules/TimelineNeue/components/Row.tsx
+++ src/parser/core/modules/TimelineNeue/components/Row.tsx
@@ -1,152 +1,200 @@
 import classNames from 'classnames'
-import React, {createContext, memo, PropsWithChildren, ReactNode, useCallback, useContext, useMemo, useRef, useState} from 'react'
+import React, {createContext, memo, ReactNode, useCallback, useContext, useEffect, useMemo, useRef, useState} from 'react'
 import Measure, {ContentRect} from 'react-measure'
 import styles from './Component.module.css'
 
-interface LabelContextValue {
-	/** Current available width for labels. */
-	width: number
-	/** Call to report own width, used to choose the final available width. */
-	reportWidth: (id: object, width?: number) => void
-	/** When true, the row should be in a collapsed state, deferring to parent for layout. */
-	collapsed?: boolean
+interface RowContextValue {
+	collapse: boolean
+	siblingSize: number
+	reportSize: (id: number, size: number) => void
 }
 
-const LabelContext = createContext<LabelContextValue>({
-	width: 0,
-	reportWidth: () => { throw new Error('Attempting to report width with no parent label context.') },
-	collapsed: false,
+const RowContext = createContext<RowContextValue>({
+	collapse: false,
+	siblingSize: 0,
+	reportSize: () => { throw new Error('No provider found') },
 })
-/**
- * Reuseable width calculator. Returned label context will represent the
- * widest reported width.
- */
-function useMaxWidthCalculator() {
-	const [width, setWidth] = useState(0)
 
-	// Map that will store all the current widths
-	const widthStore = useRef(new Map<object, number>()).current
-
-	// Calculate + return the new max width on each report. A report of undefined
-	// signals the element is being removed, and should no longer be considered.
-	const reportWidth = useCallback(
-		(id: object, newWidth?: number) => {
-			newWidth != null
-				? widthStore.set(id, newWidth)
-				: widthStore.delete(id)
-
-			const maxWidth = Math.max(0, ...widthStore.values())
-			setWidth(maxWidth)
-			return maxWidth
+function useSizeCalculator() {
+	const [sizes, setSizes] = useState<Record<number, number>>({})
+	const reportSize = useCallback(
+		(id: number, size: number) => {
+			setSizes(value => ({
+				...value,
+				[id]: size,
+			}))
 		},
 		[],
 	)
 
-	return useMemo(() => ({width, reportWidth}), [width, reportWidth])
+	const maxSize = useMemo(
+		() => Math.max(0, ...Object.values(sizes)),
+		[sizes],
+	)
+
+	return [maxSize, reportSize] as const
 }
 
 /**
  * Root row handler, provides space beside the rest of the timeline for rows
  * to put their labels.
  */
-export const LabelSpacer = memo(function LabelSpacer(props) {
-	const widthContext = useMaxWidthCalculator()
+export const LabelSpacer = memo(function LabelSpacer({children}) {
+	const [maxChildSize, reportChildSize] = useSizeCalculator()
+
+	const rowContextValue = useMemo(
+		() => ({
+			collapse: false,
+			siblingSize: maxChildSize,
+			reportSize: reportChildSize,
+		}),
+		[maxChildSize, reportChildSize],
+	)
 
 	return (
-		<div style={{paddingLeft: widthContext.width}}>
-			<LabelContext.Provider value={widthContext}>
-				{props.children}
-			</LabelContext.Provider>
+		<div style={{paddingLeft: maxChildSize}}>
+			<RowContext.Provider value={rowContextValue}>
+				{children}
+			</RowContext.Provider>
 		</div>
 	)
 })
 
 export interface RowProps {
+	children?: ReactNode,
 	/** Label to display beside this row. */
 	label?: ReactNode
 }
 
-export const Row = memo<PropsWithChildren<RowProps>>(function Row(props) {
-	// Set up width values for the parent, self, and children
-	const {
-		width: parentWidth,
-		reportWidth: parentReportWidth,
-		collapsed: parentCollapsed,
-	} = useContext(LabelContext)
-	const ownWidth = useRef(0)
-	const childWidthContext = useMaxWidthCalculator()
+export const Row = memo<RowProps>(function Row({children, label}) {
+	const rowId = useUniqueId()
 
-	// Track explicit collapsed status & read in the parent's state
-	// TODO: Figure out how this will interact with props, if any
-	const [collapsed, setCollapsed] = useState(false)
+	const {collapse, siblingSize, reportSize} = useContext(RowContext)
 
-	// We're using a... blank object... as a unique reference. Because that's Smort™.
-	const rowId = useRef({}).current
+	// Track own label size
+	const [labelSize, setLabelSize] = useState(0)
 
-	// When _we_ resize, track our width, and report it along side the max child width
-	const onResize = ({bounds}: ContentRect) => {
-		if (!bounds?.width) { return }
-		ownWidth.current = (bounds.width)
-		parentReportWidth(rowId, bounds.width + childWidthContext.width)
+	// Track the maximum size of child labels
+	const [maxChildSize, reportChildSize] = useSizeCalculator()
+
+	// Report when the total size of this label changes
+	useEffect(
+		() => reportSize(rowId, maxChildSize + labelSize),
+		[reportSize, maxChildSize, labelSize],
+	)
+
+	// A label is "collapsed" if it has active children
+	// Active children take up space, so assume maxChildSize is representative
+	const labelCollapsed = maxChildSize > 0
+
+	// Clicking a label toggles collapsing its children
+	const [collapseChildren, setCollapseChildren] = useState(false)
+	const onClick = useCallback(
+		() => setCollapseChildren(value => !value),
+		[],
+	)
+
+	const rowContextValue = {
+		collapse: collapse || collapseChildren,
+		siblingSize: siblingSize - labelSize,
+		reportSize: reportChildSize,
 	}
-
-	// When the ref is nulled, report a lack of width so the context can wipe us
-	// I'm not using a useEffect destructor here, as the label can be removed without unmounting
-	const ref = (elem: HTMLDivElement | null) => {
-		if (elem != null) { return }
-		ownWidth.current = 0
-		parentReportWidth(rowId, undefined)
-	}
-
-	// "Proxy" over the parent context, adjusting the values to nest the children
-	const childContext = {
-		width: parentWidth - ownWidth.current,
-		reportWidth: (childId: object, childWidth?: number) => {
-			const maxChildWidth = childWidthContext.reportWidth(childId, childWidth)
-			parentReportWidth(rowId, ownWidth.current + maxChildWidth)
-		},
-		collapsed: collapsed || parentCollapsed,
-	}
-
-	const hasChildLabels = childWidthContext.width > 0
-	const clickable = hasChildLabels || collapsed
-	const labelWidth = hasChildLabels ? ownWidth.current : parentWidth
-
-	const onLabelClick = useCallback(() => setCollapsed(collapsed => !collapsed), [])
 
 	return (
-		<div className={parentCollapsed ? undefined : styles.row}>
-			{props.label && !parentCollapsed && (
-				<div
-					onClick={clickable ? onLabelClick : undefined}
-					className={classNames(clickable && styles.clickable)}
+		<div className={collapse ? undefined : styles.row}>
+			{label != null && !collapse && (
+				<Label
+					collapsed={labelCollapsed}
+					offset={siblingSize}
+					reportSize={setLabelSize}
+					onClick={onClick}
 				>
-					<div
-						className={classNames(
-							styles.labelBackground,
-							hasChildLabels && styles.collapsed,
-						)}
-						style={{left: -parentWidth, width: labelWidth}}
-					/>
-					<Measure innerRef={ref} bounds onResize={onResize}>
-						{({measureRef}) => (
-							<div
-								ref={measureRef}
-								className={styles.labelContent}
-								style={{left: -parentWidth}}
-							>
-								{/* TODO: These take up space that then needs to be re-measured. Look into fixing. */}
-								{collapsed && '⯈'}
-								{!collapsed && hasChildLabels && '⯆'}
-								{props.label}
-							</div>
-						)}
-					</Measure>
-				</div>
+					{label}
+				</Label>
 			)}
-			<LabelContext.Provider value={childContext}>
-				{props.children}
-			</LabelContext.Provider>
+
+			<RowContext.Provider value={rowContextValue}>
+				{children}
+			</RowContext.Provider>
 		</div>
 	)
 })
+
+interface LabelProps {
+	children?: ReactNode
+	collapsed: boolean
+	offset: number
+	reportSize: (size: number) => void
+	onClick: () => void
+}
+
+const Label = memo<LabelProps>(function Label({
+	children,
+	collapsed,
+	offset,
+	reportSize,
+	onClick,
+}) {
+	// TODO: Maybe a ref?
+	const [availableHeight, setAvailableHeight] = useState(0)
+	const [size, setSize] = useState({width: 0, height: 0})
+
+	// Tracking background height so we can overflow the collapsed view
+	const onResizeBackground = ({client}: ContentRect) => {
+		setAvailableHeight(client?.height ?? 0)
+	}
+
+	// Track content size
+	const onResizeContent = ({client}: ContentRect) => {
+		if (client == null) { return }
+		setSize(client)
+	}
+
+	// When the content size changes, or is collapsed, report the new size to parent
+	useEffect(
+		() => reportSize(collapsed ? size.height : size.width),
+		[collapsed, size],
+	)
+
+	// Using effect just for it's destructor, will trigger a report of 0 on unmount.
+	useEffect(() => () => {
+		reportSize(0)
+	}, [])
+
+	return <>
+		<Measure client onResize={onResizeBackground}>
+			{({measureRef}) => (
+				<div
+					ref={measureRef}
+					onClick={onClick}
+					className={classNames(
+						styles.labelBackground,
+						collapsed && styles.collapsed,
+					)}
+					style={{left: -offset, width: offset}}
+				/>
+			)}
+		</Measure>
+		<Measure client onResize={onResizeContent}>
+			{({measureRef}) => (
+				<div
+					ref={measureRef}
+					onClick={onClick}
+					className={classNames(
+						styles.labelContent,
+						collapsed && styles.collapsed,
+					)}
+					style={{
+						left: -offset,
+						maxWidth: collapsed ? availableHeight : undefined,
+					}}
+				>
+					{children}
+				</div>
+			)}
+		</Measure>
+	</>
+})
+
+let nextId = 0
+const useUniqueId = () => useRef(nextId++).current
