5b67e6fa50eac654c02b00537a0b61ad5ecb404b
Oowazu Nonowazu <53187143+nonowazu@users.noreply.github.com>
2020-03-23 07:58:35 -0500

    Modify Weaving in core for message flexibility (#802)

--- src/parser/jobs/sch/modules/Weaving.js
+++ src/parser/jobs/sch/modules/Weaving.js
@@ -1,11 +1,11 @@
-import {Trans, Plural} from '@lingui/react'
+import {Trans} from '@lingui/react'
 import NormalisedMessage from 'components/ui/NormalisedMessage'
 import React from 'react'
 
 import {ActionLink} from 'components/ui/DbLink'
 import ACTIONS from 'data/ACTIONS'
 import CoreWeaving from 'parser/core/modules/Weaving'
-import {TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
+import {SEVERITY} from 'parser/core/modules/Suggestions'
 
 import DISPLAY_ORDER from './DISPLAY_ORDER'
 
@@ -18,30 +18,9 @@ const WEAVING_SEVERITY = {
 export default class Weaving extends CoreWeaving {
 	static displayOrder = DISPLAY_ORDER.WEAVING
 
-	// override to give SCH-relevant weave suggestions
-	// Pretty much copy-pasted from the CoreWeaving Module otherwise
-	_onComplete() {
-		// If there's been at least one gcd, run a cleanup on any remnant data
-		if (this._leadingGcdEvent) {
-			this._saveIfBad()
-		}
-
-		// Few triples is medium, any more is major
-		const badWeavesCount = this._badWeaves.length
-		this.suggestions.add(new TieredSuggestion({
-			icon: ACTIONS.SCH_RUIN_II.icon,
-			content: <Trans id="sch.weaving.content">
-				Try to use <ActionLink {...ACTIONS.SCH_RUIN_II}/> and <ActionLink {...ACTIONS.BIOLYSIS}/> to weave your actions, and avoid weaving more actions than you have time for in a single GCD window.
-				Doing so will delay your next GCD, reducing possible uptime. Check the <a href="javascript:void(0);" onClick={() => this.parser.scrollTo(this.constructor.handle)}><NormalisedMessage message={this.constructor.title}/></a> module below for more detailed analysis.
-			</Trans>,
-			why: <Plural
-				id="sch.weaving.why"
-				value={badWeavesCount}
-				one="# instance of incorrect weaving"
-				other="# instances of incorrect weaving"
-			/>,
-			tiers: WEAVING_SEVERITY,
-			value: badWeavesCount,
-		}))
-	}
+	suggestionContent = <Trans id="sch.weaving.content">
+		Try to use <ActionLink {...ACTIONS.SCH_RUIN_II}/> and <ActionLink {...ACTIONS.BIOLYSIS}/> to weave your actions, and avoid weaving more actions than you have time for in a single GCD window.
+		Doing so will delay your next GCD, reducing possible uptime. Check the <a href="javascript:void(0);" onClick={() => this.parser.scrollTo(this.constructor.handle)}><NormalisedMessage message={this.constructor.title}/></a> module below for more detailed analysis.
+	</Trans>
+	severity = WEAVING_SEVERITY
 }
