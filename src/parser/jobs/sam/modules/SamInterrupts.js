14f26927e16d4ccd8951e608a14444d48ca04b78
Ryan Beck <37195907+Rjb99@users.noreply.github.com>
2020-04-04 01:00:00 -0400

    New wording

--- src/parser/jobs/sam/modules/SamInterrupts.js
+++ src/parser/jobs/sam/modules/SamInterrupts.js
@@ -7,6 +7,6 @@ import {ActionLink} from 'components/ui/DbLink'
 
 export default class SamInterrupts extends Interrupts {
 	suggestionContent = <Trans id="sam.interrupts.suggestion.content">
-		Avoid interrupting <ActionLink {...ACTIONS.IAIJUTSU}/> casts. Despite the short cast time, moving immediately after casting can interrupt the skill, wasting any time spent doing so.
+		Avoid interrupting <ActionLink {...ACTIONS.IAIJUTSU}/> casts. Despite the short cast time, moving too early can interrupt the skill causing you to have to waste time re-casting it.
 	</Trans>
 }
