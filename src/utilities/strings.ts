6d60b28169081b31061fcbdc7fe4d152da35c047
Azariah Aeroborn <26804378+AzariahAeroborn@users.noreply.github.com>
2020-04-13 20:39:43 -0400

    Unify the utilities function formatDuration with parser.formatDuration

--- src/utilities/strings.ts
+++ src/utilities/strings.ts
@@ -1,17 +1,39 @@
 /**
  * Renders a time given into the format `mm:ss`
  * @param duration {number} Seconds
+ * @param secondPrecision {number} Optional value for number of decimal places to format seconds values to
+ * @param isMilliseconds {boolean} Optional value for whether the input duration is in Milliseconds.  Defaults to false (input value is in seconds)
+ * @param hideMinutesIfZero {boolean} Optional value for whether the return string should hide minutes (and only return ss.##s for values less than 1 minute).  Defaults to false (always show minutes)
  * @return {string} Formatted duration
  */
-export function formatDuration(duration: number): string {
+export function formatDuration(duration: number, secondPrecision?: number, isMilliseconds: boolean = false, hideMinutesIfZero: boolean = false): string {
 	/* tslint:disable:no-magic-numbers */
-	const formatter = new Intl.NumberFormat(
+	if (isMilliseconds)	{ duration /= 1000 }
+	const defaultSecondPrecision = duration < 10 ? 2 : 0
+	const precision = secondPrecision != null ? secondPrecision : defaultSecondPrecision
+
+	const minutesFormatter = new Intl.NumberFormat(
 		undefined,
-		{minimumIntegerDigits: 2, maximumFractionDigits: 0, useGrouping: false},
+		{
+			minimumIntegerDigits: 2,
+			maximumFractionDigits: 0,
+			useGrouping: false,
+		},
 	)
-	const seconds = Math.floor(duration % 60)
+	if (duration < 0)
+	{
+		return '< 0s'
+	}
+
+	const seconds = duration % 60
 	const minutes = Math.floor(duration / 60)
-	return `${formatter.format(minutes)}:${formatter.format(seconds)}`
+	if (minutes === 0 && hideMinutesIfZero) {
+		const secondsFormatter = new Intl.NumberFormat(undefined, {minimumIntegerDigits: 1, maximumFractionDigits: precision})
+		return `${secondsFormatter.format(seconds)}s`
+	} else {
+		const secondsFormatter = new Intl.NumberFormat(undefined, {minimumIntegerDigits: 2, maximumFractionDigits: precision})
+		return `${minutesFormatter.format(minutes)}:${secondsFormatter.format(seconds)}`
+	}
 	/* tslint:enable:no-magic-numbers */
 }
 
