2834f96fc4ecfe6f4aad8f7aed0526f2e6e28e04
Saxon Landers <saxon@ackwell.com.au>
2020-05-03 17:18:14 +1000

    Port downtime to TS

---
+++ src/parser/core/modules/Downtime.tsx
@@ -0,0 +1,74 @@
+import Module, {dependency} from 'parser/core/Module'
+import UnableToAct from './UnableToAct'
+import {Invulnerability} from './Invulnerability'
+
+interface DowntimeWindow {
+	start: number,
+	end: number
+}
+
+export default class Downtime extends Module {
+	static handle = 'downtime'
+
+	@dependency private readonly unableToAct!: UnableToAct
+	@dependency private readonly invuln!: Invulnerability
+
+	private internalDowntime(start = 0, end = this.parser.currentTimestamp) {
+		// Get all the downtime from both unableToAct and invuln, and sort it
+		const downtimePeriods = [
+			...this.unableToAct.getDowntimes(start, end),
+			...this.invuln.getInvulns('all', start, end, 'untargetable'),
+		].sort((a, b) => a.start - b.start)
+
+		// If there's nothing, just stop now
+		if (downtimePeriods.length === 0) {
+			return []
+		}
+
+		// Merge the downtimes that overlap
+		const finalDowntimes = [downtimePeriods.shift()!]
+		downtimePeriods.forEach(dt => {
+			const last = finalDowntimes[finalDowntimes.length - 1]
+			if (dt.start <= last.end) {
+				if (dt.end > last.end) {
+					last.end = dt.end
+				}
+			} else {
+				finalDowntimes.push(dt)
+			}
+		})
+
+		return finalDowntimes
+	}
+
+	isDowntime(when = this.parser.currentTimestamp) {
+		return this.internalDowntime(when, when).length > 0
+	}
+
+	getDowntime(start = 0, end = this.parser.currentTimestamp) {
+		// Return the final number
+		return this.internalDowntime(start, end).reduce((uptime, invuln) => uptime + Math.min(invuln.end, end) - Math.max(invuln.start, start), 0)
+	}
+
+	getDowntimes = (start = 0, end = this.parser.currentTimestamp, minimumDowntimeLength = -1) =>
+		this.internalDowntime(start, end).reduce<number[]>(
+			(aggregator, invuln) => {
+				if (Math.min(invuln.end, end) - Math.max(invuln.start, start) > Math.min(minimumDowntimeLength, 0)) {
+					aggregator.push(Math.min(invuln.end, end) - Math.max(invuln.start, start))
+				}
+				return aggregator
+			},
+			[],
+		)
+
+	getDowntimeWindows = (start = 0, end = this.parser.currentTimestamp, minimumWindowSize = -1) =>
+		this.internalDowntime(start, end).reduce<DowntimeWindow[]>(
+			(aggregator, invuln) => {
+				if (Math.min(invuln.end, end) - Math.max(invuln.start, start) > Math.min(minimumWindowSize, 0)) {
+					aggregator.push({start: Math.max(invuln.start, start), end: Math.min(invuln.end, end)})
+				}
+				return aggregator
+			},
+			[],
+		)
+}
