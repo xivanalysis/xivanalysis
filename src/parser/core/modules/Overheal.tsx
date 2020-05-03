6c4f5bacf6443af425feeac89f2dc1abb4c787ee
Nonowazu <oowazu.nonowazu@gmail.com>
2020-05-02 23:55:00 -0500

    Core overheal module
    Okay, there's a lot to unpack here:
    
    * I got a request to add overheal tracking to SCH, to which I did what
    any normal dev would do – immediately steal from AST/WHM. Of course,
    after solicting some feedback, it seems like the checklist percentage
    'goal' was confusing, so I wrote a suggestion instead. That lacked
    information, so after the 345th cup of coffee I decided to rig up a pie
    chart.
    * At this point, cold, dark insanity had seeped in, and I was committed
    to this, so I ported what I had to typescript and then to core. After
    testing it out with all the different modes enabled (yes, this has modes
    like a transformer), SCH seems to want them all enabled.
    
    So, onto the class and other changes:
    
    * It has a TrackedOverheal class which just takes in HealEvents and
    tallies up the heal/overheal amounts, and can be used to filter out
    HealEvents into noteworthy categories (for example, WHM found value in
    separating heals into three categories of direct heals, hots, and
    assize), which can then be displayed in any number of ways. If a heal
    doesn't belong to a category, it gets dumped into the 'direct' heal bin.
    * It has options for spitting out pie charts, suggestions, and/or a
    checklist item. I *think* this has feature parity with the existing WHM
    and AST implementations.
    * It maintains the `InvertedRequirement` class from AST/WHM overheal
    tracking (and I somewhat expect a rejection, which is why I allow
    overrides to the description for an enterprising dev to provide
    programmatic descriptions to get around this)
    * I give some SuggestedColors (that Levi at the Balance) came up with
    based on other colors found in xiva; I'm not sure if this is the best
    way to do it or not, but the SCH impelmentation uses them directly like
    that but I'll change to something else if there's a block or a better
    suggestion
    * Charts do not like 4 rows, so after talking with Ack, he suggested I
    override the CSS – this has an additional effect of fixing SMN pie
    charts with 4 pet rows
    * I actually ran `yarn extract` up front this time

--- src/parser/core/modules/Overheal.tsx
+++ src/parser/core/modules/Overheal.tsx
@@ -1,16 +1,103 @@
 import {Trans} from '@lingui/react'
 import React from 'react'
 
+import ACTIONS from 'data/ACTIONS'
 import {HealEvent} from 'fflogs'
 import Module, {dependency} from 'parser/core/Module'
 import Checklist, {Requirement, TARGET, TieredRule} from 'parser/core/modules/Checklist'
+import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
+import {PieChartStatistic, Statistics} from 'parser/core/modules/Statistics'
 
-// doing 100-x where x is the overheal % for clarity
-const SEVERITY_TIERS = {
+interface SeverityTiers {
+	[key: number]: number
+}
+
+interface TrackedOverhealOpts {
+	name: JSX.Element,
+	color?: string
+	trackedHealIds?: number[],
+}
+
+const SUGGESTION_SEVERITY_TIERS: SeverityTiers = {
+	0: SEVERITY.MINOR,
+	35: SEVERITY.MEDIUM,
+	50: SEVERITY.MAJOR,
+}
+
+const CHECKLIST_SEVERITY_TIERS: SeverityTiers = {
 	// tslint:disable-next-line: no-magic-numbers
 	[100-35]: TARGET.SUCCESS,
 	// tslint:disable-next-line: no-magic-numbers
 	[100-50]: TARGET.WARN,
+}
+
+export const SuggestedColors: string[] = [
+	'#157f1f', // dark green
+	'#12ba45', // light green
+	'#00b5ad', // dark teal
+	'#a0eade', // light teal
+	'#b5cc18', // ~~snot green~~ why are you using this?
+]
+
+export class TrackedOverheal {
+	name: JSX.Element
+	color: string = '#fff'
+	protected trackedHealIds: number[]
+	heal: number = 0
+	overheal: number = 0
+
+	constructor(opts: TrackedOverhealOpts) {
+		this.name = opts.name
+		this.color = opts.color || this.color
+		this.trackedHealIds = opts.trackedHealIds || []
+	}
+
+	/**
+	 * Get current overheal as a percentage
+	 */
+	get percent(): number {
+		if (this.heal > 0) return 100 * (this.overheal) / (this.heal + this.overheal)
+		else return 0
+	}
+
+	/**
+	 * Get current overheal as an inverted percentage (for checklist)
+	 */
+	get percentInverted(): number {
+		return 100 - this.percent
+	}
+
+	/**
+	 * Returns true if there's data to output
+	 */
+	get hasData(): boolean {
+		if (this.heal > 0 || this.overheal > 0) {
+			return true
+		}
+		return false
+	}
+
+	/**
+	 * Returns true if the action id is tracked
+	 * @param guid
+	 */
+	idIsTracked(guid: number): boolean {
+		if (this.trackedHealIds.includes(guid)) {
+			return true
+		}
+		return false
+	}
+
+	/**
+	 * Pushes a heal event in for tracking. Returns true if the heal is accepted;
+	 * false
+	 * @param event - The heal event to track
+	 */
+	pushHeal(event: HealEvent) {
+		const guid = event.ability.guid
+			this.heal += event.amount
+			this.overheal += event.overheal || 0
+	}
 }
 
 export class CoreOverheal extends Module {
@@ -18,36 +105,97 @@ export class CoreOverheal extends Module {
 	static debug: boolean = true
 
 	@dependency private checklist!: Checklist
+	@dependency private suggestions!: Suggestions
+	@dependency private statistics!: Statistics
+
+	// Overall tracking options
 
 	/**
-	 * Implementing modules MAY override this to provide HoT Status IDs to track against
+	 * Implementing modules MAY override this to provide a more relevant name for the overhealing requirement
 	 */
-	protected hotStatuses?: number[]
+	protected overhealName: JSX.Element = <Trans id="core.overheal.direct.name">Direct</Trans>
 	/**
-	 * Implementing modules MAY override this to separate out pet heals from the player. If
-	 * omitted, then pet heals will be combined with the player.
+	 * Implementing moduels MAY override this to change the color for direct overheals in the pie chart
 	 */
-	protected petHeals?: number[]
+	protected overhealColor: string = SuggestedColors[0]
 	/**
-	 * Implementing modules MAY override this to provide HoT Status IDs to track against for the pet;
-	 * if this is omitted, pet HoTs will count for the player HoT overheal.
+	 * Implementing modules MAY override this to provide a list of heal 'categories' to track
 	 */
-	protected petHotStatuses?: number[]
+	protected trackedHealCategories: TrackedOverhealOpts[] = []
+
+	// Display options
+
+	/**
+	 * Implementing modules MAY change this to true in order to spit out a spiffy pie chart
+	 * breakdown of all their categories they're tracking
+	 */
+	protected displayPieChart: boolean = false
+
+	/**
+	 * Implementing modules MAY wish set this to true in order to provide a suggestion
+	 */
+	protected displaySuggestion: boolean = false
+	/**
+	 * Implementing modules MAY change this to set the suggestion icon
+	 */
+	protected suggestionIcon: string = ACTIONS.PHYSICK.icon
+	/**
+	 * Implementing mdoules MAY change this to set the suggestion text
+	 */
+	protected suggestionContent: JSX.Element = <Trans id="core.overheal.suggestion.content">Avoid healing your party for more than is needed. Cut back on unnecessary heals and coordinate with your co-healer to plan resources efficiently.</Trans>
+	/**
+	 * Implementing modules MAY change this to define the severity tiers for the suggestion
+	 */
+	protected suggestionSeverity: SeverityTiers = SUGGESTION_SEVERITY_TIERS
+
+	/**
+	 * Implementing modules MAY wish to set this to false in order to suppress adding this as a
+	 * checklist item
+	 */
+	protected displayChecklist: boolean = true
+	/**
+	 * Implementing modules MAY wish to override this to set custom severity tiers.
+	 * Do remember that the numbers for checklist are inverted for overheal (e.g., warning at
+	 * 35% overheal means you need to set your threshold at 65)
+	 */
+	protected checklistSeverity: SeverityTiers = CHECKLIST_SEVERITY_TIERS
+	/**
+	 * Implementing modules MAY wish to override this to change the name for the checklist title
+	 */
+	protected checklistRuleName: JSX.Element = <Trans id="core.overheal.rule.name">Avoid Overheal</Trans>
+	/**
+	 * Implementing modules MAY wish to change this in order to reflect the overall healing requiement name
+	 */
+	protected checklistRequirementName: JSX.Element = <Trans id="core.overheal.requirement.all">Overall (all sources)</Trans>
+	/**
+	 * Implementing modules MAY change this to true in order to generate multiple requirements for each
+	 * category of tracked heals; leaving it at false will only generate a single requirement against
+	 * the total overheal percent
+	 */
+	protected checklistRuleBreakout: boolean = false
 
 	// direct healing
-	private healDirect: number = 0
-	private overhealDirect: number = 0
-	// HoTs
-	private healOverTime: number = 0
-	private overhealOverTime: number = 0
-	// pet - direct healing
-	private petHealDirect: number = 0
-	private petOverhealDirect: number = 0
-	// pet - HoTs
-	private petHealOverTime: number = 0
-	private petOverhealOverTime: number = 0
+	private direct!: TrackedOverheal
+	// Everything else
+	private trackedOverheals: TrackedOverheal[] = []
+
+	/**
+	 * Implementing modules MAY override this to provide the 'why' for suggestion content
+	 * @param overhealPercent
+	 */
+	protected suggestionWhy(overhealPercent: number): JSX.Element {
+		return <Trans id="core.overheal.suggestion.why">You had an overheal of { overhealPercent.toFixed(2) }%</Trans>
+	}
 
 	protected init() {
+		this.direct = new TrackedOverheal({
+			name: this.overhealName,
+			color: this.overhealColor,
+		})
+		for (const healCategoryOpts of this.trackedHealCategories) {
+			this.trackedOverheals.push(new TrackedOverheal(healCategoryOpts))
+		}
+
 		this.addEventHook('heal', {by: 'player'}, this.onHeal)
 		this.addEventHook('heal', {by: 'pet'}, this.onPetHeal)
 		this.addEventHook('complete', this.onComplete)
@@ -63,81 +211,136 @@ export class CoreOverheal extends Module {
 		return true
 	}
 
-	private onHeal(event: HealEvent) {
-		if (! this.considerHeal(event)) return
+	/**
+	 * This method MAY be overridden to provide an alternative checklist description
+	 * @param overheals - an array of all the categories of overheals you're tracking, starting with direct
+	 */
+	protected checklistDescription(overheals: TrackedOverheal[]): JSX.Element {
+		return <Trans id="core.overheal.rule.description">Avoid healing your party for more than is needed. Cut back on unnecessary heals and coordinate with your co-healer to plan resources efficiently.</Trans>
+	}
+
+	private onHeal(event: HealEvent, petHeal: boolean = false) {
+		if (! this.considerHeal(event, petHeal)) return
 
 		const guid = event.ability.guid
-		if (this.hotStatuses && this.hotStatuses.includes(guid)) {
-			this.healOverTime += event.amount
-			this.overhealOverTime += event.overheal || 0
-		} else {
-			this.healDirect += event.amount
-			this.overhealDirect += event.amount
+		for (const trackedHeal of this.trackedOverheals) {
+			if (trackedHeal.idIsTracked(guid)) {
+				trackedHeal.pushHeal(event)
+				return
+			}
 		}
-
+		this.direct.pushHeal(event)
 	}
 
 	private onPetHeal(event: HealEvent) {
-		if (! this.considerHeal(event, true)) return
-
-		const guid = event.ability.guid
-		if (this.petHotStatuses && this.petHotStatuses.includes(guid)) {
-			this.petHealOverTime += event.amount
-			this.petOverhealOverTime += event.overheal || 0
-		}
+		this.onHeal(event, true)
 	}
 
-	protected calculateOverhealPercent(heal: number, overheal: number) {
-		return 100 * heal / (heal + overheal)
+	private percentageOf(category: number, total: number): number {
+		return (100 * category) / total
 	}
 
 	private onComplete() {
-		const overhealPercent = this.calculateOverhealPercent(this.healDirect, this.overhealDirect)
-		const overhealOverTimePercent = this.calculateOverhealPercent(this.healOverTime, this.overhealOverTime)
-		const petOverhealPercent = this.calculateOverhealPercent(this.petHealDirect, this.petOverhealDirect)
-		const petOverhealOverTimePercent = this.calculateOverhealPercent(this.petHealOverTime, this.petOverhealOverTime)
-		const overallOverhealPercent = this.calculateOverhealPercent(
-			this.healDirect + this.healOverTime + this.petHealDirect + this.petHealOverTime,
-			this.overhealDirect + this.overhealOverTime + this.petOverhealDirect + this.petOverhealOverTime,
-		)
+		let healtotal = this.direct.heal
+		let overhealtotal = this.direct.overheal
 
-		this.checklist.add(new TieredRule({
-			name: <Trans id="core.overheal.rule.name">Avoid overheal</Trans>,
-			description: <>If u overheal too much u bad</>,
-			tiers: SEVERITY_TIERS,
-			requirements: [
-				new InvertedRequirement({
-					name: <Trans id="core.overheal.requirement.all">Overheal (direct)</Trans>,
-					percent: (overhealPercent),
-				}),
-				new InvertedRequirement({
-					name: <Trans id="core.overheal.requirement.all">Overheal (HoTs)</Trans>,
-					percent: (overhealOverTimePercent),
-				}),
-				new InvertedRequirement({
-					name: <Trans id="core.overheal.requirement.all">Overheal (Pet)</Trans>,
-					percent: (petOverhealPercent),
-				}),
-				new InvertedRequirement({
-					name: <Trans id="core.overheal.requirement.all">Overheal (Pet HoTs)</Trans>,
-					percent: (petOverhealOverTimePercent),
-				}),
-			],
-		}))
+		this.trackedOverheals.forEach(x => {
+			if (x.hasData) {
+				healtotal += x.heal
+				overhealtotal += x.overheal
+			}
+		})
+		const overallOverhealPercent: number = 100 * overhealtotal / (healtotal + overhealtotal)
+
+		if (this.displayPieChart) {
+			const directPercentage = this.percentageOf(this.direct.overheal, overhealtotal)
+			const data: any = [{
+				value: directPercentage,
+				color: this.direct.color,
+				columns: [
+					this.direct.name,
+					this.percentageOf(this.direct.overheal, overhealtotal).toFixed(2) + '%',
+					this.direct.percent.toFixed(2) + '%',
+				],
+			}]
+
+			for (const trackedHeal of this.trackedOverheals) {
+				if (trackedHeal.hasData) {
+					const percentage = this.percentageOf(trackedHeal.overheal, overhealtotal)
+					data.push({
+						value: percentage,
+						color: trackedHeal.color,
+						columns: [
+							trackedHeal.name,
+							percentage.toFixed(2) + '%',
+							trackedHeal.percent.toFixed(2) + '%',
+						],
+					})
+				}
+			}
+
+			this.statistics.add(new PieChartStatistic({
+				headings: [
+					'Type of heal',
+					'% of total overheal',
+					'Overheal % per type',
+				],
+				data,
+				width: 3, // chart's wide, yo
+			}))
+		}
+
+
+		if (this.displayChecklist) {
+			const requirements: InvertedRequirement[] = []
+			if (this.checklistRuleBreakout) {
+				requirements.push(new InvertedRequirement({
+					name: this.overhealName,
+					percent:  this.direct.percentInverted,
+				}))
+
+				for (const trackedHeal of this.trackedOverheals) {
+					requirements.push(new InvertedRequirement({
+						name: trackedHeal.name,
+						percent: trackedHeal.percentInverted,
+					}))
+				}
+			}
+			requirements.push(new InvertedRequirement({
+				name: this.checklistRequirementName,
+				percent: 100 - overallOverhealPercent,
+			}))
+
+			this.checklist.add(new TieredRule({
+				name: this.checklistRuleName,
+				description: this.checklistDescription([this.direct, ...this.trackedOverheals]),
+				tiers: this.checklistSeverity,
+				requirements,
+			}))
+		}
+
+		if (this.displaySuggestion) {
+			this.suggestions.add(new TieredSuggestion({
+				icon: this.suggestionIcon,
+				tiers: this.suggestionSeverity,
+				value: overallOverhealPercent,
+				content: this.suggestionContent,
+				why: this.suggestionWhy(overallOverhealPercent),
+			}))
+		}
 	}
 }
 
-// Explicitly not exporting this because there isn't a need for this sort of
-// utility outside of overhealing
+// From the original comments:
+// yeh, I'm not doing this in core, but I really want to show overheal as overheal, since that's what the community understands
+// So, in keeping with that spirit, I'm not going to export this at all.
 class InvertedRequirement extends Requirement {
 	get percentInverted() {
 		return 100 - this.percent
 	}
 
 	get content() {
-		if (this._percent !== null || this.value === null) {
-			return `${this.percentInverted.toFixed(2)}`
-		}
-		return `${this.value.toFixed(0)}/${this.target.toFixed(0)}`
+		if (this._percent !== null || this.value === null) { return `${this.percentInverted.toFixed(2)}%` }
+		return `${this.value.toFixed(0)}/${this.target.toFixed(0)}` // avoid weird floating point shit
 	}
 }
