10ea766d871f1fcf3ef39f7b22201c801039df1d
Rjb99 <rjb17f@my.fsu.edu>
2020-05-03 23:20:54 -0400

    This Shit Busted.

--- src/parser/jobs/gnb/modules/AmmoCombo.tsx
+++ src/parser/jobs/gnb/modules/AmmoCombo.tsx
@@ -23,28 +23,39 @@ const EXPECTED_USES = {
 	ABDOMEN_TEAR: 1,
 	WICKED_TALON: 1,
 	EYE_GOUGE: 1,
+	TOTAL_SKILLS_PER_COMBO: 6,
 }
 
-const GNASHING_TIME = 7500 // time in ms, this is the "fastest" time a combo can happen, but can take longer TODO: Dropped Combos?
-
 class GnashingComboState {
-	start: number
-	end?: number
+	startTime: number
+	endTime?: number
 	rotation: CastEvent[] = []
-	// isRushing: boolean = false //I've yet to figure out how I wanted to handle end of fights rushes
 	isProper: boolean = false
 
-	// the trackers
-	numGnashing: number = 0
-	numJugular: number = 0
-	numSavage: number = 0
-	numAbdomen: number = 0
-	numWicked: number = 0
-	numEye: number = 0
-	numTotal: number = 0 // This is the filter, sub 6 = possible problem
-
 	constructor(start: number) {
-		this.start = start
+		this.startTime = start
+	}
+
+	public get totalHits(): number {return this.rotation.length}
+
+	public get actualJug() : number {
+		return this.rotation.filter(cast => cast.ability.guid === ACTIONS.JUGULAR_RIP.id).length
+	}
+
+	public get actualSavage(): number {
+		return this.rotation.filter(cast => cast.ability.guid === ACTIONS.SAVAGE_CLAW.id).length
+	}
+
+	public get actualTear(): number {
+		return this.rotation.filter(cast => cast.ability.guid === ACTIONS.ABDOMEN_TEAR.id).length
+	}
+
+	public get actualTalon(): number {
+		return this.rotation.filter(cast => cast.ability.guid === ACTIONS.WICKED_TALON.id).length
+	}
+
+	public get actualEye(): number {
+		return this.rotation.filter(cast => cast.ability.guid === ACTIONS.EYE_GOUGE.id).length
 	}
 }
 
@@ -56,6 +67,7 @@ export default class AmmoCombo extends Module {
 
 	private buffs = 0
 	private actions = 0
+	private errors = 0
 
 	private gnashingComboWindows: GnashingComboState[] = [] // Store all the gnashing fang combos to be output to the rotationTable
 
@@ -63,111 +75,89 @@ export default class AmmoCombo extends Module {
 		return _.last(this.gnashingComboWindows)
 	}
 
-	protected init() { // TODO: Strip away the other hooks? I could roll them into the actual cast I have for tracking the combo, but won't be as nice looking IMO
-		this.addHook('cast',
+	protected init() {
+		this.addEventHook('cast',
 			{
 				by: 'player',
 				abilityId: RELEVANT_ACTIONS,
 			},
 			() => this.actions++)
 
-		this.addHook('cast', {by: 'player'}, this.onCast)
+		this.addEventHook('cast', {by: 'player'}, this.onCast)
 
-		this.addHook('applybuff',
+		this.addEventHook('applybuff',
 			{
 				by: 'player',
 				abilityId: RELEVANT_STATUSES,
 			},
 			() => this.buffs++)
-		this.addHook('complete', this.onComplete)
+		this.addEventHook('complete', this.onComplete)
 	}
 
 	private onCast(event: CastEvent) {
+
 		const actionId = event.ability.guid
 
-		// cases to ignore, No Mercy any other modules will handle the latter 2.
+		// cases to ignore, No Mercy any other modules will handle the latter 2 and FUCK sprint.
 
-		if (actionId === ACTIONS.ATTACK.id || actionId === ACTIONS.BURST_STRIKE.id || actionId === ACTIONS.SONIC_BREAK.id ) {
+		if (actionId === ACTIONS.ATTACK.id || actionId === ACTIONS.SPRINT.id || actionId === ACTIONS.BURST_STRIKE.id || actionId === ACTIONS.SONIC_BREAK.id ) {
 			return
 		}
 
+		let lastGnashingCombo = this.lastGnashingCombo
+
 		if (actionId === ACTIONS.GNASHING_FANG.id) {
 
-			this.onEndGnashingCombo(event) // Make damn sure the previous window has ended.
+
+			// THIS SHIT IS BREAKING PUSHING THINGS?
+
+			if (lastGnashingCombo != null && lastGnashingCombo.endTime == null) { // They dropped the combo via timeout
+				this.onEndGnashingCombo(event)
+			}
 
 			const gnashingComboState = new GnashingComboState(event.timestamp)
-			const fightTimeRemaining = this.parser.fight.end_time - event.timestamp
-			// gnashingComboState.isRushing = GNASHING_TIME >= fightTimeRemaining
-
 			this.gnashingComboWindows.push(gnashingComboState)
 		}
 
 		if (COMBO_BREAKERS.hasOwnProperty(actionId) ) {
+
 			this.onEndGnashingCombo(event)
 		}
 
 		// If the action is a gnashingCombo one, log it
 
-		const lastGnashingCombo = this.lastGnashingCombo
-		if (lastGnashingCombo != null && lastGnashingCombo.end == null) {
+		lastGnashingCombo = this.lastGnashingCombo
 
-				switch (actionId) {
-					case ACTIONS.GNASHING_FANG.id:
+		if (lastGnashingCombo != null && lastGnashingCombo.endTime == null) {
 
-						lastGnashingCombo.numGnashing++
-						lastGnashingCombo.rotation.push(event)
-						lastGnashingCombo.numTotal++
-						break
+			if (COMBO_ACTIONS.hasOwnProperty(actionId) ) {
 
-					case ACTIONS.JUGULAR_RIP.id:
+				lastGnashingCombo.rotation.push(event)
 
-						lastGnashingCombo.numJugular++
-						lastGnashingCombo.rotation.push(event)
-						lastGnashingCombo.numTotal++
-						break
-
-					case ACTIONS.SAVAGE_CLAW.id:
-
-						lastGnashingCombo.numSavage++
-						lastGnashingCombo.rotation.push(event)
-						lastGnashingCombo.numTotal++
-						break
-
-					case ACTIONS.ABDOMEN_TEAR.id:
-
-						lastGnashingCombo.numAbdomen++
-						lastGnashingCombo.rotation.push(event)
-						lastGnashingCombo.numTotal++
-						break
-
-					case ACTIONS.WICKED_TALON.id:
-
-						lastGnashingCombo.numWicked++
-						lastGnashingCombo.rotation.push(event)
-						lastGnashingCombo.numTotal++
-						break
-
-					case ACTIONS.EYE_GOUGE.id:
-						lastGnashingCombo.numEye++
-						lastGnashingCombo.numTotal++
-
-						if (lastGnashingCombo.numTotal === 6) {
-							lastGnashingCombo.isProper = true
-						}
-
-						lastGnashingCombo.rotation.push(event)
-						this.onEndGnashingCombo(event)
-						break
+				if (actionId === ACTIONS.EYE_GOUGE.id) {
+					this.onEndGnashingCombo(event)
 				}
-
 			}
+
 		}
+	}
 
 	private onEndGnashingCombo(event: CastEvent) {
+
 		const lastGnashingCombo = this.lastGnashingCombo
 
 		if (lastGnashingCombo != null) {
-			lastGnashingCombo.end = event.timestamp
+
+			lastGnashingCombo.endTime = event.timestamp
+
+			if (lastGnashingCombo.totalHits === EXPECTED_USES.TOTAL_SKILLS_PER_COMBO) {
+				lastGnashingCombo.isProper = true
+			}
+
+			if (lastGnashingCombo.isProper === false) {
+				this.errors++
+			}
+
 		}
 
 	}
@@ -176,8 +166,9 @@ export default class AmmoCombo extends Module {
 
 		const lastGnashingCombo = this.lastGnashingCombo
 
-		if (lastGnashingCombo != null) {
-			lastGnashingCombo.end = this.parser.fight.end_time
+		if (lastGnashingCombo != null && lastGnashingCombo.endTime == null) {
+
+			lastGnashingCombo.endTime = this.parser.currentTimestamp
 		}
 
 		this.checklist.add(new Rule({
@@ -198,6 +189,9 @@ export default class AmmoCombo extends Module {
 	}
 
 	output() {
+
+		if (this.errors !== 0) {
+
 		return <RotationTable
 			targets={[
 				{
@@ -225,29 +219,27 @@ export default class AmmoCombo extends Module {
 				.filter(window => !window.isProper)
 				.map(window => {
 					return ({
-						start: window.start - this.parser.fight.start_time,
-						end: window.end != null ?
-							window.end - this.parser.fight.start_time
-							: window.start - this.parser.fight.start_time,
+						start: window.startTime - this.parser.fight.start_time,
+						end: window.endTime != null ? window.endTime - this.parser.fight.start_time : window.startTime - this.parser.fight.start_time,
 						targetsData: {
 							jugularRip: {
-								actual: window.numJugular,
+								actual: window.actualJug,
 								expected: EXPECTED_USES.JUGULAR_RIP,
 							},
 							savageClaw: {
-								actual: window.numSavage,
+								actual: window.actualSavage,
 								expected: EXPECTED_USES.SAVAGE_CLAW,
 							},
 							abdomenTear: {
-								actual: window.numAbdomen,
+								actual: window.actualTear,
 								expected: EXPECTED_USES.ABDOMEN_TEAR,
 							},
 							wickedTalon: {
-								actual: window.numWicked,
+								actual: window.actualTalon,
 								expected: EXPECTED_USES.WICKED_TALON,
 							},
 							eyeGouge: {
-								actual: window.numEye,
+								actual: window.actualEye,
 								expected: EXPECTED_USES.EYE_GOUGE,
 							},
 						},
@@ -258,5 +250,5 @@ export default class AmmoCombo extends Module {
 			onGoto={this.timeline.show}
 		/>
 	}
-
+	}
 }
