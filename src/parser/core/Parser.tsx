1884348e3ec874425bfe57cba635e32209eb6934
Azariah Aeroborn <26804378+AzariahAeroborn@users.noreply.github.com>
2020-05-04 14:03:19 -0400

    FormatDuration cleanup & fixes (#877)
    * formatDuration fixes
    
    * don't show negative seconds if we're already showing negative minutes

--- src/parser/core/Parser.tsx
+++ src/parser/core/Parser.tsx
@@ -462,7 +462,7 @@ class Parser {
 	}
 
 	formatDuration(duration: number, secondPrecision?: number) {
-		return formatDuration(duration, {hideMinutesIfZero: true})
+		return formatDuration(duration, {secondPrecision, hideMinutesIfZero: true, showNegative: true})
 	}
 
 	/**
