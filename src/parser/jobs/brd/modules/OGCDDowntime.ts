f6a77d09a0a06c4a3f76eefa8801fb0ce1824d5c
yumiyafangirl <yumiyafangirl@fakeemail.com>
2020-05-13 00:05:31 -0400

    add first-use offsets to BRD's OGCDDowntime

--- src/parser/jobs/brd/modules/OGCDDowntime.ts
+++ src/parser/jobs/brd/modules/OGCDDowntime.ts
@@ -6,9 +6,16 @@ import {CooldownDowntime} from 'parser/core/modules/CooldownDowntime'
 
 export default class OGCDDowntime extends CooldownDowntime {
 	trackedCds = [
-		{cooldowns: [ACTIONS.BARRAGE]},
-		{cooldowns: [ACTIONS.RAGING_STRIKES]},
-		{cooldowns: [ACTIONS.SIDEWINDER]},
+		{
+			cooldowns: [ACTIONS.BARRAGE],
+			firstUseOffset: 12000,
+		}, {
+			cooldowns: [ACTIONS.RAGING_STRIKES],
+			firstUseOffset: -1000,
+		}, {
+			cooldowns: [ACTIONS.SIDEWINDER],
+			firstUseOffset: 12000,
+		},
 	]
 
 	checklistTarget = 100
