7cd7427b0a9273bd9cfecc3a970f11a2410d439f
Saxon Landers <saxon@ackwell.com.au>
2020-05-16 03:06:49 +1000

    ;) [skip ci]

---
+++ src/utilities/random.ts
@@ -0,0 +1,9 @@
+const RANDOM_NUMBER = 4
+/**
+ * Returns a random number – chosen by fair dice roll; guaranteed to be random
+ * Ack hates this
+ * @returns a *random* number
+ */
+const random = () => RANDOM_NUMBER
+
+export {}
