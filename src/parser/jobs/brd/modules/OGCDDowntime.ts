2560d759db75809a502217bb96926e5184573bdd
yumiyafangirl <yumiyafangirl@fakeemail.com>
2020-05-17 17:49:27 -0400

    added battle voice & ordered CDs by importance

--- src/parser/jobs/brd/modules/OGCDDowntime.ts
+++ src/parser/jobs/brd/modules/OGCDDowntime.ts
@@ -7,12 +7,16 @@ import {CooldownDowntime} from 'parser/core/modules/CooldownDowntime'
 export default class OGCDDowntime extends CooldownDowntime {
 	trackedCds = [
 		{
-			cooldowns: [ACTIONS.BARRAGE],
-			firstUseOffset: 12000,
+			cooldowns: [ACTIONS.BATTLE_VOICE],
+			firstUseOffset: 4500,
 		},
 		{
 			cooldowns: [ACTIONS.RAGING_STRIKES],
 			firstUseOffset: -1000,
+		},
+		{
+			cooldowns: [ACTIONS.BARRAGE],
+			firstUseOffset: 12000,
 		},
 		{
 			cooldowns: [
