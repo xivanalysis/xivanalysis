ec0e9218d9a8e50c9cdd5eb3a87270d8370968bb
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2020-05-26 17:17:49 -0500

    web merge thing made spacing weird

--- src/parser/jobs/dnc/changelog.tsx
+++ src/parser/jobs/dnc/changelog.tsx
@@ -64,8 +64,8 @@ export const changelog = [
 	{
 		date: new Date('2019-05-13'),
 		Changes: () => <>Adjust Esprit generation simulation and Technical Windows table to account for multi-dancer parties.</>,
-  },
-  {
+	},
+	{
 		date: new Date('2020-05-10'),
 		Changes: () => <>Leniency for rushing finishes at the end of the fight or before a downtime, and suggestions to avoid 0-step finishes.</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
