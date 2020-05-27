d612d8ecc5cb016c3a9b894037aec5019bca1739
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2020-05-26 22:25:34 -0500

    Clean up some text

--- src/parser/jobs/whm/index.js
+++ src/parser/jobs/whm/index.js
@@ -8,11 +8,11 @@ import {Meta} from 'parser/core/Meta'
 const description = t('whm.about.description')`
 This analyser aims to identify some of the low-hanging fruit that could be used to improve your WHM gameplay, as well as give a deeper insight into what happened during an encounter.
 
-Note that since healing is a very fight-dependent process, it might be that some suggestions do not apply to you, but they may they help to inform you about things you have missed.
-
 If you would like to learn more about WHM, check the guides over at [The Balance](https://thebalanceffxiv.com/), and have a chat in the #whm_questions channel.
 
 Currently this module can track DoT uptime, note when your healing lily gauge is full, detect clipping, detect interrupted casts, and report about missed oGCD casts.
+
+Healing analysis can be very subjective - even if some of the below suggestions do not apply to you, they can help inform you about things you may have missed
 `
 
 export default new Meta({
