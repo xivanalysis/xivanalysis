f8b81a6b53500703fc7e873893b383e91b5f8a40
Saxon Landers <saxon@ackwell.com.au>
2020-06-11 00:54:30 +1000

    Fill out ENCOUNTERS data

--- src/data/ENCOUNTERS.ts
+++ src/data/ENCOUNTERS.ts
@@ -9,7 +9,15 @@ const ensureEncounters = <T extends Record<string, Encounter>>(encounters: T): {
 export const ENCOUNTERS = ensureEncounters({
 	TRASH: {ids: {legacyFflogs: '0'}},
 
+	// 24mans
 	NIER1: {ids: {legacyFflogs: '2020'}},
+	NIER2: {ids: {legacyFflogs: '2021'}},
+	NIER3: {ids: {legacyFflogs: '2022'}},
+	NIER4: {ids: {legacyFflogs: '2023'}},
+
+	// Trials
+	EX_RUBY_1: {ids: {legacyFflogs: '1051'}},
+	EX_RUBY_2: {ids: {legacyFflogs: '1052'}},
 })
 
 export type EncounterKey = keyof typeof ENCOUNTERS
@@ -22,19 +30,6 @@ export function getEncounter(source: ReportSource, id: string) {
 	}
 }
 
-/*
-encounter {
-	key?: keyof encounters
-	duty: duty
-}
-
-duty {
-	id: territype id
-}
-
-boss module meta = available[encounter.key] ?? available[duty.id]
-*/
-
 export interface Duty {
 	territoryType: number
 }
@@ -42,5 +37,22 @@ export interface Duty {
 const ensureDuties = <T extends Record<string, Duty>>(duties: T): {[K in keyof T]: T[K] & Duty} => duties
 
 export const DUTIES = ensureDuties({
-	E6: {territoryType: 907},
+	// Trials
+	EX_TITANIA: {territoryType: 858},
+	EX_INNOCENCE: {territoryType: 848},
+	EX_HADES: {territoryType: 885},
+	EX_VARIS: {territoryType: 913},
+
+	// Raids
+	E1S: {territoryType: 853},
+	E2S: {territoryType: 854},
+	E3S: {territoryType: 855},
+	E4S: {territoryType: 856},
+	E5S: {territoryType: 906},
+	E6S: {territoryType: 907},
+	E7S: {territoryType: 908},
+	E8S: {territoryType: 909},
+
+	// Ultimates
+	TEA: {territoryType: 887},
 })
