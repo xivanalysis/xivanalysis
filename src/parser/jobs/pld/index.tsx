17848158b8c3959a7c23803278be4f828f03f95d
Klaas Pruiksma <qaphla@users.noreply.github.com>
2020-06-27 21:23:45 -0400

    [PLD] Assorted text cleanup (#949)
    Co-authored-by: qaphla <3596194+qaphla@users.noreply.github.com>
    [skip ci]

--- src/parser/jobs/pld/index.tsx
+++ src/parser/jobs/pld/index.tsx
@@ -10,27 +10,11 @@ const description = t('pld.about.description')`As the illegitimate child of a WH
 This analyzer attempts to find just the right things to get you to be a fearsome Tank, that will show no [~action/CLEMENCY] to his enemies, while being the true [~action/HOLY_SPIRIT] of the Party.
 `
 
-const descriptionWarning = t('pld.about.description.warning')`
-**Here be Dragons**
-
-This Analyzer is still **Work in Progress** and is missing a lot of features as well as simplifying a couple of things for now.
-
-Make sure to take the advice still with a grain of Salt.
-
-If you notice any issues, have concerns or suggestions, please drop by our Discord channel!
-`
-
 export default new Meta({
 	modules: () => import('./modules' /* webpackChunkName: "jobs-pld" */),
 
 	Description: () => <>
 		<TransMarkdown source={description} key="pld.about.description"/>
-		<Message warning icon>
-			<Icon name="warning sign"/>
-			<Message.Content>
-				<TransMarkdown source={descriptionWarning} key="'pld.about.description.warning'"/>
-			</Message.Content>
-		</Message>
 	</>,
 	supportedPatches: {
 		from: '5.0',
