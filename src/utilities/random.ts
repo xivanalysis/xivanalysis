158cea49b8b4881ed15e282d317ff980baa6a95f
Michael Braun <m.braun92@gmail.com>
2020-07-03 22:06:09 +0200

    Simple renaming transitions to typescript and a special surprise for ack ♥

--- src/utilities/random.ts
+++ src/utilities/random.ts
@@ -1,9 +1,11 @@
+type RandomRollFnc = () => 4
+
 const RANDOM_NUMBER = 4
 /**
  * Returns a random number – chosen by fair dice roll; guaranteed to be random
  * Ack hates this
  * @returns a *random* number
  */
-const random = () => RANDOM_NUMBER
+const random: RandomRollFnc = () => RANDOM_NUMBER
 
 export {}
