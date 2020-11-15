734b12ca3475f936a5bf5b903071c45126f5bea5
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2020-11-15 23:56:00 +1100

    Fix GH conflict resolver indentation
    Marge, linter's broken

--- src/parser/jobs/brd/changelog.js
+++ src/parser/jobs/brd/changelog.js
@@ -14,7 +14,7 @@ export const changelog = [
 		</>,
 		contributors: [CONTRIBUTORS.YUMIYAFANGIRL],
 	},
-  {
+	{
 		date: new Date('2020-08-24'),
 		Changes: () => <>
 			Simplified the Barrage module and squashed some longstanding bugs with <StatusLink {...STATUSES.RAGING_STRIKES}/> alignment and <StatusLink {...STATUSES.STRAIGHT_SHOT_READY}/> procs.
