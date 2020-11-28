92637796d7abb6584b01b9883c50271b4e48ecf1
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2020-11-28 21:02:51 +1100

    Provisional MNK speedmod adjustment for 5.4

--- src/parser/jobs/mnk/modules/Speedmod.ts
+++ src/parser/jobs/mnk/modules/Speedmod.ts
@@ -4,11 +4,16 @@ import CoreSpeedmod from 'parser/core/modules/Speedmod'
 import Gauge from './Gauge'
 
 const SPEED_INCREASE_PER_STACK = 5
+const SPEED_INCREASE_TRAITED_GL = 80
 
 export default class Speedmod extends CoreSpeedmod {
 	@dependency private gauge!: Gauge
 
 	getJobAdditionalSpeedbuffScalar(event: TODO) {
-		return (100 - (this.gauge.getStacksAt(event.timestamp) * SPEED_INCREASE_PER_STACK)) / 100
+		if (this.parser.patch.before('5.4')) {
+			return (100 - (this.gauge.getStacksAt(event.timestamp) * SPEED_INCREASE_PER_STACK)) / 100
+		}
+
+		return SPEED_INCREASE_TRAITED_GL
 	}
 }
