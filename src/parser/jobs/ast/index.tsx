7c6f9c5cef859ef4a9cc3d129cda6e1eab1fb5e5
Bobby Wertman <robert.wertman@gmail.com>
2020-12-01 00:27:00 -0500

    Fix Aspected Benefic's ability id
    Fixes #1045

--- src/parser/jobs/ast/index.tsx
+++ src/parser/jobs/ast/index.tsx
@@ -38,6 +38,13 @@ export default new Meta({
 	],
 	changelog: [
 		{
+			date: new Date('2020-12-01'),
+			Changes: () => <>
+				Fixed <ActionLink {...ACTIONS.ASPECTED_BENEFIC} /> detection.
+			</>,
+			contributors: [CONTRIBUTORS.CASUALSUPERMAN],
+		},
+		{
 			date: new Date('2020-10-05'),
 			Changes: () => <>
 				Fixed <ActionLink {...ACTIONS.EARTHLY_STAR} /> drift calculation.
