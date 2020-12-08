37998af4f67ab5ec9fce2bcfc779b16e2748c1a1
qaphla <3596194+qaphla@users.noreply.github.com>
2020-12-07 19:42:23 -0500

    Marks PLD as supported for 5.4.

--- src/parser/jobs/pld/index.tsx
+++ src/parser/jobs/pld/index.tsx
@@ -18,7 +18,7 @@ export default new Meta({
 	</>,
 	supportedPatches: {
 		from: '5.0',
-		to: '5.3',
+		to: '5.4',
 	},
 	contributors: [
 		{user: CONTRIBUTORS.MIKEMATRIX, role: ROLES.MAINTAINER},
@@ -27,6 +27,13 @@ export default new Meta({
 	],
 
 	changelog: [
+		{
+			date: new Date('2020-12-07'),
+			Changes: () => <>
+				Mark patch 5.4 supported.
+			</>,
+			contributors: [CONTRIBUTORS.QAPHLA],
+		},
 		{
 			date: new Date('2020-08-11'),
 			Changes: () => <>
