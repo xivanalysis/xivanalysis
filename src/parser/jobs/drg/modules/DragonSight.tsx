c60baa906510fcd538aa9f484f3c344f2967e61e
Evan Shimizu <ebshimizu@gmail.com>
2020-12-21 13:14:13 -0500

    drg: mark DS window if partner (or DRG) dies

--- src/parser/jobs/drg/modules/DragonSight.tsx
+++ src/parser/jobs/drg/modules/DragonSight.tsx
@@ -8,6 +8,17 @@ import {BuffWindowModule, BuffWindowState, BuffWindowTrackedAction} from 'parser
 import {SEVERITY} from 'parser/core/modules/Suggestions'
 import STATUSES from 'data/STATUSES'
 import DISPLAY_ORDER from './DISPLAY_ORDER'
+import {DeathEvent} from 'fflogs'
+
+// give it a gcd for marking as truncated window
+const SHORT_WINDOW_BUFFER: number = 2500
+
+// indicator for why the window was short (did you die or did your partner)
+const SHORT_WINDOW_FAULT = {
+	NONE: 0,			// everything's fine, no one's at fault
+	PARTNER: 1,
+	DRG: 2,
+}
 
 export default class DragonSight extends BuffWindowModule {
 	static handle = 'dragonsight'
@@ -18,9 +29,13 @@ export default class DragonSight extends BuffWindowModule {
 	buffStatus = STATUSES.RIGHT_EYE
 	secondaryBuffStatus = STATUSES.RIGHT_EYE_SOLO
 
+	protected rotationTableNotesColumnHeader = <Trans id="drg.ds.notes.header">Short Window Cause</Trans>
+
+	private deathTimes: number[] = []
+
 	expectedGCDs = {
 		expectedPerWindow: 8,
-		suggestionContent: <Trans id="drg.lc.suggestions.missedgcd.content">
+		suggestionContent: <Trans id="drg.ds.suggestions.missedgcd.content">
 			Try to land at least 8 GCDs during every <ActionLink {...ACTIONS.DRAGON_SIGHT} /> window.
 		</Trans>,
 		severityTiers: {
@@ -50,7 +65,7 @@ export default class DragonSight extends BuffWindowModule {
 				expectedPerWindow: 1,
 			},
 		],
-		suggestionContent: <Trans id="drg.lc.suggestions.trackedactions.content">
+		suggestionContent: <Trans id="drg.ds.suggestions.trackedactions.content">
 			Each <ActionLink {...ACTIONS.DRAGON_SIGHT} /> window should contain at least one use each of <ActionLink {...ACTIONS.CHAOS_THRUST} />, <ActionLink {...ACTIONS.FULL_THRUST} />, <ActionLink {...ACTIONS.FANG_AND_CLAW} />, and <ActionLink {...ACTIONS.WHEELING_THRUST} />. In order to ensure that these actions fall within the buff window, try to avoid using <ActionLink {...ACTIONS.DRAGON_SIGHT} /> after <ActionLink {...ACTIONS.CHAOS_THRUST} /> or <ActionLink {...ACTIONS.FULL_THRUST} />.
 		</Trans>,
 		severityTiers: {
@@ -58,6 +73,16 @@ export default class DragonSight extends BuffWindowModule {
 			3: SEVERITY.MEDIUM,
 			5: SEVERITY.MAJOR,
 		},
+	}
+
+	protected init() {
+		super.init()
+
+		this.addEventHook('death', {to: 'player'}, this.onDeath)
+	}
+
+	private onDeath(event: DeathEvent) {
+		this.deathTimes.push(event.timestamp)
 	}
 
 	protected reduceTrackedActionsEndOfFight(buffWindow: BuffWindowState, action: BuffWindowTrackedAction): number {
@@ -70,5 +95,68 @@ export default class DragonSight extends BuffWindowModule {
 		}
 
 		return 0
+	}
+
+	// ok so we don't want to penalize people for when their buff ends early because their partner died
+	protected getBaselineExpectedGCDs(buffWindow: BuffWindowState): number {
+		if (this.buffTargetDied(buffWindow) === SHORT_WINDOW_FAULT.PARTNER) {
+			// you get what you get and that's ok in this case
+			return buffWindow.gcds
+		}
+
+		// as parent
+		// note that if the drg died we don't reduce gcds because that's something you personally can fix
+		if (this.expectedGCDs) {
+			return this.expectedGCDs.expectedPerWindow
+		}
+
+		return 0
+	}
+
+	// check for a truncated window if not rushing, which would indicate that the tether partner (or the DRG) died
+	protected getBuffWindowNotes(buffWindow: BuffWindowState): JSX.Element | undefined {
+		const fault = this.buffTargetDied(buffWindow)
+		if (fault === SHORT_WINDOW_FAULT.PARTNER) {
+			return <Trans id="drg.ds.notes.partnerdead">Partner Died</Trans>
+		}
+		else if (fault === SHORT_WINDOW_FAULT.DRG) {
+			return <Trans id="drg.ds.notes.drgdied">You Died</Trans>
+		}
+
+		return undefined
+	}
+
+	// pulled into a helper due to use in multiple spots
+	// returns true if:
+	// - the buff ended early during the fight
+	private buffTargetDied(buffWindow: BuffWindowState): number {
+		const windowDurationMillis = this.buffStatus.duration * 1000
+		const fightTimeRemaining = this.parser.pull.duration - (buffWindow.start - this.parser.eventTimeOffset)
+
+		// rushing, not due to deaths
+		if (windowDurationMillis >= fightTimeRemaining) {
+			return SHORT_WINDOW_FAULT.NONE
+		}
+
+		// if the window duration does not match the actual time (within a reasonable threshold)
+		// mark it
+		const actualWindowDuration = (buffWindow?.end ?? buffWindow.start) - buffWindow.start
+		if (actualWindowDuration < (windowDurationMillis - SHORT_WINDOW_BUFFER)) {
+			// ok now check to see if a player death happened within the expected window.
+			const playerDeath = this.deathTimes.filter(deathTime => {
+				// check if time is within reasonable distance of the window end
+				return (buffWindow.start < deathTime && deathTime < buffWindow.start + (actualWindowDuration + SHORT_WINDOW_BUFFER))
+			})
+
+			// if there was a player death, oops it's your fault now
+			if (playerDeath.length > 0) {
+				return SHORT_WINDOW_FAULT.DRG
+			}
+
+			// otherwise it's your partner and you're off the hook
+			return SHORT_WINDOW_FAULT.PARTNER
+		}
+
+		return SHORT_WINDOW_FAULT.NONE
 	}
 }
