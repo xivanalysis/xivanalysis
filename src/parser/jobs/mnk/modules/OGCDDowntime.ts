3b821e04fae392be12da2445e9440530097ff0c1
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2020-12-28 15:26:16 +1100

    Adjust OGCD allowed downtime and documentation

--- src/parser/jobs/mnk/modules/OGCDDowntime.ts
+++ src/parser/jobs/mnk/modules/OGCDDowntime.ts
@@ -4,11 +4,15 @@ import {CooldownDowntime} from 'parser/core/modules/CooldownDowntime'
 // Approximate downtime window to account for aligning GCDs and unreliable FBC procs:
 // 2 GCDs at a slow 2.0s GCD, 600ms for animation lock, and another 250ms to deal with
 // high end of "sane" ping or a late FBC proc clipping the GCD.
-const DEFAULT_ALLOWED_DOWNTIME = 4850
+const DEFAULT_AVERAGE_DOWNTIME = 2425
+
+// Chakra delay offset is to allow for delaying other CDs to place an FBC since they can
+// pop super fast during BH and are largely higher potency than other cooldowns. All
+// other cooldowns should be used by the 16th second of the fight tho so here we are.
 const MAX_CHAKRA_DELAY_OFFSET = 16000
 
 export default class OGCDDowntime extends CooldownDowntime {
-	defaultAllowedAverageDowntime = DEFAULT_ALLOWED_DOWNTIME
+	defaultAllowedAverageDowntime = DEFAULT_AVERAGE_DOWNTIME
 	trackedCds = [
 		{
 			cooldowns: [ACTIONS.BROTHERHOOD],
