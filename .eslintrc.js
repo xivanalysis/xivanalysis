f01035cfb46585243140ee5e3c5699ab87f50406
ackwell <saxon@ackwell.com.au>
2021-01-08 17:17:28 +1100

    Fix linebreak-style

--- .eslintrc.js
+++ .eslintrc.js
@@ -146,7 +146,6 @@ module.exports = {
 				'array-bracket-spacing': 'off',
 				'key-spacing': 'off',
 				'space-in-parens': 'off',
-				'linebreak-style': 'off',
 				'no-duplicate-imports': 'off',
 				'space-before-blocks': 'off',
 				'block-spacing': 'off',
