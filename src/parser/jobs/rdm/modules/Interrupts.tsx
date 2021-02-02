c59fb69a52beeaf2626112632e52c751dc98431e
yumiyafangirl <yumiyafangirl@fakeemail.com>
2021-02-02 17:11:30 -0500

    better words

--- src/parser/jobs/rdm/modules/Interrupts.tsx
+++ src/parser/jobs/rdm/modules/Interrupts.tsx
@@ -5,6 +5,6 @@ import React from 'react'
 
 export default class Interrupts extends CoreInterrupts {
 	suggestionContent = <Trans id="rdm.interrupts.suggestion.content">
-		Avoid interrupting casts by either prepositioning yourself or utilizing slidecasting where possible. If you must move, try to save a <ActionLink {...this.data.actions.SWIFTCAST}/> or pool mana for a melee combo or <ActionLink {...this.data.actions.ENCHANTED_REPRISE}/>.
+		Avoid interrupting casts by either prepositioning yourself or utilizing slidecasting where possible. If you need to move, try to save a use of <ActionLink {...this.data.actions.SWIFTCAST}/> or pool mana for a melee combo or <ActionLink {...this.data.actions.ENCHANTED_REPRISE}/>.
 	</Trans>
 }
