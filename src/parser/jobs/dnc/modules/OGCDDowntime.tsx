bbedfe5637c29b0340db115da5500dddb6356902
yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>
2021-02-18 23:49:37 -0500

    add stricter allowed downtime to Dancer steps (#1106)
    Co-authored-by: yumiyafangirl <yumiyafangirl@fakeemail.com>

--- src/parser/jobs/dnc/modules/OGCDDowntime.tsx
+++ src/parser/jobs/dnc/modules/OGCDDowntime.tsx
@@ -1,10 +1,24 @@
 import ACTIONS from 'data/ACTIONS'
 import {CooldownDowntime} from 'parser/core/modules/CooldownDowntime'
 
+/**
+ * The default allowance for dances is too lenient since in practice
+ * they're just GCDs and drifting them is heavily discouraged.
+ * 250 ms should be enough to forgive light clipping.
+ */
+const ALLOWED_DANCE_DOWNTIME = 250
+
 export default class OGCDDowntime extends CooldownDowntime {
 	trackedCds = [
-		{cooldowns: [ACTIONS.TECHNICAL_STEP]},
-		{cooldowns: [ACTIONS.STANDARD_STEP], firstUseOffset: -15000},
+		{
+			cooldowns: [ACTIONS.TECHNICAL_STEP],
+			allowedAverageDowntime: ALLOWED_DANCE_DOWNTIME,
+		},
+		{
+			cooldowns: [ACTIONS.STANDARD_STEP],
+			firstUseOffset: -15000,
+			allowedAverageDowntime: ALLOWED_DANCE_DOWNTIME,
+		},
 		{cooldowns: [ACTIONS.DEVILMENT]},
 		{cooldowns: [ACTIONS.FLOURISH]},
 	]
