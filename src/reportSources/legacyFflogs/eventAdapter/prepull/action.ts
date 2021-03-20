785db665971e07fa665e7eed2b8693d780e78c4b
ackwell <saxon@ackwell.com.au>
2021-03-20 15:09:46 +1100

    Swap prepull logic to postprocess step

--- src/reportSources/legacyFflogs/eventAdapter/prepull/action.ts
+++ src/reportSources/legacyFflogs/eventAdapter/prepull/action.ts
@@ -1,36 +1,23 @@
 import {Event, Events} from 'event'
-import {FflogsEvent} from 'fflogs'
-import {isDefined} from 'utilities'
 import {AdapterStep} from '../base'
 
 export class PrepullActionAdapterStep extends AdapterStep {
-	private finished = false
-	private initialEventLocation?: Event[]
+	postprocess(adaptedEvents: Event[]): Event[] {
+		const precastEvents: Event[] = []
 
-	adapt(baseEvent: FflogsEvent, adaptedEvents: Event[]): Event[] {
-		if (this.finished) {
-			return adaptedEvents
-		}
-
-		if (!isDefined(this.initialEventLocation)) {
-			this.initialEventLocation = adaptedEvents
-		}
-
-		const synthesizedEvents: Event[] = []
 		for (const event of adaptedEvents) {
 			if (event.type === 'snapshot') {
 				// Create a new action event and push it to the front of the adapted events
-				const actionEvent = this.synthesizeActionEvent(event)
-				synthesizedEvents.push(actionEvent)
+				precastEvents.push(this.synthesizeActionEvent(event))
 			} else if (event.type === 'action') {
 				// Stop once we hit the first real action event post-pull
-				this.finished = true
+				break
 			}
 		}
 
-		this.initialEventLocation.unshift(...synthesizedEvents)
-
-		return adaptedEvents
+		return precastEvents.length > 0
+			? [...precastEvents, ...adaptedEvents]
+			: adaptedEvents
 	}
 
 	private synthesizeActionEvent(event: Events['snapshot']): Events['action'] {
