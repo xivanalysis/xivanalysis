8c0286034e7eaff43286c68fd5c0aceae5eb6694
ackwell <saxon@ackwell.com.au>
2021-03-31 00:20:09 +1100

    Move Patch to data

--- src/data/PATCHES/index.ts
+++ src/data/PATCHES/index.ts
@@ -3,5 +3,6 @@ import {PATCHES} from './patches'
 export default PATCHES
 
 export {GameEdition, languageToEdition} from './editions'
+export {Patch} from './Patch'
 export type {PatchInfo, PatchNumber} from './patches'
 export {getPatch, getPatchDate, getReportPatch, patchSupported} from './utilities'
