59890250b15c141ef9b7fc401658751d86e364da
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2021-04-08 23:43:31 -0500

    changelog

--- src/parser/jobs/blm/index.js
+++ src/parser/jobs/blm/index.js
@@ -68,7 +68,12 @@ export default new Meta({
 	},
 	{
 		date: new Date('2021-03-18'),
-		Changes: () => <>Forgive single-weave Triplecast in JP opener</>,
+		Changes: () => <>Forgive single-weave Triplecast in JP opener.</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
+	{
+		date: new Date('2021-04-08'),
+		Changes: () => <>Updated Rotation Outliers suggestions for cycles before downtimes, and Umbral Soul usage during uptime.</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
 	}],
 })
