3bf073776a62d1c9113629f008560d5a6c86e4d2
Ra-Workspace <ra.workspace@gmail.com>
2021-04-12 21:58:17 +0900

    Add patch date for KR v5.4

--- src/data/PATCHES/patches.ts
+++ src/data/PATCHES/patches.ts
@@ -83,6 +83,7 @@ const patchData = {
 	'5.4': {
 		date: {
 			[GameEdition.GLOBAL]: 1607418000, // 08/12/20 09:00:00 GMT
+			[GameEdition.KOREAN]: 1621324800, // 18/05/21 08:00:00 GMT
 		},
 	},
 }
