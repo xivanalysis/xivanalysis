9dcc4802bc40bbecd9420e2a33985b0922d73bad
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2021-04-13 23:10:01 +1000

    Patch 5.5 support for WAR, DRK, and GNB (#1138)
    * Add patch 5.5 layer
    
    * Update WAR for patch 5.5 support
    
    * Mark DRK patch 5.5 support
    
    * Mark GNB patch 5.5 support
    
    * Remove tslint override from patch 5.5 layer

--- src/data/ACTIONS/layers/patch5.5.ts
+++ src/data/ACTIONS/layers/patch5.5.ts
@@ -1,9 +1,23 @@
 import {Layer} from 'data/layer'
 import {ActionRoot} from '../root'
 
-// tslint:disable:no-magic-numbers
-
 export const patch550: Layer<ActionRoot> = {
 	patch: '5.5',
-	data: {},
+	data: {
+		// WAR 5.5 potency changes
+		MAIM: {combo: {
+			from: 31,
+			potency: 320,
+		}},
+		STORMS_EYE: {combo: {
+			from: 37,
+			potency: 420,
+			end: true,
+		}},
+		STORMS_PATH: {combo: {
+			from: 37,
+			potency: 420,
+			end: true,
+		}},
+	},
 }
