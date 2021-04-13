480df393e8dcb6dfc2e4170f3a810ec4e5bc9c9f
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2021-04-13 15:29:48 +1000

    Add patch 5.5 actions layer

---
+++ src/data/ACTIONS/layers/patch5.5.ts
@@ -0,0 +1,9 @@
+import {Layer} from 'data/layer'
+import {ActionRoot} from '../root'
+
+// tslint:disable:no-magic-numbers
+
+export const patch550: Layer<ActionRoot> = {
+	patch: '5.5',
+	data: {},
+}
