853d9face543f1e0ede8c50bfcd4a1ddc92a0c96
Pogramos <guilherme.hor@gmail.com>
2021-04-19 20:11:02 -0300

    refactor(oathgauge): extending from CoreGauge module
    refactored OathGauge class to extend from CoreGauge making it ready for future changes

--- src/parser/jobs/pld/modules/OathGauge.tsx
+++ src/parser/jobs/pld/modules/OathGauge.tsx
@@ -1,152 +1,81 @@
 import {t} from '@lingui/macro'
 import {Trans} from '@lingui/react'
-import Color from 'color'
 import {ActionLink} from 'components/ui/DbLink'
-import TimeLineChart from 'components/ui/TimeLineChart'
 import ACTIONS from 'data/ACTIONS'
 import JOBS from 'data/JOBS'
-import Module, {dependency} from 'parser/core/Module'
+import {dependency} from 'parser/core/Module'
+import {CounterGauge, Gauge as CoreGauge} from 'parser/core/modules/Gauge'
 import Suggestions, {SEVERITY, Suggestion} from 'parser/core/modules/Suggestions'
 import React, {Fragment} from 'react'
 
-// Generators
-const GAUGE_GENERATORS = [ACTIONS.ATTACK.id]
-
-const GAUGE_SPENDERS = [
-	ACTIONS.COVER.id,
-	ACTIONS.INTERVENTION.id,
-	ACTIONS.SHELTRON.id,
-]
-
 // Gauge numbers
-const GAUGE_MAX = 100
-const GAUGE_START = 0
 const GAUGE_GAIN_AMOUNT = 5
 const GAUGE_SPEND_AMOUNT = 50
 
-// Graph color
-const GRAPH_COLOR = Color(JOBS.PALADIN.colour)
-const BG_COLOR_FADE = 0.7
-const BORDER_COLOR_FADE = 0.5
-
-class GaugeState {
-	t?: number
-	y?: number
+const GAUGE_SEVERITY = {
+	10: SEVERITY.MINOR,
 }
 
-export default class OathGauge extends Module {
-	static handle = 'oathGauge'
+export default class OathGauge extends CoreGauge {
 	static title = t('pld.gauge.title')`Oath Gauge Usage`
 
 	@dependency private suggestions!: Suggestions
 
-	// Defaults
-	private gauge = GAUGE_START
-	private waste = 0
-	private history: GaugeState[] = []
-	private ability = ''
+	private oathGauge = this.add(new CounterGauge({
+		chart: {label: 'Oath Gauge', color: JOBS.PALADIN.colour},
+	}))
+
+	private oathConsumers = [
+		ACTIONS.SHELTRON.id,
+		ACTIONS.INTERVENTION.id,
+		ACTIONS.COVER.id,
+	]
 
 	protected init() {
-		this.addEventHook('cast', {
-			by: 'player',
-			abilityId: GAUGE_GENERATORS,
-		}, this.onGaugeGenerate)
+		super.init()
 
 		this.addEventHook('cast', {
 			by: 'player',
-			abilityId: GAUGE_SPENDERS,
-		}, this.onGaugeSpend)
+			abilityId: ACTIONS.ATTACK.id,
+		}, this.onDamage)
 
-		this.addEventHook('death', {to: 'player'}, this.onDeath)
+		this.addEventHook('cast', {
+			by: 'player',
+			abilityId: this.oathConsumers,
+		}, this.onConsumeOath)
+
 		this.addEventHook('complete', this.onComplete)
 	}
 
-	private onGaugeSpend() {
-		this.gauge -= GAUGE_SPEND_AMOUNT
-		this.updateHistory()
+	// HELPERS
+	private onDamage() {
+		this.oathGauge.modify(GAUGE_GAIN_AMOUNT)
 	}
 
-	private onGaugeGenerate() {
-		this.gauge += GAUGE_GAIN_AMOUNT
-		if (this.gauge > GAUGE_MAX) {
-			this.gauge = GAUGE_MAX
-			this.waste += GAUGE_GAIN_AMOUNT
-		}
-		this.updateHistory()
-	}
-
-	private onDeath() {
-		this.gauge = 0
-		this.updateHistory()
+	private onConsumeOath() {
+		this.oathGauge.modify(-GAUGE_SPEND_AMOUNT)
 	}
 
 	private onComplete() {
 		this.suggestions.add(new Suggestion({
 			icon: ACTIONS.SHELTRON.icon,
-			severity: SEVERITY.MINOR,
 			content: <Trans id="pld.gauge.waste.suggestion.content">
-					You should periodically use your gauge either on you with <ActionLink {... ACTIONS.SHELTRON}/> or in case you're off-tanking with an <ActionLink {...ACTIONS.INTERVENTION} /> on your tank partner.
+					You should periodically use your gauge either on you with a <ActionLink {...ACTIONS.SHELTRON}/> or on your tank partner with an <ActionLink {...ACTIONS.INTERVENTION} /> in case you're off-tanking.
 			</Trans>,
 			why: <Trans id="pld.gauge.waste.suggestion.why">
-				A total of {this.waste} gauge was lost due to exceeding the cap, part of this should be used to reduce incoming damage either on you or on your tank partner
+				A total of {this.oathGauge.overCap} gauge was lost due to exceeding the cap, part of this should be used to reduce incoming damage from abilities or auto-attacks.
 			</Trans>,
+			severity: SEVERITY.MINOR,
+			value: this.oathGauge.overCap,
 		}))
 	}
 
-	private updateHistory() {
-		const timestamp = this.parser.currentTimestamp - this.parser.fight.start_time
-		this.history.push({
-			t: timestamp,
-			y: this.gauge,
-		})
-	}
-
 	output() {
-		const data = {
-			datasets: [
-				{
-					label: 'Oath Gauge',
-					steppedLine: true,
-					data: this.history,
-					backgroundColor: GRAPH_COLOR.fade(BG_COLOR_FADE).toString(),
-					borderColor: GRAPH_COLOR.fade(BORDER_COLOR_FADE).toString(),
-				},
-			],
-		}
-		const rampCheck = 5
-		const options = {
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero: true,
-						min: 0,
-						max: GAUGE_MAX,
-						callback: ((value: number) => {
-							if (value % rampCheck === 0) {
-								return value
-							}
-						}),
-					},
-				}],
-			},
-			/* eslint-disable @typescript-eslint/no-explicit-any */
-			tootips: {
-				callbacks: {
-					label: function(tooltipItem: any, data: any) {
-						const datasetIndex = tooltipItem.datasetIndex
-						const valueIndex = tooltipItem.index
-						const hovered = data.datasets[datasetIndex].data[valueIndex]
-
-						return `${hovered.y} After ${hovered.source}`
-					},
-				},
-			},
-		}
-
+		const fragment = super.output()
 		return <Fragment>
-			<TimeLineChart data={data} options={options} />
+			{fragment}
 			<Trans id="pld.gauge.waste">
-				A total of {this.waste} Oath Gauge was wasted due to overcapping.
+				A total of {this.oathGauge.overCap} Oath Gauge was wasted due to overcapping.
 			</Trans>
 		</Fragment>
 	}
