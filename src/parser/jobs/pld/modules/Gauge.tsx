e5d840473a740bebf64c690641c805da8fb6edf2
Pogramos <guilherme.hor@gmail.com>
2021-05-03 15:19:43 -0300

    refactor(gauge): applying changes based on feedback PR #1153
    - Removed hooks in favor of a hook that will map both spenders and generators in a single
    function;
    - Changed the text to use more of a passive tone instead of telling the user what to do;
    
    "pr #1153"

--- src/parser/jobs/pld/modules/Gauge.tsx
+++ src/parser/jobs/pld/modules/Gauge.tsx
@@ -3,15 +3,22 @@ import {Trans} from '@lingui/react'
 import {ActionLink} from 'components/ui/DbLink'
 import ACTIONS from 'data/ACTIONS'
 import JOBS from 'data/JOBS'
+import {CastEvent} from 'fflogs'
 import {dependency} from 'parser/core/Module'
 import {CounterGauge, Gauge as CoreGauge} from 'parser/core/modules/Gauge'
 import Suggestions, {SEVERITY, Suggestion} from 'parser/core/modules/Suggestions'
 import React from 'react'
 
-// Gauge numbers
-const GAUGE_GAIN_AMOUNT = 5
-const GAUGE_SPEND_AMOUNT = 50
+interface GaugeModifier {
+	[key: string]: number | undefined
+}
 
+const OATH_MODIFIERS = new Map<number, GaugeModifier>([
+	[ACTIONS.ATTACK.id, {cast: 5}],
+	[ACTIONS.SHELTRON.id, {cast: -50}],
+	[ACTIONS.INTERVENTION.id, {cast: -50}],
+	[ACTIONS.COVER.id, {cast: -50}],
+])
 export default class Gauge extends CoreGauge {
 	static title = t('pld.gauge.title')`Oath Gauge Usage`
 
@@ -21,45 +28,31 @@ export default class Gauge extends CoreGauge {
 		chart: {label: 'Oath Gauge', color: JOBS.PALADIN.colour},
 	}))
 
-	private oathConsumers = [
-		ACTIONS.SHELTRON.id,
-		ACTIONS.INTERVENTION.id,
-		ACTIONS.COVER.id,
-	]
-
 	protected init() {
 		super.init()
 
-		this.addEventHook('cast', {
-			by: 'player',
-			abilityId: ACTIONS.ATTACK.id,
-		}, this.onDamage)
-
-		this.addEventHook('cast', {
-			by: 'player',
-			abilityId: this.oathConsumers,
-		}, this.onConsumeOath)
-
+		this.addEventHook('cast', {by: 'player', abilityId: Array.from(OATH_MODIFIERS.keys())}, this.onOathModifying)
 		this.addEventHook('complete', this.onComplete)
 	}
 
 	// HELPERS
-	private onDamage() {
-		this.oathGauge.modify(GAUGE_GAIN_AMOUNT)
-	}
+	private onOathModifying(event: CastEvent) {
+		const modifiers = OATH_MODIFIERS.get(event.ability.guid)
 
-	private onConsumeOath() {
-		this.oathGauge.modify(-GAUGE_SPEND_AMOUNT)
+		if (modifiers) {
+			const amount = modifiers[event.type] || 0
+			this.oathGauge.modify(amount)
+		}
 	}
 
 	private onComplete() {
 		this.suggestions.add(new Suggestion({
 			icon: ACTIONS.SHELTRON.icon,
 			content: <Trans id="pld.gauge.waste.suggestion.content">
-					You should periodically use your gauge either on you with a <ActionLink {...ACTIONS.SHELTRON}/> or on your tank partner with an <ActionLink {...ACTIONS.INTERVENTION} /> in case you're off-tanking.
+					Using <ActionLink {...ACTIONS.SHELTRON}/> on yourself or <ActionLink {...ACTIONS.INTERVENTION} /> on a tank partner in case you're off tanking could reduce incoming damage from abilities or auto-attacks.
 			</Trans>,
 			why: <Trans id="pld.gauge.waste.suggestion.why">
-				A total of {this.oathGauge.overCap} gauge was lost due to exceeding the cap, part of this should be used to reduce incoming damage from abilities or auto-attacks.
+				A total of {this.oathGauge.overCap} gauge was lost due to exceeding the cap.
 			</Trans>,
 			severity: SEVERITY.MINOR,
 			value: this.oathGauge.overCap,
