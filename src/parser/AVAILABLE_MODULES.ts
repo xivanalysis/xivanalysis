97f2dba8cbbbd00854104a3b709ced4dc494b0f7
Saxon Landers <saxon@ackwell.com.au>
2021-05-09 17:27:33 +1000

    Backfill config for new invulnerability module (#1164)

--- src/parser/AVAILABLE_MODULES.ts
+++ src/parser/AVAILABLE_MODULES.ts
@@ -1,7 +1,9 @@
 import BOSSES, {Boss} from 'data/BOSSES'
 import {JobKey} from 'data/JOBS'
 import {e121} from './bosses/e12_1'
+import {e122} from './bosses/e12_2'
 import {e4} from './bosses/e4'
+import {e6} from './bosses/e6'
 import {e7} from './bosses/e7'
 import {e8} from './bosses/e8'
 import {e9} from './bosses/e9'
@@ -13,8 +15,13 @@ import {exRuby1} from './bosses/exRuby1'
 import {exRuby2} from './bosses/exRuby2'
 import {exTitania} from './bosses/exTitania'
 import {exVaris} from './bosses/exVaris'
+import {neir11} from './bosses/nier11'
 import {neir3} from './bosses/nier3'
+import {neir4} from './bosses/nier4'
 import {tea} from './bosses/tea'
+import {urLeviathan} from './bosses/urLeviathan'
+import {urShiva} from './bosses/urShiva'
+import {urTitan} from './bosses/urTitan'
 import CORE from './core'
 import {Meta} from './core/Meta'
 import ASTROLOGIAN from './jobs/ast'
@@ -72,6 +79,8 @@ const AVAILABLE_MODULES: AvailableModules = {
 
 	BOSSES: {
 		[BOSSES.NIER3.logId]: neir3,
+		[BOSSES.NIER4.logId]: neir4,
+		[BOSSES.NIER11.logId]: neir11,
 
 		[BOSSES.TITANIA.logId]: exTitania,
 		[BOSSES.INNOCENCE.logId]: exInnocence,
@@ -82,11 +91,17 @@ const AVAILABLE_MODULES: AvailableModules = {
 		[BOSSES.EMERALD_WEAPON_1.logId]: exEmerald1,
 		[BOSSES.EMERALD_WEAPON_2.logId]: exEmerald2,
 
+		[BOSSES.UNREAL_SHIVA.logId]: urShiva,
+		[BOSSES.UNREAL_TITAN.logId]: urTitan,
+		[BOSSES.UNREAL_LEVIATHAN.logId]: urLeviathan,
+
 		[BOSSES.E4.logId]: e4,
+		[BOSSES.E6.logId]: e6,
 		[BOSSES.E7.logId]: e7,
 		[BOSSES.E8.logId]: e8,
 		[BOSSES.E9.logId]: e9,
 		[BOSSES.E12_1.logId]: e121,
+		[BOSSES.E12_2.logId]: e122,
 
 		[BOSSES.TEA.logId]: tea,
 	},
