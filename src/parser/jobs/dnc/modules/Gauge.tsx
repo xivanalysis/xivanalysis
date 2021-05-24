0845c20b55dd1a74db33bbb4bbd2d72a52ad8dcd
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2021-05-23 23:27:29 -0500

    Midas whale move DNC gauge to separate row too

--- src/parser/jobs/dnc/modules/Gauge.tsx
+++ src/parser/jobs/dnc/modules/Gauge.tsx
@@ -372,13 +372,13 @@ export default class Gauge extends Analyser {
 
 	/* Parse Completion and output */
 	private onComplete() {
-		this.resourceGraphs.addResource({
+		this.resourceGraphs.addGauge({
 			label: <Trans id="dnc.gauge.resource.esprit">Esprit</Trans>,
 			colour: DNC_COLOR,
 			data: this.espritGauge.history,
 		})
 
-		this.resourceGraphs.addResource({
+		this.resourceGraphs.addGauge({
 			label: <Trans id="dnc.gauge.resource.feathers">Feathers</Trans>,
 			// eslint-disable-next-line @typescript-eslint/no-magic-numbers
 			colour: DNC_COLOR.fade(0.6).toString(),
