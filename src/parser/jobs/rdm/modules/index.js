3cb4e7c49d64cfd6c9df169c490d4db1a9a974be
Oowazu Nonowazu <83298827+nonowazu@users.noreply.github.com>
2021-10-18 20:25:36 -0500

    First pass at engagement/displacement tracking (#1245)
    * First pass at engagement/displacement tracking
    
    * Oh, `yarn extract`, the warnings, the warnings are calling
    
    * set `debug` to false
    
    * Force CI rebuild
    
    Co-authored-by: Azariah Aeroborn <26804378+AzariahAeroborn@users.noreply.github.com>

--- src/parser/jobs/rdm/modules/index.js
+++ src/parser/jobs/rdm/modules/index.js
@@ -2,6 +2,7 @@ import {AlwaysBeCasting} from 'parser/core/modules/AlwaysBeCasting'
 import {Tincture} from 'parser/core/modules/Tincture'
 import Combos from './Combos'
 import {DualCast} from './Dualcast'
+import {EngagementDisplacementTracking} from './EngagementDisplacementTracking'
 import Gauge from './Gauge'
 import GeneralCDDowntime from './GeneralCDDowntime'
 import {Interrupts} from './Interrupts'
@@ -15,6 +16,7 @@ export default [
 	AlwaysBeCasting,
 	Gauge,
 	DualCast,
+	EngagementDisplacementTracking,
 	GeneralCDDowntime,
 	Interrupts,
 	Procs,
