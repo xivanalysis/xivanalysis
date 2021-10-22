63dd72bb0e7ca0b0528eabc6069db4b29595aef6
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2021-10-21 22:36:24 -0500

    AoEUsages for DNC

---
+++ src/parser/jobs/dnc/modules/AoEUsages.ts
@@ -0,0 +1,24 @@
+import {AoEUsages as CoreAoE} from 'parser/core/modules/AoEUsages'
+
+export class AoEUsages extends CoreAoE {
+	suggestionIcon = this.data.actions.WINDMILL.icon
+
+	trackedActions = [
+		{
+			aoeAction: this.data.actions.WINDMILL,
+			stActions: [this.data.actions.CASCADE],
+			minTargets: 2,
+		},
+		/* Pre-staging the following for Endwalker
+		{
+			aoeAction: this.data.actions.RISING_WINDMILL,
+			stActions: [this.data.actions.REVERSE_CASCADE],
+			minTargets: 2,
+		},
+		{
+			aoeAction: this.data.actions.BLOODSHOWER,
+			stActions: [this.data.actions.FOUNTAINFALL],
+			minTargets: 2,
+		},*/
+	]
+}
