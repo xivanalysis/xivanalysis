37469b228afcde9e30e38f29535e0805cf893f52
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2021-10-21 22:36:13 -0500

    AoEUsages for BLM

---
+++ src/parser/jobs/blm/modules/AoEUsages.ts
@@ -0,0 +1,35 @@
+import {AoEUsages as CoreAoE} from 'parser/core/modules/AoEUsages'
+
+export class AoEUsages extends CoreAoE {
+	suggestionIcon = this.data.actions.FOUL.icon
+
+	trackedActions = [
+		{
+			aoeAction: this.data.actions.FOUL,
+			stActions: [this.data.actions.XENOGLOSSY],
+			minTargets: 2,
+		},
+		{
+			aoeAction: this.data.actions.FLARE,
+			stActions: [this.data.actions.DESPAIR],
+			minTargets: 2,
+		},
+		{
+			aoeAction: this.data.actions.THUNDER_IV,
+			stActions: [this.data.actions.THUNDER_III],
+			minTargets: 3,
+		},
+		/* Pre-staging the following for Endwalker
+		{
+			aoeAction: this.data.actions.HIGH_FIRE_II,
+			stActions: [this.data.actions.FIRE_III],
+			minTargets: ,
+		},
+		{
+			aoeAction: this.data.actions.HIGH_BLIZZARD_II,
+			stActions: [this.data.actions.BLIZZARD_III],
+			minTargets: ,
+		},
+		*/
+	]
+}
