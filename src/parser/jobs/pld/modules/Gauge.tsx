65ed91f59431cabdb67adb8727c23e6d5028b4d2
Azariah Aeroborn <26804378+AzariahAeroborn@users.noreply.github.com>
2021-10-31 09:31:02 -0400

    migrate Gauge to analyser (#1293)

--- src/parser/jobs/pld/modules/Gauge.tsx
+++ src/parser/jobs/pld/modules/Gauge.tsx
@@ -1,55 +1,61 @@
 import {t} from '@lingui/macro'
 import {Trans} from '@lingui/react'
-import {ActionLink} from 'components/ui/DbLink'
-import ACTIONS from 'data/ACTIONS'
+import {DataLink} from 'components/ui/DbLink'
 import JOBS from 'data/JOBS'
-import {CastEvent} from 'fflogs'
+import {Event, Events} from 'event'
+import {filter, oneOf} from 'parser/core/filter'
 import {dependency} from 'parser/core/Module'
+import {Data} from 'parser/core/modules/Data'
 import {CounterGauge, Gauge as CoreGauge} from 'parser/core/modules/Gauge'
 import Suggestions, {SEVERITY, Suggestion} from 'parser/core/modules/Suggestions'
 import React from 'react'
 
-interface GaugeModifier {
-	[key: string]: number | undefined
-}
+type GaugeModifier = Partial<Record<Event['type'], number>>
 
-const OATH_MODIFIERS = new Map<number, GaugeModifier>([
-	[ACTIONS.ATTACK.id, {cast: 5}],
-	[ACTIONS.SHELTRON.id, {cast: -50}],
-	[ACTIONS.INTERVENTION.id, {cast: -50}],
-	[ACTIONS.COVER.id, {cast: -50}],
-])
 export default class Gauge extends CoreGauge {
 	static override title = t('pld.gauge.title')`Oath Gauge Usage`
 
+	@dependency private data!: Data
 	@dependency private suggestions!: Suggestions
 
 	private oathGauge = this.add(new CounterGauge({
 		chart: {label: 'Oath Gauge', color: JOBS.PALADIN.colour},
 	}))
+	private oathModifiers = new Map<number, GaugeModifier>([
+		[this.data.actions.ATTACK.id, {action: 5}],
+		[this.data.actions.SHELTRON.id, {action: -50}],
+		[this.data.actions.INTERVENTION.id, {action: -50}],
+		[this.data.actions.COVER.id, {action: -50}],
+	])
 
-	protected override init() {
-		super.init()
+	override initialise() {
+		super.initialise()
 
-		this.addEventHook('cast', {by: 'player', abilityId: Array.from(OATH_MODIFIERS.keys())}, this.onOathModifying)
+		this.addEventHook(
+			filter<Event>()
+				.type('action')
+				.source(this.parser.actor.id)
+				.action(oneOf(Array.from(this.oathModifiers.keys()))),
+			this.onOathModifying
+		)
 		this.addEventHook('complete', this.onComplete)
 	}
 
 	// HELPERS
-	private onOathModifying(event: CastEvent) {
-		const modifiers = OATH_MODIFIERS.get(event.ability.guid)
+	private onOathModifying(event: Events['action']) {
+		const modifier = this.oathModifiers.get(event.action)
 
-		if (modifiers) {
-			const amount = modifiers[event.type] || 0
+		if (modifier != null) {
+			const amount = modifier[event.type] ?? 0
 			this.oathGauge.modify(amount)
 		}
 	}
 
 	private onComplete() {
 		this.suggestions.add(new Suggestion({
-			icon: ACTIONS.SHELTRON.icon,
+			icon: this.data.actions.SHELTRON.icon,
 			content: <Trans id="pld.gauge.waste.suggestion.content">
-					Using <ActionLink {...ACTIONS.SHELTRON}/> on yourself or <ActionLink {...ACTIONS.INTERVENTION} /> on a tank partner in case you're off tanking could reduce incoming damage from abilities or auto-attacks.
+					Using <DataLink action="SHELTRON"/> on yourself or <DataLink action="INTERVENTION"/> on a tank partner in case you're off tanking could reduce incoming damage from abilities or auto-attacks.
 			</Trans>,
 			why: <Trans id="pld.gauge.waste.suggestion.why">
 				A total of {this.oathGauge.overCap} gauge was lost due to exceeding the cap.
