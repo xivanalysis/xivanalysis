c3ecde343300ff2bbc5112bac3f99d5fa6b86034
Saxon Landers <saxon@ackwell.com.au>
2021-10-31 21:48:54 +1100

    Fix over-eager weaving warnings on MNK (#1307)

--- src/parser/jobs/mnk/modules/Weaving.ts
+++ src/parser/jobs/mnk/modules/Weaving.ts
@@ -1,5 +1,14 @@
 import {Weaving as CoreWeaving, Weave} from 'parser/core/modules/Weaving'
 
+// Due to Greased Lightning, Monk frequently hits GCD speeds that make double
+// weaving without clipping really close to the wire, to the point implementation
+// details such as server message delay can effect an effective GCD roll or not.
+// Core weaving marks this breakpoint (with wiggle room) as 1-weave only, but monk
+// realistically uses two weaves regardless in these situations, as the resultant
+// clipping is insufficient to outweigh the damage benefit. We're hard overriding
+// with a permitted double weave here to account for that case - the ABC module
+// will report on any significant downtime stemming from this, or otherwise.
+const REGULAR_MAX_WEAVES = 2
 const SSS_MAX_WEAVES = 4
 
 export class Weaving extends CoreWeaving {
@@ -8,6 +17,6 @@ export class Weaving extends CoreWeaving {
 			return SSS_MAX_WEAVES
 		}
 
-		return super.getMaxWeaves(weave)
+		return REGULAR_MAX_WEAVES
 	}
 }
