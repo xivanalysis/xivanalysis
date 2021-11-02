29a5cf4f1ff32a707f91462ff6a9cdbde2123527
hintxiv <65056303+hintxiv@users.noreply.github.com>
2021-11-01 23:28:05 -0400

    Migrate MCH Reassemble to Analyser (#1292)

--- src/parser/jobs/mch/modules/Reassemble.tsx
+++ src/parser/jobs/mch/modules/Reassemble.tsx
@@ -1,23 +1,24 @@
-import {Trans, Plural} from '@lingui/react'
-import {ActionLink} from 'components/ui/DbLink'
-import {ActionRoot} from 'data/ACTIONS/root'
-import STATUSES from 'data/STATUSES'
-import {CastEvent, BuffEvent} from 'fflogs'
-import Module, {dependency} from 'parser/core/Module'
-import Combatants from 'parser/core/modules/Combatants'
+import {Plural, Trans} from '@lingui/react'
+import {DataLink} from 'components/ui/DbLink'
+import {ActionKey} from 'data/ACTIONS'
+import {Event, Events} from 'event'
+import {Analyser} from 'parser/core/Analyser'
+import {EventHook} from 'parser/core/Dispatcher'
+import {filter, oneOf} from 'parser/core/filter'
+import {dependency} from 'parser/core/Injectable'
 import {Data} from 'parser/core/modules/Data'
-import Suggestions, {TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
+import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
 import React from 'react'
 
 // These are the only GCDs that should be reassembled under normal circumstances
-const REASSEMBLE_GCDS: Array<keyof ActionRoot> = [
+const REASSEMBLE_GCDS: ActionKey[] = [
 	'AIR_ANCHOR',
 	'AUTO_CROSSBOW',
 	'DRILL',
 	'SPREAD_SHOT',
 ]
 
-const OTHER_GCDS: Array<keyof ActionRoot> = [
+const OTHER_GCDS: ActionKey[] = [
 	'BIOBLASTER',
 	'HEAT_BLAST',
 	'HEATED_SPLIT_SHOT',
@@ -25,70 +26,103 @@ const OTHER_GCDS: Array<keyof ActionRoot> = [
 	'HEATED_CLEAN_SHOT',
 ]
 
-export default class Reassemble extends Module {
+const SEVERITIES = {
+	BAD_USES: {
+		1: SEVERITY.MINOR,
+		2: SEVERITY.MEDIUM,
+		3: SEVERITY.MAJOR,
+	},
+	DROPPED_USES: {
+		1: SEVERITY.MEDIUM,
+		2: SEVERITY.MAJOR,
+	},
+}
+
+interface ReassembleState {
+	active: boolean
+	lastGcdTime: number
+	gcdHook?: EventHook<Events['action']>
+}
+
+interface ReassembleHistory {
+	badUses: number
+	droppedUses: number
+}
+
+export class Reassemble extends Analyser {
 	static override handle = 'reassemble'
 
-	@dependency private combatants!: Combatants
 	@dependency private data!: Data
 	@dependency private suggestions!: Suggestions
 
-	private badReassembles = 0
-	private droppedReassembles = 0
-	private lastUse = 0
+	private state: ReassembleState = {active: false, lastGcdTime: 0}
+	private history: ReassembleHistory = {badUses: 0, droppedUses: 0}
 
-	private REASSEMBLE_GCDS: number[] = []
-	private OTHER_GCDS: number[] = []
+	private reassembleGcdIds = REASSEMBLE_GCDS.map(key => this.data.actions[key].id)
+	private otherGcdIds = OTHER_GCDS.map(key => this.data.actions[key].id)
 
-	protected override init() {
-		this.REASSEMBLE_GCDS = REASSEMBLE_GCDS.map(key => this.data.actions[key].id)
-		this.OTHER_GCDS = OTHER_GCDS.map(key => this.data.actions[key].id)
+	private gcdFilter = filter<Event>()
+		.source(this.parser.actor.id)
+		.type('action')
+		.action(oneOf([...this.reassembleGcdIds, ...this.otherGcdIds]))
 
-		this.addEventHook('cast', {by: 'player', abilityId: [...this.REASSEMBLE_GCDS, ...this.OTHER_GCDS]}, this.onCast)
-		this.addEventHook('removedebuff', {by: 'player', abilityId: STATUSES.REASSEMBLED.id}, this.onRemoveReassemble)
+	override initialise() {
+		const reassembleFilter = filter<Event>()
+			.source(this.parser.actor.id)
+			.status(this.data.statuses.REASSEMBLED.id)
+
+		this.addEventHook(reassembleFilter.type('statusApply'), this.onReassemble)
+		this.addEventHook(reassembleFilter.type('statusRemove'), this.onRemove)
 		this.addEventHook('complete', this.onComplete)
 	}
 
-	private onCast(event: CastEvent) {
-		if (this.combatants.selected.hasStatus(STATUSES.REASSEMBLED.id)) {
-			this.badReassembles += this.REASSEMBLE_GCDS.includes(event.ability.guid) ? 0 : 1
+	private onReassemble() {
+		if (this.state.active) {
+			this.history.droppedUses += 1
 		}
-		this.lastUse = event.timestamp
+		this.state.gcdHook = this.addEventHook(this.gcdFilter, this.onCast)
+		this.state.active = true
 	}
 
-	private onRemoveReassemble(event: BuffEvent) {
-		if (event.timestamp !== this.lastUse) {
-			this.droppedReassembles++
+	private onCast(event: Events['action']) {
+		if (this.state.active && !this.reassembleGcdIds.includes(event.action)) {
+			this.history.badUses += 1
 		}
+		this.state.lastGcdTime = event.timestamp
+	}
+
+	private onRemove(event: Events['statusRemove']) {
+		if (event.timestamp !== this.state.lastGcdTime) {
+			this.history.droppedUses += 1
+		}
+		if (this.state.gcdHook != null) {
+			this.removeEventHook(this.state.gcdHook)
+		}
+		this.state.active = false
 	}
 
 	onComplete() {
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.REASSEMBLE.icon,
 			content: <Trans id="mch.reassemble.suggestions.bad-gcds.content">
-				On single targets <ActionLink {...this.data.actions.REASSEMBLE}/> should only ever be used on <ActionLink {...this.data.actions.DRILL}/> and <ActionLink {...this.data.actions.AIR_ANCHOR}/>, as they're your strongest GCDs by a large margin.
+				On single targets <DataLink action="REASSEMBLE" /> should only ever be used on <DataLink action="DRILL" /> and <DataLink action="AIR_ANCHOR" />, as they're your strongest GCDs by a large margin.
 			</Trans>,
-			tiers: {
-				1: SEVERITY.MEDIUM,
-				2: SEVERITY.MAJOR,
-			},
-			value: this.badReassembles,
+			tiers: SEVERITIES.BAD_USES,
+			value: this.history.badUses,
 			why: <Trans id="mch.reassemble.suggestions.bad-gcds.why">
-				You used Reassemble on a non-optimal GCD <Plural value= {this.badReassembles} one="# time" other="# times"/>.
+				You used Reassemble on a non-optimal GCD <Plural value={this.history.badUses} one="# time" other="# times"/>.
 			</Trans>,
 		}))
 
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.REASSEMBLE.icon,
 			content: <Trans id="mch.reassemble.suggestions.dropped.content">
-				Avoid using <ActionLink {...this.data.actions.REASSEMBLE}/> when a boss is about to go untargetable so you don't waste the buff.
+				Avoid using <DataLink action="REASSEMBLE" /> when a boss is about to go untargetable so you don't waste the buff.
 			</Trans>,
-			tiers: {
-				1: SEVERITY.MEDIUM,
-				2: SEVERITY.MAJOR,
-			},
-			value: this.droppedReassembles,
+			tiers: SEVERITIES.DROPPED_USES,
+			value: this.history.droppedUses,
 			why: <Trans id="mch.reassemble.suggestions.dropped.why">
-				You allowed Reassemble to fall off unused <Plural value={this.droppedReassembles} one="# time" other="# times"/>.
+				You allowed Reassemble to fall off unused <Plural value={this.history.droppedUses} one="# time" other="# times"/>.
 			</Trans>,
 		}))
 	}
