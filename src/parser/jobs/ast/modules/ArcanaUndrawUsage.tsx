bf9e46ae4e256bc2aaca85d693945307667fd9cc
hintxiv <65056303+hintxiv@users.noreply.github.com>
2021-11-01 22:58:50 -0400

    Fix incorrect AST Undraw math (#1316)

--- src/parser/jobs/ast/modules/ArcanaUndrawUsage.tsx
+++ src/parser/jobs/ast/modules/ArcanaUndrawUsage.tsx
@@ -1,6 +1,6 @@
 import {Trans, Plural} from '@lingui/react'
 import {DataLink} from 'components/ui/DbLink'
-import {Event, Events} from 'event'
+import {Event} from 'event'
 import {Analyser} from 'parser/core/Analyser'
 import {filter} from 'parser/core/filter'
 import {dependency} from 'parser/core/Injectable'
@@ -10,7 +10,7 @@ import React from 'react'
 
 export default class ArcanaUndrawUsage extends Analyser {
 	static override handle = 'arcanaundraws'
-	private _badUndraws: number = 0
+	private undraws: number = 0
 
 	@dependency private data!: Data
 	@dependency private suggestions!: Suggestions
@@ -19,35 +19,24 @@ export default class ArcanaUndrawUsage extends Analyser {
 		this.addEventHook(filter<Event>()
 			.source(this.parser.actor.id)
 			.type('action')
-			.action(this.data.actions.UNDRAW.id), this._onCast)
-		this.addEventHook('complete', this._onComplete)
-		this._badUndraws = 0
+			.action(this.data.actions.UNDRAW.id), this.onUndraw)
+		this.addEventHook('complete', this.onComplete)
 	}
 
-	_onCast(event: Events['action']) {
-		this._saveIfBadUndraw(event)
+	private onUndraw() {
+		this.undraws += 1
 	}
 
-	_saveIfBadUndraw(event: Events['action']) {
-		const actionId = event.action
-
-		// It's a OGCD Arcana Undraw. GET IT.
-		if (actionId === this.data.actions.UNDRAW.id) {
-			this._badUndraws =+ 1
-		}
-	}
-
-	_onComplete() {
-		if (this._badUndraws > 0) {
+	private onComplete() {
+		if (this.undraws > 0) {
 			this.suggestions.add(new Suggestion({
 				icon: this.data.actions.UNDRAW.icon,
 				content: <Trans id="ast.arcana-undraw-usage.suggestions.content" >
 						Due to Draw starting its cooldown the moment it's used, there is no longer any reason to <DataLink action="UNDRAW" /> instead of playing it or converting it with <DataLink action="MINOR_ARCANA" />.
 				</Trans>,
 				severity: SEVERITY.MAJOR,
-				value: this._badUndraws,
 				why: <Trans id="ast.arcana-undraw-usage.suggestions.why">
-					<Plural value={this._badUndraws} one="# instance" other="# instances" /> of using undraw.
+					<Plural value={this.undraws} one="# instance" other="# instances" /> of using undraw.
 				</Trans>,
 			}))
 		}
