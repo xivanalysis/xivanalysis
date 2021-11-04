22903e48d53053f5c89e95d2553b541eaf6b0ec6
ackwell <saxon@ackwell.com.au>
2021-11-04 22:47:14 +1100

    Remove StB encounters/duties

--- src/data/ENCOUNTERS.ts
+++ src/data/ENCOUNTERS.ts
@@ -9,16 +9,6 @@ const ensureEncounters = <T extends Record<string, Encounter>>(encounters: T): {
 
 export const ENCOUNTERS = ensureEncounters({
 	TRASH: {ids: {legacyFflogs: '0'}},
-
-	// 24mans
-	NIER1: {ids: {legacyFflogs: '2020'}},
-	NIER2: {ids: {legacyFflogs: '2021'}},
-	NIER3: {ids: {legacyFflogs: '2022'}},
-	NIER4: {ids: {legacyFflogs: '2023'}},
-
-	// Trials
-	EX_RUBY_1: {ids: {legacyFflogs: '1051'}},
-	EX_RUBY_2: {ids: {legacyFflogs: '1052'}},
 })
 
 export type EncounterKey = keyof typeof ENCOUNTERS
@@ -36,24 +26,6 @@ export interface Duty {
 const ensureDuties = <T extends Record<string, Duty>>(duties: T): {[K in keyof T]: T[K] & Duty} => duties
 
 export const DUTIES = ensureDuties({
-	// Trials
-	EX_TITANIA: {territoryType: 858},
-	EX_INNOCENCE: {territoryType: 848},
-	EX_HADES: {territoryType: 885},
-	EX_VARIS: {territoryType: 913},
-
-	// Raids
-	E1S: {territoryType: 853},
-	E2S: {territoryType: 854},
-	E3S: {territoryType: 855},
-	E4S: {territoryType: 856},
-	E5S: {territoryType: 906},
-	E6S: {territoryType: 907},
-	E7S: {territoryType: 908},
-	E8S: {territoryType: 909},
-
-	// Ultimates
-	TEA: {territoryType: 887},
 })
 
 export const getDutyBanner = (territoryType: number) =>
