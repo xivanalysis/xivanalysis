33aad08dd6c47ed22e76737545ea64f84a87794b
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2021-11-04 15:06:18 +1100

    Migrate WAR to analyser and kill old ACTIONS/STATUSES references

--- src/parser/jobs/war/modules/index.ts
+++ src/parser/jobs/war/modules/index.ts
@@ -1,15 +1,15 @@
 import {ActionTimeline} from './ActionTimeline'
+import {AoE} from './AoE'
 import {Gauge} from './Gauge'
-import InnerRelease from './InnerRelease'
-import MultiHitSkills from './MultiHitSkills'
-import OGCDDowntime from './OGCDDowntime'
-import StormsEye from './StormsEye'
+import {InnerRelease} from './InnerRelease'
+import {OGCDDowntime} from './OGCDDowntime'
+import {StormsEye} from './StormsEye'
 
 export default [
 	ActionTimeline,
 	Gauge,
 	InnerRelease,
-	MultiHitSkills,
+	AoE,
 	OGCDDowntime,
 	StormsEye,
 ]
