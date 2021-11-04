2086d074777528d9efe5849bf7ce856fbfabe282
ackwell <saxon@ackwell.com.au>
2021-11-04 21:11:32 +1100

    Add endwalker media

--- src/data/PATCHES/patches.ts
+++ src/data/PATCHES/patches.ts
@@ -35,6 +35,16 @@ const patchData = {
 			baseUrl: 'https://stormblood.xivanalysis.com',
 		},
 	},
+	'Shadowbringers': {
+		date: {
+			[GameEdition.GLOBAL]: 1561712400, // 28/06/19 09:00:00 GMT
+			[GameEdition.KOREAN]: 1575360000, // 03/12/19 08:00:00 GMT
+			[GameEdition.CHINESE]: 1571126400, // 15/10/19 08:00:00 GMT
+		},
+		branch: {
+			baseUrl: 'https://shadowbringers.xivanalysis.com',
+		},
+	},
 	'5.0': {
 		date: {
 			[GameEdition.GLOBAL]: 1561712400, // 28/06/19 09:00:00 GMT
