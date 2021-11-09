54e33b0a71b1d3008782c8d58f567de23a140c6d
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2021-11-08 22:33:53 -0600

    Scaffold SGE actions/statuses

--- src/data/STATUSES/root/SGE.ts
+++ src/data/STATUSES/root/SGE.ts
@@ -1,5 +1,96 @@
 import {ensureStatuses} from '../type'
 
+const SGE_STUB = 190705000
+
 export const SGE = ensureStatuses({
-	// TODO: Add statuses here
+	EUKRASIAN_DIAGNOSIS: {
+		id: SGE_STUB + 1,
+		name: 'Eukrasian Diagnosis',
+		icon: '',
+	},
+	HAIMATINON: {
+		id: SGE_STUB + 2,
+		name: 'Haimatinon',
+		icon: '',
+	},
+	EUKRASIAN_PROGNOSIS: {
+		id: SGE_STUB + 3,
+		name: 'Eukrasian Prognosis',
+		icon: '',
+	},
+	PANHAIMATINON: {
+		id: SGE_STUB + 4,
+		name: 'Panaimatinon',
+		icon: '',
+	},
+	PHYSIS: {
+		id: SGE_STUB + 5,
+		name: 'Physis',
+		icon: '',
+	},
+	PHYSIS_II: {
+		id: SGE_STUB + 6,
+		name: 'Physis II',
+		icon: '',
+	},
+	KARDIA: { // The buff the SGE receives when Kardia is active
+		id: SGE_STUB + 7,
+		name: 'Kardia',
+		icon: '',
+	},
+	KARDION: { // The buff the Kardia target receives, this target will be healed
+		id: SGE_STUB + 8,
+		name: 'Kardion',
+		icon: '',
+	},
+	SOTERIA: {
+		id: SGE_STUB + 9,
+		name: 'Soteria',
+		icon: '',
+	},
+	ZOE: {
+		id: SGE_STUB + 10,
+		name: 'Zoe',
+		icon: '',
+	},
+	KRASIS: {
+		id: SGE_STUB + 11,
+		name: 'Krasis',
+		icon: '',
+	},
+	KERACHOLE: {
+		id: SGE_STUB + 12,
+		name: 'Kerachole',
+		icon: '',
+	},
+	KERACHOLE_REGEN: {
+		id: SGE_STUB + 13,
+		name: 'Kerachole Regen', // TODO: Find out what this is actually called, if it is indeed separate from the damage taken component
+		icon: '',
+	},
+	TAUROCHOLE: {
+		id: SGE_STUB + 14,
+		name: 'Taurochole',
+		icon: '',
+	},
+	EUKRASIAN_DOSIS: {
+		id: SGE_STUB + 15,
+		name: 'Eukrasian Dosis',
+		icon: '',
+	},
+	EUKRASIAN_DOSIS_II: {
+		id: SGE_STUB + 16,
+		name: 'Eukrasian Dosis II',
+		icon: '',
+	},
+	EUKRASIAN_DOSIS_III: {
+		id: SGE_STUB + 17,
+		name: 'Eukrasian Dosis III',
+		icon: '',
+	},
+	PNEUMA: {
+		id: SGE_STUB + 18,
+		name: 'Pneuma',
+		icon: '',
+	},
 })
