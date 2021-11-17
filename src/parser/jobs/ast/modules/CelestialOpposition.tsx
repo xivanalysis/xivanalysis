6cf2647406b87b6edddd58e596a6f22bfc9bee3a
Keaton <keatonminers@hotmail.com>
2021-11-17 15:25:38 -0700

    [AST] port Celestial Opposition to analyser and cleanup
    - ported Celestial Opposition to analyser
    - removed sect since unused
    - updated actionlink and statuslink to datalink
    - note: there is a TODO to update uses missed since it doesn't account for downtime effectively

--- src/parser/jobs/ast/modules/CelestialOpposition.tsx
+++ src/parser/jobs/ast/modules/CelestialOpposition.tsx
@@ -1,13 +1,13 @@
 import {Trans} from '@lingui/react'
-import {ActionLink, StatusLink} from 'components/ui/DbLink'
+import {DataLink} from 'components/ui/DbLink'
 import {getDataBy} from 'data'
-import {Status} from 'data/STATUSES'
-import {BuffEvent, CastEvent} from 'fflogs'
-import Module, {dependency} from 'parser/core/Module'
+import {Event, Events} from 'event'
+import {Analyser} from 'parser/core/Analyser'
+import {filter} from 'parser/core/filter'
+import {dependency} from 'parser/core/Injectable'
 import {Data} from 'parser/core/modules/Data'
 import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
 import React from 'react'
-import Sect from './Sect'
 
 const SEVERITY_MOD = {
 	MINOR: 0.1,
@@ -15,29 +15,46 @@ const SEVERITY_MOD = {
 	MAJOR: 0.5,
 }
 
+interface Status {
+	id: number
+	name: string
+	icon: string
+	duration?: number
+	stacksApplied?: number
+	speedModifier?: number
+}
+
 // Lifted from WHM benison and adapted to AST and TSX
-export default class CelestialOpposition extends Module {
+export default class CelestialOpposition extends Analyser {
 	static override handle = 'celestialopposition'
 
 	@dependency private data!: Data
 	@dependency private suggestions!: Suggestions
-	@dependency private sect!: Sect
 
-	private lastUse = 0
-	private uses = 0
-	private totalHeld = 0
+	private lastUse: number = 0
+	private uses: number = 0
+	private totalHeld: number = 0
 
 	private activeSect: Status | undefined
 
-	protected override init() {
-		this.addEventHook('cast', {abilityId: this.data.actions.CELESTIAL_OPPOSITION.id, by: 'player'}, this.onCast)
-		this.addEventHook('applybuff', {abilityId: [this.data.statuses.DIURNAL_SECT.id, this.data.statuses.NOCTURNAL_SECT.id], by: 'player'}, this.onSect)
+	override initialise() {
+		this.addEventHook(filter<Event>()
+			.source(this.parser.actor.id)
+			.type('action')
+			.action(this.data.actions.CELESTIAL_OPPOSITION.id)
+		, this.onCast)
+
+		this.addEventHook(filter<Event>()
+			.source(this.parser.actor.id)
+			.type('statusApply')
+			.status(this.data.statuses.NOCTURNAL_SECT.id)
+		, this.onSect)
 		this.addEventHook('complete', this.onComplete)
 	}
 
-	private onCast(event: CastEvent) {
+	private onCast(event: Events['action']) {
 		this.uses++
-		if (this.lastUse === 0) { this.lastUse = this.parser.fight.start_time }
+		if (this.lastUse === 0) { this.lastUse = this.parser.pull.timestamp  }
 
 		const held = event.timestamp - this.lastUse - this.data.actions.CELESTIAL_OPPOSITION.cooldown
 		if (held > 0) {
@@ -47,15 +64,15 @@ export default class CelestialOpposition extends Module {
 		this.lastUse = event.timestamp
 	}
 
-	private onSect(event: BuffEvent) {
-		this.activeSect = getDataBy(this.data.statuses, 'id', event.ability.guid)
+	private onSect(event: Events['statusApply']) {
+		this.activeSect = getDataBy(this.data.statuses, 'id', event.status)
 	}
 
 	onComplete() {
 		const holdDuration = this.uses === 0 ? this.parser.currentDuration : this.totalHeld
-		const usesMissed = Math.floor(holdDuration / this.data.actions.CELESTIAL_OPPOSITION.cooldown)
-		const fightDuration = this.parser.fight.end_time - this.parser.fight.start_time
-		const maxUses = (fightDuration / this.data.actions.CELESTIAL_OPPOSITION.cooldown) - 1
+		const usesMissed = Math.floor(holdDuration / (this.data.actions.CELESTIAL_INTERSECTION.cooldown))
+		//TODO update max uses to not use the whole duration such as when there are no available targets
+		const maxUses = (this.parser.pull.duration / this.data.actions.CELESTIAL_INTERSECTION.cooldown) - 1
 
 		const WASTED_USE_TIERS = {
 			[maxUses * SEVERITY_MOD.MINOR]: SEVERITY.MINOR,
@@ -63,11 +80,11 @@ export default class CelestialOpposition extends Module {
 			[maxUses * SEVERITY_MOD.MAJOR]: SEVERITY.MAJOR, // if not used at all, it'll be set to 100 for severity checking
 		}
 		const suggestContentDiurnal = <Trans id="ast.celestial-opposition.suggestion.content.diurnal">
-				Use <ActionLink {...this.data.actions.CELESTIAL_OPPOSITION} /> more frequently. In <StatusLink {...this.data.statuses.DIURNAL_SECT} />, the heal and regen combined add up to the same potency of a <ActionLink {...this.data.actions.BENEFIC_II} /> on each player it reaches.
+				Use <DataLink action="CELESTIAL_OPPOSITION" /> more frequently. In <DataLink status="DIURNAL_SECT" />, the heal and regen combined add up to the same potency of a <DataLink action="BENEFIC_II" /> on each player it reaches.
 				Trusting the regens to top off the party HP will save MP and GCDs on healing.
 		</Trans>
 		const suggestContentNoct = <Trans id="ast.celestial-opposition.suggestion.content.noct">
-				Use <ActionLink {...this.data.actions.CELESTIAL_OPPOSITION} /> more frequently. In <StatusLink {...this.data.statuses.NOCTURNAL_SECT} />, the shield is the same potency as from <ActionLink {...this.data.actions.ASPECTED_HELIOS} />,
+				Use <DataLink action="CELESTIAL_OPPOSITION" /> more frequently. In <DataLink status="NOCTURNAL_SECT" />, the shield is the same potency as from <DataLink action="ASPECTED_HELIOS" />,
 				so it can save MP and GCDs casting it. Since shields last 30 seconds it can be cast much earlier than incoming damage and allow the cooldown to refresh sooner.
 		</Trans>
 
@@ -80,7 +97,7 @@ export default class CelestialOpposition extends Module {
 				tiers: WASTED_USE_TIERS,
 				value: this.uses === 0 ? 100 : usesMissed,
 				why: <Trans id="ast.celestial-opposition.suggestion.why">
-					About {usesMissed} uses of <ActionLink {...this.data.actions.CELESTIAL_OPPOSITION} /> were missed by holding it for at least a total of {this.parser.formatDuration(holdDuration)}.
+					About {usesMissed} uses of <DataLink action="CELESTIAL_OPPOSITION" /> were missed by holding it for at least a total of {this.parser.formatDuration(holdDuration)}.
 				</Trans>,
 			}))
 		}
