c5067fba922500f4c02225fef8b96a324cfcae18
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2021-11-22 13:32:41 +1100

    Stub RPR data

--- src/parser/jobs/rpr/changelog.tsx
+++ src/parser/jobs/rpr/changelog.tsx
@@ -1,6 +1,6 @@
 export const changelog = [
 	// {
-	// 	date: new Date('2020-04-20'),
+	// 	date: new Date('2021-12-03'),
 	// 	Changes: () => <>The changes you made</>,
 	// 	contrubutors: [CONTRIBUTORS.YOU],
 	// },
