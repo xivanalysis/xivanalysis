c59cfe6ad37b4832c77b0a508c2df6f9d1988e28
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2021-11-27 22:13:48 -0600

    changelog

--- src/parser/jobs/blm/changelog.tsx
+++ src/parser/jobs/blm/changelog.tsx
@@ -12,4 +12,9 @@ export const changelog = [
 		Changes: () => <>Initial data scaffolding and basic updates to handle removal of Enochian ability.</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
 	},
+	{
+		date: new Date('2021-11-27'),
+		Changes: () => <>Preliminary updates to gauge state tracking and suggestion not to waste Paradoxes.</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
 ]
