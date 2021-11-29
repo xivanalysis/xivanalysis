9743498268e511b966ff5cc5967fe9fecff089b2
Evan Shimizu <falindrith@gmail.com>
2021-11-28 19:18:01 -0800

    wip: use full thrust icon

--- src/parser/jobs/drg/modules/Combos.ts
+++ src/parser/jobs/drg/modules/Combos.ts
@@ -2,5 +2,5 @@ import ACTIONS from 'data/ACTIONS'
 import {Combos as CoreCombos} from 'parser/core/modules/Combos'
 
 export class Combos extends CoreCombos {
-	override suggestionIcon = ACTIONS.FULL_THRUST.icon
+	override suggestionIcon = ACTIONS.HEAVENS_THRUST.icon
 }
