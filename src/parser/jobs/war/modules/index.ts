8fee95acf4cbd19b94806c8183da823df16c6f64
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2021-12-02 23:34:00 +1100

    Update WAR gauge analyser and separate Infuriate

--- src/parser/jobs/war/modules/index.ts
+++ src/parser/jobs/war/modules/index.ts
@@ -1,15 +1,17 @@
 import {ActionTimeline} from './ActionTimeline'
 import {AoE} from './AoE'
 import {Gauge} from './Gauge'
+import {Infuriate} from './Infuriate'
 import {InnerRelease} from './InnerRelease'
 import {OGCDDowntime} from './OGCDDowntime'
-import {StormsEye} from './StormsEye'
+import {SurgingTempest} from './SurgingTempest'
 
 export default [
 	ActionTimeline,
 	AoE,
 	Gauge,
+	Infuriate,
 	InnerRelease,
 	OGCDDowntime,
-	StormsEye,
+	SurgingTempest,
 ]
