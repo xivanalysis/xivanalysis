cf5eb49f04c583596bdb0f431d5957742556e058
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2021-12-02 23:28:25 +1100

    Update WAR data bindings

--- src/data/STATUSES/root/WAR.ts
+++ src/data/STATUSES/root/WAR.ts
@@ -1,11 +1,24 @@
 import {ensureStatuses} from '../type'
 
 export const WAR = ensureStatuses({
-	INNER_RELEASE: {
-		id: 1177,
-		name: 'Inner Release',
-		icon: 'https://xivapi.com/i/012000/012556.png',
-		duration: 10000,
+	DEFIANCE: {
+		id: 91,
+		name: 'Defiance',
+		icon: 'https://xivapi.com/i/012000/012551.png',
+	},
+
+	SURGING_TEMPEST: {
+		id: 90,
+		name: 'Surging Tempest',
+		icon: 'https://xivapi.com/i/010000/010263.png',
+		duration: 60000,
+	},
+
+	EQUILIBRIUM: {
+		id: 0,
+		name: 'Equilibrium?',
+		icon: '',
+		duration: 15000,
 	},
 
 	THRILL_OF_BATTLE: {
@@ -15,11 +28,11 @@ export const WAR = ensureStatuses({
 		duration: 10000,
 	},
 
-	RAW_INTUITION: {
-		id: 735,
-		name: 'Raw Intuition',
-		icon: 'https://xivapi.com/i/012000/012555.png',
-		duration: 6000,
+	HOLMGANG: {
+		id: 409,
+		name: 'Holmgang',
+		icon: 'https://xivapi.com/i/000000/000266.png',
+		duration: 10000,
 	},
 
 	VENGEANCE: {
@@ -29,10 +42,39 @@ export const WAR = ensureStatuses({
 		duration: 15000,
 	},
 
-	DEFIANCE: {
-		id: 91,
-		name: 'Defiance',
-		icon: 'https://xivapi.com/i/012000/012551.png',
+	BLOODWHETTING: {
+		id: 0,
+		name: 'Bloodwhetting',
+		icon: '',
+		duration: 8000,
+	},
+
+	STEM_THE_FLOW: {
+		id: 0,
+		name: 'Stem the Flow',
+		icon: '',
+		duration: 4000,
+	},
+
+	STEM_THE_TIDE: {
+		id: 0,
+		name: 'Stem the Tide',
+		icon: '',
+		duration: 20000,
+	},
+
+	NASCENT_FLASH: {
+		id: 1857,
+		name: 'Nascent Flash',
+		icon: 'https://xivapi.com/i/012000/012558.png',
+		duration: 8000,
+	},
+
+	NASCENT_GLINT: {
+		id: 1858,
+		name: 'Nascent Glint',
+		icon: 'https://xivapi.com/i/012000/012559.png',
+		duration: 8000,
 	},
 
 	SHAKE_IT_OFF: {
@@ -42,20 +84,6 @@ export const WAR = ensureStatuses({
 		duration: 15000,
 	},
 
-	HOLMGANG: {
-		id: 409,
-		name: 'Holmgang',
-		icon: 'https://xivapi.com/i/000000/000266.png',
-		duration: 8000,
-	},
-
-	STORMS_EYE: {
-		id: 90,
-		name: 'Storm\'s Eye',
-		icon: 'https://xivapi.com/i/010000/010263.png',
-		duration: 60000,
-	},
-
 	NASCENT_CHAOS: {
 		id: 1897,
 		name: 'Nascent Chaos',
@@ -63,17 +91,18 @@ export const WAR = ensureStatuses({
 		duration: 30000,
 	},
 
-	NASCENT_FLASH: {
-		id: 1857,
-		name: 'Nascent Flash',
-		icon: 'https://xivapi.com/i/012000/012558.png',
-		duration: 6000,
+	INNER_RELEASE: {
+		id: 1177,
+		name: 'Inner Release',
+		icon: 'https://xivapi.com/i/012000/012556.png',
+		duration: 15000,
+		stacksApplied: 3,
 	},
 
-	NASCENT_GLINT: {
-		id: 1858,
-		name: 'Nascent Glint',
-		icon: 'https://xivapi.com/i/012000/012559.png',
-		duration: 6000,
+	PRIMAL_REND_READY: {
+		id: 0,
+		name: 'Primal Rend Ready',
+		icon: '',
+		duration: 30000,
 	},
 })
