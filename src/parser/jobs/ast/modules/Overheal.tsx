2d79912175f760eb02406211e02182a3c14aaaa6
Keaton <keatonminers@hotmail.com>
2021-12-12 22:05:00 -0700

    [AST] updating overheal for delayed heals
    updating overheal section to include delayed heals for additional information since AST has three distinct moves that this could relate to

--- src/parser/jobs/ast/modules/Overheal.tsx
+++ src/parser/jobs/ast/modules/Overheal.tsx
@@ -1,5 +1,6 @@
 import {Trans} from '@lingui/react'
-import {Overheal as CoreOverheal, SuggestedColors} from 'parser/core/modules/Overheal'
+import {DataLink} from 'components/ui/DbLink'
+import {Overheal as CoreOverheal, SuggestedColors, TrackedOverheal} from 'parser/core/modules/Overheal'
 import React from 'react'
 
 export class Overheal extends CoreOverheal {
@@ -8,6 +9,15 @@ export class Overheal extends CoreOverheal {
 	protected override checklistRuleBreakout = true
 	protected override displayPieChart = true
 	protected override displaySuggestion = true
+
+	protected content = <Trans id="ast.overheal.suggestion.content">
+		Avoid healing your party for more than is needed. Cut back on unnecessary heals and coordinate with your co-healer to plan resources efficiently. <br/>
+		* Delayed heals include heals such as <DataLink action="EXALTATION" />, <DataLink action="HOROSCOPE" />, and <DataLink action="MACROCOSMOS" /> when not manually activated.
+	</Trans>
+
+	protected override checklistDescription(_overheals: TrackedOverheal[]): JSX.Element {
+		return this.content
+	}
 
 	protected override trackedHealCategories = [
 		{
@@ -26,6 +36,15 @@ export class Overheal extends CoreOverheal {
 			trackedHealIds: [
 				this.data.actions.STELLAR_BURST.id,
 				this.data.actions.STELLAR_EXPLOSION.id,
+			],
+		},
+		{
+			name: <Trans id="ast.overheal.delayedheals.name">Delayed Heals*</Trans>, //these heals are ones that are planned a bit of time in advance, but not self-activated. i.e. is the astrologian planning well in advance? an overheal in this sense would imply that they don't trust the heals to top off the party.
+			color: SuggestedColors[3],
+			trackedHealIds: [
+				this.data.statuses.HOROSCOPE_HELIOS.id,
+				this.data.statuses.EXALTATION.id,
+				this.data.statuses.MACROCOSMOS.id,
 			],
 		},
 	]
