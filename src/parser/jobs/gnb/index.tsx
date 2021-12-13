ad28f5cf2d78b109f6bcbc75a102fd9ad4af4c87
hintxiv <65056303+hintxiv@users.noreply.github.com>
2021-12-13 00:45:23 -0500

    DNC description cleanup (#1398)
    Co-authored-by: yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>

--- src/parser/jobs/gnb/index.tsx
+++ src/parser/jobs/gnb/index.tsx
@@ -8,7 +8,6 @@ const description = t('gnb.about.description')`This analyzer looks for the low-h
 If you're looking to learn about how exactly the job plays and functions from the ground up, take a look at a few basic guides:
 
 * [General tanking guide by Aletin](https://goo.gl/nYzAnq)
-* [Gunbreaker guide on SaltedXIV](https://saltedxiv.com/guides/gnb)
 * [No Mercy Windows by Rin Karigani](https://i.imgur.com/o8hza9e.png)
 
 If you have any suggestions about the module, feel free to join the XIVA discord and use the feedback channels.
