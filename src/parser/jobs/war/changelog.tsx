ddcce5b5a10f2d31fb79053e652d68e98e30744a
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2021-12-17 00:31:10 +1100

    Add support for Endwalker

---
+++ src/parser/jobs/war/changelog.tsx
@@ -0,0 +1,12 @@
+import CONTRIBUTORS from 'data/CONTRIBUTORS'
+import React from 'react'
+
+export const changelog = [
+	{
+		date: new Date('2021-12-03'),
+		Changes: () => <>
+			Add support for Endwalker.
+		</>,
+		contributors: [CONTRIBUTORS.AY],
+	},
+]
