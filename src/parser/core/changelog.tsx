e1b453be0d1f6642557ea6578433cbfc4d57de47
yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>
2021-12-16 02:30:20 -0500

    unfuck the changelog comment

--- src/parser/core/changelog.tsx
+++ src/parser/core/changelog.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import {ChangelogEntry} from './Meta'
 
 export const changelog: ChangelogEntry[] = [
-	{
+	// {
 	// 	date: new Date('2021-11-19'),
 	// 	Changes: () => <>The changes you made</>,
 	// 	contributors: [CONTRIBUTORS.YOU],
+	// },
+	{
 		date: new Date('2021-12-16'),
 		Changes: () => <>Added Arcane Circle, Radiant Finale, and Searing Light raid buffs to the timeline.</>,
 		contributors: [CONTRIBUTORS.HINT],
