a6fe48eeb7ae21954e818faa2d8fd351f69f620d
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2021-12-17 17:32:57 -0600

    Endwalker - SGE - Preliminary Gauge and Overheal tracking (#1364)
    * Preliminary SGE gauge state tracking
    
    * Preliminary SGE overheal tracking
    
    * Changelog
    
    * yarn extract
    
    * Fix a mistake in the action definitions
    
    * Clean up Overheal bucketing
    
    * Add SGE (and RPR) job colors
    
    * SGE Gauge graph color definitions
    
    * Config updates & organization
    
    * Track Addersting usage (oops)
    
    * Bugfixes & naming update
    
    * yarn extract again
    
    * Update src/parser/jobs/sge/modules/Gauge.tsx
    
    Co-authored-by: Saxon Landers <saxon@ackwell.com.au>
    
    * Fix import, lol
    
    * Rhizomata resets timer if using it caps the gauge
    
    Co-authored-by: Saxon Landers <saxon@ackwell.com.au>

--- src/parser/jobs/sge/changelog.tsx
+++ src/parser/jobs/sge/changelog.tsx
@@ -1,7 +1,15 @@
+import CONTRIBUTORS from 'data/CONTRIBUTORS'
+import React from 'react'
+
 export const changelog = [
 	// {
 	// 	date: new Date('2020-04-20'),
 	// 	Changes: () => <>The changes you made</>,
-	// 	contrubutors: [CONTRIBUTORS.YOU],
+	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
+	{
+		date: new Date('2021-11-27'),
+		Changes: () => <>Preliminary gauge state and overheal tracking</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
 ]
