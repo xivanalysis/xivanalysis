548fc9aa2bcca1b53c9461af1145f0b0e6a80168
Toastdeib <lord.xela@gmail.com>
2021-12-17 15:05:20 +0000

    Changelog, yarn extract

--- src/parser/jobs/nin/index.tsx
+++ src/parser/jobs/nin/index.tsx
@@ -21,6 +21,11 @@ export const NINJA = new Meta({
 	],
 	changelog: [
 		{
+			date: new Date('2021-12-17'),
+			Changes: () => <>Fixed a false positive bug in the Raiju analyser.</>,
+			contributors: [CONTRIBUTORS.TOASTDEIB],
+		},
+		{
 			date: new Date('2021-12-11'),
 			Changes: () => <>Added an analyser for tracking dropped Forked/Fleeting Raiju Ready buffs.</>,
 			contributors: [CONTRIBUTORS.TOASTDEIB],
