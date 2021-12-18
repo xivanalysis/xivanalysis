4ab190ecea5d5020cc621d0e4dcdee0eee2ccb41
hintxiv <65056303+hintxiv@users.noreply.github.com>
2021-12-17 20:53:45 -0500

    RPR - Communio is 2.5s recast (#1425)
    * communio is 2.5s recast
    
    * default recast
    
    Co-authored-by: yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>

--- src/data/ACTIONS/root/RPR.ts
+++ src/data/ACTIONS/root/RPR.ts
@@ -152,7 +152,6 @@ export const RPR = ensureActions({
 		name: 'Communio',
 		icon: 'https://xivapi.com/i/003000/003626.png',
 		onGcd: true,
-		cooldown: 1500,
 		castTime: 1300,
 		speedAttribute: Attribute.SPELL_SPEED,
 	},
