1a7de9d0499314e61b9a9961c8f94e7dce5525dd
KelosPelmand <53376212+KelosPelmand@users.noreply.github.com>
2021-12-18 23:22:50 -0500

    Update SMN Endwalker data (#1405)

--- src/data/STATUSES/root/SMN.ts
+++ src/data/STATUSES/root/SMN.ts
@@ -2,30 +2,18 @@ import {ensureStatuses} from '../type'
 
 // TODO: Fill in the rest of this
 export const SMN = ensureStatuses({
-	GALE_ENFORCER: {
-		id: 1869,
-		name: 'Gale Enforcer',
-		icon: 'https://xivapi.com/i/012000/012721.png',
-	},
-
-	DREADWYRM_TRANCE: {
-		id: 808,
-		name: 'Dreadwyrm Trance',
-		icon: 'https://xivapi.com/i/012000/012680.png',
-		duration: 15000,
+	RADIANT_AEGIS: {
+		id: 2702,
+		name: 'Radiant Aegis',
+		icon: 'https://xivapi.com/i/012000/012691.png',
+		duration: 30000,
 	},
 
 	FURTHER_RUIN: {
 		id: 1212,
 		name: 'Further Ruin',
 		icon: 'https://xivapi.com/i/012000/012685.png',
-	},
-
-	DEVOTION: {
-		id: 1213,
-		name: 'Devotion',
-		icon: 'https://xivapi.com/i/012000/012681.png',
-		duration: 15000,
+		duration: 60000,
 	},
 
 	SEARING_LIGHT: {
@@ -35,31 +23,31 @@ export const SMN = ensureStatuses({
 		duration: 30000,
 	},
 
-	BIO_III: {
-		id: 1214,
-		name: 'Bio III',
-		icon: 'https://xivapi.com/i/012000/012682.png',
-		duration: 30000,
-	},
-
-	MIASMA_III: {
-		id: 1215,
-		name: 'Miasma III',
-		icon: 'https://xivapi.com/i/012000/012683.png',
-		duration: 30000,
-	},
-
-	RUINATION: {
-		id: 1291,
-		name: 'Ruination',
-		icon: 'https://xivapi.com/i/012000/012684.png',
-		duration: 30000,
-	},
-
 	EVERLASTING_FLIGHT: {
 		id: 1868,
 		name: 'Everlasting Flight',
 		icon: 'https://xivapi.com/i/012000/012732.png',
 		duration: 21000,
+	},
+
+	REKINDLE: {
+		id: 2704,
+		name: 'Rekindle',
+		icon: 'https://xivapi.com/i/012000/012693.png',
+		duration: 30000,
+	},
+
+	UNDYING_FLAME: {
+		id: 2705,
+		name: 'Undying Flame',
+		icon: 'https://xivapi.com/i/012000/012694.png',
+		duration: 15000,
+	},
+
+	SLIPSTREAM: {
+		id: 2706,
+		name: 'Slipstream',
+		icon: 'https://xivapi.com/i/012000/012695.png',
+		duration: 15000,
 	},
 })
