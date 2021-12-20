ee6e56f6bb7197fe522f0b6c7bc5748a33deb4a4
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2021-12-21 08:05:35 +1100

    RPR basic core modules (#1430)
    * Add RPR AoE and imports
    
    * Add RPR Combos
    
    * Add RPR Interrupts
    
    * Add RPR CDDT

--- src/parser/jobs/rpr/changelog.tsx
+++ src/parser/jobs/rpr/changelog.tsx
@@ -3,14 +3,18 @@ import CONTRIBUTORS from 'data/CONTRIBUTORS'
 import React from 'react'
 
 export const changelog = [
-	// {
-	// 	date: new Date('2021-12-03'),
-	// 	Changes: () => <>The changes you made</>,
-	// 	contributors: [CONTRIBUTORS.YOU],
-	// },
 	{
 		date: new Date('2021-12-18'),
-		Changes: () => <>Add a checklist for <DataLink status="DEATHS_DESIGN"/> uptime.</>,
+		Changes: () => <>
+			Add a checklist for <DataLink status="DEATHS_DESIGN"/> uptime.
+		</>,
 		contributors: [CONTRIBUTORS.HINT],
+	},
+	{
+		date: new Date('2021-12-18'),
+		Changes: () => <>
+			Add basic AoE, combos, weaving, and interrupted casts tracking.
+		</>,
+		contributors: [CONTRIBUTORS.AY],
 	},
 ]
---
+++ src/parser/jobs/rpr/modules/DISPLAY_ORDER.ts
@@ -0,0 +1,3 @@
+export const DISPLAY_ORDER = {
+	INTERRUPTS: 20,
+}
