10b7fc4a89d7f66d3854f9ad928671a2be8d7bf8
Keaton <keatonminers@hotmail.com>
2021-12-22 20:56:06 -0700

    [AST] update changelog to reflect missed changes
    updated based on macrocosmos and oGCD heals

--- src/parser/jobs/ast/index.tsx
+++ src/parser/jobs/ast/index.tsx
@@ -36,15 +36,15 @@ export const ASTROLOGIAN = new Meta({
 	],
 	changelog: [
 		{
-			date: new Date('2021-12-12'),
+			date: new Date('2021-12-22'),
 			Changes: () => <>
 				<strong>More Endwalker Preparation</strong>
 				<ul>
 					<li> Fixed some underlying Actions and Statuses to show up on the timeline view properly among other minor changes.</li>
 					<li> Updated Overheals to include Delayed Heals. </li>
 					<li> Added a section specific to <DataLink action="DIVINATION" /> to show players buffed, GCDs during window, and a warning if cast with another AST.</li>
-					<li> Updated <DataLink action="DIVINATION" /> section to be oGCDs to include <DataLink action="MACROCOSMOS" /> and healing oGCDs along with the corresponding checklist items.</li>
-					<li> Reordered Action Timeline specifically separating <DataLink action="MINOR_ARCANA" /> from <DataLink action="PLAY" />. </li>
+					<li> Added oGCD heals section on the checklist to show a snapshot of possible rebalancing of healing rotations.</li>
+					<li> Reordered Action Timeline specifically separating <DataLink action="MINOR_ARCANA" /> from <DataLink action="PLAY" /> and fixing <DataLink action="MACROCOSMOS" />. </li>
 				</ul>
 			</>,
 			contributors: [CONTRIBUTORS.OTOCEPHALY],
