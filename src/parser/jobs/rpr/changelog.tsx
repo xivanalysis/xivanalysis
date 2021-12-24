a21d973092d6c320b315371db958db4d1167605d
hintxiv <65056303+hintxiv@users.noreply.github.com>
2021-12-24 18:55:25 -0500

    RPR - Tincture (#1429)
    * add tincture module
    
    * imperative changelog
    
    * adjust suggestion content
    
    * add regress to ignored actions
    
    * Update index.ts
    
    github web interface sucks
    
    * yarn extract
    
    Co-authored-by: yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>

--- src/parser/jobs/rpr/changelog.tsx
+++ src/parser/jobs/rpr/changelog.tsx
@@ -3,6 +3,16 @@ import CONTRIBUTORS from 'data/CONTRIBUTORS'
 import React from 'react'
 
 export const changelog = [
+	// {
+	// 	date: new Date('2021-12-03'),
+	// 	Changes: () => <>The changes you made</>,
+	// 	contributors: [CONTRIBUTORS.YOU],
+	// },
+	{
+		date: new Date('2021-12-18'),
+		Changes: () => <>Add a Tincture module.</>,
+		contributors: [CONTRIBUTORS.HINT],
+	},
 	{
 		date: new Date('2021-12-18'),
 		Changes: () => <>
