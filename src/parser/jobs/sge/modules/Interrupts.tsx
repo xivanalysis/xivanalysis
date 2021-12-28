3654b5d9846e269072e56dad1b249510b783973a
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2021-12-27 23:46:43 -0600

    Add/update display order

--- src/parser/jobs/sge/modules/Interrupts.tsx
+++ src/parser/jobs/sge/modules/Interrupts.tsx
@@ -2,8 +2,11 @@ import {Trans} from '@lingui/react'
 import {DataLink} from 'components/ui/DbLink'
 import {Interrupts as CoreInterrupts} from 'parser/core/modules/Interrupts'
 import React from 'react'
+import {DISPLAY_ORDER} from './DISPLAY_ORDER'
 
 export class Interrupts extends CoreInterrupts {
+	static override displayOrder = DISPLAY_ORDER.INTERRUPTS
+
 	override suggestionContent = <Trans id="sge.interrupts.suggestion.content">
 		Avoid interrupting casts by either prepositioning yourself or utilizing slidecasting where possible.
 		Use windows created by normal <DataLink action="EUKRASIAN_DOSIS_III" /> refreshes to move in advance of mechanics.
