c17fb3e83a65fbe997d091e54842ed35531a9abc
ackwell <saxon@ackwell.com.au>
2021-12-28 16:36:23 +1100

    Move event sorting to report source

---
+++ src/reportSources/legacyFflogs/eventAdapter/sortEvents.ts
@@ -0,0 +1,37 @@
+import {FflogsEvent} from 'fflogs'
+import stable from 'stable'
+
+const EVENT_TYPE_ORDER: Partial<Record<FflogsEvent['type'], number>> = {
+	death: -4,
+	begincast: -3,
+	cast: -2,
+	calculateddamage: -1.5,
+	calculatedheal: -1.5,
+	targetabilityupdate: -1,
+	damage: -0.5,
+	heal: -0.5,
+	// Keep apply/remove/refresh (de)buff events in the same order as they were originally sent by the FFLogs API
+	applybuff: 1,
+	applybuffstack: 1,
+	applydebuff: 1,
+	applydebuffstack: 1,
+	removebuff: 1,
+	removebuffstack: 1,
+	removedebuff: 1,
+	removedebuffstack: 1,
+	refreshbuff: 1,
+	refreshdebuff: 1,
+}
+const DEFAULT_ORDER = 0
+
+export function sortEvents(events: FflogsEvent[]) {
+	return stable.inplace(events, (a, b) => {
+		if (a.timestamp !== b.timestamp) {
+			return a.timestamp - b.timestamp
+		}
+
+		const aTypeOrder = EVENT_TYPE_ORDER[a.type] ?? DEFAULT_ORDER
+		const bTypeOrder = EVENT_TYPE_ORDER[b.type] ?? DEFAULT_ORDER
+		return aTypeOrder - bTypeOrder
+	})
+}
