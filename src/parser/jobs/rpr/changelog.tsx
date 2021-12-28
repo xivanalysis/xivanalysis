57bfcbabbab538d2b7eadbb3c5243f2cb69b0c0e
KelosPelmand <53376212+KelosPelmand@users.noreply.github.com>
2021-12-28 15:57:40 -0500

    Added missing gcdRecast to Soul Slice and Soul Scythe to fix 200% gcd uptime.

--- src/parser/jobs/rpr/changelog.tsx
+++ src/parser/jobs/rpr/changelog.tsx
@@ -9,6 +9,11 @@ export const changelog = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2021-12-28'),
+		Changes: () => <>Fix how Always be casting counts Soul Slice and Soul Scythe.</>,
+		contributors: [CONTRIBUTORS.KELOS],
+	},
+	{
 		date: new Date('2021-12-18'),
 		Changes: () => <>Add a Tincture module.</>,
 		contributors: [CONTRIBUTORS.HINT],
