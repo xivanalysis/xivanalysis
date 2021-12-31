937c489e847791d094128b16a96aa437ef2fba26
Fürst <rexatlanti@users.noreply.github.com>
2021-12-31 05:49:28 +0100

    the rest of the owl

--- src/parser/core/changelog.tsx
+++ src/parser/core/changelog.tsx
@@ -9,6 +9,11 @@ export const changelog: ChangelogEntry[] = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2021-12-31'),
+		Changes: () => <>Changed the lowest GCD that the weaving module allows to double-weave under from 2000 ms to 1800 ms.</>,
+		contributors: [CONTRIBUTORS.FURST],
+	},
+	{
 		date: new Date('2021-12-16'),
 		Changes: () => <>Added Arcane Circle, Radiant Finale, and Searing Light raid buffs to the timeline.</>,
 		contributors: [CONTRIBUTORS.HINT],
