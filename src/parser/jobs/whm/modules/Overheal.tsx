831903fd59006593ce851bb4081f2331ea53e10c
P_G_S <P_G_S@DESKTOP-7R3C66J>
2022-01-02 22:32:52 +0000

    Added Liturgy of the Bell to overhealing and added ON_DAMAGE and ON_EXPIRY Actions to DATA

---
+++ src/parser/jobs/whm/modules/Overheal.tsx
@@ -0,0 +1,34 @@
+import {Trans} from '@lingui/react'
+import {Overheal as CoreOverheal, SuggestedColors} from 'parser/core/modules/Overheal'
+import React from 'react'
+
+export class Overheal extends CoreOverheal {
+
+	override checklistRuleBreakout = true
+	override displayPieChart = true
+	override displaySuggestion = true
+
+	override trackedHealCategories = [
+		{
+			name: <Trans id="whm.overheal.hot.name">Healing Over Time</Trans>,
+			color: SuggestedColors[1],
+			trackedHealIds: [
+				this.data.statuses.REGEN.id,
+				this.data.actions.MEDICA_II.id,
+				this.data.actions.ASYLUM.id],
+		},
+		{
+			name: <Trans id="whm.overheal.assize.name">Assize</Trans>,
+			color: SuggestedColors[2],
+			trackedHealIds: [this.data.actions.ASSIZE.id],
+		},
+		{
+			name: <Trans id="whm.overheal.LiturgyBell.name">Liturgy of the bell</Trans>,
+			color: SuggestedColors[3],
+			trackedHealIds: [
+				this.data.actions.LITURGY_OF_THE_BELL_ON_DAMAGE.id,
+				this.data.actions.LITURGY_OF_THE_BELL_ON_EXPIRY.id,
+			],
+		},
+	]
+}
--- src/parser/jobs/whm/modules/index.js
+++ src/parser/jobs/whm/modules/index.js
@@ -3,7 +3,7 @@ import {DoTs} from './DoTs'
 import Interrupts from './Interrupts'
 import Lilies from './Lilies'
 import oGCDs from './oGCDs'
-import Overheal from './Overheal'
+import {Overheal} from './Overheal'
 import Swiftcast from './Swiftcast'
 import {Thinair} from './Thinair'
 
