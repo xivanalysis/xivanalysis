88bfa0e8475825489a3b3f2109820e2aec0e62ad
Dylan <dthomash@live.com>
2022-01-02 22:52:28 -0600

    Update localization for scholar cooldowns

--- src/parser/jobs/sch/modules/ScholarCooldownDowntime.tsx
+++ src/parser/jobs/sch/modules/ScholarCooldownDowntime.tsx
@@ -18,6 +18,6 @@ export default class ScholarCooldownDowntime extends CooldownDowntime {
 	]
 	override checklistDescription = <Fragment>
 		<div><Trans id="sch.cooldown.chainstrat.description">Try to use <ActionLink {...this.data.actions.CHAIN_STRATAGEM}/> on cooldown throughout the fight, particularly when your team has other buffs up for maximum effect.</Trans></div>
-		<div><Trans id="sch.aetherflow.cooldown.description">Try to use <ActionLink {...this.data.actions.AETHERFLOW}/> on cooldown throughout the fight. If you have extra Aetherflow charges, cast Energy Drain to do additional damage before casting <ActionLink {...this.data.actions.AETHERFLOW}/>.</Trans></div>
+		<div><Trans id="sch.cooldown.aetherflow.description">Try to use <ActionLink {...this.data.actions.AETHERFLOW}/> on cooldown throughout the fight. If you have extra Aetherflow charges, cast Energy Drain to do additional damage before casting <ActionLink {...this.data.actions.AETHERFLOW}/>.</Trans></div>
 	</Fragment>
 }
