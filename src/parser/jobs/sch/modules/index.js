69a72469d02a296535014bc2044b0290572fe60d
Dylan <dthomash@live.com>
2022-01-02 22:15:33 -0600

    Update index to import renamed ChainStrat module

--- src/parser/jobs/sch/modules/index.js
+++ src/parser/jobs/sch/modules/index.js
@@ -1,18 +1,18 @@
-import ChainStrat from './ChainStrat'
 import DoTs from './DoTs'
 import Interrupts from './Interrupts'
 import Overheal from './Overheal'
 import Pets from './Pets'
+import ScholarCooldownDowntime from './ScholarCooldownDowntime'
 import Swiftcast from './Swiftcast'
 import Tincture from './Tincture'
 import Weaving from './Weaving'
 
 export default [
-	ChainStrat,
 	DoTs,
 	Interrupts,
 	Overheal,
 	Pets,
+	ScholarCooldownDowntime,
 	Swiftcast,
 	Tincture,
 	Weaving,
