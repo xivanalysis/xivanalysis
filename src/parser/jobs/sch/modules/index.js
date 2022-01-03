7ea8d23ac791d4cb79ec3505604207474b746476
Dylan <dthomash@live.com>
2022-01-03 01:28:41 -0600

    Add curly braces to imports to match convention

--- src/parser/jobs/sch/modules/index.js
+++ src/parser/jobs/sch/modules/index.js
@@ -1,11 +1,11 @@
-import DoTs from './DoTs'
-import Interrupts from './Interrupts'
-import Overheal from './Overheal'
-import Pets from './Pets'
-import CooldownDowntime from './CooldownDowntime'
-import Swiftcast from './Swiftcast'
-import Tincture from './Tincture'
-import Weaving from './Weaving'
+import {DoTs} from './DoTs'
+import {Interrupts} from './Interrupts'
+import {Overheal} from './Overheal'
+import {Pets} from './Pets'
+import {CooldownDowntime} from './CooldownDowntime'
+import {Swiftcast} from './Swiftcast'
+import {Tincture} from './Tincture'
+import {Weaving} from './Weaving'
 
 export default [
 	DoTs,
