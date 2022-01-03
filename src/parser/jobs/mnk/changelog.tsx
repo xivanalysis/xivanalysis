9b39e1ac03da8df80153f90780725c342db70c90
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-01-03 15:11:23 +1100

    Update MNK text and timeline (#1490)
    * Update Twin Snakes buff description
    
    * Reorder MNK timeline a bit
    
    * Add changelog entry

--- src/parser/jobs/mnk/changelog.tsx
+++ src/parser/jobs/mnk/changelog.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2022-01-03'),
+		Changes: () => <>Update Twin Snakes buff description, and adjust timeline order.</>,
+		contributors: [CONTRIBUTORS.AY],
+	},
+	{
 		date: new Date('2022-01-01'),
 		Changes: () => <>Fix GL speed modifier bug.</>,
 		contributors: [CONTRIBUTORS.AY],
