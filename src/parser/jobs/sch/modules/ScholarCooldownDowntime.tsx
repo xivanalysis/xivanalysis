70551c89a0807beec71bc7b6bda14cc39f7aa8a1
Dylan <dthomash@live.com>
2022-01-02 22:16:36 -0600

    Add Aetherflow to scholar cooldown tracking

--- src/parser/jobs/sch/modules/ScholarCooldownDowntime.tsx
+++ src/parser/jobs/sch/modules/ScholarCooldownDowntime.tsx
@@ -1,15 +1,23 @@
 import {Trans} from '@lingui/react'
 import {ActionLink} from 'components/ui/DbLink'
 import {CooldownDowntime} from 'parser/core/modules/CooldownDowntime'
-import React from 'react'
+import React, { Fragment } from 'react'
 
-export default class ChainStrat extends CooldownDowntime {
+export default class ScholarCooldownDowntime extends CooldownDowntime {
 	override trackedCds = [
 		{
 			cooldowns: [this.data.actions.CHAIN_STRATAGEM],
 			allowedAverageDowntime: 7500,
 			firstUseOffset: 10000,
 		},
+		{
+			cooldowns: [this.data.actions.AETHERFLOW],
+			allowedAverageDowntime: 5000,
+			firstUseOffset: 10000,
+		},
 	]
-	override checklistDescription = <Trans id="sch.chainstrat.cooldown.description">Try to use <ActionLink {...this.data.actions.CHAIN_STRATAGEM}/> on cooldown throughout the fight, particularly when your team has other buffs up for maximum effect.</Trans>
+	override checklistDescription = <Fragment>
+		<div><Trans id="sch.cooldown.chainstrat.description">Try to use <ActionLink {...this.data.actions.CHAIN_STRATAGEM}/> on cooldown throughout the fight, particularly when your team has other buffs up for maximum effect.</Trans></div>
+		<div><Trans id="sch.aetherflow.cooldown.description">Try to use <ActionLink {...this.data.actions.AETHERFLOW}/> on cooldown throughout the fight. If you have extra Aetherflow charges, cast Energy Drain to do additional damage before casting <ActionLink {...this.data.actions.AETHERFLOW}/>.</Trans></div>
+	</Fragment>
 }
