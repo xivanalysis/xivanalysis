908cfd4a1ed1c7d8329ec7b9149eb1047e6b7210
Toastdeib <lord.xela@gmail.com>
2022-01-04 18:23:46 +0000

    Updated data and Raiju analyser for the changes introduced in patch 6.05, flagged NIN as supported

--- src/parser/jobs/nin/modules/Raiju.tsx
+++ src/parser/jobs/nin/modules/Raiju.tsx
@@ -16,8 +16,6 @@ export class Raiju extends Analyser {
 	@dependency private data!: Data
 	@dependency private suggestions!: Suggestions
 
-	private totalForks: number = 0
-	private totalFleets: number = 0
 	private currentStacks: number = 0
 	private droppedStacks: number = 0
 
@@ -33,10 +31,8 @@ export class Raiju extends Analyser {
 
 	override initialise() {
 		const playerFilter = filter<Event>().source(this.parser.actor.id)
-		this.addEventHook(playerFilter.type('action').action(this.data.actions.FORKED_RAIJU.id), () => this.totalForks++)
-		this.addEventHook(playerFilter.type('action').action(this.data.actions.FLEETING_RAIJU.id), () => this.totalFleets++)
-		this.addEventHook(playerFilter.type('statusApply').status(this.data.statuses.FORKED_RAIJU_READY.id), this.onForkedRaijuReady)
-		this.addEventHook(playerFilter.type('statusRemove').status(this.data.statuses.FORKED_RAIJU_READY.id), () => this.currentStacks = 0)
+		this.addEventHook(playerFilter.type('statusApply').status(this.data.statuses.RAIJU_READY.id), this.onForkedRaijuReady)
+		this.addEventHook(playerFilter.type('statusRemove').status(this.data.statuses.RAIJU_READY.id), () => this.currentStacks = 0)
 		this.addEventHook(playerFilter.type('action').action(oneOf(this.breakingGcds)), this.onBreakingGcd)
 		this.addEventHook('complete', this.onComplete)
 	}
@@ -51,35 +47,18 @@ export class Raiju extends Analyser {
 	}
 
 	private onComplete() {
-		const droppedFleets = (this.totalForks - this.totalFleets) - (this.actors.current.hasStatus(this.data.statuses.FLEETING_RAIJU_READY.id) ? 1 : 0)
-
 		this.suggestions.add(new TieredSuggestion({
-			icon: this.data.actions.FORKED_RAIJU.icon,
-			content: <Trans id="nin.raiju.suggestions.forked.content">
-				Avoid using any of your melee GCDs while you have stacks of <StatusLink status="FORKED_RAIJU_READY"/>, as it will cause the stacks to fall off.
+			icon: this.data.actions.FLEETING_RAIJU.icon,
+			content: <Trans id="nin.raiju.suggestions.dropped.content">
+				Avoid using any of your non-Raiju melee GCDs while you have stacks of <StatusLink status="RAIJU_READY"/>, as it will cause the stacks to fall off.
 			</Trans>,
 			tiers: {
 				1: SEVERITY.MEDIUM, // TODO - Run these numbers past a theorycrafter
 				3: SEVERITY.MAJOR,
 			},
 			value: this.droppedStacks,
-			why: <Trans id="nin.raiju.suggestions.forked.why">
+			why: <Trans id="nin.raiju.suggestions.dropped.why">
 				You dropped <Plural value={this.droppedStacks} one="# stack" other="# stacks"/> of Forked Raiju Ready.
-			</Trans>,
-		}))
-
-		this.suggestions.add(new TieredSuggestion({
-			icon: this.data.actions.FLEETING_RAIJU.icon,
-			content: <Trans id="nin.raiju.suggestions.fleeting.content">
-				Avoid using any of your melee GCDs while <StatusLink status="FLEETING_RAIJU_READY"/> is up, as it will cause the buff to fall off.
-			</Trans>,
-			tiers: {
-				1: SEVERITY.MEDIUM, // TODO - Run these numbers past a theorycrafter
-				3: SEVERITY.MAJOR,
-			},
-			value: droppedFleets,
-			why: <Trans id="nin.raiju.suggestions.fleeting.why">
-				You dropped <Plural value={droppedFleets} one="# stack" other="# stacks"/> of Fleeting Raiju Ready.
 			</Trans>,
 		}))
 	}
