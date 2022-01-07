199161d2e6f91f265ba13801b032483e0e436270
otocephaly <89435408+otocephaly@users.noreply.github.com>
2022-01-06 20:34:34 -0700

    [AST] 6.05 support (#1515)
    * [AST] Support for 6.05
    
    Updated index for 6.05 support
    Reduced need for Lucid Dreaming since AST already has decent mana management
    Fixed Draw math (again because apparently it wasn't calculating as expected)
    
    * yarn extract
    
    yarn extract
    
    * revert commit - lucid dreaming
    
    reverting commit on lucid dreaming

--- src/parser/jobs/ast/index.tsx
+++ src/parser/jobs/ast/index.tsx
@@ -29,12 +29,23 @@ export const ASTROLOGIAN = new Meta({
 	</>,
 	supportedPatches: {
 		from: '6.0',
-		to: '6.0',
+		to: '6.05',
 	},
 	contributors: [
 		{user: CONTRIBUTORS.OTOCEPHALY, role: ROLES.DEVELOPER},
 	],
 	changelog: [
+		{
+			date: new Date('2022-01-06'),
+			Changes: () => <>
+				<strong>6.05 Support</strong>
+				<ul>
+					<li> Actually fixed <DataLink action="DRAW" /> math instead of whatever the other person (it was me T.T ) did. </li>
+					<li> Updated to 6.05 support which didn't involve a whole lot for AST. </li>
+				</ul>
+			</>,
+			contributors: [CONTRIBUTORS.OTOCEPHALY],
+		},
 		{
 			date: new Date('2021-12-31'),
 			Changes: () => <>
