b6b37a6db9342e1621eefed2c6dcbf48e29373bd
Ryan Beck <rjb17f@my.fsu.edu>
2022-01-09 18:54:08 -0500

    Update Display Order

--- src/parser/jobs/sam/modules/DISPLAY_ORDER.js
+++ src/parser/jobs/sam/modules/DISPLAY_ORDER.js
@@ -1,9 +1,11 @@
 export default {
-	HIGANBANA: 1,
-	MEIKYO: 2,
-	SEN: 3,
+	BUFFS: 1,
+	HIGANBANA: 2,
+	MEIKYO: 3,
+	SEN: 4,
 	//KAITEN: 4, Preallocating the slot for a future PR
 	KENKI: 5,
 	SHOHA: 6,
+	OGI: 0,
 	TINCTURES: 7,
 }
