f19bd792f42e225afc96be5d18842971ddca00c3
Ryan Beck <rjb17f@my.fsu.edu>
2022-01-09 18:49:44 -0500

    Add Higanbana

--- src/parser/jobs/sam/modules/index.js
+++ src/parser/jobs/sam/modules/index.js
@@ -2,9 +2,9 @@ import {AoeChecker} from './AoeChecker'
 import {Buffs} from './Buffs'
 import {Combos} from './Combos'
 import {Fuka} from './Fuka'
+import {Higanbana} from './Higanbana'
 import Interrupts from './Interrupts'
 import {Kenki} from './Kenki'
-// import {Higanbana} from './Higanbana'
 import {Meikyo} from './Meikyo'
 import OGCDDowntime from './OGCDDowntime'
 import {Sen} from './Sen'
@@ -16,7 +16,7 @@ export default [
 	Combos,
 	Fuka,
 	Kenki,
-	//Higanbana, SOON
+	Higanbana,
 	Interrupts,
 	Meikyo,
 	OGCDDowntime,
