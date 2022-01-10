c006e54733a7f923e5daeec3422de5ad6935b6af
Dylan <dthomash@live.com>
2022-01-09 22:51:57 -0600

    Separate downtime and drift for aetherflow vs dissipation

--- src/parser/jobs/sch/modules/Aetherflow.tsx
+++ src/parser/jobs/sch/modules/Aetherflow.tsx
@@ -1,7 +1,6 @@
 import {t} from '@lingui/macro'
 import {Trans} from '@lingui/react'
 import {ActionLink} from 'components/ui/DbLink'
-import {Action} from 'data/ACTIONS'
 import {Event, Events} from 'event'
 import {Analyser} from 'parser/core/Analyser'
 import {filter, oneOf} from 'parser/core/filter'
@@ -11,13 +10,16 @@ import React from 'react'
 import {Grid, Table} from 'semantic-ui-react'
 
 export class Aetherflow extends Analyser {
-	static handle = 'aetherflow'
+	static override handle = 'aetherflow'
 	static override title = t('sch.aetherflow.title')`Aetherflow`
 
-	private recticationActive: boolean;
-	private aetherflowWindows: Array<{timestamp: number, downtime: number, drift: number, aetherflowActions: number[]}> = [];
+	private recticationActive: boolean = false;
+	private aetherflowWindows: AetherflowWindow[] = [];
+	private totalAetherflowConsumeActions: number = 0;
+	private prevAetherflowWindow?: AetherflowWindow;
+	private prevDissipationWindow?: AetherflowWindow;
 
-	@dependency private data!: Data;
+	@dependency private data!: Data
 
 	private readonly AETHERFLOW_GENERATE_ACTIONS: number[] = [
 		this.data.actions.AETHERFLOW.id,
@@ -40,6 +42,7 @@ export class Aetherflow extends Analyser {
 	];
 
 	private readonly AETHERFLOW_CHARGES_PER_CAST = 3;
+	private readonly FIRST_AETHERFLOW_CAST_TIME = 7500;
 
 	override initialise() {
 		const generateAetherflowFilter = filter<Event>()
@@ -66,13 +69,13 @@ export class Aetherflow extends Analyser {
 			type: 'death',
 			actor: this.parser.actor.id,
 		}, this.recitationRemoved)
-		this.addEventHook('complete', this.onComplete)
 	}
 
 	override output() {
 		return <Table>
 			<Table.Header>
 				<Table.Row>
+					<Table.HeaderCell></Table.HeaderCell>
 					<Table.HeaderCell><Trans id="sch.aetherflow.cast-time">Cast Times</Trans></Table.HeaderCell>
 					<Table.HeaderCell><Trans id="sch.aetherflow.cooldown">CD</Trans></Table.HeaderCell>
 					<Table.HeaderCell><Trans id="sch.aetherflow.drift">Drift</Trans></Table.HeaderCell>
@@ -82,46 +85,63 @@ export class Aetherflow extends Analyser {
 			</Table.Header>
 			<Table.Body>
 				{this.aetherflowWindows.map(aetherflowWindow => {
-
 					return <Table.Row key={aetherflowWindow.timestamp}>
+						<Table.Cell><ActionLink {...this.data.getAction(aetherflowWindow.aetherflowGenerateActionId)}/></Table.Cell>
 						<Table.Cell>{this.parser.formatEpochTimestamp(aetherflowWindow.timestamp)}</Table.Cell>
 						<Table.Cell>{aetherflowWindow.downtime > 0 && this.parser.formatDuration(aetherflowWindow.downtime)}</Table.Cell>
 						<Table.Cell>{aetherflowWindow.drift > 0 && this.parser.formatDuration(aetherflowWindow.drift)}</Table.Cell>
 						<Table.Cell>
 							<Grid>
-								{aetherflowWindow.aetherflowActions.map((actionId, index) => <Grid.Column key={index} width={4}>
+								{aetherflowWindow.aetherflowConsumeActions.map((actionId, index) => <Grid.Column key={index} width={5}>
 									<ActionLink {...this.data.getAction(actionId)}/>
 								</Grid.Column>)}
 							</Grid>
 						</Table.Cell>
-						<Table.Cell>{this.AETHERFLOW_CHARGES_PER_CAST - aetherflowWindow.aetherflowActions.length || '-'}</Table.Cell>
+						<Table.Cell>{this.AETHERFLOW_CHARGES_PER_CAST - aetherflowWindow.aetherflowConsumeActions.length || '-'}</Table.Cell>
 					</Table.Row>
 				})}
+				<Table.Row>
+					<Table.Cell colSpan="5" textAlign="right"><Trans id="sch.aetherflow.total-stacks-wasted">Total Stacks Wasted</Trans></Table.Cell>
+					<Table.Cell>{this.aetherflowWindows.length * this.AETHERFLOW_CHARGES_PER_CAST - this.totalAetherflowConsumeActions}</Table.Cell>
+				</Table.Row>
 			</Table.Body>
 		</Table>
 	}
 
 	private onGenerateAetherflow(event: Events['action']) {
+		// Calculate any values that depend on the previous aetherflow window
+		const prevWindow = event.action === this.data.actions.AETHERFLOW.id ? this.prevAetherflowWindow : this.prevDissipationWindow
+		const cooldown = this.data.getAction(event.action)?.cooldown ?? this.data.actions.AETHERFLOW.cooldown
 		let downtime = 0
 		let drift = 0
-		if (this.aetherflowWindows.length > 1) {
-			const prevAetherflowWindow = this.aetherflowWindows[this.aetherflowWindows.length -1]
-			downtime = event.timestamp - prevAetherflowWindow.timestamp - this.data.actions.AETHERFLOW.cooldown
-			drift = prevAetherflowWindow.drift + downtime
+		if (this.aetherflowWindows.length > 1 && prevWindow) {
+			downtime = event.timestamp - prevWindow.timestamp - cooldown
+			drift = prevWindow.drift + downtime
 		}
 
-		this.aetherflowWindows.push({
+		const newAetherflowWindow = {
+			aetherflowGenerateActionId: event.action,
 			timestamp: event.timestamp,
 			downtime: downtime,
 			drift: drift,
-			aetherflowActions: [],
-		})
+			aetherflowConsumeActions: [],
+		}
+
+		// Overwrite appropriate previous window with the new window
+		if (event.action === this.data.actions.AETHERFLOW.id) {
+			this.prevAetherflowWindow = newAetherflowWindow
+		} else {
+			this.prevDissipationWindow = newAetherflowWindow
+		}
+
+		this.aetherflowWindows.push(newAetherflowWindow)
 	}
 
 	private onConsumeAetherflow(event: Events['action']) {
 		// If recitation is inactive, or if the aetherflow action is not a recitation action add the action to the aetherflow window
 		if (!this.recticationActive || !this.RECITATION_ACTIONS.some(recitationActionId => recitationActionId === event.action)) {
-			this.aetherflowWindows[this.aetherflowWindows.length - 1].aetherflowActions.push(event.action)
+			this.aetherflowWindows[this.aetherflowWindows.length - 1].aetherflowConsumeActions.push(event.action)
+			this.totalAetherflowConsumeActions++
 		}
 	}
 
@@ -132,8 +152,12 @@ export class Aetherflow extends Analyser {
 	private recitationRemoved() {
 		this.recticationActive = false
 	}
+}
 
-	private onComplete() {
-		return 1
-	}
+interface AetherflowWindow {
+	aetherflowGenerateActionId: number
+	timestamp: number
+	downtime: number
+	drift: number
+	aetherflowConsumeActions: number[]
 }
