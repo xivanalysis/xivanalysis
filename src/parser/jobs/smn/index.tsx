be2973f612b894dacdf84cf37077c9f413753a65
KelosPelmand <53376212+KelosPelmand@users.noreply.github.com>
2022-01-09 23:15:59 -0500

    Fixed github resolve.

--- src/parser/jobs/smn/index.tsx
+++ src/parser/jobs/smn/index.tsx
@@ -30,8 +30,9 @@ export const SUMMONER = new Meta({
 		{
 			date: new Date('2022-01-09'),
 			Changes: () => <>Adjusted module display order and now start Summon windows with errors opened.</>,
+			contributors: [CONTRIBUTORS.KELOS],
 		},
-    {
+		{
 			date: new Date('2022-01-09'),
 			Changes: () => <>Corrected the cooldown of Radiant Aegis.</>,
 			contributors: [CONTRIBUTORS.KELOS],
