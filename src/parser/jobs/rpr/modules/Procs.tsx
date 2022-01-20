ae1cfe30d5a3933655a8a24fe0baa19b11e3c120
Arkevorkhat <Arkevorkhat@outlook.com>
2022-01-20 02:11:38 -0800

    Progress: rewrote overwritten proc code

--- src/parser/jobs/rpr/modules/Procs.tsx
+++ src/parser/jobs/rpr/modules/Procs.tsx
@@ -43,10 +43,58 @@ export class Procs extends CoreProcs {
 	]
 	//TODO: rewrite addJobSpecificSuggestions()
 	protected override addJobSpecificSuggestions() {
-		//TODO: Overwritten Gibbet
-		//TODO: Overwritten Gallows
-		//TODO: Overwritten Void Reaping
-		//TODO: Overwritten Cross Reaping
+		const overwrittenGibbet = this.getOverwriteCountForStatus(this.data.statuses.ENHANCED_GIBBET.id)
+		const overwrittenGallows = this.getOverwriteCountForStatus(this.data.statuses.ENHANCED_GALLOWS.id)
+		const overwrittenCrossReaping = this.getOverwriteCountForStatus(this.data.statuses.ENHANCED_CROSS_REAPING.id)
+		const overwrittenVoidReaping = this.getOverwriteCountForStatus(this.data.statuses.ENHANCED_VOID_REAPING.id)
+
+		this.suggestions.add(new TieredSuggestion({
+			icon: this.data.actions.GIBBET.icon,
+			content: <Trans id="rpr.procs.suggestions.overwritten.gibbet.content">
+				<Plural value={overwrittenGibbet} one="# Enhanced Gibbet proc was" other="# Enhanced Gibbet procs were" />  overwritten by using <DataLink action="GALLOWS" />.
+			</Trans>,
+			tiers: SEVERITIES.OVERWRITTEN,
+			value: overwrittenGibbet,
+			why: <Trans id="rpr.procs.suggestions.overwritten.gibbet.why">
+				You lost {overwrittenGibbet * LOST_PROC_POTENCY} potency due to overwritten procs.
+			</Trans>,
+		}))
+
+		this.suggestions.add(new TieredSuggestion({
+			icon: this.data.actions.GALLOWS.icon,
+			content: <Trans id="rpr.procs.suggestions.overwritten.gallows.content">
+				<Plural value={overwrittenGallows} one="# Enhanced Gallows proc was" other="# Enhanced Gallows Procs were" /> overwritten by using <DataLink action="GIBBET" />.
+			</Trans>,
+			tiers: SEVERITIES.OVERWRITTEN,
+			value: overwrittenGallows,
+			why: <Trans id="rpr.procs.suggestions.overwritten.gallows.why">
+				You lost {overwrittenGallows * LOST_PROC_POTENCY} potency due to overwritten procs.
+			</Trans>,
+		}))
+
+		this.suggestions.add(new TieredSuggestion({
+			icon: this.data.actions.CROSS_REAPING.icon,
+			content: <Trans id="rpr.procs.suggestions.overwritten.cross-reaping.content">
+				<Plural value={overwrittenCrossReaping} one="# Enhanced Cross Reaping proc was" other="# Enhanced Cross Reaping procs were" /> overwritten by using <DataLink action="VOID_REAPING" />.
+			</Trans>,
+			tiers: SEVERITIES.OVERWRITTEN,
+			value: overwrittenCrossReaping,
+			why: <Trans id="rpr.procs.suggestions.overwritten.cross-reaping.why">
+				You lost {overwrittenCrossReaping * LOST_PROC_POTENCY} potency due to overwritten procs.
+			</Trans>,
+		}))
+
+		this.suggestions.add(new TieredSuggestion({
+			icon: this.data.actions.VOID_REAPING.icon,
+			content: <Trans id="rpr.procs.suggestions.overwritten.void-reaping.content">
+				<Plural value={overwrittenVoidReaping} one="# Enhanced Void Reaping proc was" other="# Enhanced Cross Reaping procs were" /> overwritten by using <DataLink action="CROSS_REAPING" />.
+			</Trans>,
+			tiers: SEVERITIES.OVERWRITTEN,
+			value: overwrittenVoidReaping,
+			why: <Trans id="rpr.procs.suggestions.overwritten.void-reaping.why">
+				You lost {overwrittenVoidReaping * LOST_PROC_POTENCY} potency due to overwritten procs.
+			</Trans>,
+		}))
 		//TODO: Expired Gibbet
 		//TODO: Expired Gallows
 		//TODO: Expired Cross Reaping
