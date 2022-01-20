bdf18618985f9764f12316402811a2f5ab396ae6
Arkevorkhat <Arkevorkhat@outlook.com>
2022-01-19 17:58:19 -0800

    Completion: Finished implementation of Enhanced Gibbet and Enhanced Gallows proc tracking.

--- src/parser/jobs/rpr/modules/index.ts
+++ src/parser/jobs/rpr/modules/index.ts
@@ -5,6 +5,7 @@ import {Combos} from './Combos'
 import {CooldownDowntime} from './CooldownDowntime'
 import {DeathsDesign} from './DeathsDesign'
 import {ImmortalSacrifice} from './ImmortalSacrifice'
+import {Reaver} from './Reaver'
 import {Tincture} from './Tincture'
 
 export default [
@@ -15,5 +16,6 @@ export default [
 	DeathsDesign,
 	ImmortalSacrifice,
 	Tincture,
+	Reaver,
 	Weaving,
 ]
