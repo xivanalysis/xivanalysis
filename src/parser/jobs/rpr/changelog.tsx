220234fdeeef81ef113d143815db92ead836a86d
Kennedy Oliveira <kennedyroliveira@gmail.com>
2022-01-21 19:10:37 -0300

    Add changelog and messages

--- src/parser/jobs/rpr/changelog.tsx
+++ src/parser/jobs/rpr/changelog.tsx
@@ -4,6 +4,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2022-01-21'),
+		Changes: () => <>Add gauges.</>,
+		contributors: [CONTRIBUTORS.MR_RAZOR],
+	},
+	{
 		date: new Date('2021-12-31'),
 		Changes: () => <>Add checklist for <DataLink status="IMMORTAL_SACRIFICE"/> consumption.</>,
 		contributors: [CONTRIBUTORS.MR_RAZOR],
