0315ca164aea2b2bd183a9504d7143cd6fc823e7
Arkevorkhat <Arkevorkhat@outlook.com>
2022-01-22 15:24:29 -0800

    Updated soulsow logic to account for unableToAct windows

--- src/parser/jobs/rpr/modules/HarvestMoon.tsx
+++ src/parser/jobs/rpr/modules/HarvestMoon.tsx
@@ -7,9 +7,11 @@ import {dependency} from 'parser/core/Injectable'
 import Checklist, {Requirement, TARGET, TieredRule} from 'parser/core/modules/Checklist'
 import {Data} from 'parser/core/modules/Data'
 import {Invulnerability} from 'parser/core/modules/Invulnerability'
+import {UnableToAct} from 'parser/core/modules/UnableToAct'
 import React from 'react'
 
 const SOULSOW_CAST_TIME = 5000 //milliseconds
+const SOULSOW_CAST_GRACE = 1000
 const USAGE_PERCENTAGE_SEVERITY = {
 	5: TARGET.FAIL,
 	10: TARGET.WARN,
@@ -22,6 +24,7 @@ export class HarvestMoon extends Analyser {
 	@dependency private checklist!: Checklist
 	@dependency private data!: Data
 	@dependency private invulnerability!: Invulnerability
+	@dependency private unableToAct!: UnableToAct
 
 	private harvestMoonCasts = 0
 
@@ -37,9 +40,22 @@ export class HarvestMoon extends Analyser {
 		this.harvestMoonCasts++
 	}
 
+	private getValidatedInvulnWindows() {
+		const ADJUSTED_CAST = SOULSOW_CAST_TIME + SOULSOW_CAST_GRACE
+		const invulnWindows = this.invulnerability.getWindows().filter((w) => { return w.end - w.start >=  ADJUSTED_CAST })
+		//only filter windows if there was any time at all during the fight where players cannot act
+		if (this.unableToAct.getDuration() > 0) {
+			return invulnWindows.filter((w) => {
+				const firstWindow = this.unableToAct.getWindows({start: w.start, end: w.end})[0]
+				return firstWindow == null ? false : (firstWindow.start - w.start >= ADJUSTED_CAST) || (w.end - firstWindow.end >= ADJUSTED_CAST)
+			})
+		}
+		return invulnWindows
+	}
+
 	private onComplete() {
 		// expected uses is the total number of invuln windows longer than 5 seconds plus 1 for pre-pull soulsow
-		const expectedUses = this.invulnerability.getWindows().filter((w) => { return w.end - w.start >= SOULSOW_CAST_TIME }).length + 1
+		const expectedUses = this.getValidatedInvulnWindows().length + 1
 		const hmUsesPercent = (this.harvestMoonCasts / expectedUses * 100).toFixed(2)
 		this.checklist.add(new TieredRule({
 			name: <Trans id="rpr.harvest-moon.checklist.title">
