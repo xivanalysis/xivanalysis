105edf2f29741130bf23c078b8878e6dca29273d
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-01-22 14:06:17 +1100

    Fix suggestion wording and handle lost Unveiled potency

--- src/parser/jobs/rpr/modules/Procs.tsx
+++ src/parser/jobs/rpr/modules/Procs.tsx
@@ -5,7 +5,6 @@ import {Procs as CoreProcs} from 'parser/core/modules/Procs'
 import {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
 import React from 'react'
 
-// these values may need to be changed in future.
 const SEVERITIES = {
 	EXPIRED: {
 		1: SEVERITY.MEDIUM,
@@ -17,11 +16,20 @@ const SEVERITIES = {
 	},
 }
 
-//potency loss due to not using any one proc, enshrouded or reaver.
 const LOST_PROC_POTENCY = 60
 
 export class Procs extends CoreProcs {
-	static override title = t('rpr.procs.title')`Soul Reaver Usage`
+	static override title = t('rpr.procs.title')`Enhanced Procs`
+	static override debug = false
+	
+	private badStalks: number = 0
+
+	override initialise() {
+		super.initialise()
+
+		// Additionally count Blood Stalk uses so we can find lost potency to Unveiled
+		this.addEventHook(filter<Event>().source(this.parser.actor.id).type('action').action(this.data.actions.BLOOD_STALK.id), () => this.badStalks++ )
+	}
 
 	trackedProcs = [
 		{
@@ -40,52 +48,63 @@ export class Procs extends CoreProcs {
 
 		const expiredGibbet = this.getDropCountForStatus(this.data.statuses.ENHANCED_GIBBET.id)
 		const expiredGallows = this.getDropCountForStatus(this.data.statuses.ENHANCED_GALLOWS.id)
+		const badGibbetStalks = this.badStalks - expiredGibbet
+		const badGallowsStalks = this.badStalks - expiredGallows
+
+		// Sanity check, this might get weird if there's a naked Blood Stalk at the start of the fight for a borked Gluttony tho, fix later
+		if (badGibbetStalks + badGallowsStalks != this.badStalks) {
+			this.debug(`Mismatched bloodstalks to lost enhanced procs: ${this.badStalks} stalks: Gibbet ${badGibbetStalks} + Gallows ${badGallowsStalks}`)
+		}
 
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.GIBBET.icon,
 			content: <Trans id="rpr.procs.suggestions.overwritten.gibbet.content">
-				<Plural value={overwrittenGibbet} one="# Enhanced Gibbet proc was" other="# Enhanced Gibbet procs were" /> overwritten by using <DataLink action="GALLOWS" />.
+				Avoid overwriting Enhanced <DataLink showIcon={false}  action="GIBBET"/> with an unenhanced <DataLink showIcon={false}  action="GALLOWS"/>.
+				Each overwrite is the same as losing a positional, and quickly adds up.
 			</Trans>,
 			tiers: SEVERITIES.OVERWRITTEN,
 			value: overwrittenGibbet,
 			why: <Trans id="rpr.procs.suggestions.overwritten.gibbet.why">
-				You lost {overwrittenGibbet * LOST_PROC_POTENCY} potency due to overwriting procs.
+				{overwrittenGibbet * LOST_PROC_POTENCY} potency lost due to overwriting procs.
 			</Trans>,
 		}))
 
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.GALLOWS.icon,
 			content: <Trans id="rpr.procs.suggestions.overwritten.gallows.content">
-				<Plural value={overwrittenGallows} one="# Enhanced Gallows proc was" other="# Enhanced Gallows Procs were" /> overwritten by using <DataLink action="GIBBET" />.
+				Avoid overwriting Enhanced <DataLink showIcon={false} action="GALLOWS"/> with an unenhanced <DataLink showIcon={false} action="GIBBET"/>.
+				Each overwrite is the same as losing a positional, and quickly adds up.
 			</Trans>,
 			tiers: SEVERITIES.OVERWRITTEN,
 			value: overwrittenGallows,
 			why: <Trans id="rpr.procs.suggestions.overwritten.gallows.why">
-				You lost {overwrittenGallows * LOST_PROC_POTENCY} potency due to overwriting procs.
+				{overwrittenGallows * LOST_PROC_POTENCY} potency lost due to overwriting procs.
 			</Trans>,
 		}))
 
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.GIBBET.icon,
 			content: <Trans id="rpr.procs.suggestions.expired.gibbet.content">
-				<Plural value={expiredGibbet} one="# Enhanced Gibbet proc was" other="# Enhanced Gibbet procs were" /> allowed to expire.
+				Avoid letting Enhanced  <DataLink showIcon={false}  action="GIBBET"/> expire. On top of the lost potency itself,
+				you're also losing the potency from <DataLink action="UNVEILED_GALLOWS"/> if <DataLink action="GLUTTONY"/> hasn't given you extra stacks of <DataLink status="SOUL_REAVER"/>.
 			</Trans>,
 			tiers: SEVERITIES.EXPIRED,
 			value: expiredGibbet,
 			why: <Trans id="rpr.procs.suggestions.expired.gibbet.why">
-				You lost {expiredGibbet * LOST_PROC_POTENCY} potency due to letting <Plural value={expiredGibbet} one="an Enhanced Gibbet proc" other="Enhanced Gibbet procs" /> expire.
+				{(expiredGibbet + badGibbetStalks) * LOST_PROC_POTENCY} potency lost to letting <Plural value={expiredGibbet} one="an Enhanced Gibbet proc" other="Enhanced Gibbet procs" /> expire.
 			</Trans>,
 		}))
 
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.GALLOWS.icon,
 			content: <Trans id="rpr.procs.suggestions.expired.gallows.content">
-				<Plural value={expiredGallows} one="# Enhanced Gallows proc was" other="# Enhanced Gallows procs were" /> allowed to expire.
+				Avoid letting Enhanced  <DataLink showIcon={false}  action="GALLOWS"/> expire. On top of the lost potency itself,
+				you're also losing the potency from <DataLink action="UNVEILED_GIBBET"/> if <DataLink action="GLUTTONY"/> hasn't given you extra stacks of <DataLink status="SOUL_REAVER"/>.
 			</Trans>,
 			tiers: SEVERITIES.EXPIRED,
 			value: expiredGallows,
 			why: <Trans id="rpr.procs.suggestions.expired.gallows.why">
-				You lost {expiredGallows * LOST_PROC_POTENCY} potency due to letting <Plural value={expiredGallows} one="an Enhanced Gallows proc" other="Enhanced Gallows procs" /> expire.
+				{(expiredGallows + badGallowsStalks) * LOST_PROC_POTENCY} potency lost to letting <Plural value={expiredGallows} one="an Enhanced Gallows proc" other="Enhanced Gallows procs" /> expire.
 			</Trans>,
 		}))
 	}
