5e0392a05b25728fe05de4fb1c8d6d0aeba2f011
Joao Lippi <joao.p.lippi@gmail.com>
2022-01-24 16:50:02 -0300

    BRD - AABA tracker in RS

--- src/parser/jobs/brd/index.tsx
+++ src/parser/jobs/brd/index.tsx
@@ -24,6 +24,11 @@ export const BARD = new Meta({
 	],
 	changelog: [
 		{
+			date: new Date('2022-01-24'),
+			Changes: () => <>Added Apex Arrow and Blast Arrow tracker in Raging Strikes windows. Also fixed the incorrect expected usage amount of Empyreal Arrows in the cooldowns checklist.</>,
+			contributors: [CONTRIBUTORS.YUMIYA],
+		},
+		{
 			date: new Date('2022-01-14'),
 			Changes: () => <>Fixed an issue with "Always be casting" calculations.</>,
 			contributors: [CONTRIBUTORS.HINT],
