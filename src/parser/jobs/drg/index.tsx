1c80a7d7f1391ba808afe51dabd90fc25134a3a6
Evan Shimizu <ebshimizu@gmail.com>
2022-01-25 09:50:14 -0800

    DRG 6.08 support

--- src/parser/jobs/drg/index.tsx
+++ src/parser/jobs/drg/index.tsx
@@ -12,7 +12,7 @@ export const DRAGOON = new Meta({
 	</>,
 	supportedPatches: {
 		from: '6.0',
-		to: '6.05',
+		to: '6.08',
 	},
 	contributors: [
 		{user: CONTRIBUTORS.FALINDRITH, role: ROLES.MAINTAINER},
