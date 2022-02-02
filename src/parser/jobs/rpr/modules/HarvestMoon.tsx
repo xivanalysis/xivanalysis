d861eb3e3a2a45395ef4bf6b5cb3d091a2fb146d
ChristopherJTrent <ChristopherJTrent@Outlook.com>
2022-02-02 01:29:04 -0800

    Updated logic and added whitespace (thanks acc for the mentorship)

--- src/parser/jobs/rpr/modules/HarvestMoon.tsx
+++ src/parser/jobs/rpr/modules/HarvestMoon.tsx
@@ -4,18 +4,13 @@ import {Event} from 'event'
 import {Analyser} from 'parser/core/Analyser'
 import {filter} from 'parser/core/filter'
 import {dependency} from 'parser/core/Injectable'
-import Checklist, {Requirement, TARGET, TieredRule} from 'parser/core/modules/Checklist'
+import Checklist, {Requirement, Rule} from 'parser/core/modules/Checklist'
 import {Data} from 'parser/core/modules/Data'
 import {Invulnerability} from 'parser/core/modules/Invulnerability'
 import {UnableToAct} from 'parser/core/modules/UnableToAct'
 import React from 'react'
 
-const SOULSOW_CAST_TIME = 5000 //milliseconds
 const SOULSOW_CAST_GRACE = 1000
-const USAGE_PERCENTAGE_SEVERITY = {
-	0: TARGET.FAIL,
-	100: TARGET.SUCCESS,
-}
 
 export class HarvestMoon extends Analyser {
 	static override handle = 'HarvestMoon'
@@ -31,40 +26,43 @@ export class HarvestMoon extends Analyser {
 		super.initialise()
 
 		this.addEventHook(filter<Event>().source(this.parser.actor.id).type('action')
-			.action(this.data.actions.HARVEST_MOON.id), this.onHarvestMoon)
+			.action(this.data.actions.HARVEST_MOON.id), () => { this.harvestMoonCasts++ })
+
 		this.addEventHook('complete', this.onComplete)
 	}
 
-	private onHarvestMoon() {
-		this.harvestMoonCasts++
-	}
-
-	private getValidatedInvulnWindows() {
-		const ADJUSTED_CAST = SOULSOW_CAST_TIME + SOULSOW_CAST_GRACE
+	private getExpectedUses(): number {
+		const ADJUSTED_CAST = this.data.actions.SOULSOW.castTime + SOULSOW_CAST_GRACE
 		const invulnWindows = this.invulnerability.getWindows().filter((w) => { return w.end - w.start >=  ADJUSTED_CAST })
-		//only filter windows if there was any time at all during the fight where players cannot act
+
 		if (this.unableToAct.getDuration() > 0) {
 			return invulnWindows.filter((w) => {
 				const firstWindow = this.unableToAct.getWindows({start: w.start, end: w.end})[0]
-				return firstWindow == null ? false : (firstWindow.start - w.start >= ADJUSTED_CAST) || (w.end - firstWindow.end >= ADJUSTED_CAST)
-			})
+
+				return firstWindow == null ? false : w.start - firstWindow.start >= ADJUSTED_CAST || firstWindow.end - w.end >= ADJUSTED_CAST
+			}).length + 1
 		}
-		return invulnWindows
+
+		return invulnWindows.length + 1
+	}
+
+	private getUsedPercentage(expectedUses: number): string {
+		return (this.harvestMoonCasts / expectedUses * 100).toFixed(2)
 	}
 
 	private onComplete() {
-		// expected uses is the total number of invuln windows longer than 5 seconds plus 1 for pre-pull soulsow
-		const expectedUses = this.getValidatedInvulnWindows().length + 1
-		const hmUsesPercent = (this.harvestMoonCasts / expectedUses * 100).toFixed(2)
-		this.checklist.add(new TieredRule({
-			name: <Trans id="rpr.harvest-moon.checklist.title">
+		const expectedUses = this.getExpectedUses()
+		const hmUsesPercent = this.getUsedPercentage(expectedUses)
+
+		this.checklist.add(new Rule({
+			name: <Trans id="rpr.harvestmoon.checklist.title">
 				Use <DataLink action="SOULSOW" /> during downtime.
 			</Trans>,
-			description: <Trans id="rpr.harvest-moon.checklist.description">
+			description: <Trans id="rpr.harvestmoon.checklist.description">
 				Harvest Moon is one of your highest potency abilities.
 				Aim to get at least one use each time the boss is targetable.
 			</Trans>,
-			tiers: USAGE_PERCENTAGE_SEVERITY,
+
 			requirements: [
 				new Requirement({
 					name: <Trans id="rpr.harvestmoon.checklist.requirement.name">
@@ -74,6 +72,9 @@ export class HarvestMoon extends Analyser {
 					percent: hmUsesPercent,
 				}),
 			],
+
+			target: 100,
+
 		}))
 	}
 }
