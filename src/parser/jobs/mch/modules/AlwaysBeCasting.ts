72bc123758dea9eb48290b1f16918836c3e5e1b9
Dean <eatmorehufu@users.noreply.github.com>
2022-02-02 20:35:25 -0500

    Fix flamethrower duration tracking.
    Early cancellation counts as a GCD because FT is a GCD at a min anyway
    
    Duration is tracked from cast => statusRemove OR another cast.

--- src/parser/jobs/mch/modules/AlwaysBeCasting.ts
+++ src/parser/jobs/mch/modules/AlwaysBeCasting.ts
@@ -1,6 +1,7 @@
 import {Action} from 'data/ACTIONS'
 import {Event, Events} from 'event'
-import {filter} from 'parser/core/filter'
+import {EventHook} from 'parser/core/Dispatcher'
+import {filter, noneOf} from 'parser/core/filter'
 import {AlwaysBeCasting as CoreAlwaysBeCasting} from 'parser/core/modules/AlwaysBeCasting'
 
 interface FlameWindow {
@@ -11,38 +12,57 @@ interface FlameWindow {
 export class AlwaysBeCasting extends CoreAlwaysBeCasting {
 	private flameHistory: FlameWindow[] = []
 	private currentFlame: FlameWindow | undefined = undefined
+	private flamethrowerInterruptingActionHook?: EventHook<Events['action']>
 
 	override initialise() {
 		super.initialise()
 
-		const flamethrowerFilter = filter<Event>()
-			.source(this.parser.actor.id)
+		const playerFilter = filter<Event>().source(this.parser.actor.id)
+		const flamethrowerCastFilter = playerFilter
+			.action(this.data.actions.FLAMETHROWER.id)
+			.type('action')
+		const flamethrowerStatusFilter = playerFilter
 			.status(this.data.statuses.FLAMETHROWER.id)
+			.type('statusRemove')
 
-		this.addEventHook(flamethrowerFilter.type('statusApply'), this.onApplyFlamethrower)
-		this.addEventHook(flamethrowerFilter.type('statusRemove'), this.onRemoveFlamethrower)
+		this.addEventHook(flamethrowerCastFilter, this.onApplyFlamethrower)
+		this.addEventHook(flamethrowerStatusFilter, this.onRemoveFlamethrower)
 	}
 
-	private onApplyFlamethrower(event: Events['statusApply']) {
+	private onApplyFlamethrower(event: Events['action']) {
 		if (this.currentFlame != null) { return }
 
 		this.currentFlame = {
 			start: event.timestamp,
-			end: this.parser.pull.timestamp + this.parser.pull.duration,
+			end: event.timestamp + this.data.statuses.FLAMETHROWER.duration,
 		}
+		const anyActionFilter = filter<Event>()
+			.source(this.parser.actor.id)
+			.action(noneOf([this.data.actions.FLAMETHROWER.id]))
+			.type('action')
+		this.flamethrowerInterruptingActionHook = this.addEventHook(anyActionFilter, this.onRemoveFlamethrower)
 
 		this.flameHistory.push(this.currentFlame)
 	}
 
-	private onRemoveFlamethrower(event: Events['statusRemove']) {
-		if (this.currentFlame != null) {
-			this.currentFlame.end = event.timestamp
-			this.currentFlame = undefined
+	private onRemoveFlamethrower(event: Events['statusRemove'] | Events['action']) {
+		if (this.currentFlame == null) {
+			return
 		}
+		if (this.flamethrowerInterruptingActionHook == null) {
+			return
+		}
+
+		this.currentFlame.end = event.timestamp
+		this.removeEventHook(this.flamethrowerInterruptingActionHook)
+
+		this.currentFlame = undefined
+		this.flamethrowerInterruptingActionHook = undefined
+
 	}
 
 	override considerCast(action: Action, castStart: number): boolean {
-		if (action === this.data.actions.FLAMETHROWER)  {
+		if (action === this.data.actions.FLAMETHROWER) {
 			this.debug(`Flamethrower began channeling at ${this.parser.formatEpochTimestamp(castStart)}`)
 			return false
 		}
@@ -57,7 +77,8 @@ export class AlwaysBeCasting extends CoreAlwaysBeCasting {
 				flame.start,
 				flame.end,
 			)
-			return acc + flame.end - flame.start - downtime
+			const flamethrowerDurationOrGCD = Math.max(flame.end - flame.start, this.globalCooldown.getDuration())
+			return acc + flamethrowerDurationOrGCD - downtime
 		}, 0)
 		const uptime = (this.gcdUptime + flameDuration) / (fightDuration) * 100
 
