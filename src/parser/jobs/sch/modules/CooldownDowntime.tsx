8a57a081e13a6aea328e9f026a3a1afaa24040a3
Dylan <dthomash@live.com>
2022-02-02 19:32:27 -0600

    Update checklist description, remove dissipation

--- src/parser/jobs/sch/modules/CooldownDowntime.tsx
+++ src/parser/jobs/sch/modules/CooldownDowntime.tsx
@@ -16,14 +16,13 @@ export class CooldownDowntime extends CooldownDowntimeCore {
 	override defensiveCooldowns = [
 		{cooldowns: [this.data.actions.SCH_WHISPERING_DAWN]},
 		{cooldowns: [this.data.actions.SCH_FEY_ILLUMINATION]},
-		{cooldowns: [this.data.actions.DISSIPATION]},
 		{cooldowns: [this.data.actions.RECITATION]},
 		{cooldowns: [this.data.actions.SUMMON_SERAPH]},
 		{cooldowns: [this.data.actions.PROTRACTION]},
 		{cooldowns: [this.data.actions.EXPEDIENT]},
 	]
 	override defenseChecklistDescription = <Trans id="sch.cooldownDowntime.defense-cd-metric">
-		Using your mitigation and healing cooldowns allows you to help keep the party healthy while continuing to deal damage and healing to your target.
+		Using your mitigation and healing cooldowns allows you to help keep the party healthy while continuing to deal damage.
 		While you shouldn't waste these actions, you should try to plan out when to use them to maximize their utility.
 	</Trans>
 }
