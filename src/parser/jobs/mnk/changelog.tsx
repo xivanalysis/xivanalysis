69d0053b74118078e80aa6f4191675096be6b139
Malpp <cpt.ortan@gmail.com>
2022-02-13 22:11:18 -0500

    changelog

--- src/parser/jobs/mnk/changelog.tsx
+++ src/parser/jobs/mnk/changelog.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2022-02-13'),
+		Changes: () => <>Add Brotherhood module.</>,
+		contributors: [CONTRIBUTORS.MALP],
+	},
+	{
 		date: new Date('2022-02-10'),
 		Changes: () => <>Cleanup Riddle of Fire module.</>,
 		contributors: [CONTRIBUTORS.MALP],
