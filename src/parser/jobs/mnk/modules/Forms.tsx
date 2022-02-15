a028393790110581cf8c5e030412d612f33db67a
Malpp <Malpp@users.noreply.github.com>
2022-02-14 20:22:17 -0500

    Fix Opo Opo form reset bug when Dragon Kick is spammed (#1632)

--- src/parser/jobs/mnk/modules/Forms.tsx
+++ src/parser/jobs/mnk/modules/Forms.tsx
@@ -111,6 +111,9 @@ export class Forms extends Analyser {
 	private onGain(event: Events['statusApply']): void {
 		this.lastFormChanged = event.timestamp
 
+		// Reset forms - we need this to avoid DK spam rotations leaving trailing hooks
+		this.resetFormHook()
+
 		this.formHook = this.addEventHook(
 			filter<Event>()
 				.source(this.parser.actor.id)
@@ -122,6 +125,10 @@ export class Forms extends Analyser {
 	private onRemove(event: Events['statusRemove']): void {
 		this.lastFormDropped = event.timestamp
 
+		this.resetFormHook()
+	}
+
+	private resetFormHook() {
 		if (this.formHook != null) {
 			this.removeEventHook(this.formHook)
 			this.formHook = undefined
