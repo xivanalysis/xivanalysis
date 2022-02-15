8710474c0890e5ed16939848c51c74e973118c7c
Malpp <cpt.ortan@gmail.com>
2022-02-14 19:58:15 -0500

    changelog

--- src/parser/jobs/mnk/changelog.tsx
+++ src/parser/jobs/mnk/changelog.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2022-02-14'),
+		Changes: () => <>Fix opo opo form combo resets.</>,
+		contributors: [CONTRIBUTORS.MALP],
+	},
+	{
 		date: new Date('2022-02-10'),
 		Changes: () => <>Cleanup Riddle of Fire module.</>,
 		contributors: [CONTRIBUTORS.MALP],
