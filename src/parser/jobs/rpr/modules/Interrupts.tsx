a0ea64cbef9edc567881a45c52eead7eaea5715c
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-02-17 22:45:27 +1100

    Update Interrupts text

--- src/parser/jobs/rpr/modules/Interrupts.tsx
+++ src/parser/jobs/rpr/modules/Interrupts.tsx
@@ -8,8 +8,7 @@ export class Interrupts extends CoreInterrupts {
 	static override displayOrder = DISPLAY_ORDER.INTERRUPTS
 
 	override suggestionContent = <Trans id="rpr.interrupts.suggestion.content">
-		Avoid interrupting casts by either prepositioning yourself, utilising slidecasting where possible.
-		If you need to move, consider using <ActionLink action="HELLS_INGRESS"/> or <ActionLink action="HELLS_EGRESS"/>
-		and getting a free <ActionLink action="HARPE"/> out of it.
+		Avoid interrupting casts by either pre-positioning yourself or slidecasting where possible.
+		If you need to move, consider using <ActionLink action="HELLS_INGRESS"/> or <ActionLink action="HELLS_EGRESS"/> and getting a free <ActionLink action="HARPE"/> out of it.
 	</Trans>
 }
