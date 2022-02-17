55ca0a05fd1e996ff0e79c5e30c32b4c699c3b95
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-02-17 22:44:41 +1100

    Finalise-ish display order

--- src/parser/jobs/rpr/modules/DISPLAY_ORDER.ts
+++ src/parser/jobs/rpr/modules/DISPLAY_ORDER.ts
@@ -1,6 +1,11 @@
 export const DISPLAY_ORDER = {
-	IMMORTAL_SACRIFICE: 10,
-	ARCANE_CIRCLE: 15,
-	INTERRUPTS: 20,
-	ENSHROUD: 90,
+	IMMORTAL_SACRIFICE: 1,
+	ARCANE_CIRCLE: 2,
+	ENSHROUD: 3,
+
+	// Super low priority, some of these override default ordering
+	WEAVING: 50,
+	POSITIONALS: 51,
+	INTERRUPTS: 52,
+	TINCTURES: 53,
 }
