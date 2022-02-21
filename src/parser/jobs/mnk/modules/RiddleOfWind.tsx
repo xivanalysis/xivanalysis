089e5f82084b3720a2061c1a486a0fbd021fe69c
Malpp <cpt.ortan@gmail.com>
2022-02-20 21:33:30 -0500

    Add proper info and title

--- src/parser/jobs/mnk/modules/RiddleOfWind.tsx
+++ src/parser/jobs/mnk/modules/RiddleOfWind.tsx
@@ -1,4 +1,7 @@
 import {t} from '@lingui/macro'
+import {Trans} from '@lingui/react'
+import {ActionLink} from 'components/ui/DbLink'
+import {ActionRoot} from 'data/ACTIONS'
 import {dependency} from 'parser/core/Injectable'
 import {BuffWindow, EvaluatedAction, WindowEvaluator} from 'parser/core/modules/ActionWindow'
 import {HistoryEntry} from 'parser/core/modules/ActionWindow/History'
@@ -7,7 +10,14 @@ import {SimpleStatistic, Statistics} from 'parser/core/modules/Statistics'
 import React from 'react'
 import {DISPLAY_ORDER} from './DISPLAY_ORDER'
 
-const MAX_ATTACKS_PER_ROW_WINDOW = 16
+/**
+ * While it is possible to get 16 attacks in a Riddle of Wind
+ * window, it is such a niche and time based thing that I'm
+ * not sure if it's worth it. I've looked into a lot of top
+ * parsing monk logs, and it almost feels like it's RNG if
+ * someone has a 16 RoW attack window.
+ */
+const MAX_ATTACKS_PER_ROW_WINDOW = 15
 
 class CallbackEvaluator implements WindowEvaluator {
 	private readonly callback: (windows: Array<HistoryEntry<EvaluatedAction[]>>) => void;
@@ -27,7 +37,6 @@ class CallbackEvaluator implements WindowEvaluator {
 }
 
 export class RiddleOfWind extends BuffWindow {
-	static override debug = true
 	static override handle = 'riddleofwind'
 	static override title = t('mnk.row.title')`Riddle of Wind`
 	static override displayOrder = DISPLAY_ORDER.RIDDLE_OF_WIND
@@ -35,6 +44,7 @@ export class RiddleOfWind extends BuffWindow {
 	@dependency globalCooldown!: GlobalCooldown
 	@dependency statistics!: Statistics
 
+	private riddleOfWind: ActionRoot['RIDDLE_OF_WIND'] = this.data.actions.RIDDLE_OF_WIND
 	buffStatus = this.data.statuses.RIDDLE_OF_WIND
 
 	override initialise() {
@@ -52,12 +62,21 @@ export class RiddleOfWind extends BuffWindow {
 		const maxAttacks = windows.length * MAX_ATTACKS_PER_ROW_WINDOW
 		const actualAttacks = windows.map(history => history.data.length).reduce((previousValue, currentValue) => previousValue + currentValue, 0)
 
-		this.debug(windows)
-
 		this.statistics.add(new SimpleStatistic({
-			title: 'Hello',
+			title: <Trans id="mnk.row.statistic.title">
+				Auto attacks hits
+			</Trans>,
 			icon: this.data.actions.RIDDLE_OF_WIND.icon,
 			value: `${actualAttacks}/${maxAttacks}`,
+			info: <Trans id="mnk.row.statistic.info">
+				Missing auto attacks during <ActionLink id={this.riddleOfWind.id}/> means you were not
+				able get full uptime on the boss. This is a DPS loss due to missing auto attacks. <br/>
+				<br/>
+				❗❗❗Cursed monk stuff ahead❗❗❗<br/>
+				It is also possible to get 16 auto attacks in the <ActionLink id={this.riddleOfWind.id}/> duration,
+				but this requires very precise timing. More information can be found in The Balance discord under the
+				#mnk_resources channel.
+			</Trans>,
 		}))
 	}
 
