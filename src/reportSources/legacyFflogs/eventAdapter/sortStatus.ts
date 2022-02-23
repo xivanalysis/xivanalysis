b8cbe1b7c0b56ee0d95622ae684ea1ff49e283a8
yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>
2022-02-23 01:14:27 -0500

    rewrite to accommodate end-of-status sorting too

--- src/reportSources/legacyFflogs/eventAdapter/sortStatus.ts
+++ src/reportSources/legacyFflogs/eventAdapter/sortStatus.ts
@@ -1,14 +1,18 @@
 import {getDataArrayBy} from 'data'
 import {getActions} from 'data/ACTIONS'
 import {StatusKey, getStatuses} from 'data/STATUSES'
-import {Event, Events} from 'event'
+import {Event} from 'event'
 import _ from 'lodash'
 import {AdapterStep} from './base'
 
-type EventSequenceKey = `${number}-${string}`
+interface EventNode {
+	event: Event
+	isRoot: boolean
+	before: Event[]
+	after: Event[]
+}
 
 export class SortStatusAdapterStep extends AdapterStep {
-
 	override postprocess(adaptedEvents: Event[]): Event[] {
 		const sortedEvents: Event[] = []
 		let currentEvents: Event[] = []
@@ -25,67 +29,53 @@ export class SortStatusAdapterStep extends AdapterStep {
 		return sortedEvents
 	}
 
-	private getSequenceKey = (action: number, source: string) =>
-		<EventSequenceKey> `${action}-${source}`
-
-	private getPossibleStatusKeys(event: Events['statusApply']): EventSequenceKey[] | undefined {
-		const statusKey = _.findKey(getStatuses(this.report), status => status.id === event.status) as StatusKey | undefined
+	private didActionApplyStatus = _.memoize((action: number, status: number) => {
+		const statusKey = _.findKey(getStatuses(this.report), other => status === other.id) as StatusKey | undefined
 
 		// This shouldn't be possible, but bail if we don't find a matching status
-		if (!statusKey) { return }
+		if (statusKey == null) { return false }
 
 		const applyingActions = getDataArrayBy(getActions(this.report), 'statusesApplied', statusKey)
 
 		// No actions are known to apply this status, so no need to sequence this event
-		if (applyingActions.length === 0) { return }
+		if (applyingActions.length === 0) { return false }
 
-		return applyingActions.map(action => this.getSequenceKey(action.id, event.source))
-	}
+		return applyingActions.some(other => action === other.id)
+	})
 
 	private sortTimestampBucket(events: Event[]): Event[] {
-		const sortedEvents: Event[] = []
-		const eventSequences: Map<EventSequenceKey, Event[]> = new Map()
-		const rootEvents: Event[] = []
+		const nodes: EventNode[] = events.map(event => ({
+			event: event,
+			isRoot: true,
+			before: [],
+			after: [],
+		}))
 
-		// Build sequences of action -> status events
-		for (const event of events.slice().reverse()) {
-			if (event.type === 'action') {
-				eventSequences.set(this.getSequenceKey(event.action, event.source), [event])
-				rootEvents.push(event)
+		for (const [index, node] of nodes.entries()) {
+			const event = node.event
 
-			} else if (event.type === 'statusApply') {
-				const possibleKeys = this.getPossibleStatusKeys(event)
-				const applyingActionKey = possibleKeys?.find(key => eventSequences.has(key))
-
-				if (applyingActionKey != null) {
-					const sequence = eventSequences.get(applyingActionKey)
-					sequence?.push(event)
-
-				} else {
-					// An applying action does not exist or is already in the correct order,
-					// so no need to sort this event
-					rootEvents.push(event)
+			if (event.type === 'statusApply') {
+				// Sorts the event *after* its corresponding action event
+				for (const other of nodes.slice(index + 1)) {
+					if (other.event.type === 'action' && this.didActionApplyStatus(other.event.action, event.status)) {
+						other.after.push(event)
+						node.isRoot = false
+					}
 				}
-
-			} else {
-				rootEvents.push(event)
+			} else if (event.type === 'statusRemove') {
+				// Sorts the event *before* the target's first prepare event
+				for (const other of nodes.slice(0, index)) {
+					if (other.event.type === 'damage' && other.event.source === event.target) {
+						other.before.push(event)
+						node.isRoot = false
+					}
+				}
 			}
 		}
 
-		for (const event of rootEvents.reverse()) {
-			if (event.type === 'action') {
-				const key = this.getSequenceKey(event.action, event.source)
-				const sequence = eventSequences.get(key)
-
-				if (sequence != null) {
-					sortedEvents.push(...sequence)
-				}
-
-			} else {
-				sortedEvents.push(event)
-			}
-		}
-
-		return sortedEvents
+		return nodes.reduce((events: Event[], node) => node.isRoot ?
+			events.concat(...node.before, node.event, ...node.after) :
+			events
+		, [])
 	}
 }
