5900d90c576afe8d4ebe9b6fdf8dfc73ebc6a78e
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-03-14 16:33:45 +1100

    Fix rushed window handling and clarify suggestion text

--- src/parser/jobs/rpr/modules/ArcaneCircle.tsx
+++ src/parser/jobs/rpr/modules/ArcaneCircle.tsx
@@ -8,6 +8,9 @@ import React from 'react'
 import {OPENER_BUFFER} from '../constants'
 import {DISPLAY_ORDER} from './DISPLAY_ORDER'
 
+// The minimum AC length to hard require a Communio
+const COMMUNIO_BUFFER = 8000
+
 const SEVERITIES = {
 	1: SEVERITY.MINOR,
 	3: SEVERITY.MEDIUM,
@@ -26,13 +29,19 @@ export class ArcaneCircle extends RaidBuffWindow {
 
 		this.addEvaluator(new ExpectedActionsEvaluator({
 			expectedActions: [
-				{action: this.data.actions.COMMUNIO, expectedPerWindow: 2},
-				{action: this.data.actions.PLENTIFUL_HARVEST, expectedPerWindow: 1},
+				{
+					action: this.data.actions.COMMUNIO,
+					expectedPerWindow: 2,
+				},
+				{
+					action: this.data.actions.PLENTIFUL_HARVEST,
+					expectedPerWindow: 1,
+				},
 			],
 			suggestionIcon: this.data.actions.ARCANE_CIRCLE.icon,
 			suggestionContent: <Trans id="rpr.arcanecircle.suggestions.content">
-				Each <ActionLink action="ARCANE_CIRCLE"/> window should contain 2 uses of <ActionLink action="COMMUNIO"/> and a use
-				of <ActionLink action="PLENTIFUL_HARVEST"/>.
+				Each <ActionLink action="ARCANE_CIRCLE"/> window should contain 2 uses of <ActionLink action="COMMUNIO"/>
+				and 1 use of <ActionLink action="PLENTIFUL_HARVEST"/>. In your opener, only 1 <ActionLink showIcon={false} action="COMMUNIO"/> is expected.
 			</Trans>,
 			suggestionWindowName: <ActionLink action="ARCANE_CIRCLE" showIcon={false} />,
 			severityTiers: SEVERITIES,
@@ -41,9 +50,33 @@ export class ArcaneCircle extends RaidBuffWindow {
 	}
 
 	private adjustCount(window: HistoryEntry<EvaluatedAction[]>, action: TrackedAction): number {
+		const fightTimeRemaining = (this.parser.pull.timestamp + this.parser.pull.duration) - window.start
+
+		if (this.isRushedEndOfPullWindow(window)) {
+			if (fightTimeRemaining < COMMUNIO_BUFFER) {
+				switch (action.action) {
+				case this.data.actions.COMMUNIO:
+					// I hate this linter bypass, but -2 is technically magic and is weird as a const
+					// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+					return -2
+
+				case this.data.actions.PLENTIFUL_HARVEST:
+					return -1
+
+				default:
+					return 0
+				}
+			}
+
+			// We have enough time for 1 Communio, but not both
+			if (action.action === this.data.actions.COMMUNIO) {
+				return -1
+			}
+		}
+
+		// If it's not Communio, we don't care at this point
 		if (action.action !== this.data.actions.COMMUNIO) { return 0 }
 
 		return (window.start - OPENER_BUFFER <= this.parser.pull.timestamp) ? -1 : 0
 	}
-
 }
