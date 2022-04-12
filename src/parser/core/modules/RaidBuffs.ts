638af26bb91922e5c5c035a1afdc3b432789cdf9
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-04-12 14:24:31 +1000

    Update RaidBuffs for proper Mug status

--- src/parser/core/modules/RaidBuffs.ts
+++ src/parser/core/modules/RaidBuffs.ts
@@ -63,10 +63,7 @@ export class RaidBuffs extends Analyser {
 				{key: 'TRICK_ATTACK_VULNERABILITY_UP', name: 'Trick Attack'}
 			)
 		} else {
-			this.settings.set(
-				this.data.statuses.MUG_VULNERABILITY_UP.id,
-				{key: 'MUG_VULNERABILITY_UP', name: 'Mug'}
-			)
+			this.settings.set(this.data.statuses.MUG.id, {key: 'MUG'})
 		}
 
 		// Event hooks
