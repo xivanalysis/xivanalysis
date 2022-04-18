d0bd37c903c42604e67f9236a014913918e7a141
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-04-18 11:58:42 +1000

    Mark patch 6.1 support for WAR

--- src/parser/jobs/war/changelog.tsx
+++ src/parser/jobs/war/changelog.tsx
@@ -3,6 +3,13 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2022-04-12'),
+		Changes: () => <>
+			Add pre-6.1 Inner Release and Beast gauge handling for the reduced set of stack consumers.
+		</>,
+		contributors: [CONTRIBUTORS.AY],
+	},
+	{
 		date: new Date('2022-01-01'),
 		Changes: () => <>
 			Fix bugged Inner Release rushed window handling in suggestions.
