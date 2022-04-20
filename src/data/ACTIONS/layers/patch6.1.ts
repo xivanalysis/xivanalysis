0048a4a90014c15e7a375fa6ceda5f7bc9457c02
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-04-20 12:00:31 +1000

    Update patch 6.1 layer for NIN raid buff changes

--- src/data/ACTIONS/layers/patch6.1.ts
+++ src/data/ACTIONS/layers/patch6.1.ts
@@ -4,6 +4,10 @@ import {ActionRoot} from '../root'
 export const patch610: Layer<ActionRoot> = {
 	patch: '6.1',
 	data: {
+		// NIN 6.1 raid buff changes
+		TRICK_ATTACK: {statusesApplied: []},
+		MUG: {statusesApplied: ['MUG_VULNERABILITY_UP']},
+
 		// Tank 6.1 cooldown changes
 		DEFIANCE: {cooldown: 3000},
 		GRIT: {cooldown: 3000},
