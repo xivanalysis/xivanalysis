0d4903194ca375758713670f5af21fdd190a4a0a
ChristopherJTrent <ChristopherJTrent@outlook.com>
2022-04-28 01:24:24 -0700

    Continued updates

--- src/parser/jobs/war/modules/SurgingTempest.tsx
+++ src/parser/jobs/war/modules/SurgingTempest.tsx
@@ -8,14 +8,15 @@ import Checklist, {Rule, Requirement} from 'parser/core/modules/Checklist'
 import {Gauge as CoreGauge, TimerGauge} from 'parser/core/modules/Gauge'
 import {Invulnerability} from 'parser/core/modules/Invulnerability'
 import {Statuses} from 'parser/core/modules/Statuses'
-import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
+import Suggestions, {SEVERITY, Suggestion,  TieredSuggestion} from 'parser/core/modules/Suggestions'
 import React from 'react'
 
 type GaugeModifier = Partial<Record<Event['type'], number>>
 
 const SURGE_MAX = 60000
 
-const EYE_BUFFER = 7500
+const EYE_BUFFER = 15000
+const SURGE_END_BELOW = 35000
 const PATH_LOST_GAUGE = 10
 
 const SUGGESTION_TIERS = {
@@ -65,23 +66,20 @@ export class SurgingTempest extends CoreGauge {
 		const modifier = this.surgeModifiers.get(event.action)
 		let isExtender = false
 
-		/* eslint-disable no-fallthrough */
-		switch (event.action) {
-		case this.data.actions.STORMS_EYE.id:
+		if (event.action === this.data.actions.STORMS_EYE.id) {
 			if (this.surgingTempest.remaining > EYE_BUFFER) {
 				this.earlyEyes++
 			}
-
-		case this.data.actions.INNER_RELEASE.id:
-			isExtender = true
-
-		default:
-			if (modifier != null) {
-				const amount = modifier[event.type] ?? 0
-				this.surgingTempest.extend(amount, isExtender)
-			}
 		}
-		/* eslint-enable no-fallthrough */
+
+		if (event.action === this.data.actions.INNER_RELEASE.id) {
+			isExtender = true
+		}
+
+		if (modifier != null) {
+			const amount = modifier[event.type] ?? 0
+			this.surgingTempest.extend(amount, isExtender)
+		}
 	}
 
 	onComplete(): void {
@@ -96,6 +94,19 @@ export class SurgingTempest extends CoreGauge {
 				}),
 			],
 		}))
+
+		if (this.surgingTempest.remaining > SURGE_END_BELOW) {
+			this.suggestions.add(new Suggestion({
+				icon: this.data.actions.STORMS_PATH.icon,
+				content: <Trans id="war.surgingtempest.suggestions.fightend.content">
+					Avoid having more than 35 seconds of Surging Tempest at the end of the fight, as doing so will lose you a use of Storm's Path.
+				</Trans>,
+				why: <Trans id="war.surgingtempest.suggestions.fightend.why">
+					You may have lost a use of Fell Cleave due to refreshing your Surging Tempest too early.
+				</Trans>,
+				severity: SEVERITY.MINOR,
+			}))
+		}
 
 		this.suggestions.add(new TieredSuggestion({
 			content: <Trans id="war.surgingtempest.suggestions.early.content">
