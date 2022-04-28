c7aa42c84f0facbc585ce4f85226b31d1dc2fd97
Azariah Aeroborn <26804378+AzariahAeroborn@users.noreply.github.com>
2022-04-28 02:24:00 -0400

    Fix incorrect DRK MP calculations (#1690)
    * Fix MP calculations for Blood Weapon and Delirium
    
    * Migrate Blood Weapon back to statusApply from action in Gauge
    
    Co-authored-by: Ay'yaruq Dotharl <acchan.ff14@gmail.com>

--- src/parser/jobs/drk/modules/BloodGauge.tsx
+++ src/parser/jobs/drk/modules/BloodGauge.tsx
@@ -60,7 +60,7 @@ export class BloodGauge extends CoreGauge {
 		this.addEventHook(playerFilter.type('combo').action(oneOf([...this.onComboModifiers.keys()])), this.onModifier(this.onComboModifiers))
 
 		// We hook the action for BW so we can just ignore stacks entirely
-		this.addEventHook(playerFilter.type('action').action(this.data.actions.BLOOD_WEAPON.id), this.onApplyBloodWeapon)
+		this.addEventHook(playerFilter.type('statusApply').status(this.data.statuses.BLOOD_WEAPON.id), this.onApplyBloodWeapon)
 		this.addEventHook(playerFilter.type('statusRemove').status(this.data.statuses.BLOOD_WEAPON.id), this.onRemoveBloodWeapon)
 
 		this.addEventHook('complete', this.onComplete)
@@ -75,12 +75,16 @@ export class BloodGauge extends CoreGauge {
 	}
 
 	private onApplyBloodWeapon() {
-		this.activeGcdHook = this.addEventHook(
-			filter<Event>()
-				.source(this.parser.actor.id)
-				.type('damage'),
-			this.onHitUnderBloodWeapon
-		)
+		if (this.activeGcdHook == null) {
+			// Buffs with stacks generate separate apply events for each stack with a single remove at the end.
+			// Make sure we only start hooking for actions effected by Blood Weapon on the first apply event -- no duplicate hooks on the "reapply" events as the stacks go down
+			this.activeGcdHook = this.addEventHook(
+				filter<Event>()
+					.source(this.parser.actor.id)
+					.type('damage'),
+				this.onHitUnderBloodWeapon
+			)
+		}
 	}
 
 	private onRemoveBloodWeapon() {
