6b9fdb3f821aec71b1b5144daf0a7e0545ff1f0c
Khayle <MKhayle@users.noreply.github.com>
2022-06-22 07:19:46 +0200

    Fix magic numbers in nin/Positionals.ts

--- src/parser/jobs/nin/modules/Positionals.ts
+++ src/parser/jobs/nin/modules/Positionals.ts
@@ -1,15 +1,24 @@
 import {PotencyModifier, Positionals as CorePositionals} from 'parser/core/modules/Positionals'
 
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+const ARMOR_CRUSH_BASE_POTENCY = 140
+const ARMOR_CRUSH_COMBO_POTENCY_6_0 = 340
+const ARMOR_CRUSH_COMBO_POTENCY_6_1 = 360
+const AEOLIAN_EDGE_BASE_POTENCY = 140
+const AEOLIAN_EDGE_COMBO_POTENCY_6_0 = 360
+const AEOLIAN_EDGE_COMBO_POTENCY_6_1 = 380
+/* eslint-enable @typescript-eslint/no-magic-numbers */
+
 export class Positionals extends CorePositionals {
 	positionals = [{
 		action: this.data.actions.ARMOR_CRUSH,
 		potencies: [
 			{
-				value: 140,
+				value: ARMOR_CRUSH_BASE_POTENCY,
 				modifiers: [],
 			},
 			{
-				value: this.parser.patch.before('6.1') ? 340 : 360,
+				value: this.parser.patch.before('6.1') ? ARMOR_CRUSH_COMBO_POTENCY_6_0 : ARMOR_CRUSH_COMBO_POTENCY_6_1,
 				modifiers: [PotencyModifier.COMBO],
 			},
 		],
@@ -21,11 +30,11 @@ export class Positionals extends CorePositionals {
 		action: this.data.actions.AEOLIAN_EDGE,
 		potencies: [
 			{
-				value: 140,
+				value: AEOLIAN_EDGE_BASE_POTENCY,
 				modifiers: [],
 			},
 			{
-				value: this.parser.patch.before('6.1') ? 360 : 380,
+				value: this.parser.patch.before('6.1') ? AEOLIAN_EDGE_COMBO_POTENCY_6_0 : AEOLIAN_EDGE_COMBO_POTENCY_6_1,
 				modifiers: [PotencyModifier.COMBO],
 			},
 		],
