ebcf6c929c2535e8f018e690bca60c0dbf35042f
otocephaly <89435408+otocephaly@users.noreply.github.com>
2022-08-31 04:04:48 -0600

    [AST] 6.2 bump (#1725)

--- src/parser/jobs/ast/index.tsx
+++ src/parser/jobs/ast/index.tsx
@@ -29,7 +29,7 @@ export const ASTROLOGIAN = new Meta({
 	</>,
 	supportedPatches: {
 		from: '6.0',
-		to: '6.1',
+		to: '6.2',
 	},
 	contributors: [
 		{user: CONTRIBUTORS.SUSHIROU, role: ROLES.MAINTAINER},
