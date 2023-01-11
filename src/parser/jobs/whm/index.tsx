7f974c94ef81adeccc4730b49deb83f7510ecf69
Inni <70386111+Dot-Inni@users.noreply.github.com>
2023-01-11 12:13:27 +0100

    6.3 support for WHM, no relevant analysis changes. (#1761)

--- src/parser/jobs/whm/index.tsx
+++ src/parser/jobs/whm/index.tsx
@@ -21,7 +21,7 @@ export const WHITE_MAGE = new Meta({
 	</>,
 	supportedPatches: {
 		from: '6.0',
-		to: '6.2',
+		to: '6.3',
 	},
 	contributors: [
 		// {user: CONTRIBUTORS.YOU, role: ROLES.DEVELOPER},
