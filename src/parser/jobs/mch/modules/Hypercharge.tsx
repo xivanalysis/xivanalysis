a46e90d0414f478ab511c2f40adb2e7a72c40187
yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>
2023-01-14 10:43:00 -0500

    layer hypercharge duration by patch

--- src/parser/jobs/mch/modules/Hypercharge.tsx
+++ src/parser/jobs/mch/modules/Hypercharge.tsx
@@ -14,7 +14,8 @@ import {Timeline} from 'parser/core/modules/Timeline'
 import React, {Fragment} from 'react'
 import {Message, Accordion, Button} from 'semantic-ui-react'
 
-const HYPERCHARGE_DURATION_MS = 10000
+const HYPERCHARGE_DURATION_PATCH_600 = 8000
+const HYPERCHARGE_DURATION_PATCH_630 = 10000
 const HYPERCHARGE_GCD_TARGET = 5
 const HYPERCHARGE_GCD_WARNING = 4
 const HYPERCHARGE_GCD_ERROR = 0
@@ -34,6 +35,9 @@ export class Hypercharge extends Analyser {
 	private history: History<Array<Events['action']>> = new History(() => [])
 	private rotationHook: EventHook<Events['action']> | undefined
 	private hyperchargeGcds = HYPERCHARGE_GCDS.map(key => this.data.actions[key].id)
+	private hyperchargeDuration = this.parser.patch.before('6.3')
+		? HYPERCHARGE_DURATION_PATCH_600
+		: HYPERCHARGE_DURATION_PATCH_630
 
 	override initialise() {
 		const hyperchargeFilter = filter<Event>()
@@ -74,7 +78,7 @@ export class Hypercharge extends Analyser {
 
 		if (currentWindow == null) { return }
 
-		if (event.timestamp > currentWindow.start + HYPERCHARGE_DURATION_MS) {
+		if (event.timestamp > currentWindow.start + this.hyperchargeDuration) {
 			this.endCurrentWindow(event)
 			return
 		}
