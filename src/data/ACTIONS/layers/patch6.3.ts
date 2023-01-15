0fcc63e19f175882c272a4874a383e7e0b5c1719
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2023-01-15 13:03:19 -0600

    6.3 reduced Phlegma's cooldown

--- src/data/ACTIONS/layers/patch6.3.ts
+++ src/data/ACTIONS/layers/patch6.3.ts
@@ -10,5 +10,10 @@ export const patch630: Layer<ActionRoot> = {
 		LIFE_SURGE: {cooldown: 40000},
 		// WHM - Assize CD change
 		ASSIZE: {cooldown: 40000},
+
+		/// SGE - Phlegma's cooldown (all tiers) was reduced to 40s
+		PHLEGMA: {cooldown: 40000},
+		PHLEGMA_II: {cooldown: 40000},
+		PHLEGMA_III: {cooldown: 40000},
 	},
 }
