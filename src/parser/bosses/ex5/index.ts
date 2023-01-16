3e20f01d33665b73f3ba7e1340ce36eee0f31117
Saxon Landers <saxon@ackwell.com.au>
2023-01-16 21:05:04 +1100

    Fix ex5 but less broken this time (#1774)

--- src/parser/bosses/ex5/index.ts
+++
@@ -1,5 +0,0 @@
-import {Meta} from 'parser/core/Meta'
-
-export const EX5 = new Meta({
-	modules: () => import('./modules' /* webpackChunkName: "bosses-ex5" */),
-})
--- src/parser/bosses/ex5/modules/index.ts
+++
@@ -1,3 +0,0 @@
-import {RetargetPull} from './RetargetPull'
-
-export default [RetargetPull]
