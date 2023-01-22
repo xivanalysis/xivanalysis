5ff859dd31c3a30e398cad3f59402527ad76a8ec
Saxon Landers <saxon@ackwell.com.au>
2023-01-22 23:44:52 +1100

    undefined -> UNKNOWN
    much like with actions, the root defines the source of truth for the keys, and as such, setting them back to undefined will risk invalid types permitting null access. this is actually a flaw in the types, it shouldn't let you do this - sorry!

--- src/data/STATUSES/layers/patch6.3.ts
+++ src/data/STATUSES/layers/patch6.3.ts
@@ -1,11 +1,12 @@
 import {Layer} from 'data/layer'
 import {StatusRoot} from '../root'
+import {SHARED} from '../root/SHARED'
 
 export const patch630: Layer<StatusRoot> = {
 	patch: '6.3',
 	data: {
 		// PLD 6.3 rework changes
-		GORING_BLADE: undefined,
+		GORING_BLADE: SHARED.UNKNOWN,
 		REQUIESCAT: {stacksApplied: 4},
 		FIGHT_OR_FLIGHT: {duration: 20000},
 		DIVINE_MIGHT: {
@@ -21,15 +22,15 @@ export const patch630: Layer<StatusRoot> = {
 			icon: 'https://xivapi.com/i/012000/012509.png',
 			duration: 30000,
 		},
-		DIVINE_VEIL_PROC: undefined,
+		DIVINE_VEIL_PROC: SHARED.UNKNOWN,
 		BULWARK: {
 			id: 77,
 			name: 'Bulwark',
 			icon: 'https://xivapi.com/i/010000/010156.png',
 			duration: 10000,
 		},
-		BLADE_OF_VALOR: undefined,
-		BLADE_OF_FAITH_READY: undefined,
+		BLADE_OF_VALOR: SHARED.UNKNOWN,
+		BLADE_OF_FAITH_READY: SHARED.UNKNOWN,
 		CONFITEOR_READY: {
 			id: 3019,
 			name: 'Confiteor Ready',
