3e5fe8eb5794e1bad18c9c14c863b5c6fb4a0406
Saxon Landers <saxon@ackwell.com.au>
2023-01-23 11:45:52 +1100

    Add types and ignore cases for new fflogs events (#1780)
    [skip ci]

--- src/reportSources/legacyFflogs/__tests__/eventAdapter.ts
+++ src/reportSources/legacyFflogs/__tests__/eventAdapter.ts
@@ -4,7 +4,7 @@ import {Actor, Pull, Report, Team} from 'report'
 import {adaptEvents} from '../eventAdapter'
 import {AdapterStep} from '../eventAdapter/base'
 import {ReassignUnknownActorStep} from '../eventAdapter/reassignUnknownActor'
-import {CastEvent, CombatantInfoAura, DamageEvent, FflogsEvent, HitType, ReportLanguage} from '../eventTypes'
+import {ActorType, CastEvent, CombatantInfoAura, DamageEvent, FflogsEvent, HitType, ReportLanguage} from '../eventTypes'
 
 // "Mock" the reassign unknown actor step with its real implementation. We use this mock handling later
 // to disable the step on a test-by-test basis.
@@ -804,6 +804,74 @@ const fakeEvents: Record<FflogsEvent['type'], FflogsEvent[]> = {
 			facing: -158,
 			absorb: 52,
 		},
+	}],
+	headmarker: [{
+		timestamp: 1794311,
+		type: 'headmarker',
+		sourceID: 106,
+		sourceIsFriendly: true,
+		target: {
+			name: 'Environment',
+			id: -1,
+			guid: 0,
+			type: ActorType.NPC,
+			icon: 'NPC',
+		},
+		targetIsFriendly: false,
+		ability: {
+			name: 'Unknown Ability',
+			guid: 0,
+			type: 0,
+			abilityIcon: '000000-000405.png',
+		},
+		markerID: 328,
+	}, {
+		timestamp: 1456535,
+		type: 'headmarker',
+		sourceID: 107,
+		sourceIsFriendly: true,
+		target: {
+			name: 'Environment',
+			id: -1,
+			guid: 0,
+			type: ActorType.NPC,
+			icon: 'NPC',
+		},
+		targetIsFriendly: false,
+		ability: {
+			name: 'Unknown Ability',
+			guid: 0,
+			type: 0,
+			abilityIcon: '000000-000405.png',
+		},
+		markerID: 86,
+		markerType: 'dice8',
+		markerDuration: 9000,
+	}],
+	tether: [{
+		timestamp: 1822478,
+		type: 'tether',
+		sourceID: 109,
+		sourceIsFriendly: true,
+		targetID: 104,
+		targetIsFriendly: true,
+		ability: {
+			name: 'Unknown Ability',
+			guid: 0,
+			type: 0,
+			abilityIcon: '000000-000405.png',
+		},
+		tetherID: 202,
+	}],
+	gaugeupdate: [{
+		...fakeBaseFields,
+		timestamp: 1800638,
+		type: 'gaugeupdate',
+		gaugeID: '10331D9A',
+		data1: '1C',
+		data2: '50',
+		data3: 'E0000000',
+		data4: '00',
 	}],
 }
 
