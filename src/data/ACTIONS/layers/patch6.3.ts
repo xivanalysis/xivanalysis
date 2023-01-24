553e8a93e6e7c308fa064166f5dd397fe0594935
Greg Rogers <g_n_rogers1991@yahoo.com>
2023-01-24 03:34:29 -0600

    Feature/pld update 6.3 (#1773)

--- src/data/ACTIONS/layers/patch6.3.ts
+++ src/data/ACTIONS/layers/patch6.3.ts
@@ -11,6 +11,48 @@ export const patch630: Layer<ActionRoot> = {
 		// WHM - Assize CD change
 		ASSIZE: {cooldown: 40000},
 
+		// PLD 6.3 rework changes
+		GORING_BLADE: {
+			cooldown: 60000,
+			gcdRecast: 2500,
+			combo: undefined,
+			statusesApplied: undefined,
+		},
+		ROYAL_AUTHORITY: {
+			statusesApplied: ['SWORD_OATH', 'DIVINE_MIGHT'],
+		},
+		CONFITEOR: {
+			combo: undefined,
+		},
+		BLADE_OF_FAITH: {
+			combo: undefined,
+		},
+		BLADE_OF_TRUTH: {
+			combo: undefined,
+		},
+		BLADE_OF_VALOR: {
+			combo: undefined,
+		},
+		IRON_WILL: {
+			cooldown: 2000,
+		},
+		RELEASE_IRON_WILL: {
+			id: 32065,
+			name: 'Release Iron Will',
+			icon: 'https://xivapi.com/i/002000/002521.png',
+			onGcd: false,
+			cooldown: 1000,
+		},
+		BULWARK: {
+			id: 22,
+			name: 'Bulwark',
+			icon: 'https://xivapi.com/i/000000/000167.png',
+			onGcd: false,
+			cooldown: 90000,
+			statusesApplied: ['BULWARK'],
+		},
+		DIVINE_VEIL: {statusesApplied: ['DIVINE_VEIL']},
+
 		/// SGE - Phlegma's cooldown (all tiers) was reduced to 40s
 		PHLEGMA: {cooldown: 40000},
 		PHLEGMA_II: {cooldown: 40000},
--- src/parser/jobs/pld/modules/StatusTimeline.ts
+++
@@ -1,8 +0,0 @@
-import STATUSES from 'data/STATUSES'
-import {StatusTimeline as CoreStatusTimeline} from 'parser/core/modules/StatusTimeline'
-
-export class StatusTimeline extends CoreStatusTimeline {
-	static override statusesStackMapping = {
-		[STATUSES.DIVINE_VEIL_PROC.id]: STATUSES.DIVINE_VEIL.id,
-	}
-}
