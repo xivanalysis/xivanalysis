0e429a07ba3483fd4dcc14ebbf66f4ff411edb5a
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2023-02-04 19:12:20 -0600

    remove handle/title overrides for consistency

--- src/parser/jobs/brd/modules/Mitigation.tsx
+++ src/parser/jobs/brd/modules/Mitigation.tsx
@@ -1,10 +1,6 @@
-import {t} from '@lingui/macro'
 import {Defensives} from 'parser/core/modules/Defensives'
 
 export class Mitigation extends Defensives {
-	static override handle = 'mitigation'
-	static override title = t('brd.mitigation.title')`Mitigation`
-
 	protected override trackedDefensives = [
 		this.data.actions.NATURES_MINNE,
 		this.data.actions.TROUBADOUR,
