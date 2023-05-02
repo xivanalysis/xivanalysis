80f1d7293096ddad3c5de690695c186137cb27d8
hugmeir <fraserbn@gmail.com>
2023-05-02 16:49:00 +0200

    BLU: minor linting fix

--- src/parser/jobs/blu/modules/index.tsx
+++ src/parser/jobs/blu/modules/index.tsx
@@ -1,3 +1,4 @@
+import {Swiftcast} from 'parser/core/modules/Swiftcast'
 import {Tincture} from 'parser/core/modules/Tincture'
 import {AlwaysBeCasting} from './AlwaysBeCasting'
 import {ColdFog} from './ColdFog'
@@ -6,7 +7,6 @@ import {GeneralCDDowntime} from './GeneralCDDowntime'
 import {Interrupts} from './Interrupts'
 import {MoonFlute} from './MoonFlute'
 import {RevengeBlast} from './RevengeBlast'
-import {Swiftcast} from 'parser/core/modules/Swiftcast'
 import Weaving from './Weaving'
 
 export default [
