a3a6cac65c6b674cf6cdf59ef9ca984e7f061514
hugmeir <fraserbn@gmail.com>
2023-05-02 16:14:13 +0200

    BLU: Add the Interrupted Casts report

---
+++ src/parser/jobs/blu/modules/DISPLAY_ORDER.ts
@@ -0,0 +1,3 @@
+export const DISPLAY_ORDER = {
+	INTERRUPTED_CASTS: 1,
+}
--- src/parser/jobs/blu/modules/index.tsx
+++ src/parser/jobs/blu/modules/index.tsx
@@ -2,6 +2,7 @@ import {Tincture} from 'parser/core/modules/Tincture'
 import {AlwaysBeCasting} from './AlwaysBeCasting'
 import {DoTs} from './DoTs'
 import {GeneralCDDowntime} from './GeneralCDDowntime'
+import {Interrupts} from './Interrupts'
 import Weaving from './Weaving'
 
 export default [
@@ -9,5 +10,6 @@ export default [
 	AlwaysBeCasting,
 	DoTs,
 	Tincture,
+	Interrupts,
 	GeneralCDDowntime,
 ]
