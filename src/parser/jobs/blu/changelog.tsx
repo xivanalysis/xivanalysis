9ba5d711e52f6b15d96a8e649d5b047f8a74033b
hugmeir <fraserbn@gmail.com>
2023-05-08 21:45:44 +0200

    BLU: Note in the changelog that BLU had some changes

--- src/parser/jobs/blu/changelog.tsx
+++ src/parser/jobs/blu/changelog.tsx
@@ -1,7 +1,10 @@
+import React from 'react'
+import CONTRIBUTORS from 'data/CONTRIBUTORS'
+
 export const changelog = [
-	// {
-	// 	date: new Date('2021-11-19'),
-	// 	Changes: () => <>The changes you made</>,
-	// 	contrubutors: [CONTRIBUTORS.YOU],
-	// },
+	{
+		date: new Date('2023-05-08'),
+		Changes: () => <>Support for BLU for 6.0 - 6.4</>,
+		contrubutors: [CONTRIBUTORS.HUGMEIR],
+	},
 ]
