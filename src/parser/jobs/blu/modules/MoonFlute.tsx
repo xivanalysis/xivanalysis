9abeea808af341a22d87893a4ce7d5e96f943bc4
hugmeir <fraserbn@gmail.com>
2023-05-09 01:40:12 +0200

    BLU: tsc linter issues in the MoonFlute module

--- src/parser/jobs/blu/modules/MoonFlute.tsx
+++ src/parser/jobs/blu/modules/MoonFlute.tsx
@@ -1,8 +1,10 @@
 import {t} from '@lingui/macro'
 import {Trans} from '@lingui/react'
 import {ActionLink} from 'components/ui/DbLink'
+import {Action} from 'data/ACTIONS'
 import {dependency} from 'parser/core/Injectable'
-import {BuffWindow, ExpectedActionsEvaluator, ExpectedGcdCountEvaluator} from 'parser/core/modules/ActionWindow'
+import {BuffWindow, EvaluatedAction, TrackedAction, ExpectedActionsEvaluator, ExpectedGcdCountEvaluator} from 'parser/core/modules/ActionWindow'
+import {HistoryEntry} from 'parser/core/modules/ActionWindow/History'
 import {GlobalCooldown} from 'parser/core/modules/GlobalCooldown'
 import {SEVERITY} from 'parser/core/modules/Suggestions'
 import React from 'react'
@@ -75,11 +77,10 @@ export class MoonFlute extends BuffWindow {
 			severityTiers: SEVERITIES.TOO_FEW_GCDS,
 		}))
 
-		this.addEvaluator(new MoonFluteExpectedActionsEvaluator({
+		const mfActionEvaluator = new MoonFluteExpectedActionsEvaluator({
 			expectedActions: [
 				{
 					action: this.data.actions.J_KICK,
-					altAction: this.data.actions.QUASAR,
 					expectedPerWindow: 1,
 				},
 				{
@@ -96,7 +97,6 @@ export class MoonFlute extends BuffWindow {
 				},
 				{
 					action: this.data.actions.SHOCK_STRIKE,
-					altAction: this.data.actions.BLU_MOUNTAIN_BUSTER,
 					expectedPerWindow: 1,
 				},
 				{
@@ -113,7 +113,6 @@ export class MoonFlute extends BuffWindow {
 				},
 				{
 					action: this.data.actions.FEATHER_RAIN,
-					altAction: this.data.actions.ERUPTION,
 					expectedPerWindow: 1,
 				},
 				{
@@ -134,19 +133,31 @@ export class MoonFlute extends BuffWindow {
 			</Trans>,
 			suggestionWindowName,
 			severityTiers: SEVERITIES.MISSING_EXPECTED_USES,
-		}))
+		})
+		mfActionEvaluator.setAltAction(this.data.actions.FEATHER_RAIN, this.data.actions.ERUPTION)
+		mfActionEvaluator.setAltAction(this.data.actions.SHOCK_STRIKE, this.data.actions.BLU_MOUNTAIN_BUSTER)
+		mfActionEvaluator.setAltAction(this.data.actions.J_KICK, this.data.actions.QUASAR)
+		this.addEvaluator(mfActionEvaluator)
 	}
 
 }
 
 class MoonFluteExpectedActionsEvaluator extends ExpectedActionsEvaluator {
+	private altActions = new Map<Action['id'], Action>()
+	private foundAltActions = new Map<Action['id'], Action>()
+
+	public setAltAction(action: Action, altAction: Action) {
+		this.altActions.set(action.id, altAction)
+	}
+
 	// Just a small subclass that handles our alternative actions.
 	override countUsed(window: HistoryEntry<EvaluatedAction[]>, action: TrackedAction) {
-		if (action.altAction !== undefined) {
-			const altActionID = action.altAction.id
+		const altAction = this.altActions.get(action.action.id)
+		if (altAction !== undefined) {
+			const altActionID = altAction.id
 			const foundAlt = window.data.filter(cast => cast.action.id === altActionID).length
 			if (foundAlt > 0) {
-				action.foundAltAction = true
+				this.foundAltActions.set(action.action.id, altAction)
 				return foundAlt
 			}
 		}
@@ -155,12 +166,13 @@ class MoonFluteExpectedActionsEvaluator extends ExpectedActionsEvaluator {
 	}
 
 	override actionHeader(action: TrackedAction) {
-		if (action.foundAltAction === undefined || action.foundAltAction === false) {
+		const foundAlt = this.foundAltActions.get(action.action.id)
+		if (foundAlt === undefined) {
 			return super.actionHeader(action)
 		}
 
 		// We have alternative actions used
-		return <ActionLink showName={false} {...action.altAction}/>
+		return <ActionLink showName={false} {...foundAlt}/>
 	}
 }
 
