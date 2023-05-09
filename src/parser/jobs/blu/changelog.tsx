90511eca09e17bcebf4ed9e6453d50882ee33c08
hugmeir <fraserbn@gmail.com>
2023-05-09 02:54:32 +0200

    BLU changelog: Fix a typo

--- src/parser/jobs/blu/changelog.tsx
+++ src/parser/jobs/blu/changelog.tsx
@@ -5,6 +5,6 @@ export const changelog = [
 	{
 		date: new Date('2023-05-08'),
 		Changes: () => <>Support for BLU for 6.0 - 6.4</>,
-		contrubutors: [CONTRIBUTORS.HUGMEIR],
+		contributors: [CONTRIBUTORS.HUGMEIR],
 	},
 ]
