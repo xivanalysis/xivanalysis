c013a4ca0e347f0426abfef9c9deac13110cfe3c
mali <maliziacat@gmail.com>
2023-06-05 15:57:23 -0700

    Proper import of Timeline module

--- src/parser/jobs/brd/modules/RefulgentProcs.tsx
+++ src/parser/jobs/brd/modules/RefulgentProcs.tsx
@@ -4,9 +4,14 @@ import {ActionLink, StatusLink} from 'components/ui/DbLink'
 import {Procs} from 'parser/core/modules/Procs'
 import React, {Fragment} from 'react'
 import {Table, Button} from 'semantic-ui-react'
+import {dependency} from 'parser/core/Injectable'
+import {Timeline} from 'parser/core/modules/Timeline'
 
 export class RefulgentProcs extends Procs {
+	@dependency private timeline!: Timeline
+
 	static override title = t('brd.procs.title')`Straight Shot Ready Overwrites`
+
 	override trackedProcs = [
 		{
 			procStatus: this.data.statuses.STRAIGHT_SHOT_READY,
