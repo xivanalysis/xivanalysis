043cf991290f741558d021bf85be85066af8504e
Christopher Czyba <czyba@users.noreply.github.com>
2023-06-07 15:50:04 +0200

    feat(PLD): Atonement no longer breaks combo as of 6.4 (#1830)

---
+++ src/data/ACTIONS/layers/patch6.4.ts
@@ -0,0 +1,11 @@
+import {Layer} from 'data/layer'
+import {ActionRoot} from '../root'
+
+export const patch640: Layer<ActionRoot> = {
+	patch: '6.4',
+	data: {
+		ATONEMENT: {
+			breaksCombo: false,
+		},
+	},
+}
