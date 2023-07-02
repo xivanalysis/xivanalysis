5f60ecbc0ef7b6949d98eb5b8c00daa7bcf0653f
yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>
2023-07-02 11:36:25 -0400

    mark support for 6.4

--- src/parser/jobs/brd/index.tsx
+++ src/parser/jobs/brd/index.tsx
@@ -21,13 +21,18 @@ export const BARD = new Meta({
 	</>,
 	supportedPatches: {
 		from: '6.0',
-		to: '6.3',
+		to: '6.4',
 	},
 	contributors: [
 		{user: CONTRIBUTORS.HINT, role: ROLES.MAINTAINER},
 		{user: CONTRIBUTORS.YUMIYA, role: ROLES.DEVELOPER},
 	],
 	changelog: [
+		{
+			date: new Date('2023-01-14'),
+			Changes: () => <>Marked as supported for 6.4.</>,
+			contributors: [CONTRIBUTORS.HINT],
+		},
 		{
 			date: new Date('2023-01-14'),
 			Changes: () => <>Updated for patch 6.3.</>,
