f7bc07413c3b307c59fd35bbe4ee3db502246d43
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2023-07-17 22:54:54 -0500

    Make action/status patch layer files cause BLU's definitely gonna need 'em

---
+++ src/data/ACTIONS/layers/patch6.45.ts
@@ -0,0 +1,9 @@
+import {Layer} from 'data/layer'
+import {ActionRoot} from '../root'
+
+export const patch645: Layer<ActionRoot> = {
+	patch: '6.45',
+	data: {
+
+	},
+}
---
+++ src/data/STATUSES/layers/patch6.45.ts
@@ -0,0 +1,9 @@
+import {Layer} from 'data/layer'
+import {StatusRoot} from '../root'
+
+export const patch645: Layer<StatusRoot> = {
+	patch: '6.45',
+	data: {
+
+	},
+}
