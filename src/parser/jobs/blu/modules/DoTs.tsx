8e4e9b6e701a700e820d6eb7fadbb98cf6dbc6ac
B Fraser <fraserbn@gmail.com>
2023-07-28 17:25:41 +0200

    BLU 6.45: New DoTs! (#1871)
    * BLU DoTs: Handle Breath of Magic and Mortal Flame
    
    Besides the usual uptime/clipping suggestions, this also adds
    MAJOR severity suggestions if these are ever cast without Bristle
    and Moon Flute.
    
    Moon Fluting every minute just to reapply Breath of Magic is worth
    it, even if you then spend the rest of the Moon Flute window staring
    at your thumbs.  It's that good.
    
    * BLU: Changelog for the 6.45 DoTs
    
    * BLU DoTs: Replace the TieredRule for DoT uptime  with just a plain Rule
    
    * BLU DoTs: Single suggestion for unbuffed BoM/Mortal Flame
    
    * BLU DoTs: Move the "unbuffed DoT" suggestion computation to a helper sub
    
    * BLU: yarn extract for the new DoTs

--- src/parser/jobs/blu/modules/DoTs.tsx
+++ src/parser/jobs/blu/modules/DoTs.tsx
@@ -5,7 +5,7 @@ import {Event} from 'event'
 import {filter} from 'parser/core/filter'
 import {dependency} from 'parser/core/Injectable'
 import {Actors} from 'parser/core/modules/Actors'
-import Checklist, {Requirement, TARGET, TieredRule} from 'parser/core/modules/Checklist'
+import Checklist, {Requirement, TARGET, Rule} from 'parser/core/modules/Checklist'
 import {DoTs as CoreDoTs} from 'parser/core/modules/DoTs'
 import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
 import React from 'react'
@@ -33,9 +33,13 @@ export class DoTs extends CoreDoTs {
 
 	private bristleId?: Status['id']
 	private unbuffedSongsOfTorment = 0
+	private unbuffedBoM = 0
+	private unbuffedMortal = 0
 
 	protected override trackedStatuses = [
 		this.data.statuses.BLEEDING.id,
+		this.data.statuses.BREATH_OF_MAGIC.id,
+		this.data.statuses.MORTAL_FLAME.id,
 	]
 
 	override initialise() {
@@ -43,9 +47,11 @@ export class DoTs extends CoreDoTs {
 
 		this.bristleId = this.data.statuses.BRISTLE.id
 
-		const playerFilter = filter<Event>().source(this.parser.actor.id)
+		const playerActionFilter = filter<Event>().source(this.parser.actor.id).type('action')
 
-		this.addEventHook(playerFilter.type('action').action(this.data.actions.SONG_OF_TORMENT.id), this.onApplyingSoT)
+		this.addEventHook(playerActionFilter.action(this.data.actions.SONG_OF_TORMENT.id), this.onApplyingSoT)
+		this.addEventHook(playerActionFilter.action(this.data.actions.BREATH_OF_MAGIC.id), this.onApplyingBoM)
+		this.addEventHook(playerActionFilter.action(this.data.actions.MORTAL_FLAME.id), this.onApplyingMortalFlame)
 		this.addEventHook(filter<Event>().type('complete'), this.onCompleteExtra)
 
 	}
@@ -59,7 +65,50 @@ export class DoTs extends CoreDoTs {
 		this.unbuffedSongsOfTorment++
 	}
 
+	private onApplyingBoM() {
+		if (!this.myactors.current.hasStatus(this.data.statuses.BRISTLE.id)) {
+			this.unbuffedBoM++
+			return
+		}
+		if (!this.myactors.current.hasStatus(this.data.statuses.WAXING_NOCTURNE.id)) {
+			this.unbuffedBoM++
+		}
+	}
+
+	private onApplyingMortalFlame() {
+		if (!this.myactors.current.hasStatus(this.data.statuses.BRISTLE.id)) {
+			this.unbuffedMortal++
+			return
+		}
+		if (!this.myactors.current.hasStatus(this.data.statuses.WAXING_NOCTURNE.id)) {
+			this.unbuffedMortal++
+		}
+	}
+	private unbuffedBigDoTWhy(unbuffedBoM: number, unbuffedMortal: number): JSX.Element {
+		if (unbuffedBoM && unbuffedMortal) {
+			// Both BoM and Mortal Flame were unbuffed
+			return <Trans id ="blu.big-dots.suggestion.unbuffed.why">
+				<Plural value={this.unbuffedBoM} one="# Breath of Magic" other="# Breaths of Magic"/> and
+				<Plural value={this.unbuffedMortal} one=" # Mortal Flame" other=" # Mortal Flames"/> were cast without both buffs.
+			</Trans>
+		}
+
+		if (unbuffedBoM) {
+			// unbuffed Breath of Magic
+			return <Trans id ="blu.breath_of_magic.suggestion.unbuffed.why">
+				<Plural value={this.unbuffedBoM} one="# Breath of Magic was" other="# Breaths of Magic were"/> cast without both buffs.
+			</Trans>
+		}
+
+		// unbuffed Mortal Flame
+		return <Trans id ="blu.mortal_flame.suggestion.unbuffed.why">
+			<Plural value={this.unbuffedMortal} one="# Mortal Flame was" other="# Mortal Flames were"/> cast without both buffs.
+		</Trans>
+	}
+
 	private onCompleteExtra() {
+		// TODO: For the Breath of Magic applier, one of their Songs of Torment will be reapplied
+		// under Moon Flute, so they SHOULD be Bristling it.
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.SONG_OF_TORMENT.icon,
 			content: <Trans id = "blu.song_of_torment.suggestion.unbuffed.content">
@@ -72,36 +121,78 @@ export class DoTs extends CoreDoTs {
 			</Trans>,
 			value: this.unbuffedSongsOfTorment,
 		}))
+
+		this.suggestions.add(new TieredSuggestion({
+			icon: this.unbuffedMortal ? this.data.actions.MORTAL_FLAME.icon : this.data.actions.BREATH_OF_MAGIC.icon,
+			content: <Trans id = "blu.big-dot.suggestion.unbuffed.content">
+				<DataLink action="BREATH_OF_MAGIC"/> and <DataLink action="MORTAL_FLAME"/> must always be buffed with <DataLink action="BRISTLE"/> and <DataLink action="MOON_FLUTE"/> for a colossal damage gain.
+			</Trans>,
+			tiers: {1: SEVERITY.MAJOR},
+			why: this.unbuffedBigDoTWhy(this.unbuffedBoM, this.unbuffedMortal),
+			value: this.unbuffedBoM + this.unbuffedMortal,
+		}))
 	}
 
 	protected override addChecklistRules() {
-		const uptimePercent = this.getUptimePercent(this.data.statuses.BLEEDING.id)
-		this.checklist.add(new TieredRule({
-			name: <Trans id="blu.dots.rule.name">Keep your DoTs up </Trans>,
+		const SoTUptimePercent = this.getUptimePercent(this.data.statuses.BLEEDING.id)
+		const requirements: Requirement[] = [
+			new Requirement({
+				name: <Trans id="blu.dots.requirement.uptime-bleed.name"><DataLink status="BLEEDING" showIcon={false} /> uptime</Trans>,
+				percent: SoTUptimePercent,
+			}),
+		]
+
+		const bomUptimePercent = this.getUptimePercent(this.data.statuses.BREATH_OF_MAGIC.id)
+		if (bomUptimePercent > 1) {
+			requirements.push(new Requirement({
+				name: <Trans id="blu.dots.requirement.uptime-bom.name"><DataLink status="BREATH_OF_MAGIC" showIcon={false} /> uptime</Trans>,
+				percent: bomUptimePercent,
+			}))
+		}
+
+		const flameUptimePercent = this.getUptimePercent(this.data.statuses.MORTAL_FLAME.id)
+		if (flameUptimePercent > 1) {
+			requirements.push(new Requirement({
+				name: <Trans id="blu.dots.requirement.uptime-flame.name"><DataLink status="MORTAL_FLAME" showIcon={false} /> uptime</Trans>,
+				percent: flameUptimePercent,
+			}))
+		}
+
+		this.checklist.add(new Rule({
+			name: <Trans id="blu.dots.rule.name">Keep your DoTs up</Trans>,
 			description: <Trans id="blu.dots.rule.description">
-				The <DataLink status="BLEEDING" showIcon={false} showTooltip={false} /> effect from Nightbloom and SoT is a solid 15% of your total damage.
+				The <DataLink status="BLEEDING" showIcon={false} showTooltip={false} /> effect from <DataLink action="NIGHTBLOOM" showIcon={false} showTooltip={false} /> and <DataLink action="SONG_OF_TORMENT" showIcon={false} showTooltip={false} /> is a solid 15% of your total damage. <br />
+			If you are one of the DPSes applying <DataLink action="BREATH_OF_MAGIC" showIcon={false} showTooltip={false} /> or <DataLink action="MORTAL_FLAME" showIcon={false} showTooltip={false} />, it should be a top priority to keep these DoTs rolling.
 			</Trans>,
-			tiers: SEVERITIES.UPTIME,
-			requirements: [
-				new Requirement({
-					name: <Trans id="blu.dots.requirement.uptime-bleed.name"><DataLink status="BLEEDING" showIcon={false} /> uptime</Trans>,
-					percent: uptimePercent,
-				}),
-			],
+			target: 95,
+			requirements: requirements,
 		}))
 	}
 
 	protected addClippingSuggestions() {
-		const clippingPerMinute = this.getClippingAmount(this.data.statuses.BLEEDING.id)
+		const soTclippingPerMinute = this.getClippingAmount(this.data.statuses.BLEEDING.id)
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.SONG_OF_TORMENT.icon,
 			content: <Trans id="blu.dots.suggestion.clip-bleed.content">
-				Avoid refreshing Song of Torment significantly before its expiration, this will allow you to cast more Sonic Boom.
+				Avoid refreshing <DataLink action="SONG_OF_TORMENT" showIcon={false} showTooltip={false} /> significantly before its expiration.
 			</Trans>,
 			tiers: SEVERITIES.CLIPPING,
-			value: clippingPerMinute,
+			value: soTclippingPerMinute,
 			why: <Trans id="blu.dots.suggestion.clip-bleed.why">
-				An average of {this.parser.formatDuration(clippingPerMinute)} of <DataLink status="BLEEDING" showIcon={false} /> clipped per minute due to early refreshes.
+				An average of {this.parser.formatDuration(soTclippingPerMinute)} of <DataLink status="BLEEDING" showIcon={false} /> clipped per minute due to early refreshes.
+			</Trans>,
+		}))
+
+		const boMclippingPerMinute = this.getClippingAmount(this.data.statuses.BREATH_OF_MAGIC.id)
+		this.suggestions.add(new TieredSuggestion({
+			icon: this.data.actions.BREATH_OF_MAGIC.icon,
+			content: <Trans id="blu.dots.suggestion.clip-bleed.content">
+				Avoid refreshing <DataLink action="BREATH_OF_MAGIC" showIcon={false} showTooltip={false} /> significantly before its expiration.
+			</Trans>,
+			tiers: SEVERITIES.CLIPPING,
+			value: boMclippingPerMinute,
+			why: <Trans id="blu.dots.suggestion.clip-bleed.why">
+				An average of {this.parser.formatDuration(boMclippingPerMinute)} of <DataLink status="BREATH_OF_MAGIC" showIcon={false} /> clipped per minute due to early refreshes.
 			</Trans>,
 		}))
 	}
