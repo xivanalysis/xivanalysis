aa97ffd6677f4f480def6e50c413997a1f925dfb
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2023-07-30 11:04:49 -0500

    convert leylines to buffwindow

--- src/parser/jobs/blm/modules/Leylines.tsx
+++ src/parser/jobs/blm/modules/Leylines.tsx
@@ -1,61 +1,56 @@
 import {t} from '@lingui/macro'
 import {Trans} from '@lingui/react'
 import {ActionLink} from 'components/ui/DbLink'
+import {Status} from 'data/STATUSES'
 import {Event, Events} from 'event'
-import {Analyser} from 'parser/core/Analyser'
-import {filter, oneOf} from 'parser/core/filter'
+import {filter} from 'parser/core/filter'
 import {dependency} from 'parser/core/Injectable'
+import {BuffWindow, EvaluatedAction} from 'parser/core/modules/ActionWindow'
+import {RulePassedEvaluator} from 'parser/core/modules/ActionWindow/evaluators/RulePassedEvaluator'
+import {History, HistoryEntry} from 'parser/core/modules/ActionWindow/History'
 import CastTime from 'parser/core/modules/CastTime'
 import Checklist, {Rule, Requirement} from 'parser/core/modules/Checklist'
-import {Data} from 'parser/core/modules/Data'
-import {SimpleRow, StatusItem, Timeline} from 'parser/core/modules/Timeline'
+import {SimpleRow, StatusItem} from 'parser/core/modules/Timeline'
 import React from 'react'
-import {Table, Button} from 'semantic-ui-react'
 import DISPLAY_ORDER from './DISPLAY_ORDER'
 
-interface LeyLinesWindow {
-	start: number,
-	stop?: number
-}
+//pretty random. Should be revised, maybe based on fights? 10% is ~ 1 GCD. So we allow that.
+const TARGET_UPTIME_PCT = 90
 
-interface LeyLinesWindows {
-	current?: LeyLinesWindow,
-	history: LeyLinesWindow[]
-}
-
-export default class Leylines extends Analyser {
+export default class Leylines extends BuffWindow {
 	static override handle = 'leylines'
 	static override title = t('blm.leylines.title')`Ley Lines`
 	static override displayOrder = DISPLAY_ORDER.LEY_LINES
 
-	@dependency private data!: Data
 	@dependency private checklist!: Checklist
-	@dependency private timeline!: Timeline
 	@dependency private castTime!: CastTime
 
-	private leyLinesStatuses: number[] = [
-		this.data.statuses.LEY_LINES.id,
-		this.data.statuses.CIRCLE_OF_POWER.id,
-	]
+	override buffStatus = [this.data.statuses.LEY_LINES]
 
-	private buffWindows: {[key: number]: LeyLinesWindows} = {}
+	private circleOfPowerHistory = new History<null>(() => null)
 	private castTimeIndex: number | null = null
 
 	override initialise() {
-		const leyLinesFilter = filter<Event>()
+		super.initialise()
+
+		const circleOfPowerFilter = filter<Event>()
 			.source(this.parser.actor.id)
-			.status(oneOf(this.leyLinesStatuses))
-		this.addEventHook(leyLinesFilter.type('statusApply'), this.onGain)
-		this.addEventHook(leyLinesFilter.type('statusRemove'), this.onDrop)
+			.status(this.data.statuses.CIRCLE_OF_POWER.id)
+		this.addEventHook(circleOfPowerFilter.type('statusApply'), this.onGainCircle)
+		this.addEventHook(circleOfPowerFilter.type('statusRemove'), this.onDropCircle)
 		this.addEventHook({
 			type: 'death',
 			actor: this.parser.actor.id,
 		}, this.onDeath)
-		this.addEventHook('complete', this.onComplete)
 
-		this.leyLinesStatuses.forEach(status => {
-			this.buffWindows[status] = {history: []}
-		})
+		this.addEvaluator(new RulePassedEvaluator({
+			header: {
+				header: <Trans id="blm.leylines.uptime-header">Uptime</Trans>,
+				accessor: 'powertime',
+			},
+			passesRule: this.encircled.bind(this),
+			ruleContext: this.uptimeContext.bind(this),
+		}))
 	}
 
 	public getStatusDurationInRange(
@@ -63,74 +58,42 @@ export default class Leylines extends Analyser {
 		start: number = this.parser.pull.timestamp,
 		end: number = this.parser.pull.timestamp + this.parser.pull.duration
 	) {
-		let duration = 0
-		for (const window of this.buffWindows[statusId].history) {
-			if (window.stop == null || window.stop <= start || window.start >= end) {
-				continue
+		const history = statusId === this.data.statuses.LEY_LINES.id ? this.history : this.circleOfPowerHistory
+		return history.entries.map(window => {
+			return {
+				start: window.start,
+				end: window.end ?? end,
 			}
-			duration += Math.max(0, Math.min(window.stop, end) - Math.max(window.start, start))
-		}
-
-		const currentWindows = this.buffWindows[statusId].current
-		if (currentWindows != null) {
-			duration += Math.max(end - Math.max(currentWindows.start, start), 0)
-		}
-
-		return duration
+		}).reduce((total, window) => {
+			if (window.end <= start || window.start >= end) {
+				return total
+			}
+			return total + Math.max(0, Math.min(window.end, end) - Math.max(window.start, start))
+		}, 0)
 	}
 
-	// Manage buff windows
-	private onGain(event: Events['statusApply']) {
-		const status = this.data.getStatus(event.status)
+	// Manage circle of power windows
+	private onGainCircle(event: Events['statusApply']) {
+		this.circleOfPowerHistory.getCurrentOrOpenNew(event.timestamp)
 
-		// Something is not right
-		if (!status) { return }
-
-		// Track the new window
-		const tracker = this.buffWindows[status.id]
-
-		// Don't open a new window if one's already going
-		if (tracker.current) { return }
-
-		tracker.current = {
-			start: event.timestamp,
-		}
-
-		if (status.id === this.data.statuses.CIRCLE_OF_POWER.id) {
-			this.castTimeIndex = this.castTime.setPercentageAdjustment('all', this.data.statuses.CIRCLE_OF_POWER.speedModifier, 'both')
-		}
+		this.castTimeIndex = this.castTime.setPercentageAdjustment('all', this.data.statuses.CIRCLE_OF_POWER.speedModifier, 'both')
 	}
 
-	private onDrop(event: Events['statusRemove']) {
-		this.stopAndSave(event.status, event.timestamp)
+	private onDropCircle(event: Events['statusRemove']) {
+		this.stopAndSave(event.timestamp)
 	}
 
-	// We died, close windows
+	// We died, make sure circle of power gets closed
 	private onDeath(event: Events['death']) {
-		this.stopAndSave(this.data.statuses.LEY_LINES.id, event.timestamp)
+		this.stopAndSave(event.timestamp)
 	}
 
 	// Finalise a buff window
-	private stopAndSave(statusId: number, endTime: number = this.parser.currentEpochTimestamp) {
-		const tracker = this.buffWindows[statusId]
+	private stopAndSave(endTime: number = this.parser.currentEpochTimestamp) {
+		this.circleOfPowerHistory.closeCurrent(endTime)
 
-		// Already closed, nothing to do here
-		if (!tracker.current) { return }
-
-		// Close the window
-		tracker.current.stop = endTime
-		tracker.history.push(tracker.current)
-		tracker.current = undefined
-
-		// Close dependency windows
-		if (statusId === this.data.statuses.LEY_LINES.id) {
-			this.stopAndSave(this.data.statuses.CIRCLE_OF_POWER.id, endTime)
-		}
-
-		if (statusId === this.data.statuses.CIRCLE_OF_POWER.id) {
-			this.castTime.reset(this.castTimeIndex)
-			this.castTimeIndex = null
-		}
+		this.castTime.reset(this.castTimeIndex)
+		this.castTimeIndex = null
 	}
 
 	// A reminder of man's ability to generate electricity
@@ -138,9 +101,25 @@ export default class Leylines extends Analyser {
 		return (power / lines) * 100
 	}
 
-	private onComplete() {
+	private encircled(window: HistoryEntry<EvaluatedAction[]>): boolean {
+		return this.getUptimeForWindow(window) >= TARGET_UPTIME_PCT
+	}
+
+	private uptimeContext(window: HistoryEntry<EvaluatedAction[]>): string {
+		return this.getUptimeForWindow(window).toFixed(2) + '%'
+	}
+
+	private getUptimeForWindow(window: HistoryEntry<EvaluatedAction[]>) {
+		const linesDuration = (window.end ?? this.parser.pull.timestamp + this.parser.pull.duration) - window.start
+		const copDuration = this.getStatusDurationInRange(this.data.statuses.CIRCLE_OF_POWER.id, window.start, window.end)
+		return this.dontMovePercent(copDuration, linesDuration)
+	}
+
+	override onComplete() {
 		// Current time will be end of fight so no need to pass it here
-		this.stopAndSave(this.data.statuses.LEY_LINES.id)
+		this.stopAndSave(this.data.statuses.CIRCLE_OF_POWER.id)
+
+		super.onComplete()
 
 		// Build the grouping row
 		const parentRow = this.timeline.addRow(new SimpleRow({
@@ -148,24 +127,9 @@ export default class Leylines extends Analyser {
 			order: 0,
 		}))
 
-		const fightStart = this.parser.pull.timestamp
-
 		// For each buff, add it to timeline
-		this.leyLinesStatuses.forEach(buff => {
-			const status = this.data.getStatus(buff)
-			if (!status) { return }
-
-			const row = parentRow.addRow(new SimpleRow({label: status.name}))
-
-			this.buffWindows[buff].history.forEach(window => {
-				if (!window.stop) { return }
-				row.addItem(new StatusItem({
-					status,
-					start: window.start - fightStart,
-					end: window.stop - fightStart,
-				}))
-			})
-		})
+		this.addHistoryToTimeline(this.data.statuses.LEY_LINES, this.history, parentRow)
+		this.addHistoryToTimeline(this.data.statuses.CIRCLE_OF_POWER, this.circleOfPowerHistory, parentRow)
 
 		// Get the total duration of CoP uptime and Ley Lines, so we can get the overall percentage uptime
 		const copDuration = this.getStatusDurationInRange(this.data.statuses.CIRCLE_OF_POWER.id)
@@ -180,46 +144,20 @@ export default class Leylines extends Analyser {
 					percent: this.dontMovePercent(copDuration, linesDuration),
 				}),
 			],
-			//pretty random. Should be revised, maybe based on fights? 10% is ~ 1 GCD. So we allow that.
-			target: 90,
+			target: TARGET_UPTIME_PCT,
 		}))
 	}
 
-	override output() {
-		const fightEnd = this.parser.pull.timestamp + this.parser.pull.duration
-		return <Table collapsing unstackable compact="very">
-			<Table.Header>
-				<Table.Row>
-					<Table.HeaderCell><Trans id="blm.leylines.timestamp-header">Timestamp</Trans></Table.HeaderCell>
-					<Table.HeaderCell><Trans id="blm.leylines.uptime-header">Uptime</Trans></Table.HeaderCell>
-					<Table.HeaderCell></Table.HeaderCell>
-				</Table.Row>
-			</Table.Header>
-			<Table.Body>
-				{this.buffWindows[this.data.statuses.LEY_LINES.id].history.map(leyLinesEvent => {
-					// Find the CoPs that were inside this Ley Lines
-					const thisCoPHistory = this.buffWindows[this.data.statuses.CIRCLE_OF_POWER.id].history.filter(cops => ((cops.start >= leyLinesEvent.start) && ((cops.stop || 0) <= (leyLinesEvent.stop || 0))))
-
-					// For this set of CoPs, get the uptime
-					const thisCoPUptime = thisCoPHistory.reduce((duration, cop) => duration + Math.max((cop.stop || 0) - cop.start, 0), 0)
-
-					// Note that since we're getting the actual duration, rather than the expected duration,
-					// technically we'll call it 100% uptime if you stay in the lines and die halfway through...
-					// However, since that'll get flagged as a morbid checklist item, that's probably ok.
-					const thisPercent = this.dontMovePercent(thisCoPUptime, (leyLinesEvent.stop || fightEnd) - leyLinesEvent.start).toFixed(2)
-
-					return <Table.Row key={leyLinesEvent.start}>
-						<Table.Cell>{this.parser.formatEpochTimestamp(leyLinesEvent.start)}</Table.Cell>
-						<Table.Cell>{thisPercent}%</Table.Cell>
-						<Table.Cell>
-							<Button onClick={() =>
-								this.timeline.show(leyLinesEvent.start - this.parser.pull.timestamp, (leyLinesEvent.stop || fightEnd) - this.parser.pull.timestamp)}>
-								<Trans id="blm.leylines.timelinelink-button">Jump to Timeline</Trans>
-							</Button>
-						</Table.Cell>
-					</Table.Row>
-				})}
-			</Table.Body>
-		</Table>
+	private addHistoryToTimeline(status: Status, history: History<Array<Events['action']> | null>, parentRow: SimpleRow) {
+		const row = parentRow.addRow(new SimpleRow({label: status.name}))
+		const fightStart = this.parser.pull.timestamp
+		history.entries.forEach(window => {
+			if (window.end == null) { return }
+			row.addItem(new StatusItem({
+				status,
+				start: window.start - fightStart,
+				end: window.end - fightStart,
+			}))
+		})
 	}
 }
