01568f7cfa9db36f92f483f34a1542c5b19f28ee
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2023-10-23 22:10:24 -0500

    Stacked Gauge timeline display and mark MNK as supported (#1844)
    * Allow gauges to display stacked
    
    * MNK Beast/Nadi visualisation w/ stacked gauge
    
    * Update RPR Death Gauge visualisaiton to stacked
    
    * Fix i18n
    
    * Mark MNK as supported
    
    * Safety on beast gauge generation & style fix for nadi
    
    * oops, missed cleaning up commented out previous attempt
    
    * bump patch for mnk
    
    * fix coalesce syntax
    
    * make i18n consistent with follow-up branches
    
    * bump patch to 6.5
    
    * Make the comment not weird

--- src/parser/jobs/mnk/modules/PerfectBalance.tsx
+++ src/parser/jobs/mnk/modules/PerfectBalance.tsx
@@ -1,18 +1,23 @@
 import {t} from '@lingui/macro'
 import {Plural, Trans} from '@lingui/react'
+import Color from 'color'
 import {DataLink} from 'components/ui/DbLink'
 import {Action, ActionKey} from 'data/ACTIONS'
 import {Event, Events} from 'event'
-import {Analyser} from 'parser/core/Analyser'
-import {EventHook} from 'parser/core/Dispatcher'
+import {EventHook, TimestampHook} from 'parser/core/Dispatcher'
 import {filter, oneOf} from 'parser/core/filter'
 import {dependency} from 'parser/core/Injectable'
-import {Data} from 'parser/core/modules/Data'
+import {CounterGauge, Gauge} from 'parser/core/modules/Gauge'
 import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
 import React from 'react'
-import {FORM_ACTIONS} from './constants'
+import {BLITZ_ACTIONS, COEURL_ACTIONS, FORM_ACTIONS, OPO_OPO_ACTIONS, RAPTOR_ACTIONS} from './constants'
 import {DISPLAY_ORDER} from './DISPLAY_ORDER'
 import {fillActions} from './utilities'
+
+const BEAST_GAUGE_HANDLE = 'beastgauge'
+const NADI_GAUGE_HANDLE = 'nadigauge'
+
+const BEAST_GAUGE_TIMEOUT_MILLIS = 20000
 
 const SUGGESTION_TIERS = {
 	1: SEVERITY.MEDIUM,
@@ -27,36 +32,114 @@ const PB_BAD_ACTIONS: ActionKey[] = [
 
 interface Balance {
 	bads: number
-	stacks: number
+	stacks: number // this is the number of stacks the window began with (may be less than the max for dungeons/24mans with a carried-over PB)
 	used: number
+	start: number
 }
 
-export class PerfectBalance extends Analyser {
+const GAUGE_FADE = 0.25
+const OPO_GAUGE_COLOR = Color('#a256dc')
+const RAPTOR_GAUGE_COLOR = Color('#57b39a')
+const COEURL_GAUGE_COLOR = Color('#d7548e')
+const LUNAR_NADI_COLOR = Color('#8a57c4')
+const SOLAR_NADI_COLOR = Color('#e7dbd3')
+
+export class PerfectBalance extends Gauge {
 	static override debug = false
 	static override handle = 'perfectBalance'
 	static override title = t('mnk.pb.title')`Perfect Balance`
 	static override displayOrder = DISPLAY_ORDER.PERFECT_BALANCE
 
-	@dependency private data!: Data
 	@dependency private suggestions!: Suggestions
 
 	private badActions: Array<Action['id']> = []
 	private formActions: Array<Action['id']> = []
+	private opoActions: Array<Action['id']> = []
+	private raptorActions: Array<Action['id']> = []
+	private coeurlActions: Array<Action['id']> = []
+	private blitzActions: Array<Action['id']> = []
 
 	private current: Balance | undefined
 	private history: Balance[] = []
 
 	private perfectHook?: EventHook<Events['action']>
+	private beastTimeoutHook?: TimestampHook
+
+	private opoBeastGauge = this.add(new CounterGauge({
+		maximum: 3,
+		initialValue: 0,
+		graph: {
+			handle: BEAST_GAUGE_HANDLE,
+			label: <Trans id="mnk.gauge.resource.beast.opo">Opo-opo</Trans>,
+			color: OPO_GAUGE_COLOR.fade(GAUGE_FADE),
+		},
+	}))
+	private raptorBeastGauge = this.add(new CounterGauge({
+		maximum: 3,
+		initialValue: 0,
+		graph: {
+			handle: BEAST_GAUGE_HANDLE,
+			label: <Trans id="mnk.gauge.resource.beast.raptor">Raptor</Trans>,
+			color: RAPTOR_GAUGE_COLOR.fade(GAUGE_FADE),
+		},
+	}))
+	private coeurlBeastGauge = this.add(new CounterGauge({
+		maximum: 3,
+		initialValue: 0,
+		graph: {
+			handle: BEAST_GAUGE_HANDLE,
+			label: <Trans id="mnk.gauge.resource.beast.coeurl">Coeurl</Trans>,
+			color: COEURL_GAUGE_COLOR.fade(GAUGE_FADE),
+		},
+	}))
+	private lunarNadiGauge = this.add(new CounterGauge({
+		maximum: 1,
+		initialValue: 0,
+		graph: {
+			handle: NADI_GAUGE_HANDLE,
+			label: <Trans id="mnk.gauge.resource.nadi.lunar">Lunar Nadi</Trans>,
+			color: LUNAR_NADI_COLOR.fade(GAUGE_FADE),
+		},
+	}))
+	private solarNadiGauge = this.add(new CounterGauge({
+		maximum: 1,
+		initialValue: 0,
+		graph: {
+			handle: NADI_GAUGE_HANDLE,
+			label: <Trans id="mnk.gauge.resource.nadi.solar">Solar Nadi</Trans>,
+			color: SOLAR_NADI_COLOR.fade(GAUGE_FADE),
+		},
+	}))
 
 	override initialise() {
+		super.initialise()
+
 		this.badActions = fillActions(PB_BAD_ACTIONS, this.data)
 		this.formActions = fillActions(FORM_ACTIONS, this.data)
+		this.opoActions = fillActions(OPO_OPO_ACTIONS, this.data)
+		this.raptorActions = fillActions(RAPTOR_ACTIONS, this.data)
+		this.coeurlActions = fillActions(COEURL_ACTIONS, this.data)
+		this.blitzActions = fillActions(BLITZ_ACTIONS, this.data)
 
 		const playerFilter = filter<Event>().source(this.parser.actor.id)
 		this.addEventHook(playerFilter.type('statusApply').status(this.data.statuses.PERFECT_BALANCE.id), this.onStacc)
 		this.addEventHook(playerFilter.type('statusRemove').status(this.data.statuses.PERFECT_BALANCE.id), this.onDrop)
+		this.addEventHook(playerFilter.type('action').action(oneOf(this.blitzActions)), this.onBlitz)
 
 		this.addEventHook('complete', this.onComplete)
+
+		this.resourceGraphs.addDataGroup({
+			handle: BEAST_GAUGE_HANDLE,
+			label: <Trans id="mnk.gauge.resource.beast.chakra">Beast Chakra</Trans>,
+			collapse: true,
+			forceCollapsed: true,
+			stacking: true,
+		})
+		this.resourceGraphs.addDataGroup({
+			handle: NADI_GAUGE_HANDLE,
+			label: <Trans id="mnk.gauge.resource.nadi">Nadi Gauge</Trans>,
+			collapse: false,
+		})
 	}
 
 	private onCast(event: Events['action']): void {
@@ -65,10 +148,53 @@ export class PerfectBalance extends Analyser {
 		if (action == null || !(action.onGcd ?? false)) { return }
 
 		if (this.current) {
-			this.current.used++
+			// Flag any bad GCDs
+			if (this.badActions.includes(action.id)) {
+				this.current.bads++
+			} else {
+				this.current.used++
+			}
 
-			// Additionally flag any bad GCDs
-			if (this.badActions.includes(action.id)) { this.current.bads++ }
+			// Make sure we don't record additional gauge generation if the player doesn't immediately blitz after using their PB stacks
+			if (this.current.used <= this.current.start) {
+				if (this.opoActions.includes(action.id)) {
+					this.opoBeastGauge.generate(1)
+				}
+				if (this.raptorActions.includes(action.id)) {
+					this.raptorBeastGauge.generate(1)
+				}
+				if (this.coeurlActions.includes(action.id)) {
+					this.coeurlBeastGauge.generate(1)
+				}
+			}
+		}
+	}
+
+	private onBlitz(event: Events['action']): void {
+		const action = this.data.getAction(event.action)
+
+		if (action == null || !(action.onGcd ?? false)) { return }
+
+		// The blitz action closes the window if it wasn't already
+		this.stopAndSave()
+
+		switch (action.id) {
+		case this.data.actions.ELIXIR_FIELD.id:
+			this.lunarNadiGauge.generate(1)
+			break
+		case this.data.actions.RISING_PHOENIX.id:
+			this.solarNadiGauge.generate(1)
+			break
+		case this.data.actions.PHANTOM_RUSH.id:
+			this.lunarNadiGauge.reset()
+			this.solarNadiGauge.reset()
+			break
+		case this.data.actions.CELESTIAL_REVOLUTION.id:
+			if (this.lunarNadiGauge.empty) {
+				this.lunarNadiGauge.generate(1)
+			} else {
+				this.solarNadiGauge.generate(1)
+			}
 		}
 	}
 
@@ -77,8 +203,9 @@ export class PerfectBalance extends Analyser {
 		if (event.data == null) { return }
 
 		// New window who dis - check for new window before updating just in case
-		if (event.data === this.data.statuses.PERFECT_BALANCE.stacksApplied) {
-			this.current = {bads: 0, stacks: event.data, used: 0}
+		// If we didn't have a window already, go ahead and create one anyways since it probably means they began the fight with a partially-used PB (24-man or dungeon)
+		if (event.data === this.data.statuses.PERFECT_BALANCE.stacksApplied || this.current == null) {
+			this.current = {bads: 0, stacks: event.data, used: 0, start: event.timestamp}
 
 			// Create the hook to check GCDs in PB
 			this.perfectHook = this.addEventHook(
@@ -92,18 +219,19 @@ export class PerfectBalance extends Analyser {
 			return
 		}
 
-		// Update stacks for existing window
-		if (this.current != null) {
-			this.current.stacks = event.data
-			return
-		}
-
 		// We should have returned by now, so debug if not
 		this.debug('New PB window with incorrect initial stacks')
 	}
 
 	private onDrop(): void {
-		this.stopAndSave()
+		const statusDuration = this.parser.currentEpochTimestamp - (this.current?.start ?? this.parser.pull.timestamp)
+		// If Perfect Balance fell off because they took too long, go ahead and close the window now, they won't be able to blitz
+		if (statusDuration === this.data.statuses.PERFECT_BALANCE.duration) {
+			this.stopAndSave()
+		} else {
+			// Otherwise, set a timestamp hook to close the window if they also take too long to use their blitz action
+			this.beastTimeoutHook = this.addTimestampHook(this.parser.currentEpochTimestamp + BEAST_GAUGE_TIMEOUT_MILLIS, this.stopAndSave)
+		}
 	}
 
 	private stopAndSave(): void {
@@ -111,12 +239,20 @@ export class PerfectBalance extends Analyser {
 		if (this.current != null) {
 			this.history.push(this.current)
 
+			// Remove the perfect balance event hook so we don't keep trying to generate
 			if (this.perfectHook != null) {
 				this.removeEventHook(this.perfectHook)
 				this.perfectHook = undefined
 			}
+			if (this.beastTimeoutHook != null) {
+				this.removeTimestampHook(this.beastTimeoutHook)
+				this.beastTimeoutHook = undefined
+			}
 		}
 
+		this.opoBeastGauge.reset()
+		this.raptorBeastGauge.reset()
+		this.coeurlBeastGauge.reset()
 		this.current = undefined
 	}
 
@@ -125,13 +261,13 @@ export class PerfectBalance extends Analyser {
 		this.stopAndSave()
 
 		// Stacks are hard set instead of being subtracted, so we need to take used from max rather than raw remaining
-		const droppedGcds = this.history.reduce((drops, current) => drops + (current.used - this.data.statuses.PERFECT_BALANCE.stacksApplied), 0)
+		const droppedGcds = this.history.reduce((drops, current) => drops + (current.stacks - current.used), 0)
 		const badActions = this.history.reduce((bads, current) => bads + current.bads, 0)
 
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.PERFECT_BALANCE.icon,
 			content: <Trans id="mnk.pb.suggestions.stacks.content">
-				Try to consume all 6 stacks during every <DataLink action="PERFECT_BALANCE"/> window.
+				Try to consume all {this.data.statuses.PERFECT_BALANCE.stacksApplied} stacks during every <DataLink action="PERFECT_BALANCE"/> window.
 			</Trans>,
 			tiers: SUGGESTION_TIERS,
 			value: droppedGcds,
