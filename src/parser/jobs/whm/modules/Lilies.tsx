8e2dda67be3e7f526b48ebe49e17ce82b662e61b
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-05-18 16:49:07 -0500

    Remove patch-specific code from WHM

--- src/parser/jobs/whm/modules/Lilies.tsx
+++ src/parser/jobs/whm/modules/Lilies.tsx
@@ -11,8 +11,7 @@ import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Sugge
 import React from 'react'
 
 const LILY_MAX_STACKS = 3
-const LILY_INTERVAL_600 = 30000
-const LILY_INTERVAL_610 = 20000
+const LILY_INTERVAL = 20000
 
 const MISERY_COST = 3
 
@@ -50,10 +49,7 @@ const SEVERITIES = {
 		2: SEVERITY.MEDIUM,
 		3: SEVERITY.MAJOR,
 	},
-	LILY_OVERCAP_600: {
-		1: SEVERITY.MINOR,
-	},
-	LILY_OVERCAP_610: {
+	LILY_OVERCAP: {
 		1: SEVERITY.MEDIUM,
 		3: SEVERITY.MAJOR,
 	},
@@ -70,7 +66,7 @@ export class Lilies extends CoreGauge {
 
 	private wastedGcds = 0;
 
-	private lilyInterval = this.parser.patch.before('6.1') ? LILY_INTERVAL_600 : LILY_INTERVAL_610
+	private lilyInterval = LILY_INTERVAL
 
 	private lilyGauge = this.add(new CounterGauge({
 		maximum: LILY_MAX_STACKS,
@@ -144,14 +140,6 @@ export class Lilies extends CoreGauge {
 		// Calculate lost Lilies for the fight, does not correct for UTA or other downtime
 		const lostLilies = Math.floor(this.lilyTimer.getExpirationTime() / this.lilyInterval)
 
-		const lilyOvercapSuggestion_600 = <Trans id="whm.gauge.lily.suggestions.overcap.content.600">
-			Try to use <DataLink action="AFFLATUS_RAPTURE" /> or <DataLink action="AFFLATUS_SOLACE" /> before using other GCD heals. It's okay to cap your lilies if you don't need to heal, move, or weave with them.
-		</Trans>
-
-		const lilyOvercapSuggestion_610 = <Trans id="whm.gauge.lily.suggestions.overcap.content.610">
-			Try to use <DataLink action="AFFLATUS_RAPTURE" /> or <DataLink action="AFFLATUS_SOLACE" /> before using other GCD heals. It's okay to overheal with your lilies as they are to be used for mana management and movement aswell as healing.
-		</Trans>
-
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.AFFLATUS_MISERY.icon,
 			content: <Trans id="whm.gauge.bloodlily.suggestions.overcap.content">
@@ -167,8 +155,10 @@ export class Lilies extends CoreGauge {
 
 		this.suggestions.add(new TieredSuggestion({
 			icon: this.data.actions.AFFLATUS_SOLACE.icon,
-			content: this.parser.patch.before('6.1') ? lilyOvercapSuggestion_600 : lilyOvercapSuggestion_610,
-			tiers: this.parser.patch.before('6.1') ? SEVERITIES.LILY_OVERCAP_600 : SEVERITIES.LILY_OVERCAP_610,
+			content: <Trans id="whm.gauge.lily.suggestions.overcap.content.610">
+				Try to use <DataLink action="AFFLATUS_RAPTURE" /> or <DataLink action="AFFLATUS_SOLACE" /> before using other GCD heals. It's okay to overheal with your lilies as they are to be used for mana management and movement aswell as healing.
+			</Trans>,
+			tiers: SEVERITIES.LILY_OVERCAP,
 			value: lostLilies,
 			why: <Trans id="whm.gauge.lily.suggestions.overcap.why">
 				{<Plural value={lostLilies} one="# Lily" other="# Lilies" />} were wasted due to overcapping the gauge.
