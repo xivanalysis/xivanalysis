2fae58f1e80206f4dbe60a3ff539a4321368ceb7
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-05-18 16:48:59 -0500

    Remove patch-specific code from WAR

--- src/parser/jobs/war/modules/InnerRelease.tsx
+++ src/parser/jobs/war/modules/InnerRelease.tsx
@@ -1,5 +1,5 @@
 import {t} from '@lingui/macro'
-import {Plural, Trans} from '@lingui/react'
+import {Trans} from '@lingui/react'
 import {DataLink} from 'components/ui/DbLink'
 import {Action, ActionKey} from 'data/ACTIONS'
 import {Event, Events} from 'event'
@@ -9,16 +9,11 @@ import {filter} from 'parser/core/filter'
 import {dependency} from 'parser/core/Injectable'
 import Checklist, {Requirement, Rule} from 'parser/core/modules/Checklist'
 import {Data} from 'parser/core/modules/Data'
-import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
+import Suggestions from 'parser/core/modules/Suggestions'
 import React from 'react'
 
 // Default case
 const IR_STACKS_APPLIED = 3
-
-const CHAOS_GCDS: ActionKey[] = [
-	'INNER_CHAOS',
-	'CHAOTIC_CYCLONE',
-]
 
 const GOOD_GCDS: ActionKey[] = [
 	'FELL_CLEAVE',
@@ -79,15 +74,12 @@ export class InnerRelease extends Analyser {
 
 			this.current.rushing = this.data.statuses.INNER_RELEASE.duration >= (this.parser.pull.timestamp + this.parser.pull.duration) - event.timestamp
 
-			// If we're 6.1+, we only want FC+Decimate since nothing else eats stacks
 			const playerFilter = filter<Event>().source(this.parser.actor.id)
-			this.innerHook = this.parser.patch.before('6.1')
-				? this.addEventHook(playerFilter.type('action'), this.onCast)
-				: this.addEventHook(
-					playerFilter
-						.type('action')
-						.action(this.data.matchActionId(GOOD_GCDS)),
-					this.onCast)
+			this.innerHook = this.addEventHook(
+				playerFilter
+					.type('action')
+					.action(this.data.matchActionId(GOOD_GCDS)),
+				this.onCast)
 		}
 	}
 
@@ -142,44 +134,6 @@ export class InnerRelease extends Analyser {
 				}),
 			],
 		}))
-
-		if (this.parser.patch.before('6.1')) {
-			// Build our GCD filters
-			const chaosGcds = CHAOS_GCDS.map(actionKey => this.data.actions[actionKey].id)
-			const goodGcds = GOOD_GCDS.map(actionKey => this.data.actions[actionKey].id)
-
-			// We ignore rushed windows for IC as a user might push for higher potency at end of fight
-			const badGcds = this.history.reduce((total, current) => total + (this.stacksApplied - this.accountGcds(current, goodGcds)), 0)
-			const veryBadGcds = nonRushedReleases.reduce((total, current) => total + this.accountGcds(current, chaosGcds), 0)
-
-			this.suggestions.add(new TieredSuggestion({
-				icon: this.data.actions.FELL_CLEAVE.icon,
-				content: <Trans id="war.ir.suggestions.badgcd.content">
-					GCDs used during <DataLink action="INNER_RELEASE"/> should be limited to <DataLink action="FELL_CLEAVE"/> for optimal damage (or <DataLink action="DECIMATE"/> if three or more targets are present).
-				</Trans>,
-				tiers: {
-					1: SEVERITY.MAJOR,
-				},
-				value: badGcds,
-				why: <Trans id="war.ir.suggestions.badgcd.why">
-					<Plural value={badGcds} one="# GCD" other="# GCDs"/> wasted <DataLink showIcon={false} status="INNER_RELEASE"/> stacks.
-				</Trans>,
-			}))
-
-			this.suggestions.add(new TieredSuggestion({
-				icon: this.data.actions.INNER_CHAOS.icon,
-				content: <Trans id="war.ir.suggestions.verybadgcd.content">
-					Avoid using <DataLink action="INNER_CHAOS"/> or <DataLink action="CHAOTIC_CYCLONE"/> inside of <DataLink status="INNER_RELEASE"/> unless pushing downtime. These abilities are guaranteed to be a critical direct hit, and make no use of <DataLink showIcon={false} status="INNER_RELEASE"/>'s benefits.
-				</Trans>,
-				tiers: {
-					1: SEVERITY.MAJOR,
-				},
-				value: veryBadGcds,
-				why: <Trans id="war.ir.suggestions.verybadgcd.why">
-					<Plural value={veryBadGcds} one="# stack" other="# stacks"/> of <DataLink status="INNER_RELEASE"/> <Plural value={veryBadGcds} one="was" other="were"/> lost to <DataLink action="INNER_CHAOS"/> or <DataLink action="CHAOTIC_CYCLONE"/>.
-				</Trans>,
-			}))
-		}
 	}
 
 	private accountGcds(window: ReleaseWindow, targetGcds: Array<Action['id']>): number {
