9b2669fe17903fb23e5c454893e8247191ac5808
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-05-18 16:40:32 -0500

    Squash patch definitions

--- src/data/PATCHES/patches.ts
+++ src/data/PATCHES/patches.ts
@@ -46,59 +46,19 @@ export const PATCHES = ensureRecord<PatchInfo>()({
 			baseUrl: 'https://shadowbringers.xivanalysis.com',
 		},
 	},
-	'6.0': {
+	'Endwalker': {
 		date: {
 			[GameEdition.GLOBAL]: 1637312400, // 19/11/21 09:00:00 GMT
-		},
-	},
-	'6.05': {
-		date: {
-			[GameEdition.GLOBAL]: 1641286800, // 04/01/22 09:00:00 GMT
-		},
-	},
-	'6.08': {
-		date: {
-			[GameEdition.GLOBAL]: 1643101200, // 25/01/22 09:00:00 GMT
-			[GameEdition.KOREAN]: 1652169600, // 10/05/22 08:00:00 GMT
-			[GameEdition.CHINESE]: 1647417600, // 16/03/22 08:00:00 GMT
-		},
-	},
-	'6.1': {
-		date: {
-			[GameEdition.GLOBAL]: 1649757600, // 12/04/22 10:00:00 GMT
-			[GameEdition.CHINESE]: 1660032000, // 09/08/22 08:00:00 GMT
 			[GameEdition.KOREAN]: 1664870400, // 04/10/22 08:00:00 GMT
+			[GameEdition.CHINESE]: 1660032000, // 09/08/22 08:00:00 GMT
+		},
+		branch: {
+			baseUrl: 'https://endwalker.xivanalysis.com',
 		},
 	},
-	'6.2': {
+	'7.0': {
 		date: {
-			[GameEdition.GLOBAL]: 1661248800, // 23/08/22 10:00:00 GMT
-			[GameEdition.CHINESE]: 1671523200, // 20/12/22 08:00:00 GMT
-			[GameEdition.KOREAN]: 1676361600, // 14/02/23 08:00:00 GMT
-		},
-	},
-	'6.3': {
-		date: {
-			[GameEdition.GLOBAL]: 1673298000, // 09/01/23 08:00:00 GMT
-			[GameEdition.CHINESE]: 1683619200, // 09/05/23 08:00:00 GMT
-			[GameEdition.KOREAN]: 1687852800, // 27/06/23 08:00:00 GMT
-		},
-	},
-	'6.4': {
-		date: {
-			[GameEdition.GLOBAL]: 1684828800, // 23/05/23 08:00:00 GMT
-		},
-	},
-	'6.45': {
-		date: {
-			[GameEdition.GLOBAL]: 1689667200, // 18/07/23 08:00:00 GMT
-			[GameEdition.KOREAN]: 1698739200, // 31/10/23 08:00:00 GMT
-		},
-	},
-	'6.5': {
-		date: {
-			[GameEdition.GLOBAL]: 1696320000, // 03/10/23 08:00:00 GMT
-			[GameEdition.KOREAN]: 1712044800, // 02/04/24 08:00:00 GMT
+			[GameEdition.GLOBAL]: 1719565200, // 28/06/24 09:00:00 GMT
 		},
 	},
 })
