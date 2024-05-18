ab1d51d26cf92b8e932d137001f22ca21c63f416
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-05-18 16:47:50 -0500

    Remove patch-specific code from DRG

--- src/parser/jobs/drg/modules/Weaving.ts
+++ src/parser/jobs/drg/modules/Weaving.ts
@@ -7,24 +7,12 @@ const JUMPS: ActionKey[] = [
 	'STARDIVER',
 ]
 
-const JUMPS_600: ActionKey[] = [
-	'JUMP',
-	'HIGH_JUMP',
-	'SPINESHATTER_DIVE',
-	'DRAGONFIRE_DIVE',
-]
-
 export default class Weaving extends CoreWeaving {
 	static override displayOrder = DISPLAY_ORDER.WEAVING
 
 	private jumpIds = JUMPS.map(key => this.data.actions[key].id)
 
 	override getMaxWeaves(weave: Weave) {
-		if (this.parser.patch.before('6.1')) {
-			const jump600Ids = JUMPS_600.map(key => this.data.actions[key].id)
-			this.jumpIds.push(...jump600Ids)
-		}
-
 		if (weave.weaves.some(weave => this.jumpIds.includes(weave.action))) {
 			return 1
 		}
