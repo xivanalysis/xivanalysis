674a5466e1b57ff21653fa0519d513714df5d59b
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-05-18 16:46:43 -0500

    Add stub parser files for new jobs

---
+++ src/parser/jobs/pct/index.tsx
@@ -0,0 +1,28 @@
+import {t} from '@lingui/macro'
+import TransMarkdown from 'components/ui/TransMarkdown'
+// import CONTRIBUTORS, {ROLES} from 'data/CONTRIBUTORS'
+import {Meta} from 'parser/core/Meta'
+import React from 'react'
+import {changelog} from './changelog'
+
+const description = t('pct.about.description')`
+`
+
+export const PICTOMANCER = new Meta({
+	modules: () => import('./modules' /* webpackChunkName: "jobs-pct" */),
+
+	Description: () => <>
+		<TransMarkdown source={description}/>
+	</>,
+
+	supportedPatches: {
+		from: '✖',
+		to: '✖',
+	},
+
+	contributors: [
+	// {user: CONTRIBUTORS.YOU, role: ROLES.YOUR_ROLE},
+	],
+
+	changelog,
+})
