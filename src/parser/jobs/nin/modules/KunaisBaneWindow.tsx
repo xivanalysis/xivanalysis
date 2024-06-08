151986ff12070fb3a7f82184222ee0fd9bf4d93d
Alex Levine <lord.xela@gmail.com>
2024-06-08 18:07:04 -0400

    Refactored Trick Attack modules for the upgraded action, removed old Huton modules

--- src/parser/jobs/nin/modules/KunaisBaneWindow.tsx
+++ src/parser/jobs/nin/modules/KunaisBaneWindow.tsx
@@ -12,8 +12,8 @@ import React from 'react'
 // TCJ windows should have 5-6 Ninjutsu + 2-3 (or 3-4, with good ping) weaponskills, non-TCJ windows should have 3 Ninjutsu + 4 weaponskills
 const BASE_GCDS_PER_WINDOW = 7
 
-// For opener detection - first Trick should be 8-9s into the fight, with a bit of wiggle room for late starts
-const FIRST_TRICK_BUFFER = 12000
+// For opener detection - first Kunai's Bane should be 8-9s into the fight, with a bit of wiggle room for late starts
+const FIRST_KUNAIS_BANE_BUFFER = 12000
 
 const MUDRAS: ActionKey[] = [
 	'TEN',
@@ -36,40 +36,40 @@ class TCJEvaluator extends NotesEvaluator {
 	}
 
 	header = {
-		header: <Trans id ="nin.taWindow.chart.notes.header">TCJ Used</Trans>,
+		header: <Trans id ="nin.kb-window.chart.notes.header">TCJ Used</Trans>,
 		accessor: 'tcjused',
 	}
 
 	override generateNotes(window: HistoryEntry<EvaluatedAction[]>) {
 		// TODO - Add a note in here about Meisui-buffed Bhava, maybe?
 		return window.data.find(cast => cast.action.id === this.tcjId) ?
-			<Trans id="nin.taWindow.chart.notes.yes">Yes</Trans> :
-			<Trans id="nin.taWindow.chart.notes.no">No</Trans>
+			<Trans id="nin.kb-window.chart.notes.yes">Yes</Trans> :
+			<Trans id="nin.kb-window.chart.notes.no">No</Trans>
 	}
 }
 
-export class TrickAttackWindow extends BuffWindow {
-	static override handle = 'taWindow'
-	static override title = t('nin.taWindow.title')`Trick Attack Windows`
+export class KunaisBaneWindow extends BuffWindow {
+	static override handle = 'kbWindow'
+	static override title = t('nin.kb-window.title')`Kunai's Bane Windows`
 
 	@dependency globalCooldown!: GlobalCooldown
 
-	override buffStatus = this.data.statuses.TRICK_ATTACK
+	override buffStatus = this.data.statuses.KUNAIS_BANE
 
 	override initialise() {
 		super.initialise()
 
 		this.ignoreActions(MUDRAS.map(k => this.data.actions[k].id))
 
-		const suggestionIcon = this.data.actions.TRICK_ATTACK.icon
-		const suggestionWindowName = <ActionLink action="TRICK_ATTACK" showIcon={false}/>
+		const suggestionIcon = this.data.actions.KUNAIS_BANE.icon
+		const suggestionWindowName = <ActionLink action="KUNAIS_BANE" showIcon={false}/>
 		this.addEvaluator(new ExpectedGcdCountEvaluator({
 			expectedGcds: BASE_GCDS_PER_WINDOW,
 			globalCooldown: this.globalCooldown,
 			hasStacks: false,
 			suggestionIcon,
-			suggestionContent: <Trans id="nin.taWindow.suggestions.gcds.content">
-				While the exact number of GCDs per window will vary depending on whether <ActionLink action="TEN_CHI_JIN"/> is up, every <ActionLink action="TRICK_ATTACK"/> window should contain at least <Plural value={BASE_GCDS_PER_WINDOW} one="# GCD" other="# GCDs" />.
+			suggestionContent: <Trans id="nin.kb-window.suggestions.gcds.content">
+				While the exact number of GCDs per window will vary depending on whether <ActionLink action="TEN_CHI_JIN"/> is up, every <ActionLink action="KUNAIS_BANE"/> window should contain at least <Plural value={BASE_GCDS_PER_WINDOW} one="# GCD" other="# GCDs" />.
 			</Trans>,
 			suggestionWindowName,
 			severityTiers: {
@@ -95,8 +95,8 @@ export class TrickAttackWindow extends BuffWindow {
 				},
 			],
 			suggestionIcon,
-			suggestionContent: <Trans id="nin.taWindow.suggestions.trackedactions.content">
-				Every <ActionLink action="TRICK_ATTACK"/> window should contain <ActionLink action="HYOSHO_RANRYU"/>, 2 <ActionLink action="RAITON"/> casts, and <ActionLink action="DREAM_WITHIN_A_DREAM"/> in order to maximize damage.
+			suggestionContent: <Trans id="nin.kb-window.suggestions.trackedactions.content">
+				Every <ActionLink action="KUNAIS_BANE"/> window should contain <ActionLink action="HYOSHO_RANRYU"/>, 2 <ActionLink action="RAITON"/> casts, and <ActionLink action="DREAM_WITHIN_A_DREAM"/> in order to maximize damage.
 			</Trans>,
 			suggestionWindowName,
 			severityTiers: {
@@ -110,12 +110,12 @@ export class TrickAttackWindow extends BuffWindow {
 			expectedActions: [
 				{
 					action: this.data.actions.ARMOR_CRUSH,
-					expectedPerWindow: 0,
+					expectedPerWindow: 0, // TODO - Implement an adjustment for this to allow for 1 AC in the opener (since you start with 0 Kazematoi stacks)
 				},
 			],
 			suggestionIcon: this.data.actions.ARMOR_CRUSH.icon,
-			suggestionContent: <Trans id="nin.taWindow.suggestions.badtrackedactions.content">
-				Avoid using <ActionLink action="ARMOR_CRUSH"/> under <ActionLink action="TRICK_ATTACK"/> unless <ActionLink action="HUTON"/> is about to fall off or you can only hit the flank positional, as <ActionLink action="AEOLIAN_EDGE"/> is otherwise a higher potency finisher.
+			suggestionContent: <Trans id="nin.kb-window.suggestions.badtrackedactions.content">
+				Avoid using <ActionLink action="ARMOR_CRUSH"/> under <ActionLink action="KUNAIS_BANE"/> unless you have no stacks of Kazematoi or you can only hit the flank positional, as <ActionLink action="AEOLIAN_EDGE"/> is otherwise a higher potency finisher when buffed.
 			</Trans>,
 			suggestionWindowName,
 			severityTiers: {
@@ -131,10 +131,10 @@ export class TrickAttackWindow extends BuffWindow {
 	}
 
 	private adjustExpectedRaitonCount(window: HistoryEntry<EvaluatedAction[]>, action: TrackedAction) {
-		const bufferedWindowStart = window.start - FIRST_TRICK_BUFFER
+		const bufferedWindowStart = window.start - FIRST_KUNAIS_BANE_BUFFER
 
 		if (action.action.id === this.data.actions.RAITON.id) {
-			// We push one Raiton outside the Trick window in the opener to fit an extra Bhava
+			// We push one Raiton outside the Kunai's Bane window in the opener to fit an extra Bhava
 			if (bufferedWindowStart <= this.parser.pull.timestamp) {
 				return -1
 			}
