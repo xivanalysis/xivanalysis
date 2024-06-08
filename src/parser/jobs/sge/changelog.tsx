be212d38fd20ed9146c3878803f4be00e6ae248b
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-06-08 16:12:35 -0500

    EProg2 is affected by Zoe, so keep track of it

--- src/parser/jobs/sge/changelog.tsx
+++ src/parser/jobs/sge/changelog.tsx
@@ -8,6 +8,11 @@ export const changelog = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2024-07-09'),
+		Changes: () => <>Add Eukrasian Prognosis II to list of heals affected by Zoe</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
+	{
 		date: new Date('2024-06-27'),
 		Changes: () => <>Initial data scaffolding for Dawntrail</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
