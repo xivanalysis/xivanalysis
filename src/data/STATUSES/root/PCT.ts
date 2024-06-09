54e55ce685043416d99774b41c35b1a2bfa829c3
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-06-08 20:41:17 -0500

    Action and Status data scaffolding for PCT

---
+++ src/data/STATUSES/root/PCT.ts
@@ -0,0 +1,91 @@
+import {ensureStatuses} from '../type'
+
+// TODO: Fill in the rest of this
+export const PCT = ensureStatuses({
+	AETHERHUES: {
+		id: 0,
+		name: 'Aetherhues',
+		icon: 'TODO',
+		duration: 30000,
+	},
+	AETHERHUES_II: {
+		id: 1,
+		name: 'Aetherhues II',
+		icon: 'TODO',
+		duration: 30000,
+	},
+	TEMPURA_COAT: {
+		id: 2,
+		name: 'Tempura Coat',
+		icon: 'TODO',
+		duration: 10000,
+	},
+	TEMPURA_GRASSA: {
+		id: 2,
+		name: 'Tempura Grassa',
+		icon: 'TODO',
+		duration: 10000,
+	},
+	SMUDGE: {
+		id: 3,
+		name: 'Smudge',
+		icon: 'TODO',
+		duration: 5000,
+	},
+	HAMMER_TIME: {
+		id: 4,
+		name: 'Hammer Time',
+		icon: 'TODO',
+		duration: 30000,
+		stacksApplied: 3,
+	},
+	SUBTRACTIVE_PALLETTE: {
+		id: 5,
+		name: 'Subtractive Pallette',
+		icon: 'TODO',
+		duration: 30000,
+		stacksApplied: 3,
+	},
+	MONOCHROME_TONES: {
+		id: 5,
+		name: 'Monochrome Tones',
+		icon: 'TODO',
+	},
+	STARRY_MUSE: {
+		id: 5,
+		name: 'Starry Muse',
+		icon: 'TODO',
+		duration: 20000,
+	},
+	SUBTRACTIVE_SPECTRUM: {
+		id: 5,
+		name: 'Subtractive Spectrum',
+		icon: 'TODO',
+		duration: 30000,
+	},
+	INSPIRATION: {
+		id: 5,
+		name: 'Inspiration',
+		icon: 'TODO',
+		duration: 30000,
+	},
+	HYPERPHANTASIA: {
+		id: 5,
+		name: 'Hyperphantasia',
+		icon: 'TODO',
+		duration: 30000,
+		stacksApplied: 5,
+	},
+	STARSTRUCK: {
+		id: 5,
+		name: 'Starstruck',
+		icon: 'TODO',
+		duration: 20000,
+	},
+	RAINBOWW_BRIGHT: {
+		id: 5,
+		name: 'Rainbow Bright',
+		icon: 'TODO',
+		duration: 20000,
+	},
+})
