008e261276feb2c32b254ea26bbe6e87fdfda152
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-06-09 15:50:25 -0500

    Core modules for PCT

--- src/parser/jobs/pct/modules/index.ts
+++ src/parser/jobs/pct/modules/index.ts
@@ -1,3 +1,11 @@
-export default [
+import {Interrupts} from 'parser/core/modules/Interrupts'
+import {Swiftcast} from 'parser/core/modules/Swiftcast'
+import {Tincture} from 'parser/core/modules/Tincture'
+import {Weaving} from 'parser/core/modules/Weaving'
 
+export default [
+	Interrupts,
+	Swiftcast,
+	Tincture,
+	Weaving,
 ]
