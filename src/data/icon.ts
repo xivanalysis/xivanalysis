b60819f8708b69eb6a0e57da4d3b01350eefcf00
ackwell <saxon@ackwell.com.au>
2024-06-25 23:12:30 +1000

    Use old xivapi for icon urls

--- src/data/icon.ts
+++ src/data/icon.ts
@@ -1,5 +1,4 @@
 export function iconUrl(icon: number): string {
 	const group = Math.floor(icon / 1000) * 1000
-	const gamePath = `ui/icon/${group.toString(10).padStart(6, '0')}/${icon.toString(10).padStart(6, '0')}_hr1.tex`
-	return `https://beta.xivapi.com/api/1/asset/${gamePath}?format=png`
+	return `https://xivapi.com/i/${group.toString(10).padStart(6, '0')}/${icon.toString(10).padStart(6, '0')}.png`
 }
