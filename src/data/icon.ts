a5371bb565b001cfa091f935beadcbf5518eab0a
ackwell <saxon@ackwell.com.au>
2024-06-27 20:24:12 +1000

    Specify version for icon urls

--- src/data/icon.ts
+++ src/data/icon.ts
@@ -1,5 +1,5 @@
 export function iconUrl(icon: number): string {
 	const group = Math.floor(icon / 1000) * 1000
 	const gamePath = `ui/icon/${group.toString(10).padStart(6, '0')}/${icon.toString(10).padStart(6, '0')}_hr1.tex`
-	return `https://beta.xivapi.com/api/1/asset/${gamePath}?format=png`
+	return `https://beta.xivapi.com/api/1/asset/${gamePath}?format=png&version=6.58x2`
 }
