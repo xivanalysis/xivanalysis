758e41ae7d1ee3e7fb343f4c1e2e420fbd7c0a03
yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>
2024-06-30 16:33:33 -0400

    add contributor info and changelog entry and increment patch support

--- src/data/CONTRIBUTORS.ts
+++ src/data/CONTRIBUTORS.ts
@@ -1,7 +1,7 @@
 import {MessageDescriptor} from '@lingui/core'
 import {t} from '@lingui/macro'
 import {ensureRecord} from 'utilities'
-import {Job} from './JOBS'
+import {JOBS, Job} from './JOBS'
 
 export interface Contributor {
 	name: string
@@ -17,6 +17,13 @@ const CONTRIBUTORS = ensureRecord<Contributor>()({
 	// 		JOBS.SOME_JOB,
 	// 	],
 	// },
+	HINT: {
+		name: 'Hint',
+		avatar: require('./avatar/hint.jpg'),
+		jobs: [
+			JOBS.MACHINIST,
+		],
+	},
 })
 
 export default CONTRIBUTORS
