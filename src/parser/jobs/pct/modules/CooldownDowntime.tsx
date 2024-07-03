e9cce59e40ef360c16cd9ba24a653f54e31930d1
Akairyu Gestalter <AkairyuGestalter@users.noreply.github.com>
2024-07-03 11:51:53 -0400

    add cddt to index and fix up some issues

--- src/parser/jobs/pct/modules/CooldownDowntime.tsx
+++ src/parser/jobs/pct/modules/CooldownDowntime.tsx
@@ -1,35 +1,26 @@
 import {CooldownDowntime as CoreCooldownDowntime} from 'parser/core/modules/CooldownDowntime'
 
-// Subtractive Palette not tracked here because it has a gauge cost associated with it
+// Subtractive Palette, Mog, and Madeeon not tracked here because they have gauge requirements to use as well
 // TODO: First use offsets?
 
 export class CooldownDowntime extends CoreCooldownDowntime {
 	trackedCds = [
 		{
 			cooldowns: [
-				this.data.actions.CREATURE_MOTIF,
-				this.data.actions.POM_MOTIF,
-				this.data.actions.WING_MOTIF,
-				this.data.actions.CLAW_MOTIF,
-				this.data.actions.MAW_MOTIF,
+				this.data.actions.POM_MUSE,
+				this.data.actions.WINGED_MUSE,
+				this.data.actions.CLAWED_MUSE,
+				this.data.actions.FANGED_MUSE,
 			],
 		},
 		{
 			cooldowns: [
-				this.data.actions.WEAPON_MOTIF,
-				this.data.actions.STEEL_MUSE,
+				this.data.actions.STRIKING_MUSE,
 			],
 		},
 		{
 			cooldowns: [
-				this.data.actions.LANDSCAPE_MOTIF,
-				this.data.actions.STARRY_SKY_MOTIF,
-			],
-		},
-		{
-			cooldowns: [
-				this.data.actions.MOG_OF_THE_AGES,
-				this.data.actions.RETRIBUTION_OF_THE_MADEEN,
+				this.data.actions.STARRY_MUSE,
 			],
 		},
 	]
--- src/parser/jobs/pct/modules/index.ts
+++ src/parser/jobs/pct/modules/index.ts
@@ -1,8 +1,10 @@
 import {Interrupts} from 'parser/core/modules/Interrupts'
 import {Tincture} from 'parser/core/modules/Tincture'
+import {CooldownDowntime} from './CooldownDowntime'
 import {Swiftcast} from './Swiftcast'
 
 export default [
+	CooldownDowntime,
 	Interrupts,
 	Swiftcast,
 	Tincture,
