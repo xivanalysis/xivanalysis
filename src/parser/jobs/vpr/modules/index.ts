8c2293d993191f8d0b4d70f5b0ca09c5ed8970a6
Rjb99 <Rjb99coding@gmail.com>
2024-07-02 22:53:25 -0400

    add swiftscaled speed mod

--- src/parser/jobs/vpr/modules/index.ts
+++ src/parser/jobs/vpr/modules/index.ts
@@ -2,10 +2,12 @@ import {Buffs} from './Buffs'
 import {NoxiousGnash} from './NoxiousGnash'
 import OGCDDowntime from './OGCDDowntime'
 import {Positionals} from './Positionals'
+import {Swiftscaled} from './Swiftscaled'
 
 export default [
 	Buffs,
 	Positionals,
 	NoxiousGnash,
 	OGCDDowntime,
+	Swiftscaled,
 ]
