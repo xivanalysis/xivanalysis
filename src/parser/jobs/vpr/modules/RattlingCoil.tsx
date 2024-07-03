d56fa0966674c422af92ac068d236ea49f3d2d4e
Rjb99 <Rjb99coding@gmail.com>
2024-07-02 23:25:41 -0400

    Add Rattling Coil Gauge Module

---
+++ src/parser/jobs/vpr/modules/RattlingCoil.tsx
@@ -0,0 +1,110 @@
+import {t} from '@lingui/macro'
+import {Trans, Plural} from '@lingui/react'
+import {ActionLink} from 'components/ui/DbLink'
+import {JOBS} from 'data/JOBS'
+import {Event, Events} from 'event'
+import {filter, oneOf} from 'parser/core/filter'
+import {dependency} from 'parser/core/Injectable'
+import Checklist, {Requirement, Rule} from 'parser/core/modules/Checklist'
+import {CounterGauge, Gauge as CoreGauge} from 'parser/core/modules/Gauge'
+import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
+import React from 'react'
+
+type GaugeModifier = Partial<Record<Event['type'], number>>
+
+const LEFTOVER_COIL_SEVERITY_TIERS = {
+	1: SEVERITY.MINOR,
+	2: SEVERITY.MEDIUM,
+}
+
+const MAX_COILS = 3
+
+export class RattlingCoil extends CoreGauge {
+	static override handle = 'rattlingcoil'
+	static override title = t('vpr.rattlingcoil.title')`Rattling Coil Timeline`
+
+	@dependency private suggestions!: Suggestions
+	@dependency private checklist!: Checklist
+
+	private coilGauge = this.add(new CounterGauge({
+		maximum: MAX_COILS,
+		graph: {
+			handle: 'coils',
+			label: <Trans id="vpr.gauge.resource.coilsLabel">Rattling Coils</Trans>,
+			color: JOBS.VIPER.colour,
+			forceCollapsed: true,
+		},
+	}))
+
+	// Used for Checklist as CounterGauge seems to lack total tracking at this time.
+	private totalGeneratedCoils = 0
+
+	private CoilModifiers = new Map<number, GaugeModifier>([
+
+		[this.data.actions.DREADWINDER.id, {action: 1}],
+		[this.data.actions.PIT_OF_DREAD.id, {action: 1}],
+		[this.data.actions.SERPENTS_IRE.id, {action: 1}],
+
+		[this.data.actions.UNCOILED_FURY.id, {action: -1}],
+	])
+
+	override initialise() {
+		super.initialise()
+
+		const coilActions = Array.from(this.CoilModifiers.keys())
+
+		this.addEventHook(
+			filter<Event>()
+				.source(this.parser.actor.id)
+				.type(oneOf(['action', 'combo']))
+				.action(oneOf(coilActions)),
+			this.onGaugeModifier,
+		)
+
+		this.addEventHook('complete', this.onComplete)
+	}
+
+	private onGaugeModifier(event: Events['action' | 'combo']) {
+		const modifier = this.CoilModifiers.get(event.action)
+
+		if (modifier != null) {
+			const amount = modifier[event.type] ?? 0
+
+			if (amount > 0) {
+				this.totalGeneratedCoils += amount //Increment total tracker for generated ammo
+			}
+			this.coilGauge.modify(amount)
+		}
+	}
+
+	private onComplete() {
+		this.suggestions.add(new TieredSuggestion({
+			icon: this.data.actions.UNCOILED_FURY.icon,
+			content: <Trans id="vpr.rattlingcoil.leftover-coil.content">
+				Avoid having leftover rattling coils at the end of a fight, consider using <ActionLink action="UNCOILED_FURY"/> earlier if possible.
+			</Trans>,
+			why: <Trans id="vpr.rattlingcoil.leftover-coil.why">
+				You had <Plural value={this.coilGauge.value} one="# rattling coil" other="# rattling coils"/> remaining at the end of the fight.
+			</Trans>,
+			tiers: LEFTOVER_COIL_SEVERITY_TIERS,
+			value: this.coilGauge.value,
+		}))
+
+		this.checklist.add(new Rule({
+			name: <Trans id="vpr.rattlingcoil.usage.title">Cartridge usage</Trans>,
+			description: <Trans id="vpr.rattlingcoilwaste.content">
+				Wasted rattling coil generation, ending the fight with rattling coils remaining, or dying with rattling coils coiled is a
+				direct potency loss. Use <ActionLink action="UNCOILED_FURY"/> to avoid wasting rattling coils.
+			</Trans>,
+			requirements: [
+				new Requirement({
+					name: <Trans id="vpr.rattlingcoil.checklist.requirement.waste.name">
+						Use as many of your rattling coils as possible
+					</Trans>,
+					value: this.totalGeneratedCoils - this.coilGauge.overCap,
+					target: this.totalGeneratedCoils,
+				}),
+			],
+		}))
+	}
+}
--- src/parser/jobs/vpr/modules/index.ts
+++ src/parser/jobs/vpr/modules/index.ts
@@ -2,6 +2,7 @@ import {Buffs} from './Buffs'
 import {NoxiousGnash} from './NoxiousGnash'
 import OGCDDowntime from './OGCDDowntime'
 import {Positionals} from './Positionals'
+import {RattlingCoil} from './RattlingCoil'
 import {Snaps} from './Snaps'
 import {Swiftscaled} from './Swiftscaled'
 
@@ -10,6 +11,7 @@ export default [
 	Positionals,
 	NoxiousGnash,
 	OGCDDowntime,
+	RattlingCoil,
 	Swiftscaled,
 	Snaps,
 ]
