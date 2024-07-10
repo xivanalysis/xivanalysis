b1c991435ee3d1cf892afc6ab5f3a590adf90da2
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-07-10 19:14:51 -0400

    Dawntrail - PCT Defensives (#1977)
    * Action and Status data scaffolding for PCT
    
    * Track Tempura Coat, and handle cooldown refunding
    
    * Spelling .-.
    
    * Add defensives override to index
    
    * Data bugfix for Starry Muse
    
    * changelog

--- src/parser/jobs/pct/changelog.tsx
+++ src/parser/jobs/pct/changelog.tsx
@@ -1,3 +1,4 @@
+import {DataLink} from 'components/ui/DbLink'
 import CONTRIBUTORS from 'data/CONTRIBUTORS'
 import React from 'react'
 
@@ -7,6 +8,11 @@ export const changelog = [
 	// 	Changes: () => <>The changes you made</>,
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
+	{
+		date: new Date('2024-07-07'),
+		Changes: () => <>Add <DataLink showIcon={false} action="TEMPERA_GRASSA" /> to Defensives tracking and handle cooldown refunding on shield break from it and <DataLink showIcon={false} action="TEMPERA_COAT" />.</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
 	{
 		date: new Date('2024-07-03'),
 		Changes: () => <>Initial data and core module support for PCT</>,
