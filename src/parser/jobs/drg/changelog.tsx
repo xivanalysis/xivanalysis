8155ae2d44aad2dcddf3e9a7616b1fca1b655216
Evan Shimizu <ebshimizu@gmail.com>
2024-07-11 21:34:40 -0400

    DRG 7.0 support (#1986)

--- src/parser/jobs/drg/changelog.tsx
+++ src/parser/jobs/drg/changelog.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2024-07-09'),
+		Changes: () => <>DRG marked as supported for Patch 7.0.</>,
+		contributors: [CONTRIBUTORS.FALINDRITH],
+	},
+	{
 		date: new Date('2024-06-28'),
 		Changes: () => <>Initial DRG support for Dawntrail.</>,
 		contributors: [CONTRIBUTORS.FALINDRITH],
