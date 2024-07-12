143f389dc9d6b5758e72c0c6a2bc33819b55ded9
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-07-11 20:04:05 -0500

    Add to index

--- src/parser/jobs/pct/modules/index.ts
+++ src/parser/jobs/pct/modules/index.ts
@@ -1,10 +1,12 @@
 import {Interrupts} from 'parser/core/modules/Interrupts'
 import {Tincture} from 'parser/core/modules/Tincture'
+import {AoEUsages} from './AoEUsages'
 import {CooldownDowntime} from './CooldownDowntime'
 import {Defensives} from './Defensives'
 import {Swiftcast} from './Swiftcast'
 
 export default [
+	AoEUsages,
 	CooldownDowntime,
 	Defensives,
 	Interrupts,
