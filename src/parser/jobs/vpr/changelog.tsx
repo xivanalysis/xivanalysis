929cebd2db1ed666fc0008ab7ea11f9231e96d07
yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>
2024-07-11 21:02:15 -0400

    changelog

--- src/parser/jobs/vpr/changelog.tsx
+++ src/parser/jobs/vpr/changelog.tsx
@@ -1,7 +1,13 @@
+import {DataLink} from 'components/ui/DbLink'
 import CONTRIBUTORS from 'data/CONTRIBUTORS'
 import React from 'react'
 
 export const changelog = [
+	{
+		date: new Date('2024-07-11'),
+		Changes: () => <>Organize the timeline into groupings for each <DataLink action="DREADWINDER" />, <DataLink action="UNCOILED_FURY" />, and <DataLink action="REAWAKEN" /> combo</>,
+		contributors: [CONTRIBUTORS.HINT],
+	},
 	{
 		date: new Date('2024-07-04'),
 		Changes: () => <>Initial 7.0 Viper Support</>,
