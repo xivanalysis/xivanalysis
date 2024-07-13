a0b83cc27f68d2f9d599d453369ee2b0b6e4b376
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-07-13 09:06:30 -0400

    Dawntrail - PCT AoEUsage tracking (#1990)
    * AoE checking for PCT
    
    * Update for launch potencies
    
    * lol, icon
    
    * Add to index
    
    * Changelog

--- src/parser/jobs/pct/changelog.tsx
+++ src/parser/jobs/pct/changelog.tsx
@@ -9,6 +9,11 @@ export const changelog = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2024-07-11'),
+		Changes: () => <>Add checks for using AoE spells with too few targets</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
+	{
 		date: new Date('2024-07-07'),
 		Changes: () => <>Add <DataLink showIcon={false} action="TEMPERA_GRASSA" /> to Defensives tracking and handle cooldown refunding on shield break from it and <DataLink showIcon={false} action="TEMPERA_COAT" />.</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
