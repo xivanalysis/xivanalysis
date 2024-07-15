d80be64cee1ae7845a7331b87fec4aec1fa0ff6d
otocephaly <89435408+otocephaly@users.noreply.github.com>
2024-07-14 20:46:21 -0600

    [7.0] AST reorder defensives to match timeline
    reordered some defensives to match timeline

--- src/parser/jobs/ast/modules/Defensives.tsx
+++ src/parser/jobs/ast/modules/Defensives.tsx
@@ -1,12 +1,16 @@
 import {Defensives as CoreDefensives} from 'parser/core/modules/Defensives'
+import DISPLAY_ORDER from './DISPLAY_ORDER'
 
 export class Defensives extends CoreDefensives {
+	static override displayOrder = DISPLAY_ORDER.DEFENSIVES
 	protected override trackedDefensives = [
-		this.data.actions.EXALTATION,
 		this.data.actions.CELESTIAL_INTERSECTION,
 		this.data.actions.CELESTIAL_OPPOSITION,
-		this.data.actions.COLLECTIVE_UNCONSCIOUS,
 		this.data.actions.EARTHLY_STAR,
 		this.data.actions.MACROCOSMOS,
+		this.data.actions.EXALTATION,
+		this.data.actions.HOROSCOPE,
+		this.data.actions.NEUTRAL_SECT,
+		this.data.actions.COLLECTIVE_UNCONSCIOUS,
 	]
 }
