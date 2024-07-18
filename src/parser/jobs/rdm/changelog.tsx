03f99c6c14ee551c672386c4a974cd45a04245fd
Kaiser12@gmail.com <Kaiser12@gmail.com>
2024-07-17 19:26:21 -0500

    patch support and change log

--- src/parser/jobs/rdm/changelog.tsx
+++ src/parser/jobs/rdm/changelog.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2024-07-17'),
+		Changes: () => <>Fixed issue with Manafication not breaking melee combos</>,
+		contributors: [CONTRIBUTORS.LEYLIA],
+	},
+	{
 		date: new Date('2024-07-07'),
 		Changes: () => <>Initial Support and Updates for DawnTrail</>,
 		contributors: [CONTRIBUTORS.LEYLIA],
