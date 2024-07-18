056f1c80461ba1b46838a6ed40a524865b70152a
Rjb99 <Rjb99coding@gmail.com>
2024-07-18 00:42:56 -0400

    update buff module

--- src/parser/jobs/rpr/modules/ArcaneCircle.tsx
+++ src/parser/jobs/rpr/modules/ArcaneCircle.tsx
@@ -1,7 +1,8 @@
 import {t} from '@lingui/macro'
 import {Trans} from '@lingui/react'
 import {ActionLink} from 'components/ui/DbLink'
-import {RaidBuffWindow, EvaluatedAction, ExpectedActionsEvaluator, TrackedAction} from 'parser/core/modules/ActionWindow'
+import {RaidBuffWindow, EvaluatedAction, TrackedActionGroup, ExpectedActionGroupsEvaluator} from 'parser/core/modules/ActionWindow'
+import {DisplayedActionEvaluator} from 'parser/core/modules/ActionWindow/evaluators/DisplayedActionEvaluator'
 import {HistoryEntry} from 'parser/core/modules/ActionWindow/History'
 import {SEVERITY} from 'parser/core/modules/Suggestions'
 import React from 'react'
@@ -17,6 +18,15 @@ const SEVERITIES = {
 	5: SEVERITY.MAJOR,
 }
 
+const REAPINGS_PER_BURST = 7
+const LEMURES_PER_BURST = 4
+const SACRIFICIUM_PER_BURST = 2
+const HARVEST_PER_BURST = 1
+const COMMUNIO_PER_BURST = 2
+
+const INVERTOR = -1 // Magic numbers strikes again!
+const OPENER_FEE = -0.5 // Opener costs half a burst
+
 export class ArcaneCircle extends RaidBuffWindow {
 	static override handle = 'arcaneCircle'
 	static override title = t('rpr.arcanecircle.title')`Arcane Circle`
@@ -27,15 +37,45 @@ export class ArcaneCircle extends RaidBuffWindow {
 	override initialise() {
 		super.initialise()
 
-		this.addEvaluator(new ExpectedActionsEvaluator({
-			expectedActions: [
+		this.addEvaluator(new ExpectedActionGroupsEvaluator({
+			expectedActionGroups: [
 				{
-					action: this.data.actions.COMMUNIO,
-					expectedPerWindow: 2,
+					actions: [
+						this.data.actions.CROSS_REAPING,
+						this.data.actions.VOID_REAPING,
+						this.data.actions.GRIM_REAPING,
+					],
+					expectedPerWindow: REAPINGS_PER_BURST,
+
 				},
 				{
-					action: this.data.actions.PLENTIFUL_HARVEST,
-					expectedPerWindow: 1,
+					actions: [
+						this.data.actions.LEMURES_SLICE,
+						this.data.actions.LEMURES_SCYTHE,
+					],
+					expectedPerWindow: LEMURES_PER_BURST,
+
+				},
+				{
+					actions: [
+						this.data.actions.SACRIFICIUM,
+					],
+					expectedPerWindow: SACRIFICIUM_PER_BURST,
+
+				},
+				{
+					actions: [
+						this.data.actions.PLENTIFUL_HARVEST,
+					],
+					expectedPerWindow: HARVEST_PER_BURST,
+
+				},
+				{
+					actions: [
+						this.data.actions.COMMUNIO,
+					],
+					expectedPerWindow: COMMUNIO_PER_BURST,
+
 				},
 			],
 			suggestionIcon: this.data.actions.ARCANE_CIRCLE.icon,
@@ -47,36 +87,103 @@ export class ArcaneCircle extends RaidBuffWindow {
 			severityTiers: SEVERITIES,
 			adjustCount: this.adjustCount.bind(this),
 		}))
+
+		this.addEvaluator(new DisplayedActionEvaluator([this.data.actions.PERFECTIO]))
 	}
 
-	private adjustCount(window: HistoryEntry<EvaluatedAction[]>, action: TrackedAction): number {
+	private adjustCount(window: HistoryEntry<EvaluatedAction[]>, action: TrackedActionGroup) {
 		const fightTimeRemaining = (this.parser.pull.timestamp + this.parser.pull.duration) - window.start
 
 		if (this.isRushedEndOfPullWindow(window)) {
 			if (fightTimeRemaining < COMMUNIO_BUFFER) {
-				switch (action.action) {
-				case this.data.actions.COMMUNIO:
-					// I hate this linter bypass, but -2 is technically magic and is weird as a const
-					// eslint-disable-next-line @typescript-eslint/no-magic-numbers
-					return -2
+				for (const skill of action.actions) {
+					switch (skill) {
 
-				case this.data.actions.PLENTIFUL_HARVEST:
-					return -1
+					case this.data.actions.CROSS_REAPING:
+					case this.data.actions.VOID_REAPING:
+					case this.data.actions.GRIM_REAPING:
+						return (INVERTOR * REAPINGS_PER_BURST)
+
+					case this.data.actions.LEMURES_SLICE:
+					case this.data.actions.LEMURES_SCYTHE:
+						return (INVERTOR * LEMURES_PER_BURST)
+
+					case this.data.actions.SACRIFICIUM:
+						return (INVERTOR * SACRIFICIUM_PER_BURST)
+
+					case this.data.actions.PLENTIFUL_HARVEST:
+						return (INVERTOR * HARVEST_PER_BURST)
+
+					case this.data.actions.COMMUNIO:
+						return (INVERTOR * COMMUNIO_PER_BURST)
+
+					default:
+						return 0
+					}
+				}
+
+				// We have enough time for 1 Communio, but not both
+				for (const skill of action.actions) {
+					switch (skill) {
+
+					case this.data.actions.CROSS_REAPING:
+					case this.data.actions.VOID_REAPING:
+					case this.data.actions.GRIM_REAPING:
+						return (OPENER_FEE * REAPINGS_PER_BURST)
+
+					case this.data.actions.LEMURES_SLICE:
+					case this.data.actions.LEMURES_SCYTHE:
+						return (INVERTOR * LEMURES_PER_BURST)
+
+					case this.data.actions.SACRIFICIUM:
+						return (INVERTOR * SACRIFICIUM_PER_BURST)
+
+					case this.data.actions.PLENTIFUL_HARVEST:
+						return (INVERTOR * HARVEST_PER_BURST)
+
+					case this.data.actions.COMMUNIO:
+						return (OPENER_FEE * COMMUNIO_PER_BURST)
+
+					default:
+						return 0
+					}
+				}
+			}
+
+			// If it's not Communio, we don't care at this point
+			for (const skill of action.actions) {
+				if (skill !== this.data.actions.COMMUNIO) { return 0 }
+			}
+
+		}
+
+		//If opener, everything but harvest is cut in half
+		if (window.start - OPENER_BUFFER <= this.parser.pull.timestamp) {
+
+			for (const skill of action.actions) {
+				switch (skill) {
+
+				case this.data.actions.CROSS_REAPING:
+				case this.data.actions.VOID_REAPING:
+				case this.data.actions.GRIM_REAPING:
+					return Math.ceil((OPENER_FEE * REAPINGS_PER_BURST))
+
+				case this.data.actions.LEMURES_SLICE:
+				case this.data.actions.LEMURES_SCYTHE:
+					return Math.ceil(OPENER_FEE * LEMURES_PER_BURST)
+
+				case this.data.actions.SACRIFICIUM:
+					return Math.ceil(OPENER_FEE * SACRIFICIUM_PER_BURST)
+
+				case this.data.actions.COMMUNIO:
+					return Math.ceil(OPENER_FEE * COMMUNIO_PER_BURST)
 
 				default:
 					return 0
 				}
 			}
-
-			// We have enough time for 1 Communio, but not both
-			if (action.action === this.data.actions.COMMUNIO) {
-				return -1
-			}
 		}
 
-		// If it's not Communio, we don't care at this point
-		if (action.action !== this.data.actions.COMMUNIO) { return 0 }
-
-		return (window.start - OPENER_BUFFER <= this.parser.pull.timestamp) ? -1 : 0
+		return 0
 	}
 }
