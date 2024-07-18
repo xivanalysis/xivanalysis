77f86be289e44cca5c2e1dd75f03070f8af6a039
B Fraser <fraserbn@gmail.com>
2024-07-19 01:56:41 +0200

    RDM: Grand Impact is a GCD, and it breaks the combo (#2008)
    * RDM: Grand Impact is a GCD, and it breaks the combo
    
    * RDM: changelog entry for Grand Impact being a GCD
    
    * Add myself back as a contributor.  Hopefully git is smart enough to recognize that this file is already in the repo history
    
    ---------
    
    Co-authored-by: Azariah Aeroborn <26804378+AzariahAeroborn@users.noreply.github.com>

--- src/parser/jobs/rdm/changelog.tsx
+++ src/parser/jobs/rdm/changelog.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2024-07-14'),
+		Changes: () => <>Mark Grand Impact as a GCD</>,
+		contributors: [CONTRIBUTORS.HUGMEIR],
+	},
+	{
 		date: new Date('2024-07-07'),
 		Changes: () => <>Initial Support and Updates for DawnTrail</>,
 		contributors: [CONTRIBUTORS.LEYLIA],
