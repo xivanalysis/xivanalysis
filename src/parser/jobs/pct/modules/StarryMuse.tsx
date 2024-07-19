6e6e896e7ebbb0ad04ca1aa9ca592fa886fb0ff5
Akairyu Gestalter <AkairyuGestalter@users.noreply.github.com>
2024-07-19 14:59:47 -0500

    Add suggestion against using additive spells, plus some cleanup

--- src/parser/jobs/pct/modules/StarryMuse.tsx
+++ src/parser/jobs/pct/modules/StarryMuse.tsx
@@ -3,12 +3,12 @@ import {Trans} from '@lingui/react'
 import {DataLink} from 'components/ui/DbLink'
 import {Action} from 'data/ACTIONS'
 import {Status} from 'data/STATUSES'
-import {RaidBuffWindow, ExpectedActionGroupsEvaluator, EvaluatedAction, TrackedActionGroup} from 'parser/core/modules/ActionWindow'
+import {RaidBuffWindow, ExpectedActionGroupsEvaluator, EvaluatedAction, TrackedActionGroup, LimitedActionsEvaluator, TrackedAction} from 'parser/core/modules/ActionWindow'
 import {HistoryEntry} from 'parser/core/modules/ActionWindow/History'
 import {SEVERITY} from 'parser/core/modules/Suggestions'
 import React from 'react'
 import {Message} from 'semantic-ui-react'
-import {CREATURE_MUSES, SUBTRACTIVE_SINGLE_TARGET} from './CommonData'
+import {ADDITIVE_SPELLS, CREATURE_MUSES, SUBTRACTIVE_SINGLE_TARGET} from './CommonData'
 
 export class StarryMuse extends RaidBuffWindow {
 	static override handle = 'starrymuse'
@@ -19,11 +19,12 @@ export class StarryMuse extends RaidBuffWindow {
 	private museActions = CREATURE_MUSES.map(key => this.data.actions[key])
 	private subtractiveActions = SUBTRACTIVE_SINGLE_TARGET.map(key => this.data.actions[key])
 
-	override prependMessages = <Message><Trans id="pct.starrymuse.table-header">
-		Your <DataLink status="STARRY_MUSE" /> windows should contain your full <DataLink status="HAMMER_TIME" /> combo, <DataLink action="STAR_PRISM" />, <DataLink action="RAINBOW_DRIP" />, two <DataLink action="COMET_IN_BLACK" /> (one in the opener), and fill the remainder with your <DataLink status="SUBTRACTIVE_PALLETTE" /> spells.<br/>
-		If you are doing a triple Muse burst for alignment, you will push some of your <DataLink showIcon={false} status="SUBTRACTIVE_PALLETTE" /> spells and <DataLink showIcon={false} action="RAINBOW_DRIP" /> out of the window.<br/>
-		Try to make sure you use all of the expected actions in each window as seen below.
-	</Trans>
+	override prependMessages = <Message>
+		<Trans id="pct.starrymuse.table-header">
+			Your <DataLink status="STARRY_MUSE" /> windows should contain your full <DataLink status="HAMMER_TIME" /> combo, <DataLink action="STAR_PRISM" />, <DataLink action="RAINBOW_DRIP" />, two <DataLink action="COMET_IN_BLACK" /> (one in the opener), and fill the remainder with your <DataLink status="SUBTRACTIVE_PALLETTE" /> spells.<br/>
+			If you are doing a triple Muse burst for alignment, you will push some of your <DataLink showIcon={false} status="SUBTRACTIVE_PALLETTE" /> spells and <DataLink showIcon={false} action="RAINBOW_DRIP" /> out of the window.<br/>
+			Try to make sure you use all of the expected actions in each window as seen below.
+		</Trans>
 	</Message>
 
 	override initialise(): void {
@@ -71,13 +72,30 @@ export class StarryMuse extends RaidBuffWindow {
 			],
 			suggestionIcon: this.data.actions.STARRY_MUSE.icon,
 			suggestionWindowName: <DataLink showIcon={false} action="STARRY_MUSE" />,
-			suggestionContent: <Trans id="pct.starrymuse.suggestions.expectedactions.content">Make sure your <DataLink status="STARRY_MUSE" /> windows contain all of the recommended actions in order to maximize your damage.</Trans>,
+			suggestionContent: <Trans id="pct.starrymuse.suggestions.expected-actions.content">To maximize your damage, you should make sure your <DataLink status="STARRY_MUSE" /> windows contain all of the recommended actions.</Trans>,
 			severityTiers: {
 				1: SEVERITY.MINOR,
 				3: SEVERITY.MEDIUM,
 				5: SEVERITY.MAJOR,
 			},
 			adjustCount: this.adjustExpectedActionGroupCounts.bind(this),
+		}))
+
+		this.addEvaluator(new LimitedActionsEvaluator({
+			expectedActions: ADDITIVE_SPELLS.map<TrackedAction>(key => {
+				return {
+					action: this.data.actions[key],
+					expectedPerWindow: 0,
+				}
+			}),
+			suggestionIcon: this.data.actions.FIRE_IN_RED.icon,
+			suggestionWindowName: <DataLink showIcon={false} action="STARRY_MUSE" />,
+			suggestionContent: <Trans id="pct.starrymuse.suggestions.limited-actions.content">Avoid using the additive paint spells during your <DataLink status="STARRY_MUSE" /> windows. The <DataLink status="SUBTRACTIVE_PALLETTE" /> spells have higher potency, allowing you to do more damage during your burst.</Trans>,
+			severityTiers: {
+				1: SEVERITY.MINOR,
+				3: SEVERITY.MEDIUM,
+				5: SEVERITY.MAJOR,
+			},
 		}))
 	}
 
