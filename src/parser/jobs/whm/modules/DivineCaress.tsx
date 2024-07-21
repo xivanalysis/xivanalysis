c85f24f34e40c97d17fc76049127f68306f56b1e
Inni <inni48@web.de>
2024-07-21 23:09:23 +0200

    Attempt to add debug messages and fix some other messes

--- src/parser/jobs/whm/modules/DivineCaress.tsx
+++ src/parser/jobs/whm/modules/DivineCaress.tsx
@@ -1,13 +1,10 @@
-//import {t} from '@lingui/macro'
 import {Trans} from '@lingui/react'
-import {iconUrl} from 'data/icon'
 import {Procs as CoreProcs} from 'parser/core/modules/Procs'
 import React from 'react'
-//https://www.fflogs.com/reports/tbXrQWDn3fawqz9g#fight=44&type=casts&source=759&translate=true
-const ICON_DIVINE_CARESS = 2128
 
+//https://www.fflogs.com/reports/tbXrQWDn3fawqz9g#fight=44&type=casts&source=759&translate=true
 export class DivineCaress extends CoreProcs {
-	//static override title = t('whm.caress.title')`Divine Caress Misses`
+	static override debug = true;
 
 	//Track Divine Caress as the only status currently
 	override trackedProcs = [
@@ -17,9 +14,10 @@ export class DivineCaress extends CoreProcs {
 		},
 	]
 	override showDroppedProcSuggestion = true;
-	override droppedProcIcon = iconUrl(ICON_DIVINE_CARESS);
+	override droppedProcIcon = this.data.statuses.DIVINE_CARESS.icon;
 	override droppedProcContent =
 		<Trans id="whm.procs.suggestions.dropped-divine-caress.content">
 			Try to use Divine Caress whenever you have used Temperance. It is a free, groupwide oGCD shield and heal over time!
 		</Trans>
+	this.debug(`Drop count for Divine Grace: ${this.getDropCountForStatus(this.data.statuses.DIVINE_GRACE)}`)
 }
