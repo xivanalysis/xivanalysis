8520aaf842c06e008504f98add1a4cd48b7f2ebe
Inni <inni48@web.de>
2024-07-21 23:41:36 +0200

    Fix severity tiers, remove debug, finalize changes

--- src/parser/jobs/whm/modules/DivineCaress.tsx
+++ src/parser/jobs/whm/modules/DivineCaress.tsx
@@ -1,10 +1,9 @@
 import {Trans} from '@lingui/react'
 import {Procs as CoreProcs} from 'parser/core/modules/Procs'
+import {SEVERITY} from 'parser/core/modules/Suggestions'
 import React from 'react'
 
-//https://www.fflogs.com/reports/tbXrQWDn3fawqz9g#fight=44&type=casts&source=759&translate=true
 export class DivineCaress extends CoreProcs {
-	static override debug = true;
 
 	//Track Divine Caress as the only status currently
 	override trackedProcs = [
@@ -14,10 +13,13 @@ export class DivineCaress extends CoreProcs {
 		},
 	]
 	override showDroppedProcSuggestion = true;
-	override droppedProcIcon = this.data.statuses.DIVINE_CARESS.icon;
+	override droppedProcIcon = this.data.actions.DIVINE_CARESS.icon;
 	override droppedProcContent =
 		<Trans id="whm.procs.suggestions.dropped-divine-caress.content">
 			Try to use Divine Caress whenever you have used Temperance. It is a free, groupwide oGCD shield and heal over time!
 		</Trans>
-	this.debug(`Drop count for Divine Grace: ${this.getDropCountForStatus(this.data.statuses.DIVINE_GRACE)}`)
+	override droppedProcSeverityTiers = {
+		1: SEVERITY.MEDIUM,
+		3: SEVERITY.MAJOR,
+	}
 }
