f4e02f26b461a2b2a64edff71b5b6b34764409c6
Ryan Beck <37195907+Rjb99@users.noreply.github.com>
2024-07-21 21:25:39 -0400

    Sam 7.01 patch bump (#2030)

--- src/parser/jobs/sam/changelog.tsx
+++ src/parser/jobs/sam/changelog.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2024-07-21'),
+		Changes: () => <>7.01 support bump, Tendo recasts adjusted </>,
+		contributors: [CONTRIBUTORS.RYAN],
+	},
+	{
 		date: new Date('2024-07-02'),
 		Changes: () => <>Initial 7.0 SAM Support</>,
 		contributors: [CONTRIBUTORS.RYAN],
