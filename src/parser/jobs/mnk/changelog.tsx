acac151f55d46fbfdc05667e8d509438eb6765cc
yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>
2024-07-25 21:39:17 -0400

    add missing blitzes

--- src/parser/jobs/mnk/changelog.tsx
+++ src/parser/jobs/mnk/changelog.tsx
@@ -1,7 +1,13 @@
+import {ActionLink} from 'components/ui/DbLink'
 import CONTRIBUTORS from 'data/CONTRIBUTORS'
 import React from 'react'
 
 export const changelog = [
+	{
+		date: new Date('2024-07-25'),
+		Changes: () => <>Add Nadi tracking for <ActionLink action="ELIXIR_BURST" /> and pre-upgrade Blitz actions</>,
+		contributors: [CONTRIBUTORS.HINT],
+	},
 	{
 		date: new Date('2024-07-04'),
 		Changes: () => <>Initial data scaffolding for Dawntrail</>,
