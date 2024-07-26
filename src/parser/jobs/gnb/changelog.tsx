22a01b84a6a5d3613d1c3d1f84391766c21034cb
Ryan Beck <37195907+Rjb99@users.noreply.github.com>
2024-07-26 09:20:24 -0400

    Update cooldowns for revised openers and add allowed downtime for BF (#2040)

--- src/parser/jobs/gnb/changelog.tsx
+++ src/parser/jobs/gnb/changelog.tsx
@@ -1,7 +1,13 @@
+import {DataLink} from 'components/ui/DbLink'
 import CONTRIBUTORS from 'data/CONTRIBUTORS'
 import React from 'react'
 
 export const changelog = [
+	{
+		date: new Date('2024-07-24'),
+		Changes: () => <> Updated Cooldown offsets for the updated 2.4X and 2.5 Openers and added allowed downtime to <DataLink action="BLOODFEST"/> to allow it to be delayed back under <DataLink action="NO_MERCY"/>.</>,
+		contributors: [CONTRIBUTORS.RYAN],
+	},
 	{
 		date: new Date('2024-07-04'),
 		Changes: () => <>Initial 7.0 GNB Support</>,
