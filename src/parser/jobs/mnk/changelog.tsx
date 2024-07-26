d7889faa16670f6c19998f1f3f12ff5fc159df70
hintxiv <65056303+hintxiv@users.noreply.github.com>
2024-07-26 18:17:52 -0400

    add proc tracking for monk (#2042)
    Co-authored-by: yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>

--- src/parser/jobs/mnk/changelog.tsx
+++ src/parser/jobs/mnk/changelog.tsx
@@ -1,7 +1,13 @@
+import {ActionLink} from 'components/ui/DbLink'
 import CONTRIBUTORS from 'data/CONTRIBUTORS'
 import React from 'react'
 
 export const changelog = [
+	{
+		date: new Date('2024-07-25'),
+		Changes: () => <>Add suggestions for dropped uses of <ActionLink action="FIRES_REPLY" /> and <ActionLink action="WINDS_REPLY" /></>,
+		contributors: [CONTRIBUTORS.HINT],
+	},
 	{
 		date: new Date('2024-07-04'),
 		Changes: () => <>Initial data scaffolding for Dawntrail</>,
