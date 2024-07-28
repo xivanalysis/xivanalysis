79c963c706513992a3ba7ab5921ed80448dd5c62
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2024-07-28 15:44:22 -0500

    yarn extract and changelog update

--- src/parser/jobs/sch/changelog.tsx
+++ src/parser/jobs/sch/changelog.tsx
@@ -8,6 +8,11 @@ export const changelog = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2024-07-28'),
+		Changes: () => <>Initial support for 7.0: Refactor overhealing; overcapping fairy gauge is a always a minor warning now.</>,
+		contributors: [CONTRIBUTORS.NONO],
+	},
+	{
 		date: new Date('2024-07-13'),
 		Changes: () => <>Add Impact Imminent proc tracking</>,
 		contributors: [CONTRIBUTORS.NONO],
