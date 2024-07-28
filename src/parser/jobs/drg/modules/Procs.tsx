b0eb75c85babfdbe21bf5f53b18959dee239bd90
Evan Shimizu <ebshimizu@gmail.com>
2024-07-28 09:57:51 -0700

    enable suggestions for drg procs

--- src/parser/jobs/drg/modules/Procs.tsx
+++ src/parser/jobs/drg/modules/Procs.tsx
@@ -1,6 +1,17 @@
 import {Procs as CoreProcs} from 'parser/core/modules/Procs'
+import {SEVERITY} from 'parser/core/modules/Suggestions'
 
 export default class Procs extends CoreProcs {
+	protected override showDroppedProcSuggestion = true
+	// DRG wants to use every proc, and dropping any of them is a major issue
+	protected override droppedProcSeverityTiers = {
+		1: SEVERITY.MEDIUM,
+		2: SEVERITY.MAJOR,
+	}
+
+	// we'll show invuln stuff too, though some of the drg actions are cleaves so will have to monitor for spurious suggestions
+	protected override showInvulnProcSuggestion = true
+
 	trackedProcs = [
 		{
 			procStatus: this.data.statuses.NASTROND_READY,
