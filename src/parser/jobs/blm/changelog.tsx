9761b21a2b8617caad29c35047110dbb63cc5677
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-08-03 12:17:26 -0500

    Opt BLM into core proc issue display

--- src/parser/jobs/blm/changelog.tsx
+++ src/parser/jobs/blm/changelog.tsx
@@ -9,6 +9,11 @@ export const changelog = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2024-08-03'),
+		Changes: () => <>Add a table displaying timestamps when procs were dropped, for better clarity around when those issues occurred.</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
+	{
 		date: new Date('2024-07-31'),
 		Changes: () => <>Revive support for Umbral Ice <DataLink action="PARADOX" />, and handle the new timer pause functionality from <DataLink action="UMBRAL_SOUL" />.</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
