b2a3bf00fd1bd0695f44cc27713001dfbbedbec2
nikobee <nikki@baileycharm.net>
2024-08-04 10:40:42 +0100

    feat: add tincture module for warrior

---
+++ src/parser/jobs/war/modules/Tincture.tsx
@@ -0,0 +1,59 @@
+import {Trans} from '@lingui/react'
+import {DataLink} from 'components/ui/DbLink'
+import {dependency} from 'parser/core/Injectable'
+import {ExpectedActionsEvaluator, ExpectedGcdCountEvaluator} from 'parser/core/modules/ActionWindow'
+import {GlobalCooldown} from 'parser/core/modules/GlobalCooldown'
+import {SEVERITY} from 'parser/core/modules/Suggestions'
+import {Tincture as CoreTincture} from 'parser/core/modules/Tincture'
+import React from 'react'
+
+export class Tincture extends CoreTincture {
+
+	@dependency private globalCooldown!: GlobalCooldown
+
+	override initialise() {
+		super.initialise()
+
+		const suggestionIcon = this.data.actions.G2_GEMDRAUGHT_STR.icon
+		const suggestionWindowName = <DataLink item="G2_GEMDRAUGHT_STR" />
+		this.addEvaluator(new ExpectedGcdCountEvaluator({
+			expectedGcds: 12,
+			globalCooldown: this.globalCooldown,
+			hasStacks: false,
+			suggestionIcon,
+			suggestionContent: <Trans id="war.tincture.suggestions.missedgcd.content">
+				Try to land 12 GCDs during every <DataLink item="INFUSION_STR" /> window.
+			</Trans>,
+			suggestionWindowName,
+			severityTiers: {
+				1: SEVERITY.MINOR,
+				2: SEVERITY.MEDIUM,
+				3: SEVERITY.MAJOR,
+			},
+		}))
+
+		this.addEvaluator(new ExpectedActionsEvaluator({
+			expectedActions: [
+				{action: this.data.actions.PRIMAL_REND, expectedPerWindow: 1},
+				{action: this.data.actions.PRIMAL_RUINATION, expectedPerWindow: 1},
+				{action: this.data.actions.PRIMAL_WRATH, expectedPerWindow: 1},
+				{action: this.data.actions.INNER_CHAOS, expectedPerWindow: 2},
+				{action: this.data.actions.FELL_CLEAVE, expectedPerWindow: 3},
+				{action: this.data.actions.ONSLAUGHT, expectedPerWindow: 3},
+				{action: this.data.actions.UPHEAVAL, expectedPerWindow: 1},
+			],
+			suggestionIcon,
+			suggestionContent: <Trans id="war.tincture.suggestions.trackedactions.content">
+				Try to land at least one cast of <DataLink action="PRIMAL_REND" />, <DataLink action="PRIMAL_RUINATION" />
+				, <DataLink action="PRIMAL_WRATH" />, <DataLink action="UPHEAVAL" />, three or more casts of
+				<DataLink action="FELL_CLEAVE" /> and <DataLink action="ONSLAUGHT" />, and two or more uses of <DataLink action="INNER_CHAOS" /> during every <DataLink item="G2_GEMDRAUGHT_STR" /> window.
+			</Trans>,
+			suggestionWindowName,
+			severityTiers: {
+				2: SEVERITY.MINOR,
+				4: SEVERITY.MEDIUM,
+				6: SEVERITY.MAJOR,
+			},
+		}))
+	}
+}
