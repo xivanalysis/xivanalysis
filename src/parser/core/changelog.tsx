5c73f5e90856328a9390b621bb9dee4e1b8847be
Saxon Landers <saxon@ackwell.com.au>
2024-08-11 01:59:42 +1000

    Fix ABC calculations for precast and end-of-fight actions (#2088)
    * Add inspiration to module list, fix stacking from repeat gain events
    
    * Changelog
    
    * Fix rainbow bright recast adjustment
    
    * Grammar
    
    * Fix precast and EOF uptime calcs

--- src/parser/core/changelog.tsx
+++ src/parser/core/changelog.tsx
@@ -10,6 +10,11 @@ export const changelog: ChangelogEntry[] = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2024-08-10'),
+		Changes: () => <>Fix uptime calculations for precast and end-of-fight actions.</>,
+		contributors: [CONTRIBUTORS.ACKWELL],
+	},
+	{
 		date: new Date('2024-07-03'),
 		Changes: () => <>Update core support for the Dawntrail expansion</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
