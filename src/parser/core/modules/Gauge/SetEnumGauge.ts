1160706f734fb93db89bfe3346a43c10b66a7fd2
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2024-08-26 22:40:07 -0500

    SetEnumGauge as a modified SetGauge with multiple options per slot
    Bring implementation back closer in line with SetGauge

--- src/parser/core/modules/Gauge/SetEnumGauge.ts
+++ src/parser/core/modules/Gauge/SetEnumGauge.ts
@@ -1,71 +1,76 @@
-import Color from 'color'
 import _ from 'lodash'
-import {ReactNode} from 'react'
-import {GAUGE_HANDLE, ResourceData, ResourceGraphOptions, ResourceGroupOptions} from '../ResourceGraphs/ResourceGraphs'
+import {GAUGE_HANDLE, ResourceGraphOptions, ResourceGroupOptions} from '../ResourceGraphs/ResourceGraphs'
 import {AbstractGauge, AbstractGaugeOptions} from './AbstractGauge'
+import {SetEntryOption, SetGraphOptions, SetResourceData} from './SetGauge'
 
 const FORCE_COLLAPSE = true || process.env.NODE_ENV === 'production'
 
-interface SetHistory {
+interface SetEnumHistory {
 	timestamp: number
-	values: Array<SetEntryOption['value']>
+	groups: SetEnumHistoryEntry[],
 }
 
-export interface SetEntryOption {
-	value: string,
-	color: Color,
-	label: ReactNode
+interface SetEnumHistoryEntry {
+	handle: string,
+	value: SetEntryOption['value']
 }
 
-export interface SetGaugeOptions extends AbstractGaugeOptions {
+export interface SetEnumGroup {
+	handle: string,
 	options: SetEntryOption[],
+}
+
+export interface SetEnumGaugeOptions extends AbstractGaugeOptions {
+	groups: SetEnumGroup[],
 	/** Graph options. Omit to disable graphing in the timeline for this gauge. */
 	graph?: SetGraphOptions
 }
 
-export type SetGraphOptions = Omit<ResourceGraphOptions, 'tooltipHideMaximum'>
-
-export interface SetResourceData extends ResourceData {
-	type: 'discrete'
-	base: number,
-	value: string,
+export interface SetEnumResourceData extends SetResourceData {
+	handle: string,
 }
 
-export class SetGauge extends AbstractGauge {
-	private options: SetEntryOption[]
+export class SetEnumGauge extends AbstractGauge {
+	private groups: SetEnumGroup[]
 
 	private graphOptions?: ResourceGraphOptions
 
-	public history: SetHistory[] = []
+	public history: SetEnumHistory[] = []
 	public overcap = 0
 
-	get current(): Array<SetEntryOption['value']> {
+	get current(): SetEnumHistoryEntry[] {
 		return this.getValuesAt(this.parser.currentEpochTimestamp)
 	}
 
 	get capped(): boolean {
-		return this.current.length === this.options.length
+		return this.current.every(group => group.value != null)
 	}
 
 	get empty(): boolean {
-		return this.current.length === 0
+		return this.current.every(group => group.value == null)
 	}
 
-	constructor(opts: SetGaugeOptions) {
+	private copyCurrent(): SetEnumHistoryEntry[] {
+		const copy: SetEnumHistoryEntry[] = []
+		this.current.forEach(group => copy.push({handle: group.handle, value: group.value}))
+		return copy
+	}
+
+	constructor(opts: SetEnumGaugeOptions) {
 		super(opts)
 
-		this.options = opts.options
+		this.groups = opts.groups
 
 		this.graphOptions = opts.graph
 	}
 
-	getStateAt(value: string, timestamp: number = this.parser.currentEpochTimestamp): boolean {
-		return this.getValuesAt(timestamp).some(v => v === value)
+	getStateAt(handle: string, value: string, timestamp: number = this.parser.currentEpochTimestamp): boolean {
+		return this.getValuesAt(timestamp).find(group => group.handle === handle)?.value === value
 	}
 
-	getValuesAt(timestamp: number): Array<SetEntryOption['value']> {
+	getValuesAt(timestamp: number): SetEnumHistoryEntry[] {
 		const entry = _.findLast(this.history, gauge => gauge.timestamp <= timestamp)
-		return entry != null ? entry.values : []
+		return entry?.groups ?? []
 	}
 
 	/** @inheritdoc */
@@ -73,8 +78,8 @@ export class SetGauge extends AbstractGauge {
 		this.set([])
 	}
 
-	clear(value: string) {
-		this.set(this.current.filter(v => v !== value))
+	clear(handle: string) {
+		this.set(this.copyCurrent().filter(group => group.handle !==  handle))
 	}
 
 	raise() {
@@ -86,42 +91,64 @@ export class SetGauge extends AbstractGauge {
 		if (this.history.length === 0) {
 			this.history.push({
 				timestamp: this.parser.pull.timestamp,
-				values: [],
+				groups: [],
 			})
 		}
 	}
 
 	/** Adds the specified value to the gauge  */
-	generate = (value: string) => {
-		if (!this.options.some(option => option.value === value)) { return }
-		if (this.getStateAt(value)) {
+	generate = (handle: string, value: string) => {
+		const group = this.groups.find(group => group.handle === handle)
+		if (group == null) { return }
+		if (!group.options.some(option => option.value === value)) { return }
+
+		// Overwriting with same value, note and exist
+		if (this.getStateAt(handle, value)) {
 			this.overcap++
 			return
 		}
-		const generatedValues = [...this.current]
-		generatedValues.push(value)
+
+		// Overwriting with a different value, note and continue
+		if (group.options.length > 0) {
+			this.overcap++
+		}
+
+		const generatedValues = this.copyCurrent()
+		const generatedGroup = generatedValues.find(genGroup => genGroup.handle === handle)
+
+		if (generatedGroup == null) {
+			generatedValues.push({handle, value})
+		} else {
+			generatedGroup.value = value
+		}
+
 		this.set(generatedValues)
 	}
 
 	/** Removes the specified value from the gauge */
-	spend = (value: string) => {
-		if (!this.options.some(option => option.value === value)) { return }
-		if (!this.getStateAt(value)) { return } // TODO: Implement prior state correction here
-		const spentValues = [...this.current]
-		const valueIndex = _.lastIndexOf(spentValues, value)
-		if (valueIndex < 0) { return }
-		spentValues.splice(valueIndex, 1)
+	spend = (handle: string, value: string) => {
+		const group = this.groups.find(group => group.handle === handle)
+		if (group == null) { return }
+		if (!group.options.some(option => option.value === value)) { return }
+		if (!this.getStateAt(handle, value)) { return } // TODO: Implement prior state correction here
+
+		const spentValues = this.copyCurrent().filter(spentGroup => spentGroup.handle !== handle)
+
 		this.set(spentValues)
 	}
 
 	/** Set the current value of the gauge. Value will automatically be bounded to valid values */
-	set(values: Array<SetEntryOption['value']>) {
-		const correctedValues = values.filter(value => this.options.some(option => option.value === value)).slice(0, this.options.length)
+	set(values: SetEnumHistoryEntry[]) {
+		// Filter out input where the handle is invalid, or the value is missing from that groups options, and remove duplicate handles
+		const correctedValues = values.filter((value, index) => this.groups.some(group => group.handle === value.handle &&
+			group.options.some(option => option.value === value.value) &&
+			values.findIndex(val => val.handle === value.handle) === index
+		))
 
 		this.pushHistory(correctedValues)
 	}
 
-	private pushHistory(values: Array<SetEntryOption['value']>) {
+	private pushHistory(groups: SetEnumHistoryEntry[]) {
 		const timestamp = this.parser.currentEpochTimestamp
 
 		// Ensure we're not generating multiple entries at the same timestamp
@@ -134,7 +161,7 @@ export class SetGauge extends AbstractGauge {
 
 		this.history.push({
 			timestamp,
-			values,
+			groups,
 		})
 	}
 
@@ -144,22 +171,31 @@ export class SetGauge extends AbstractGauge {
 
 		const {handle, tooltipHideWhenEmpty} = this.graphOptions
 		// Set up a ResourceData object for each of the enum options
-		const graphDatas = this.options.map<SetResourceData>(option => {
-			return {
-				label: option.label,
-				colour: option.color,
-				data: [],
-				tooltipHideWhenEmpty,
-				tooltipHideMaximum: true, // A side effect of the way this works is the tooltip would imply a 'maximum' of however many options are in the set. Don't do that.
-				base: this.options.indexOf(option),
-				value: option.value,
-				type: 'discrete',
-			}
+		const graphDatas: SetEnumResourceData[] = []
+		this.groups.forEach(group => {
+			graphDatas.push(...group.options.map<SetEnumResourceData>(option => {
+				return {
+					label: option.label,
+					colour: option.color,
+					data: [],
+					tooltipHideWhenEmpty,
+					tooltipHideMaximum: true, // A side effect of the way this works is the tooltip would imply a 'maximum' of however many options are in the set. Don't do that.
+					base: this.groups.indexOf(group), // data within the same group shares a common base, so they exist within the same timeline row
+					value: option.value,
+					handle: group.handle,
+					type: 'discrete',
+				}
+			}))
 		})
 		// Convert the list of history entries into a stacked set of counter-gauge style history entries
 		this.history.forEach(entry => {
 			graphDatas.forEach(graphData => {
-				graphData.data.push({time: entry.timestamp, maximum: this.options.length, current: entry.values.filter(v => v === graphData.value).length, base: graphData.base})
+				graphData.data.push({
+					time: entry.timestamp,
+					maximum: this.groups.length,
+					current: entry.groups.find(group => group.handle === graphData.handle)?.value === graphData.value ? 1 : 0,
+					base: graphData.base,
+				})
 			})
 		})
 
