960532f99f4037a4519f9d7e099b06c42176a683
Akairyu Gestalter <AkairyuGestalter@users.noreply.github.com>
2024-11-10 09:10:21 -0600

    Fix Pneuma counting twice for GCD uptime

--- src/data/ACTIONS/root/SGE.ts
+++ src/data/ACTIONS/root/SGE.ts
@@ -341,16 +341,10 @@ export const SGE = ensureActions({
 		mpCost: 700,
 		statusesApplied: ['PNEUMA'],
 	},
-	// The heal from Pneuma is technically a different action source because reasons apparently. Unsure how much of this data should stay around but at least removing the statusesApplied array...
+	// The heal from Pneuma is technically a different action source because reasons
 	PNEUMA_HEAL: {
 		id: 27524,
 		name: 'Pneuma',
 		icon: iconUrl(3686),
-		onGcd: true,
-		speedAttribute: Attribute.SPELL_SPEED,
-		castTime: 1500,
-		gcdRecast: 2500,
-		cooldown: 120000,
-		mpCost: 700,
 	},
 })
--- src/parser/jobs/sge/changelog.tsx
+++ src/parser/jobs/sge/changelog.tsx
@@ -8,6 +8,11 @@ export const changelog = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2024-11-10'),
+		Changes: () => <>Fixed a data issue causing Pneuma's heal effect to be treated as second simultaneous GCD use</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
+	{
 		date: new Date('2024-08-23'),
 		Changes: () => <>Fix Philosophia overheal ignoring to correctly attribute its healing to the Eudaimonia status effect</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
