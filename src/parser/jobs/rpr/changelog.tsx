4364a049ae33ac6db3f23c7e476a06143e6a2e23
Rjb99 <Rjb99coding@gmail.com>
2024-11-14 22:20:29 -0500

    7.1 RPR bump

--- src/parser/jobs/rpr/changelog.tsx
+++ src/parser/jobs/rpr/changelog.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 
 export const changelog = [
 	{
+		date: new Date('2024-11-14'),
+		Changes: () => <> Reaper 7.1 Support added</>,
+		contributors: [CONTRIBUTORS.RYAN],
+	},
+	{
 		date: new Date('2024-10-20'),
 		Changes: () => <>Fixed Harpe & Harvest Moon not generating gauge.</>,
 		contributors: [CONTRIBUTORS.RYAN],
--- src/parser/jobs/rpr/index.tsx
+++ src/parser/jobs/rpr/index.tsx
@@ -17,7 +17,7 @@ export const REAPER = new Meta({
 
 	supportedPatches: {
 		from: '7.0',
-		to: '7.05',
+		to: '7.1',
 	},
 
 	contributors: [
