16b606f87d19f0f4b85d7a2950d790be17cb204e
KelosPelmand <53376212+KelosPelmand@users.noreply.github.com>
2024-11-16 22:06:33 -0500

    Marked 7.1 as supported.
    (Also corrected date for initial DT implementation.)

--- src/parser/jobs/smn/changelog.tsx
+++ src/parser/jobs/smn/changelog.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 
 export const changelog = [
 	{
-		date: new Date('2021-11-19'),
+		date: new Date('2024-11-14'),
+		Changes: () => <>Added new Crimson Strike Ready status for 7.1</>,
+		contributors: [CONTRIBUTORS.KELOS],
+	},
+	{
+		date: new Date('2024-7-11'),
 		Changes: () => <>Initial Dawntrail implementation</>,
 		contributors: [CONTRIBUTORS.KELOS],
 	},
