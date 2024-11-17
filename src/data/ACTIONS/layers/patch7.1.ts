54aab6ffe9d8c7c3a8928c3bba07456143cfc4b7
KelosPelmand <53376212+KelosPelmand@users.noreply.github.com>
2024-11-16 22:03:36 -0500

    Added new Crimson Strike Ready status.

--- src/data/ACTIONS/layers/patch7.1.ts
+++ src/data/ACTIONS/layers/patch7.1.ts
@@ -4,6 +4,8 @@ import {ActionRoot} from '../root'
 export const patch710: Layer<ActionRoot> = {
 	patch: '7.1',
 	data: {
-
+		CRIMSON_CYCLONE: {
+			statusesApplied: ['CRIMSON_STRIKE_READY'],
+		},
 	},
 }
--- src/data/STATUSES/layers/patch7.1.ts
+++ src/data/STATUSES/layers/patch7.1.ts
@@ -1,9 +1,15 @@
+import {iconUrl} from 'data/icon'
 import {Layer} from 'data/layer'
 import {StatusRoot} from '../root'
 
 export const patch710: Layer<StatusRoot> = {
 	patch: '7.1',
 	data: {
-
+		CRIMSON_STRIKE_READY: {
+			id: 4403,
+			name: 'Crimson Strike Ready',
+			icon: iconUrl(12699),
+			duration: 30000,
+		},
 	},
 }
