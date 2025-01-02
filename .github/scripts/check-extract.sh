6551fe87d6ddbfd11e92151124fda35f2d0b9927
ackwell <saxon@ackwell.au>
2025-01-02 22:55:47 +1100

    Ok lets actually try to do it with GHA properly

--- .github/scripts/check-extract.sh
+++ .github/scripts/check-extract.sh
@@ -4,18 +4,9 @@ yarn extract;
 
 if [[ $(git diff --stat locale) != '' ]]; then
 	echo "Extract diff found.";
-	DIFF=$(git diff --color=never locale)
-	OUTPUT="<?xml version=\"1.0\" encoding=\"utf-8\"?>
-	<testsuites>
-		<testsuite package=\"xiva.extract\" time=\"0\" tests=\"1\" errors=\"1\" name=\"i18n message extraction\" >
-			<testcase time=\"0\" name=\"xiva.extract.failure\">
-				<failure message=\"Changes to locale files detected. Please run \`yarn extract\` and commit the changes to ensure translations remain in sync.\"><![CDATA[$DIFF]]></failure>
-			</testcase>
-		</testsuite>
-	</testsuites>
-	";
+	# DIFF=$(git diff --color=never locale)
+	git diff
 
-	mkdir -p reports/extract;
-	echo "$OUTPUT" > reports/extract/results.xml;
+	echo "::error file=package.json,title=i18n message extraction::Changes to locale files detected. Please run \`yarn extract\` and commit the changes to ensure translations remain in sync.";
 	exit 1;
 fi;
