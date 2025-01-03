415b29a7f36bddb662c1c3b847f4a10a28528fa1
ackwell <saxon@ackwell.au>
2025-01-03 14:06:32 +1100

    Add deploy webhooks

---
+++ .github/scripts/success.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo 'Sending Discord Webhook';
+export BACKTICK='`';
+export COMPARE_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/compare/$COMPARE"
+export WORKFLOW_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
+export COMMIT_FORMATTED="[$BACKTICK${GITHUB_SHA:0:7}$BACKTICK]($COMPARE_URL)";
+export COMMIT_MESSAGE=$(git log --format=%s -n 1 $GITHUB_SHA);
+curl -v -H User-Agent:bot -H Content-Type:application/json -d "{\"embeds\":[{\"author\":{\"name\":\"Build #$GITHUB_RUN_NUMBER ($GITHUB_REF_NAME) Passed\",\"url\":\"$WORKFLOW_URL\"},\"url\":\"$COMPARE_URL\",\"color\":4303448,\"description\":\"$COMMIT_FORMATTED - $COMMIT_MESSAGE\"}]}" $DISCORD_WEBHOOK_URL;
