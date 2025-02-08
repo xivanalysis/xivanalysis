26c3ad55eeddd48acc855bffee325c00337a1c58
ackwell <saxon@ackwell.au>
2025-02-08 23:46:40 +1100

    sam

--- src/parser/jobs/sam/modules/Interrupts.tsx
+++ src/parser/jobs/sam/modules/Interrupts.tsx
@@ -4,7 +4,7 @@ import ACTIONS from 'data/ACTIONS'
 import {Interrupts as CoreInterrupts} from 'parser/core/modules/Interrupts'
 
 export default class Interrupts extends CoreInterrupts {
-	suggestionContent = <Trans id="sam.interrupts.suggestion.content">
+	override suggestionContent = <Trans id="sam.interrupts.suggestion.content">
 		Avoid interrupting <ActionLink {...ACTIONS.IAIJUTSU}/> casts. Despite the short cast time, moving too early can interrupt the skill causing you to have to waste time re-casting it.
 	</Trans>
 }
