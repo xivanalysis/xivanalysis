7f601ece21d49185167bff2d833abb135dc4c96b
ackwell <saxon@ackwell.au>
2025-02-08 23:50:52 +1100

    sch

--- src/parser/jobs/sch/modules/Interrupts.js
+++
@@ -1,7 +0,0 @@
-import {Trans} from '@lingui/react'
-import {DataLink} from 'components/ui/DbLink'
-import {Interrupts as CoreInterrupts} from 'parser/core/modules/Interrupts'
-
-export default class Interrupts extends CoreInterrupts {
-	suggestionContent = <Trans id="sch.interrupts.suggestion.content">If you can, try to preposition yourself so you don't have to move during mechanics as much as possible. Utilizing slidecasting will lower the need to use <DataLink action="SCH_RUIN_II" /> to instantly relocate or interrupt your current <DataLink action="BROIL_IV" /> cast.</Trans>
-}
--- src/parser/jobs/sch/modules/Tincture.ts
+++ src/parser/jobs/sch/modules/Tincture.ts
@@ -2,5 +2,5 @@ import ACTIONS from 'data/ACTIONS'
 import {Tincture as CoreTincture} from 'parser/core/modules/Tincture'
 
 export default class Tincture extends CoreTincture {
-	buffAction = ACTIONS.INFUSION_MIND
+	buffAction = ACTIONS.INFUSION_MND
 }
