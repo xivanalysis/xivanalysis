414c94280387e2e3c3ba6d09ac7f1702828893ef
ackwell <saxon@ackwell.au>
2025-02-12 02:43:09 +1100

    Port report search

--- src/components/Home/ReportSearch.tsx
+++ src/components/Home/ReportSearch.tsx
@@ -1,10 +1,9 @@
 import {t} from '@lingui/macro'
 import {Trans} from '@lingui/react'
 import {NormalisedMessage} from 'components/ui/NormalisedMessage'
-import {action, observable} from 'mobx'
 import {observer} from 'mobx-react'
-import {ChangeEvent, Component} from 'react'
-import {RouteComponentProps, withRouter, Redirect} from 'react-router-dom'
+import {ChangeEvent, useCallback, useState} from 'react'
+import {Redirect} from 'react-router-dom'
 import {SearchHandlerResult} from 'reportSources'
 import {Button, Input, InputOnChangeData} from 'semantic-ui-react'
 import {parseInput} from './parseInput'
@@ -12,61 +11,55 @@ import styles from './ReportSearch.module.css'
 
 const DEFAULT_REASON = t('core.home.report-search.unknown-query-error')`An unknown error occured when parsing the provided query.`
 
-@observer
-class ReportSearchImpl extends Component<RouteComponentProps> {
-	@observable.ref private value = ''
-	@observable.ref private result: SearchHandlerResult = {valid: false}
-
-	@action.bound
-	private onChange(event: ChangeEvent, data: InputOnChangeData) {
-		this.value = data.value
-
-		// 'aint nobody got time to click a button.
-		this.onSearch()
-	}
-
-	@action.bound
-	private onSearch() {
-		this.result = parseInput(this.value)
-	}
-
-	override render() {
-		if (this.result.valid) {
-			return <Redirect to={this.result.path}/>
-		}
-
-		const hasErrors = !this.result.valid && this.value !== ''
-		const reason = this.result.valid
-			? undefined
-			: this.result.reason
-
-		return <>
-			<strong>
-				{hasErrors ? (
-					<span className="text-error">
-						<NormalisedMessage message={reason ?? DEFAULT_REASON}/>
-					</span>
-				) : (
-					<Trans id="core.home.paste-url">
-						Paste your log URL to get started
-					</Trans>
-				)}
-			</strong>
-			<Input
-				type="text"
-				placeholder="https://www.fflogs.com/reports/..."
-				action={(
-					<Button onClick={this.onSearch} negative={hasErrors}>
-						<Trans id="core.home.analyse">Analyse</Trans>
-					</Button>
-				)}
-				onChange={this.onChange}
-				className={styles.input}
-				inverted
-				error={hasErrors}
-			/>
-		</>
-	}
+type InputState = {
+	value: string,
+	result: SearchHandlerResult,
 }
 
-export const ReportSearch = withRouter(ReportSearchImpl)
+export const ReportSearch = observer(function ReportSearch() {
+	const [{value, result}, setState] = useState<InputState>({
+		value: '',
+		result: {valid: false},
+	})
+
+	const onChange = useCallback((event: ChangeEvent, {value}: InputOnChangeData) => {
+		const result = parseInput(value)
+		setState({value, result})
+	}, [])
+
+	if (result.valid) {
+		return <Redirect to={result.path}/>
+	}
+
+	// Any valid searches will have already been redirected by now, so the only
+	// non-erroneous state is a blank one.
+	const hasErrors = value !== ''
+
+	return <>
+		<strong>
+			{hasErrors ? (
+				<span className="text-error">
+					<NormalisedMessage message={result.reason ?? DEFAULT_REASON}/>
+				</span>
+			) : (
+				<Trans id="core.home.paste-url">
+						Paste your log URL to get started
+				</Trans>
+			)}
+		</strong>
+		<Input
+			type="text"
+			placeholder="https://www.fflogs.com/reports/..."
+			action={(
+				// No onClick on this - you can't realistically click it if there's a valid input, as it will have already redirected out.
+				<Button negative={hasErrors}>
+					<Trans id="core.home.analyse">Analyse</Trans>
+				</Button>
+			)}
+			onChange={onChange}
+			className={styles.input}
+			inverted
+			error={hasErrors}
+		/>
+	</>
+})
