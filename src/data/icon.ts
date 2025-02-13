416fbca7295160f84cb8042937230941eeece8ae
ackwell <saxon@ackwell.au>
2025-02-13 19:46:47 +1100

    Update icon url

--- src/data/icon.ts
+++ src/data/icon.ts
@@ -1,5 +1,6 @@
 export function iconUrl(icon: number): string {
 	const group = Math.floor(icon / 1000) * 1000
 	const gamePath = `ui/icon/${group.toString(10).padStart(6, '0')}/${icon.toString(10).padStart(6, '0')}_hr1.tex`
-	return `https://beta.xivapi.com/api/1/asset/${gamePath}?format=png&version=6.58x2`
+	// Yeah, 7.0 is a bit anachronistic, but we dropped 6.58x2 support to save storage space pending better HIST support, so 7.0'll do.
+	return `https://v2.xivapi.com/api/asset?path=${gamePath}&format=png&version=7.0`
 }
