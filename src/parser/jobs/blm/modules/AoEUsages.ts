6b8375ba66a2fa22ef595964948677b3c237950f
Akairyu Gestalter <AkairyuGestalter@users.noreply.github.com>
2025-03-19 21:14:01 -0500

    Adjust AoE usages for Flare, technically due to 7.1 but overlooked

--- src/parser/jobs/blm/modules/AoEUsages.ts
+++ src/parser/jobs/blm/modules/AoEUsages.ts
@@ -11,8 +11,9 @@ export class AoEUsages extends CoreAoE {
 		},
 		{
 			aoeAction: this.data.actions.FLARE,
-			stActions: [this.data.actions.DESPAIR],
-			minTargets: 3,
+			stActions: [this.data.actions.FIRE_IV, this.data.actions.DESPAIR],
+			// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+			minTargets: this.parser.patch.before('7.1') ? 3 : 2,
 		},
 		{
 			aoeAction: this.data.actions.THUNDER_IV,
