fe1f877fab28b82e813c9557882292540e0d07b6
Akairyu Gestalter <AkairyuGestalter@users.noreply.github.com>
2025-12-30 19:16:36 -0600

    this boss gives me an ulcer

--- src/parser/core/changelog.tsx
+++ src/parser/core/changelog.tsx
@@ -9,6 +9,11 @@ export const changelog: ChangelogEntry[] = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2025-12-30'),
+		Changes: () => <>Fixed invulnerability checks for Doomtrain Extreme to ensure correct DoT uptime calculations.</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
+	{
 		date: new Date('2025-02-12'),
 		Changes: () => <>Fixed an issue that caused AOE attacks to incorrectly report broken combos if they cleaved an invulnerable target.</>,
 		contributors: [CONTRIBUTORS.AZARIAH],
