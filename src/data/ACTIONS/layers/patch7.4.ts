6aaf19d40101651f42d786a71f17ae9400523c7a
Akairyu Gestalter <AkairyuGestalter@users.noreply.github.com>
2026-01-05 23:33:43 -0600

    Add Patch 7.4 potency changes

--- src/data/ACTIONS/layers/patch7.4.ts
+++ src/data/ACTIONS/layers/patch7.4.ts
@@ -4,5 +4,14 @@ import {ActionRoot} from '../root'
 export const patch740: Layer<ActionRoot> = {
 	patch: '7.4',
 	data: {
+		APEX_ARROW: {
+			potency: 700,
+		},
+		BLAST_ARROW: {
+			potency: 700,
+		},
+		RADIANT_ENCORE: {
+			potency: [700, 800, 1100],
+		},
 	},
 }
