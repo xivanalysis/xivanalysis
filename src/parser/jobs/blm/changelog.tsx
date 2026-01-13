47742d49ad2c1394c5b852bf4defb96cd8616182
AkairyuGestalter <AkairyuGestalter@users.noreply.github.com>
2026-01-12 21:04:57 -0600

    [BugFix] - Ley Lines cast counting when used consecutively (#2283)

--- src/parser/jobs/blm/changelog.tsx
+++ src/parser/jobs/blm/changelog.tsx
@@ -8,6 +8,11 @@ export const changelog = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2026-01-04'),
+		Changes: () => <>Fixed a bug with Ley Lines cast counting when two uses of Ley Lines were used consecutively.</>,
+		contributors: [CONTRIBUTORS.AKAIRYU],
+	},
+	{
 		date: new Date('2025-12-29'),
 		Changes: () => <>Add informational output about Blizzard III casts in Astral Fire.</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
