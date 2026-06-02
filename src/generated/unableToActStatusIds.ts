654d52ac5067efac268c67468028fdfcbf69c316
Akairyu Gestalter <AkairyuGestalter@users.noreply.github.com>
2026-06-02 18:50:20 -0500

    update utaStatuses for UMAD

--- src/generated/unableToActStatusIds.ts
+++ src/generated/unableToActStatusIds.ts
@@ -272,11 +272,13 @@ export const UNABLE_TO_ACT_STATUS_IDS = [
 	4578, // Fetters (lockActions, lockControl)
 	4618, // Standing Firm (lockActions, lockControl)
 	4619, //  (lockActions, lockControl)
+	4894, // Sleep (lockActions)
 	4973, // In Event (lockActions, lockControl)
 	5043, // Stun (lockActions)
 	5047, // Bubble Gaol (lockActions, lockControl)
 	5058, // Stone Curse (lockActions)
 	5110, //  (lockActions, lockControl)
+	5144, // _rsv_5144_-1_1_0_0_S74CFC3B0_E74CFC3B0 (lockControl)
 	5174, // Forced March (lockActions)
 	5407, // Strung Up (lockControl)
 ]
