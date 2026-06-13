7e9df89037095c7629bd5b1da47ad4ffb14239b4
yumiyafangirl <65056303+yumiyafangirl@users.noreply.github.com>
2026-06-13 16:09:13 -0400

    Add changelog entry

--- src/parser/core/changelog.tsx
+++ src/parser/core/changelog.tsx
@@ -9,6 +9,11 @@ export const changelog: ChangelogEntry[] = [
 	// 	contributors: [CONTRIBUTORS.YOU],
 	// },
 	{
+		date: new Date('2026-06-13'),
+		Changes: () => <>Add "Expand module" buttons to the Timeline and Action Window modules.</>,
+		contributors: [CONTRIBUTORS.HINT],
+	},
+	{
 		date: new Date('2026-03-15'),
 		Changes: () => <>Update Always be Casting GCD Delay calculation to better handle longer than normal GCD recasts, such as PCT's Motif spells.</>,
 		contributors: [CONTRIBUTORS.AKAIRYU],
